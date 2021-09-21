91978ff5cfacdee4670193e362552c28e1c744b5
Joshua Shinavier <josh@fortytwo.net>
2021-09-20 18:13:05 -0700

    Add a function to express a Type as a Term

--- hydra-haskell/src/main/haskell/Hydra/Prototyping.hs
+++ hydra-haskell/src/main/haskell/Hydra/Prototyping.hs
@@ -1,7 +1,9 @@
 module Hydra.Prototyping (
     freeVariables,
     termVariant,
+    typeAsTerm,
     typeVariant,
+    unitTerm,
   ) where
 
 import Hydra.Core
@@ -48,6 +50,54 @@ termVariant term = case term of
   TermUnion _ -> TermVariantUnion
   TermVariable _ -> TermVariantVariable
 
+typeAsTerm :: Type -> Term
+typeAsTerm typ = case typ of
+    TypeAtomic at -> variant "atomic" $ atomicTypeAsTerm at
+    TypeElement t -> variant "element" $ typeAsTerm t
+    TypeFunction ft -> variant "function" $ functionTypeAsTerm ft
+    TypeList t -> variant "list" $ typeAsTerm t
+    TypeNominal name -> variant "nominal" $ string name
+    TypeRecord fields -> variant "record" $ TermList $ fmap fieldAsTerm fields
+    TypeUnion fields -> variant "union" $ TermList $ fmap fieldAsTerm fields
+
+  where
+    atomicTypeAsTerm at = case at of
+      AtomicTypeBinary -> unitVariant "binary"
+      AtomicTypeBoolean -> unitVariant "boolean"
+      AtomicTypeFloat ft -> variant "float" $ floatTypeAsTerm ft
+      AtomicTypeInteger it -> variant "integer" $ integerTypeAsTerm it
+      AtomicTypeString -> unitVariant "string"
+
+    fieldAsTerm (FieldType fname t) = TermRecord [
+      Field "name" $ string fname,
+      Field "type" $ typeAsTerm t]
+
+    floatTypeAsTerm ft = unitVariant $ case ft of
+      FloatTypeBigfloat -> "bigfloat"
+      FloatTypeFloat32 -> "float32"
+      FloatTypeFloat64 -> "float64"
+
+    functionTypeAsTerm (FunctionType dom cod) = TermRecord [
+      Field "domain" $ typeAsTerm dom,
+      Field "codomain" $ typeAsTerm cod]
+
+    integerTypeAsTerm it = unitVariant $ case it of
+      IntegerTypeBigint -> "bigint"
+      IntegerTypeInt8 -> "int8"
+      IntegerTypeInt16 -> "int16"
+      IntegerTypeInt32 -> "int32"
+      IntegerTypeInt64 -> "int64"
+      IntegerTypeUint8 -> "uint8"
+      IntegerTypeUint16 -> "uint16"
+      IntegerTypeUint32 -> "uint32"
+      IntegerTypeUint64 -> "uint64"
+
+    string = TermAtomic . AtomicValueString
+
+    unitVariant fname = variant fname unitTerm
+
+    variant fname term = TermUnion (Field fname term)
+
 typeVariant :: Type -> TypeVariant
 typeVariant typ = case typ of
   TypeAtomic _ -> TypeVariantAtomic
@@ -57,3 +107,6 @@ typeVariant typ = case typ of
   TypeNominal _ -> TypeVariantNominal
   TypeRecord _ -> TypeVariantRecord
   TypeUnion _ -> TypeVariantUnion
+
+unitTerm :: Term
+unitTerm = TermRecord []
