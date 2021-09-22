e5e8141a04fb934ba1cac9be38566e67b3501e60
Joshua Shinavier <josh@fortytwo.net>
2021-09-22 16:50:31 -0700

    Add module for prototyping primitive functions (Haskell only)

---
+++ hydra-haskell/src/main/haskell/Hydra/Prototyping/Primitives.hs
@@ -0,0 +1,15 @@
+module Hydra.Prototyping.Primitives (
+    lookupPrimitiveFunction,
+  ) where
+
+import Hydra.Core
+import Hydra.Evaluation
+
+import qualified Data.Char as C
+import qualified Data.List as L
+import qualified Data.Map  as M
+import qualified Data.Set  as S
+
+
+lookupPrimitiveFunction :: Context -> Name -> Maybe PrimitiveFunction
+lookupPrimitiveFunction context fn = M.lookup fn $ contextFunctions context
