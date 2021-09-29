9ba3a3a98560fd53730f7f67e87db955478973f4
Joshua Shinavier <josh@fortytwo.net>
2021-09-29 12:18:08 -0700

    Rename YAML model to hydra/ext/yaml/model

--- hydra-scala/src/gen-main/scala/hydra/ext/yaml/model/Node.scala
+++ hydra-scala/src/gen-main/scala/hydra/ext/yaml/model/Node.scala
@@ -1,7 +1,7 @@
 /**
  * @comments Every YAML node has an optional scalar tag or non-specific tag (omitted from this model)
  */
-package hydra.ext.yaml.representation
+package hydra.ext.yaml.model
 
 /**
  * @comments Every YAML node has an optional scalar tag or non-specific tag (omitted from this model)
@@ -10,21 +10,21 @@ enum Node:
     /**
      * @comments Failsafe schema: tag:yaml.org,2002:map
      * @type map:
-     *         keys: hydra/ext/yaml/representation.Node
-     *         values: hydra/ext/yaml/representation.Node
+     *         keys: hydra/ext/yaml/model.Node
+     *         values: hydra/ext/yaml/model.Node
      */
     case mapping(value: Map[Node, Node]) extends Node
     /**
-     * @type hydra/ext/yaml/representation.Scalar
+     * @type hydra/ext/yaml/model.Scalar
      */
     case scalar(value: Scalar) extends Node
     /**
      * @comments Failsafe schema: tag:yaml.org,2002:seq
-     * @type list: hydra/ext/yaml/representation.Node
+     * @type list: hydra/ext/yaml/model.Node
      */
     case sequence(value: Seq[Node]) extends Node
 
-val _Node: String = "hydra/ext/yaml/representation.Node"
+val _Node: String = "hydra/ext/yaml/model.Node"
 val _Node_mapping: String = "mapping"
 val _Node_scalar: String = "scalar"
 val _Node_sequence: String = "sequence"
