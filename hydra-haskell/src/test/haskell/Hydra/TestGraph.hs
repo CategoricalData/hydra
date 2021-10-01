006833ea2805aac112a331862690e8588377d4ed
Joshua Shinavier <josh@fortytwo.net>
2021-10-01 14:50:07 -0700

    Add more realistic test graphs

--- hydra-haskell/src/test/haskell/Hydra/TestGraph.hs
+++ hydra-haskell/src/test/haskell/Hydra/TestGraph.hs
@@ -1,5 +1,5 @@
 module Hydra.TestGraph (
-  graphElementsForContext,
+  graphElementsMap,
   testContext,
   testGraph,
   testStrategy,
@@ -11,6 +11,7 @@ import Hydra.Evaluation
 import Hydra.Prototyping.Basics
 import Hydra.Prototyping.Helpers
 import Hydra.Prototyping.Primitives
+import Hydra.Prototyping.CoreEncoding
 
 import qualified Data.Char as C
 import qualified Data.List as L
@@ -18,16 +19,20 @@ import qualified Data.Map  as M
 import qualified Data.Set  as S
 
 
-graphElementsForContext :: Graph -> M.Map Name Element
-graphElementsForContext g = M.fromList $ fmap toPair $ graphElements g
-  where
-    toPair e = (elementName e, e)
-
 testContext :: Context
-testContext = Context elements functions strategy
-  where
-    elements = graphElementsForContext testGraph
-    strategy = EvaluationStrategy {
+testContext = Context {
+    contextGraphs = GraphSet {
+      graphSetGraphs = M.fromList [
+        ("testGraph", testGraph),
+        ("testSchemaGraph", testSchemaGraph),
+        ("hydra/core", hydraCorePlaceholder)],
+      graphSetRoot = "testGraph"},
+    contextElements = graphElementsMap testGraph,
+    contextFunctions = M.fromList [
+      ("concat", PrimitiveFunction concatFunction (FunctionType stringType (TypeFunction $ FunctionType stringType stringType))),
+      ("toLower", PrimitiveFunction toLowerFunction (FunctionType stringType stringType)),
+      ("toUpper", PrimitiveFunction toUpperFunction (FunctionType stringType stringType))],
+    contextStrategy = EvaluationStrategy {
       evaluationStrategyOpaqueTermVariants = S.fromList [ -- TODO: revisit this list
         TermVariantAtomic,
         TermVariantCases,
@@ -35,33 +40,59 @@ testContext = Context elements functions strategy
         TermVariantElement,
         TermVariantFunction,
         TermVariantLambda,
-        TermVariantProjection]}
-    functions = M.fromList [
-      ("concat", PrimitiveFunction concat (FunctionType stringType (TypeFunction $ FunctionType stringType stringType))),
-      ("toLower", PrimitiveFunction toLower (FunctionType stringType stringType)),
-      ("toUpper", PrimitiveFunction toUpper (FunctionType stringType stringType))]
+        TermVariantProjection]}}
 
-    concat :: [Term] -> Term
-    concat [x, y] = case (x, y) of
-      (TermAtomic (AtomicValueString x'), TermAtomic (AtomicValueString y'))
-        -> TermAtomic $ AtomicValueString $ x' ++ y'
+concatFunction :: [Term] -> Term
+concatFunction [x, y] = case (x, y) of
+  (TermAtomic (AtomicValueString x'), TermAtomic (AtomicValueString y'))
+    -> TermAtomic $ AtomicValueString $ x' ++ y'
 
-    toLower :: [Term] -> Term
-    toLower [x] = case x of
-      TermAtomic (AtomicValueString s)
-        -> TermAtomic $ AtomicValueString $ fmap C.toLower s
+toLowerFunction :: [Term] -> Term
+toLowerFunction [x] = case x of
+  TermAtomic (AtomicValueString s)
+    -> TermAtomic $ AtomicValueString $ fmap C.toLower s
 
-    toUpper :: [Term] -> Term
-    toUpper [x] = case x of
-      TermAtomic (AtomicValueString s)
-        -> TermAtomic $ AtomicValueString $ fmap C.toUpper s
+toUpperFunction :: [Term] -> Term
+toUpperFunction [x] = case x of
+  TermAtomic (AtomicValueString s)
+    -> TermAtomic $ AtomicValueString $ fmap C.toUpper s
 
 testGraph :: Graph
-testGraph = Graph e d s
+testGraph = Graph "testGraph" [arthur] allTerms "testSchemaGraph"
   where
-    e = []
-    d = \t -> True
-    s = testGraph
+    arthur = Element {
+      elementName = "ArthurDent",
+      elementSchema = TermElement "Person",
+      elementData = TermRecord [
+        Field "firstName" $ stringValue "Arthur",
+        Field "lastName" $ stringValue "Dent",
+        Field "age" $ int32Value 42]}
+
+testSchemaGraph :: Graph
+testSchemaGraph = Graph "testSchemaGraph" [exampleNominalType] allTerms "hydra/core"
+  where
+    exampleNominalType = typeElement "StringTypeAlias" stringType
+    exampleVertexType = typeElement "Person" $
+      TypeRecord [
+        FieldType "firstName" stringType,
+        FieldType "lastName" stringType,
+        FieldType "age" int32Type]
 
 testStrategy :: EvaluationStrategy
 testStrategy = contextStrategy testContext
+
+-- Note: here, the element namespace "hydra/core" doubles as a graph name
+hydraCorePlaceholder :: Graph
+hydraCorePlaceholder = Graph "hydra/core" noElements allTerms "hydra/core"
+
+noElements :: [Element]
+noElements = []
+
+allTerms :: Term -> Bool
+allTerms _ = True
+
+typeElement :: Name -> Type -> Element
+typeElement name typ = Element {
+  elementName = name,
+  elementSchema = encodeType $ TypeNominal "hydra/core.Type",
+  elementData = encodeType typ}
