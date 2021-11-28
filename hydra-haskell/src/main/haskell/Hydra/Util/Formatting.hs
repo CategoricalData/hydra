c12c0a363eb2b6bf29abd0cbe32e6934063229af
Joshua Shinavier <josh@fortytwo.net>
2021-11-28 08:24:05 -0800

    Add string formatting utils

---
+++ hydra-haskell/src/main/haskell/Hydra/Util/Formatting.hs
@@ -0,0 +1,14 @@
+module Hydra.Util.Formatting where
+
+import qualified Data.Char as C
+
+
+capitalize :: String -> String
+capitalize s = case s of
+  [] -> []
+  (h:r) -> C.toLower h : r
+
+decapitalize :: String -> String
+decapitalize s = case s of
+  [] -> []
+  (h:r) -> C.toUpper h : r
