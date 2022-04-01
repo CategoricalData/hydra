2f4c617fc6a23264a8f966ea4de9000e1c41e9d0
Joshua Shinavier <josh@fortytwo.net>
2022-04-01 16:16:55 -0700

    Use the Types DSL to make hydra/core definitions friendlier

--- hydra-haskell/src/main/haskell/Hydra/Impl/Haskell/Sources/CoreGraph.hs
+++ hydra-haskell/src/main/haskell/Hydra/Impl/Haskell/Sources/CoreGraph.hs
@@ -3,110 +3,110 @@ module Hydra.Impl.Haskell.Sources.CoreGraph where
 import Hydra.Core
 import Hydra.Evaluation
 import Hydra.Graph
-import Hydra.Impl.Haskell.Dsl.Terms
+import Hydra.Impl.Haskell.Dsl.Types
 import Hydra.Impl.Haskell.Dsl.Standard
 
 
 hydraCoreGraph :: Graph Meta
-hydraCoreGraph = Graph modName elements (const True) "hydra/core"
+hydraCoreGraph = Graph gname elements (const True) "hydra/core"
   where
     -- Note: here, the element namespace "hydra/core" doubles as a graph name
-    modName = "hydra/core"
+    gname = "hydra/core"
 
-    qualify lname = modName ++ "." ++ lname
+    qualify lname = gname ++ "." ++ lname
 
-    local = TypeNominal . qualify
+    core = nominal . qualify
 
-    element lname doc typ = typeElement standardContext (qualify lname) doc typ
+    datatype lname doc typ = typeElement standardContext (qualify lname) doc typ
 
     elements = [
 
-      element "Application"
+      datatype "Application"
         "A term which applies a function to an argument" $
-        universal "m" $ TypeRecord [
-          FieldType "function" $ universal "m" $ local "Term",
-          FieldType "argument" $ universal "m" $ local "Term"],
+        universal "m" $ record [
+          field "function" $ universal "m" $ core "Term",
+          field "argument" $ universal "m" $ core "Term"],
 
-      element "BooleanValue"
+      datatype "BooleanValue"
         "A boolean literal value" $
         enum [
           "false",
           "true"],
 
-      element "Comparison"
+      datatype "Comparison"
         "An equality judgement: less than, equal to, or greater than" $
         enum [
           "lessThan",
           "equalTo",
           "greaterThan"],
 
-      element "Expression"
+      datatype "Expression"
         "A term expression" $
-        universal "m" $ TypeUnion [
-          FieldType "application" $ universal "m" $ local "Application",
-          FieldType "literal" $ local "Literal",
-          FieldType "element" $ local "Name",
-          FieldType "function" $ universal "m" $ local "Function",
-          FieldType "let" $ universal "m" $ local "Let",
-          FieldType "list" $ TypeList $ universal "m" $ local "Term",
-          FieldType "map" $ TypeMap $ MapType (universal "m" $ local "Term") (universal "m" $ local "Term"),
-          FieldType "nominal" $ universal "m" $ local "NominalTerm",
-          FieldType "optional" $ TypeOptional $ universal "m" $ local "Term",
-          FieldType "record" $ TypeList $ universal "m" $ local "Field",
-          FieldType "set" $ TypeSet $ universal "m" $ local "Term",
-          FieldType "typeAbstraction" $ universal "m" $ local "TypeAbstraction",
-          FieldType "typeApplication" $ universal "m" $ local "TypeApplication",
-          FieldType "union" $ universal "m" $ local "Field",
-          FieldType "variable" $ local "Variable"],
+        universal "m" $ union [
+          field "application" $ universal "m" $ core "Application",
+          field "literal" $ core "Literal",
+          field "element" $ core "Name",
+          field "function" $ universal "m" $ core "Function",
+          field "let" $ universal "m" $ core "Let",
+          field "list" $ list $ universal "m" $ core "Term",
+          field "map" $ TypeMap $ MapType (universal "m" $ core "Term") (universal "m" $ core "Term"),
+          field "nominal" $ universal "m" $ core "NominalTerm",
+          field "optional" $ optional $ universal "m" $ core "Term",
+          field "record" $ list $ universal "m" $ core "Field",
+          field "set" $ TypeSet $ universal "m" $ core "Term",
+          field "typeAbstraction" $ universal "m" $ core "TypeAbstraction",
+          field "typeApplication" $ universal "m" $ core "TypeApplication",
+          field "union" $ universal "m" $ core "Field",
+          field "variable" $ core "Variable"],
 
-      element "Field"
+      datatype "Field"
         "A labeled term" $
-        universal "m" $ TypeRecord [
-          FieldType "name" $ local "FieldName",
-          FieldType "term" $ universal "m" $ local "Term"],
+        universal "m" $ record [
+          field "name" $ core "FieldName",
+          field "term" $ universal "m" $ core "Term"],
 
-      element "FieldName"
+      datatype "FieldName"
         "The name of a field"
-        stringType,
+        string,
 
-      element "FieldType"
+      datatype "FieldType"
         "The name and type of a field" $
-        TypeRecord [
-          FieldType "name" $ local "FieldName",
-          FieldType "type" $ local "Type"],
+        record [
+          field "name" $ core "FieldName",
+          field "type" $ core "Type"],
 
-      element "FloatType"
+      datatype "FloatType"
         "A floating-point type" $
         enum [
           "bigfloat",
           "float32",
           "float64"],
 
-      element "FloatValue"
+      datatype "FloatValue"
         "A floating-point literal value" $
-        TypeUnion [
-          FieldType "bigfloat" bigfloatType,
-          FieldType "float32" float32Type,
-          FieldType "float64" float64Type],
+        union [
+          field "bigfloat" bigfloat,
+          field "float32" float32,
+          field "float64" float64],
 
-      element "Function"
+      datatype "Function"
         "A function" $
-        universal "m" $ TypeUnion [
-          FieldType "cases" $ TypeList $ universal "m" $ local "Field",
-          FieldType "compareTo" $ universal "m" $ local "Term",
-          FieldType "data" unitType,
-          FieldType "lambda" $ universal "m" $ local "Lambda",
-          FieldType "optionalCases" $ universal "m" $ local "OptionalCases",
-          FieldType "primitive" $ local "Name",
-          FieldType "projection" $ local "FieldName"],
+        universal "m" $ union [
+          field "cases" $ list $ universal "m" $ core "Field",
+          field "compareTo" $ universal "m" $ core "Term",
+          field "data" unit,
+          field "lambda" $ universal "m" $ core "Lambda",
+          field "optionalCases" $ universal "m" $ core "OptionalCases",
+          field "primitive" $ core "Name",
+          field "projection" $ core "FieldName"],
 
-      element "FunctionType"
+      datatype "FunctionType"
         "A function type, also known as an arrow type" $
-        TypeRecord [
-          FieldType "domain" $ local "Type",
-          FieldType "codomain" $ local "Type"],
+        record [
+          field "domain" $ core "Type",
+          field "codomain" $ core "Type"],
 
-      element "FunctionVariant"
+      datatype "FunctionVariant"
         "The identifier of a function constructor" $
         enum [
           "cases",
@@ -117,7 +117,7 @@ hydraCoreGraph = Graph modName elements (const True) "hydra/core"
           "primitive",
           "projection"],
 
-      element "IntegerType"
+      datatype "IntegerType"
         "An integer type" $
         enum [
           "bigint",
@@ -130,51 +130,51 @@ hydraCoreGraph = Graph modName elements (const True) "hydra/core"
           "uint32",
           "uint64"],
 
-      element "IntegerValue"
+      datatype "IntegerValue"
         "An integer literal value" $
-        TypeUnion [
-          FieldType "bigint" bigintType,
-          FieldType "int8" int8Type,
-          FieldType "int16" int16Type,
-          FieldType "int32" int32Type,
-          FieldType "int64" int64Type,
-          FieldType "uint8" uint8Type,
-          FieldType "uint16" uint16Type,
-          FieldType "uint32" uint32Type,
-          FieldType "uint64" uint64Type],
+        union [
+          field "bigint" bigint,
+          field "int8" int8,
+          field "int16" int16,
+          field "int32" int32,
+          field "int64" int64,
+          field "uint8" uint8,
+          field "uint16" uint16,
+          field "uint32" uint32,
+          field "uint64" uint64],
 
-      element "Lambda"
+      datatype "Lambda"
         "A function abstraction (lambda)" $
-        universal "m" $ TypeRecord [
-          FieldType "parameter" $ local "Variable",
-          FieldType "body" $ universal "m" $ local "Term"],
+        universal "m" $ record [
+          field "parameter" $ core "Variable",
+          field "body" $ universal "m" $ core "Term"],
 
-      element "Let"
+      datatype "Let"
         "A 'let' binding" $
-        universal "m" $ TypeRecord [
-          FieldType "key" $ local "Variable",
-          FieldType "value" $ universal "m" $ local "Term",
-          FieldType "environment" $ universal "m" $ local "Term"],
+        universal "m" $ record [
+          field "key" $ core "Variable",
+          field "value" $ universal "m" $ core "Term",
+          field "environment" $ universal "m" $ core "Term"],
 
-      element "Literal"
+      datatype "Literal"
         "A term constant; an instance of a literal type" $
-        TypeUnion [
-          FieldType "binary" binaryType,
-          FieldType "boolean" $ local "BooleanValue",
-          FieldType "float" $ local "FloatValue",
-          FieldType "integer" $ local "IntegerValue",
-          FieldType "string" stringType],
+        union [
+          field "binary" binary,
+          field "boolean" $ core "BooleanValue",
+          field "float" $ core "FloatValue",
+          field "integer" $ core "IntegerValue",
+          field "string" string],
 
-      element "LiteralType"
+      datatype "LiteralType"
         "Any of a fixed set of literal types, also called atomic types, base types, primitive types, or type constants" $
-        TypeUnion [
-          FieldType "binary" unitType,
-          FieldType "boolean" unitType,
-          FieldType "float" $ local "FloatType",
-          FieldType "integer" $ local "IntegerType",
-          FieldType "string" unitType],
+        union [
+          field "binary" unit,
+          field "boolean" unit,
+          field "float" $ core "FloatType",
+          field "integer" $ core "IntegerType",
+          field "string" unit],
 
-      element "LiteralVariant"
+      datatype "LiteralVariant"
         "The identifier of a literal constructor" $
         enum [
           "binary",
@@ -183,53 +183,53 @@ hydraCoreGraph = Graph modName elements (const True) "hydra/core"
           "integer",
           "string"],
 
-      element "MapType"
+      datatype "MapType"
         "A map type" $
-        TypeRecord [
-          FieldType "keys" $ local "Type",
-          FieldType "values" $ local "Type"],
+        record [
+          field "keys" $ core "Type",
+          field "values" $ core "Type"],
 
-      element "Meta"
+      datatype "Meta"
         "A built-in metadata container for terms" $
-        TypeRecord [
-          FieldType "description" (TypeOptional stringType),
-          FieldType "type" (TypeOptional $ local "Type")],
+        record [
+          field "description" (optional string),
+          field "type" (optional $ core "Type")],
 
-      element "Name"
+      datatype "Name"
         "A unique element name"
-        stringType,
+        string,
 
-      element "NominalTerm"
+      datatype "NominalTerm"
         "A term annotated with a fixed, named type; an instance of a newtype" $
-        universal "m" $ TypeRecord [
-          FieldType "typeName" (local "Name"),
-          FieldType "term" (universal "m" $ local "Term")],
+        universal "m" $ record [
+          field "typeName" (core "Name"),
+          field "term" (universal "m" $ core "Term")],
 
-      element "OptionalCases"
+      datatype "OptionalCases"
         "A case statement for matching optional terms" $
-        universal "m" $ TypeRecord [
-          FieldType "nothing" (universal "m" $ local "Term"),
-          FieldType "just" (universal "m" $ local "Term")],
+        universal "m" $ record [
+          field "nothing" (universal "m" $ core "Term"),
+          field "just" (universal "m" $ core "Term")],
 
-      element "OptionalExpression"
+      datatype "OptionalExpression"
         "An encoded optional value, for languages which do not natively support optionals" $
-        universal "m" $ TypeUnion [
-          FieldType "just" (universal "m" $ local "Term"),
-          FieldType "nothing" unitType],
+        universal "m" $ union [
+          field "just" (universal "m" $ core "Term"),
+          field "nothing" unit],
 
-      element "Precision"
+      datatype "Precision"
         "Numeric precision: arbitrary precision, or precision to a specified number of bits" $
-        TypeUnion [
-          FieldType "arbitrary" unitType,
-          FieldType "bits" int32Type],
+        union [
+          field "arbitrary" unit,
+          field "bits" int32],
 
-      element "Term"
+      datatype "Term"
         "A data term" $
-        universal "m" $ TypeRecord [
-          FieldType "data" $ universal "m" $ local "Expression",
-          FieldType "meta" $ typeVariable "m"],
+        universal "m" $ record [
+          field "data" $ universal "m" $ core "Expression",
+          field "meta" $ variable "m"],
 
-      element "TermVariant"
+      datatype "TermVariant"
         "The identifier of a term expression constructor" $
         enum [
           "application",
@@ -248,45 +248,45 @@ hydraCoreGraph = Graph modName elements (const True) "hydra/core"
           "union",
           "variable"],
 
-      element "Type"
+      datatype "Type"
         "A data type" $
-        TypeUnion [
-          FieldType "literal" $ local "LiteralType",
-          FieldType "element" $ local "Type",
-          FieldType "function" $ local "FunctionType",
-          FieldType "list" $ local "Type",
-          FieldType "map" $ local "MapType",
-          FieldType "nominal" $ local "Name",
-          FieldType "optional" $ local "Type",
-          FieldType "record" $ TypeList $ local "FieldType",
-          FieldType "set" $ local "Type",
-          FieldType "union" $ TypeList $ local "FieldType",
-          FieldType "universal" $ local "UniversalType",
-          FieldType "variable" $ local "TypeVariable"],
+        union [
+          field "literal" $ core "LiteralType",
+          field "element" $ core "Type",
+          field "function" $ core "FunctionType",
+          field "list" $ core "Type",
+          field "map" $ core "MapType",
+          field "nominal" $ core "Name",
+          field "optional" $ core "Type",
+          field "record" $ list $ core "FieldType",
+          field "set" $ core "Type",
+          field "union" $ list $ core "FieldType",
+          field "universal" $ core "UniversalType",
+          field "variable" $ core "TypeVariable"],
 
-      element "TypeAbstraction"
+      datatype "TypeAbstraction"
         "A type abstraction (generalization), which binds a type variable to a term" $
-        universal "m" $ TypeRecord [
-          FieldType "parameter" (local "TypeVariable"),
-          FieldType "body" (universal "m" $ local "Term")],
+        universal "m" $ record [
+          field "parameter" (core "TypeVariable"),
+          field "body" (universal "m" $ core "Term")],
 
-      element "TypeApplication"
+      datatype "TypeApplication"
         "A type application (instantiation), which applies a term to a type" $
-        universal "m" $ TypeRecord [
-          FieldType "function" (universal "m" $ local "Term"),
-          FieldType "argument" (local "Type")],
+        universal "m" $ record [
+          field "function" (universal "m" $ core "Term"),
+          field "argument" (core "Type")],
 
-      element "TypeScheme"
+      datatype "TypeScheme"
         "A type expression together with free type variables occurring in the expression" $
-        TypeRecord [
-          FieldType "variables" (TypeList $ local "TypeVariable"),
-          FieldType "type" (local "Type")],
+        record [
+          field "variables" (list $ core "TypeVariable"),
+          field "type" (core "Type")],
 
-      element "TypeVariable"
+      datatype "TypeVariable"
         "A symbol which stands in for a type"
-        stringType,
+        string,
 
-      element "TypeVariant"
+      datatype "TypeVariant"
         "The identifier of a type constructor" $
         enum [
           "element",
@@ -302,18 +302,18 @@ hydraCoreGraph = Graph modName elements (const True) "hydra/core"
           "universal",
           "variable"],
 
-      element "TypedTerm"
+      datatype "TypedTerm"
         "A type together with an instance of the type" $
-        universal "m" $ TypeRecord [
-          FieldType "type" $ local "Type",
-          FieldType "term" $ universal "m" $ local "Term"],
+        universal "m" $ record [
+          field "type" $ core "Type",
+          field "term" $ universal "m" $ core "Term"],
 
-      element "UniversalType"
+      datatype "UniversalType"
         "A universally quantified ('forall') type, parameterized by a type variable" $
-        TypeRecord [
-          FieldType "variable" stringType,
-          FieldType "body" $ local "Type"],
+        record [
+          field "variable" string,
+          field "body" $ core "Type"],
 
-      element "Variable"
+      datatype "Variable"
         "A symbol which stands in for a term"
-        stringType]
+        string]
