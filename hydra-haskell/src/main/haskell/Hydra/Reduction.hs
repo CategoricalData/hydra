1761a81e3bfc2d065f7cb0bd69c36482ce7a82bd
Joshua Shinavier <josh@fortytwo.net>
2022-06-23 10:29:48 -0700

    Fold the (somewhat dated) interpreter / term reduction code into Hydra.Reduction

--- hydra-haskell/src/main/haskell/Hydra/Reduction.hs
+++ hydra-haskell/src/main/haskell/Hydra/Reduction.hs
@@ -5,10 +5,116 @@ import Hydra.Impl.Haskell.Extras
 import Hydra.Evaluation
 import qualified Hydra.Impl.Haskell.Dsl.Types as Types
 import Hydra.Rewriting
+import Hydra.Basics
+import Hydra.Impl.Haskell.Dsl.Terms
+import Hydra.Primitives
+
+import qualified Control.Monad as CM
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 
-betaReduceTypeRecursively :: (Default m, Ord m, Show m) => Bool -> Context m -> Type m -> Type m
-betaReduceTypeRecursively eager cx = rewriteType mapExpr id
+-- | A beta reduction function which is designed for safety, not speed.
+--   This function does not assume that term to be evaluated is in a normal form,
+--   and will provide an informative error message if evaluation fails.
+--   Type checking is assumed to have already occurred.
+betaReduceTerm :: (Ord a, Show a, Default a) => Context a -> Term a -> Result (Term a)
+betaReduceTerm context term = reduce M.empty term
+  where
+    reduce bindings term = if termIsOpaque (contextStrategy context) term
+      then pure term
+      else case termExpr term of
+        TermExprApplication (Application func arg) -> reduceb func >>= reduceApplication bindings [arg]
+        TermExprLiteral _ -> done
+        TermExprElement _ -> done
+        TermExprFunction f -> reduceFunction f
+        TermExprList terms -> defaultTerm . TermExprList <$> CM.mapM reduceb terms
+        TermExprMap map -> defaultTerm . TermExprMap <$> fmap M.fromList (CM.mapM reducePair $ M.toList map)
+          where
+            reducePair (k, v) = (,) <$> reduceb k <*> reduceb v
+        TermExprNominal (Named name term') -> (\t -> defaultTerm $ TermExprNominal (Named name t)) <$> reduce bindings term'
+        TermExprOptional m -> defaultTerm . TermExprOptional <$> CM.mapM reduceb m
+        TermExprRecord fields -> defaultTerm  . TermExprRecord <$> CM.mapM reduceField fields
+        TermExprSet terms -> defaultTerm . TermExprSet <$> fmap S.fromList (CM.mapM reduceb $ S.toList terms)
+        TermExprUnion f -> defaultTerm . TermExprUnion <$> reduceField f
+        TermExprVariable var@(Variable v) -> case M.lookup var bindings of
+          Nothing -> fail $ "cannot reduce free variable " ++ v
+          Just t -> reduceb t
+      where
+        done = pure term
+        reduceb = reduce bindings
+        reduceField (Field n t) = Field n <$> reduceb t
+        reduceFunction f = case f of
+          FunctionElimination el -> case el of
+            EliminationElement -> done
+            EliminationOptional (OptionalCases nothing just) -> defaultTerm . TermExprFunction . FunctionElimination . EliminationOptional <$>
+              (OptionalCases <$> reduceb nothing <*> reduceb just)
+            EliminationRecord _ -> done
+            EliminationUnion cases -> defaultTerm . TermExprFunction . FunctionElimination . EliminationUnion <$> CM.mapM reduceField cases
+          FunctionCompareTo other -> defaultTerm . TermExprFunction . FunctionCompareTo <$> reduceb other
+          FunctionLambda (Lambda v body) -> defaultTerm . TermExprFunction . FunctionLambda . Lambda v <$> reduceb body
+          FunctionPrimitive _ -> done
+
+    -- Assumes that the function is closed and fully reduced. The arguments may not be.
+    reduceApplication bindings args f = if L.null args then pure f else case termExpr f of
+      TermExprApplication (Application func arg) -> reduce bindings func
+         >>= reduceApplication bindings (arg:args)
+
+      TermExprFunction f -> case f of
+        FunctionElimination e -> case e of
+          EliminationElement -> do
+            arg <- reduce bindings $ L.head args
+            case termExpr arg of
+              TermExprElement name -> dereferenceElement context name
+                >>= reduce bindings
+                >>= reduceApplication bindings (L.tail args)
+              _ -> fail "tried to apply data (delta) to a non- element reference"
+
+          EliminationOptional (OptionalCases nothing just) -> do
+            arg <- (reduce bindings $ L.head args) >>= deref context
+            case termExpr arg of
+              TermExprOptional m -> case m of
+                Nothing -> reduce bindings nothing
+                Just t -> reduce bindings just >>= reduceApplication bindings (t:L.tail args)
+              _ -> fail $ "tried to apply an optional case statement to a non-optional term: " ++ show arg
+
+          EliminationUnion cases -> do
+            arg <- (reduce bindings $ L.head args) >>= deref context
+            case termExpr arg of
+              TermExprUnion (Field fname t) -> if L.null matching
+                  then fail $ "no case for field named " ++ unFieldName fname
+                  else reduce bindings (fieldTerm $ L.head matching)
+                    >>= reduceApplication bindings (t:L.tail args)
+                where
+                  matching = L.filter (\c -> fieldName c == fname) cases
+              _ -> fail $ "tried to apply a case statement to a non- union term: " ++ show arg
+
+        -- TODO: FunctionCompareTo
+
+        FunctionPrimitive name -> do
+             prim <- requirePrimitiveFunction context name
+             let arity = primitiveFunctionArity prim
+             if L.length args >= arity
+               then (mapM (reduce bindings) $ L.take arity args)
+                 >>= primitiveFunctionImplementation prim
+                 >>= reduce bindings
+                 >>= reduceApplication bindings (L.drop arity args)
+               else unwind
+           where
+             unwind = pure $ L.foldl apply (defaultTerm $ TermExprFunction f) args
+
+        FunctionLambda (Lambda v body) -> reduce (M.insert v (L.head args) bindings) body
+          >>= reduceApplication bindings (L.tail args)
+
+        -- TODO: FunctionProjection
+
+        _ -> fail $ "unsupported function variant: " ++ show (functionVariant f)
+
+      _ -> fail $ "tried to apply a non-function: " ++ show (termVariant f)
+
+betaReduceType :: (Default m, Ord m, Show m) => Bool -> Context m -> Type m -> Type m
+betaReduceType eager cx = rewriteType mapExpr id
   where
     mapExpr _ t = case typeExpr t of
         TypeExprApplication (TypeApplication lhs rhs) -> case typeExpr lhs' of
@@ -21,4 +127,46 @@ betaReduceTypeRecursively eager cx = rewriteType mapExpr id
             lhs' = recurse lhs
             rhs' = if eager then recurse rhs else rhs
         _ -> t
-    recurse = betaReduceTypeRecursively eager cx
+    recurse = betaReduceType eager cx
+
+-- | Whether a term is closed, i.e. represents a complete program
+termIsClosed :: Term a -> Bool
+termIsClosed = S.null . freeVariablesInTerm
+
+-- | Whether a term is opaque to reduction, i.e. need not be reduced
+termIsOpaque :: EvaluationStrategy -> Term a -> Bool
+termIsOpaque strategy term = S.member (termVariant term) (evaluationStrategyOpaqueTermVariants strategy)
+
+-- | Whether a term has been fully reduced to a "value"
+termIsValue :: EvaluationStrategy -> Term a -> Bool
+termIsValue strategy term = termIsOpaque strategy term || case termExpr term of
+      TermExprApplication _ -> False
+      TermExprLiteral _ -> True
+      TermExprElement _ -> True
+      TermExprFunction f -> functionIsValue f
+      TermExprList els -> forList els
+      TermExprMap map -> L.foldl
+        (\b (k, v) -> b && termIsValue strategy k && termIsValue strategy v)
+        True $ M.toList map
+      TermExprOptional m -> case m of
+        Nothing -> True
+        Just term -> termIsValue strategy term
+      TermExprRecord fields -> checkFields fields
+      TermExprSet els -> forList $ S.toList els
+      TermExprUnion field -> checkField field
+      TermExprVariable _ -> False
+  where
+    forList els = L.foldl (\b t -> b && termIsValue strategy t) True els
+    checkField = termIsValue strategy . fieldTerm
+    checkFields = L.foldl (\b f -> b && checkField f) True
+
+    functionIsValue f = case f of
+      FunctionCompareTo other -> termIsValue strategy other
+      FunctionElimination e -> case e of
+        EliminationElement -> True
+        EliminationNominal _ -> True
+        EliminationOptional (OptionalCases nothing just) -> termIsValue strategy nothing && termIsValue strategy just
+        EliminationRecord _ -> True
+        EliminationUnion cases -> checkFields cases
+      FunctionLambda (Lambda _ body) -> termIsValue strategy body
+      FunctionPrimitive _ -> True
--- hydra-haskell/src/test/haskell/Hydra/ReductionSpec.hs
+++ hydra-haskell/src/test/haskell/Hydra/ReductionSpec.hs
@@ -13,8 +13,71 @@ import Hydra.TestData
 import qualified Test.Hspec as H
 import qualified Test.QuickCheck as QC
 import qualified Data.List as L
-import qualified Data.Set as S
+import qualified Data.Char as C
 
+
+checkLiterals :: H.SpecWith ()
+checkLiterals = do
+  H.describe "Tests for literal values" $ do
+
+    H.it "Literal terms have no free variables" $
+      QC.property $ \av -> termIsClosed (literal av :: Term Meta)
+
+    H.it "Literal terms are fully reduced; check using a dedicated function" $
+      QC.property $ \av -> termIsValue testStrategy (literal av :: Term Meta)
+
+    H.it "Literal terms are fully reduced; check by trying to reduce them" $
+      QC.property $ \av ->
+        H.shouldBe
+          (eval (literal av))
+          (pure (literal av :: Term Meta))
+
+    H.it "Literal terms cannot be applied" $
+      QC.property $ \av (TypedTerm _ term) -> isFailure (eval $ apply (literal av) term)
+
+checkMonomorphicPrimitives :: H.SpecWith ()
+checkMonomorphicPrimitives = do
+  H.describe "Tests for monomorphic primitive functions" $ do
+
+    H.it "Example primitives have the expected arity" $ do
+      H.shouldBe
+        (primitiveFunctionArity <$> lookupPrimitiveFunction testContext _strings_toUpper)
+        (Just 1)
+      H.shouldBe
+        (primitiveFunctionArity <$> lookupPrimitiveFunction testContext _strings_splitOn)
+        (Just 2)
+
+    H.it "Simple applications of a unary function succeed" $
+      QC.property $ \s ->
+        H.shouldBe
+          (eval (apply (primitive _strings_toUpper) $ string s))
+          (pure (string $ fmap C.toUpper s))
+
+    H.it "Simple applications of a binary function succeed" $
+      QC.property $ \i1 i2 ->
+        H.shouldBe
+          (eval (apply (apply (primitive _math_add) $ int32 i1) $ int32 i2))
+          (pure (int32 $ i1 + i2))
+
+    H.it "Incomplete application of a primitive function leaves the term unchanged" $
+      QC.property $ \s1 ->
+        H.shouldBe
+          (eval (apply (primitive _strings_splitOn) $ string s1))
+          (pure (apply (primitive _strings_splitOn) $ string s1))
+
+    H.it "Extra arguments to a primitive function cause failure" $
+      QC.property $ \s1 s2 ->
+        isFailure (eval (apply (apply (primitive _strings_toUpper) $ string s1) $ string s2))
+
+checkPolymorphicPrimitives :: H.SpecWith ()
+checkPolymorphicPrimitives = do
+  H.describe "Tests for polymorphic primitive functions" $ do
+
+    H.it "Test polymorphic list length" $ do
+      QC.property $ \l ->
+        H.shouldBe
+          (eval (apply (primitive _lists_length) $ list l))
+          (pure (int32 $ L.length l))
 
 testBetaReduceTypeRecursively :: H.SpecWith ()
 testBetaReduceTypeRecursively = do
@@ -53,7 +116,7 @@ testBetaReduceTypeRecursively = do
         (Types.record [Types.field "foo" $ Types.function Types.string Types.string])
   where
     ResultSuccess scx = schemaContext testContext
-    reduce eager = betaReduceTypeRecursively eager scx 
+    reduce eager = betaReduceType eager scx 
     app1 = Types.apply (Types.lambda "t" $ Types.function (Types.variable "t") (Types.variable "t")) Types.string :: Type Meta
     app2 = Types.apply (Types.lambda "x" latLonType) Types.int32 :: Type Meta
     app3 = Types.apply (Types.lambda "a" $ Types.record [Types.field "foo" $ Types.variable "a"]) Types.unit :: Type Meta
@@ -62,6 +125,12 @@ testBetaReduceTypeRecursively = do
       Types.field "f2" $ Types.variable "y"]) Types.int32) Types.int64 :: Type Meta
     app5 = Types.apply (Types.lambda "a" $ Types.record [Types.field "foo" $ Types.variable "a"]) app1
 
+eval :: Term Meta -> Result (Term Meta)
+eval = betaReduceTerm testContext
+
 spec :: H.Spec
 spec = do
   testBetaReduceTypeRecursively
+  checkLiterals
+  checkMonomorphicPrimitives
+  checkPolymorphicPrimitives
