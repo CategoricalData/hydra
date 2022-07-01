3a8a784714f159becd0427bd7bbfbf4cff1e57ba
Joshua Shinavier <josh@fortytwo.net>
2022-07-01 14:29:32 -0700

    Add a JSON coder. Very similar to the YAML coder

--- hydra-haskell/src/main/haskell/Hydra/Lib/Literals.hs
+++ hydra-haskell/src/main/haskell/Hydra/Lib/Literals.hs
@@ -1,6 +1,14 @@
 module Hydra.Lib.Literals where
 
 
+-- TODO: expose as a primitive
+bigfloatToBigint :: Double -> Integer
+bigfloatToBigint = round
+
+-- TODO: expose as a primitive
+bigintToBigfloat :: Integer -> Double
+bigintToBigfloat = fromIntegral
+
 showInt32 :: Int -> String
 showInt32 = show
 
