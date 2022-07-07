23bab6f5e0eb4730f5cc03072ce6dd0d4fa33fd3
Joshua Shinavier <josh@fortytwo.net>
2022-07-07 09:56:31 -0700

    Add SHACL language constraints

---
+++ hydra-haskell/src/main/haskell/Hydra/Ext/Shacl/Language.hs
@@ -0,0 +1,37 @@
+module Hydra.Ext.Shacl.Language where
+  
+import Hydra.Core
+import Hydra.Adapter
+import Hydra.Basics
+
+import qualified Data.Set as S
+
+
+language :: Language m
+language = Language (LanguageName "hydra/ext/shacl") $ LanguageConstraints {
+  languageConstraintsEliminationVariants = S.empty,
+  languageConstraintsLiteralVariants = S.fromList literalVariants,
+  languageConstraintsFloatTypes = S.fromList floatTypes,
+  languageConstraintsFunctionVariants = S.empty,
+  languageConstraintsIntegerTypes = S.fromList integerTypes,
+  languageConstraintsTermVariants = S.fromList [
+    TermVariantElement,
+    TermVariantList,
+    TermVariantLiteral, 
+    TermVariantMap,
+    TermVariantNominal, 
+    TermVariantOptional, 
+    TermVariantRecord,
+    TermVariantSet,
+    TermVariantUnion],
+  languageConstraintsTypeVariants = S.fromList [
+    TypeVariantElement,
+    TypeVariantList, 
+    TypeVariantLiteral, 
+    TypeVariantMap, 
+    TypeVariantNominal, 
+    TypeVariantOptional, 
+    TypeVariantRecord, 
+    TypeVariantSet, 
+    TypeVariantUnion], 
+  languageConstraintsTypes = const True }
