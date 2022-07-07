efa495c7d0cb434f769e0a02320e5af338ef6457
Joshua Shinavier <josh@fortytwo.net>
2022-07-06 18:51:30 -0700

    Refine SHACL model and add links to the specification

--- hydra-haskell/src/main/haskell/Hydra/Impl/Haskell/Sources/Ext/Shacl/Model.hs
+++ hydra-haskell/src/main/haskell/Hydra/Impl/Haskell/Sources/Ext/Shacl/Model.hs
@@ -9,7 +9,6 @@ import Hydra.Impl.Haskell.Dsl.Standard
 import Hydra.Impl.Haskell.Sources.Ext.Rdf.Syntax
 
 
-
 shaclModelModule :: Module Meta
 shaclModelModule = Module shaclModel [rdfSyntaxModule]
 
@@ -24,39 +23,149 @@ shaclModel = Graph shaclModelName elements (const True) hydraCoreName
     rdf = nominal . qualify rdfSyntaxName . Name
 
     elements = [
+
+      def "CommonConstraints" $
+        doc "Any of a number of constraint parameters which can be applied either to node or property shapes" $
+        union [
+          field "and" $
+            doc "See https://www.w3.org/TR/shacl/#AndConstraintComponent" $
+            list $ shacl "Shape",
+            
+          field "closed" $
+            doc "See https://www.w3.org/TR/shacl/#ClosedConstraintComponent"
+            boolean,
+            
+          field "datatype" $
+            doc "See https://www.w3.org/TR/shacl/#DatatypeConstraintComponent" $
+            set $ rdf "Iri",
+            
+          field "hasValue" $
+            doc ("Specifies the condition that at least one value node is equal to the given RDF term. " ++
+                 "See https://www.w3.org/TR/shacl/#HasValueConstraintComponent") $
+            set $ rdf "Node",
+            
+          field "ignoredProperties" $
+            doc "See https://www.w3.org/TR/shacl/#ClosedConstraintComponent" $
+            list $ element $ rdf "Property",
+            
+          field "in" $
+            doc ("Specifies the condition that each value node is a member of a provided SHACL list. " ++
+                 "See https://www.w3.org/TR/shacl/#InConstraintComponent") $
+            list $ rdf "Node",
+            
+          field "node" $
+            doc "See https://www.w3.org/TR/shacl/#NodeConstraintComponent" $
+            set $ shacl "NodeShape",
+            
+          field "not" $
+            doc "See https://www.w3.org/TR/shacl/#NotConstraintComponent" $
+            set $ shacl "Shape",
+            
+          field "property" $
+            doc "See https://www.w3.org/TR/shacl/#PropertyConstraintComponent" $
+            set $ shacl "PropertyShape",
+            
+          field "or" $
+            doc "See https://www.w3.org/TR/shacl/#OrConstraintComponent" $
+            list $ shacl "Shape",
+            
+          field "xone" $
+            doc "See https://www.w3.org/TR/shacl/#XoneConstraintComponent" $
+            list $ shacl "Shape"],
+
+      def "CommonProperties" $
+        doc "Common constraint parameters and other properties for SHACL shapes" $
+        record [
+          field "constraints" $
+            doc "Common constraint parameters attached to this shape"
+            $ shacl "CommonConstraints",
+            
+          field "deactivated" $
+            doc "See https://www.w3.org/TR/shacl/#deactivated" $
+            optional boolean,
+            
+          field "message" $
+            doc "See https://www.w3.org/TR/shacl/#message" $
+            rdf "LangStrings",
+            
+          field "severity" $
+            doc "See https://www.w3.org/TR/shacl/#severity" $
+            optional $ shacl "Severity",
+            
+          field "targetClass" $
+            doc "See https://www.w3.org/TR/shacl/#targetClass" $
+            set $ element $ rdf "RdfsClass",
+
+          field "targetNode" $
+            doc "See https://www.w3.org/TR/shacl/#targetNode" $
+            optional $ rdf "IriOrLiteral",
+            
+          field "targetObjectsOf" $
+            doc "See https://www.w3.org/TR/shacl/#targetObjectsOf" $
+            set $ element $ rdf "Property",
+          
+          field "targetSubjectsOf" $
+            doc "See https://www.w3.org/TR/shacl/#targetSubjectsOf" $
+            set $ element $ rdf "Property"],
+
+      def "NodeShape" $
+        doc "A SHACL node shape. See https://www.w3.org/TR/shacl/#node-shapes" $
+        record [
+          field "common" $ shacl "CommonProperties"],
+
+      def "PropertyShape" $
+        doc "A SHACL property shape. See https://www.w3.org/TR/shacl/#property-shapes" $
+        record [
+          field "common" $ shacl "CommonProperties",
+          
+          field "constraints" $
+            doc "Any property shape -specific constraint parameters" $
+            shacl "PropertyShapeConstraints",
+            
+          field "defaultValue" $
+            doc "See https://www.w3.org/TR/shacl/#defaultValue" $
+            optional $ rdf "Node",
+            
+          field "description" $
+            doc "See https://www.w3.org/TR/shacl/#name" $
+            rdf "LangStrings",
+            
+          field "name" $
+            doc "See https://www.w3.org/TR/shacl/#name" $
+            rdf "LangStrings",
+            
+          field "order" $
+            doc "See https://www.w3.org/TR/shacl/#order" $
+            optional int32,
+            
+          field "path"  $ rdf "Resource"], -- TODO
+          -- Note: sh:group is omitted for now, for lack of a clear definition of PropertyGroup
+
+      def "PropertyShapeConstraints" $
+        doc "Any of a number of constraint parameters which are specific to property shapes, and cannot be applied to node shapes" $
+        union [
+          field "maxCount" $
+            doc ("The maximum cardinality. Node shapes cannot have any value for sh:maxCount. " ++
+                 "See https://www.w3.org/TR/shacl/#MaxCountConstraintComponent")
+            int32,
+            
+          field "minCount" $
+            doc ("The minimum cardinality. Node shapes cannot have any value for sh:minCount. " ++
+                 "See https://www.w3.org/TR/shacl/#MinCountConstraintComponent")
+            int32],
+
+      def "Severity" $ union [
+        field "info" $ doc "A non-critical constraint violation indicating an informative message" unit,
+        field "warning" $ doc "A non-critical constraint violation indicating a warning" unit,         
+        field "violation" $ doc "A constraint violation" unit],
+
       def "Shape" $
-        doc "A NodeShape or PropertyShape. For the official definition, see https://www.w3.org/TR/shacl/#dfn-shape" $
+        doc "A SHACL node or property shape. See https://www.w3.org/TR/shacl/#shapes" $
         union [
           field "node" $ shacl "NodeShape",
           field "property" $ shacl "PropertyShape"],
 
-
-
-
-
-
-      def "NodeShape" $
-        doc "A shape which matches nodes in the data graph. For the official definition, see https://www.w3.org/TR/shacl/#node-shapes" $
-        record [
-          field "property"  $ list $ shacl "PropertyShape",
-          field "xone" $ list $ shacl "Shape",
-          field "and" $ list $ shacl "Shape",
-          field "node" $ list $ shacl "NodeShape"],
-
-      def "PropertyShape" $
-        doc "A shape which matches a property or property path from a node. For the official definition, see https://www.w3.org/TR/shacl/#property-shapes" $
-        record [
-          field "name" $ set string,
-          field "description" $ set string,
-          field "path"  $ rdf "Iri",
-          field "datatype" $ optional $ rdf "Iri",
-          field "minCount" $ optional int32,
-          field "maxCount" $ optional int32,
-          field "node" $ list $ shacl "NodeShape",
-          field "property" $ list $ shacl "PropertyShape",
-          field "order" $ optional int32],
-
       def "ShapesGraph" $
-        doc "An RDF graph containing zero or more shapes that is passed into a SHACL validation process so that a data graph can be validated against the shapes" $
-        record [
-          field "shapes" $ list $ shacl "Shape"]]
+        doc ("An RDF graph containing zero or more shapes that is passed into a SHACL validation process " ++
+             "so that a data graph can be validated against the shapes") $
+        set $ shacl "Shape"]
