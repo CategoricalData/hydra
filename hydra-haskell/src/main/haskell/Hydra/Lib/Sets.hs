32b60410d76a14edc26338bbc7b92421e5d293d8
Joshua Shinavier <josh@fortytwo.net>
2022-08-21 09:13:56 -0700

    Add 'set' primitives

--- hydra-haskell/src/main/haskell/Hydra/Lib/Sets.hs
+++ hydra-haskell/src/main/haskell/Hydra/Lib/Sets.hs
@@ -2,14 +2,28 @@ module Hydra.Lib.Sets where
 
 import qualified Data.Set as S
 
+
 add :: Ord a => a -> S.Set a -> S.Set a
 add = S.insert
 
 contains :: Ord a => a -> S.Set a -> Bool
 contains = S.member
 
+fromList :: Ord a => [a] -> S.Set a
+fromList = S.fromList
+
 isEmpty :: S.Set a -> Bool
 isEmpty = S.null
 
+-- Note: the presence of a 'map' function does not imply that sets are a functor in Hydra
+map :: Ord b => (a -> b) -> S.Set a -> S.Set b
+map f = S.fromList . fmap f . S.toList
+
 remove :: Ord a => a -> S.Set a -> S.Set a
 remove = S.delete
+
+singleton :: a -> S.Set a
+singleton = S.singleton
+
+toList :: Ord a => S.Set a -> [a]
+toList = S.toList
