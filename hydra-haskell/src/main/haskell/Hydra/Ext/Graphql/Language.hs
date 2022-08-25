cb292d8ab421ebcfb6b073d86fbcb97f3030dcb8
Joshua Shinavier <josh@fortytwo.net>
2022-08-24 18:45:34 -0700

    Add language constraints for GraphQL

---
+++ hydra-haskell/src/main/haskell/Hydra/Ext/Graphql/Language.hs
@@ -0,0 +1,39 @@
+module Hydra.Ext.Graphql.Language where
+
+import Hydra.Adapter
+import Hydra.Core
+
+import qualified Data.Set as S
+
+
+language :: Language m
+language = Language (LanguageName "hydra/ext/graphql") $ LanguageConstraints {
+  -- Note: this language is for schemas and data only; support for queries may be added later
+  languageConstraintsEliminationVariants = S.empty,
+  languageConstraintsLiteralVariants = S.fromList [
+    LiteralVariantBoolean,
+    LiteralVariantFloat,
+    LiteralVariantInteger,
+    LiteralVariantString],
+  languageConstraintsFloatTypes = S.fromList [
+    FloatTypeBigfloat],
+  languageConstraintsFunctionVariants = S.empty,
+  languageConstraintsIntegerTypes = S.fromList [
+    IntegerTypeBigint],
+  languageConstraintsTermVariants = S.fromList [
+    TermVariantList,
+    TermVariantLiteral,
+    TermVariantOptional,
+    TermVariantRecord,
+    TermVariantUnion],
+  languageConstraintsTypeVariants = S.fromList [
+    TypeVariantApplication,
+    TypeVariantLambda,
+    TypeVariantList,
+    TypeVariantLiteral,
+    TypeVariantNominal,
+    TypeVariantOptional,
+    TypeVariantRecord,
+    TypeVariantUnion,
+    TypeVariantVariable],
+  languageConstraintsTypes = const True }
