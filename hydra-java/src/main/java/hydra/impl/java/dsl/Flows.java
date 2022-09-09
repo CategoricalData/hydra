8c9bcc273c13ba40e4ee1118e0fc131a5e860172
Joshua Shinavier <josh@fortytwo.net>
2022-09-08 18:08:55 -0700

    Add some flow utilities in Java

---
+++ hydra-java/src/main/java/hydra/impl/java/dsl/Flows.java
@@ -0,0 +1,25 @@
+package hydra.impl.java.dsl;
+
+import hydra.evaluation.Flow;
+import hydra.evaluation.FlowWrapper;
+import hydra.evaluation.Trace;
+import hydra.impl.java.FlowException;
+import java.util.Collections;
+
+
+public final class Flows {
+  private Flows() {
+  }
+
+  private static final Trace EMPTY_TRACE = new Trace(Collections.emptyList(), Collections.emptyList());
+
+  public static <S, A> A fromFlow(Flow<S, A> flow) throws FlowException {
+    FlowWrapper<S, A> wrapper = flow.value.apply(null).apply(EMPTY_TRACE);
+
+    if (!wrapper.value.isPresent()) {
+      throw new FlowException(wrapper.trace);
+    } else {
+      return wrapper.value.get();
+    }
+  }
+}
