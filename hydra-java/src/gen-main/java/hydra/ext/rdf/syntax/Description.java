102c3e88aa9f1cab55568ffa272ec9f008e90499
Joshua Shinavier <josh@fortytwo.net>
2022-09-22 14:19:51 -0700

    Update generated Java

--- hydra-java/src/gen-main/java/hydra/ext/rdf/syntax/Description.java
+++ hydra-java/src/gen-main/java/hydra/ext/rdf/syntax/Description.java
@@ -1,18 +1,18 @@
 package hydra.ext.rdf.syntax;
 
 /**
- * A graph of RDF statements together with a distinguished subject
+ * A graph of RDF statements together with a distinguished subject and/or object node
  */
 public class Description {
   public static final hydra.core.Name NAME = new hydra.core.Name("hydra/ext/rdf/syntax.Description");
   
   public final hydra.ext.rdf.syntax.Node subject;
   
-  public final hydra.ext.rdf.syntax.Graph triples;
+  public final hydra.ext.rdf.syntax.Graph graph;
   
-  public Description (hydra.ext.rdf.syntax.Node subject, hydra.ext.rdf.syntax.Graph triples) {
+  public Description (hydra.ext.rdf.syntax.Node subject, hydra.ext.rdf.syntax.Graph graph) {
     this.subject = subject;
-    this.triples = triples;
+    this.graph = graph;
   }
   
   @Override
@@ -21,19 +21,19 @@ public class Description {
       return false;
     }
     Description o = (Description) (other);
-    return subject.equals(o.subject) && triples.equals(o.triples);
+    return subject.equals(o.subject) && graph.equals(o.graph);
   }
   
   @Override
   public int hashCode() {
-    return 2 * subject.hashCode() + 3 * triples.hashCode();
+    return 2 * subject.hashCode() + 3 * graph.hashCode();
   }
   
   public Description withSubject(hydra.ext.rdf.syntax.Node subject) {
-    return new Description(subject, triples);
+    return new Description(subject, graph);
   }
   
-  public Description withTriples(hydra.ext.rdf.syntax.Graph triples) {
-    return new Description(subject, triples);
+  public Description withGraph(hydra.ext.rdf.syntax.Graph graph) {
+    return new Description(subject, graph);
   }
 }
