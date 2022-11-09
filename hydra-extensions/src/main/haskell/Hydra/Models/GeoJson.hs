7ba29d5d05678eccef4ff57f31709d003fb0f7eb
Joshua Shinavier <josh@fortytwo.net>
2022-11-09 11:05:42 -0800

    Add hydra-extensions submodule, providing extra models and utilities which are likely to be useful, but do not belong in Hydra proper

---
+++ hydra-extensions/src/main/haskell/Hydra/Models/GeoJson.hs
@@ -0,0 +1,166 @@
+module Hydra.Models.GeoJson where
+
+import Hydra.Compute
+import Hydra.Core
+import Hydra.Module
+import Hydra.Impl.Haskell.Dsl.Types as Types
+import Hydra.Impl.Haskell.Dsl.Standard
+import Hydra.Impl.Haskell.Sources.Compute
+import Hydra.Impl.Haskell.Sources.Core
+
+import Hydra.Impl.Haskell.Sources.Ext.Json.Model
+
+
+geoJsonModule :: Module Meta
+geoJsonModule = Module ns elements [jsonModelModule] $
+    Just ("A GeoJSON model based on the specification at https://www.rfc-editor.org/rfc/rfc7946. " ++
+          "This model provides some additional structure beyond the JSON encoding described in the specification; " ++
+          "For example, it provides MultiPoint not as an object with an array of coordinates which encode points, " ++
+          "but as an array of Points. A BoundingBox is not simply an list of numbers, but a list of CoordinateRanges.")
+  where
+    ns = Namespace "org/geojson/model"
+    def = datatype ns
+    geoj = nominal . qualify ns . Name
+    json = nsref $ moduleNamespace jsonModelModule
+
+    elements = [
+
+      def "BoundingBox" $
+        doc ("A GeoJSON object MAY have a member named \"bbox\" to include " ++
+             "information on the coordinate range for its Geometries, Features, or " ++
+             "FeatureCollections.  The value of the bbox member MUST be an array of " ++
+             "length 2*n where n is the number of dimensions represented in the " ++
+             "contained geometries, with all axes of the most southwesterly point " ++
+             "followed by all axes of the more northeasterly point.  The axes order " ++
+             "of a bbox follows the axes order of geometries.") $
+        list $ geoj "CoordinateRange",
+
+      def "CoordinateRange" $
+        record [
+          "min">: bigfloat,
+          "max">: bigfloat],
+
+      def "Feature" $
+       doc ("A Feature object represents a spatially bounded thing.  Every Feature " ++
+            "object is a GeoJSON object no matter where it occurs in a GeoJSON " ++
+            "text.") $
+       record [
+        "geometry">:
+          doc ("A Feature object has a member with the name \"geometry\".  The value " ++
+               "of the geometry member SHALL be either a Geometry object as " ++
+               "defined above or, in the case that the Feature is unlocated, a " ++
+               "JSON null value.") $
+          optional $ geoj "Geometry",
+        "properties">:
+          doc ("A Feature object has a member with the name \"properties\".  The " ++
+               "value of the properties member is an object (any JSON object or a " ++
+               "JSON null value).") $
+          optional $ Types.map string (json "Value"),
+        "id">:
+          doc ("If a Feature has a commonly used identifier, that identifier " ++
+               "SHOULD be included as a member of the Feature object with the name " ++
+               "\"id\", and the value of this member is either a JSON string or " ++
+               "number.") $
+          optional $ geoj "Id",
+        "bbox">: optional $ geoj "BoundingBox"],
+
+      def "FeatureCollection" $
+        doc ("A GeoJSON object with the type \"FeatureCollection\" is a " ++
+             "FeatureCollection object.  A FeatureCollection object has a member " ++
+             "with the name \"features\".  The value of \"features\" is a JSON array. " ++
+             "Each element of the array is a Feature object as defined above.  It " ++
+             "is possible for this array to be empty.") $
+        record [
+          "features">: list $ geoj "Feature",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "GeometryCollection" $
+        doc ("A GeoJSON object with type \"GeometryCollection\" is a Geometry object. " ++
+             "A GeometryCollection has a member with the name \"geometries\".  The " ++
+             "value of \"geometries\" is an array.  Each element of this array is a " ++
+             "GeoJSON Geometry object.  It is possible for this array to be empty.") $
+        record [
+          "geometries">: list $ geoj "Geometry",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "Geometry" $
+        doc ("A Geometry object represents points, curves, and surfaces in " ++
+             "coordinate space.  Every Geometry object is a GeoJSON object no " ++
+             "matter where it occurs in a GeoJSON text.") $
+        union [
+          "point">: geoj "Point",
+          "multiPoint">: geoj "MultiPoint",
+          "lineString">: geoj "LineString",
+          "multiLineString">: geoj "MultiLineString",
+          "polygon">: geoj "Polygon",
+          "multiPolygon">: geoj "MultiPolygon",
+          "geometryCollection">: geoj "GeometryCollection"],
+
+      def "Id" $
+        union [
+          "number">: bigfloat,
+          "string">: string],
+
+      def "LineString" $
+        doc ("For type \"LineString\", the \"coordinates\" member is an array of two or " ++
+             "more positions.") $
+        record [
+          "coordinates">: list $ geoj "Position",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "MultiLineString" $
+        doc ("For type \"MultiLineString\", the \"coordinates\" member is an array of " ++
+             "LineString coordinate arrays.") $
+        record [
+          "coordinates">: list $ geoj "LineString",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "MultiPoint" $
+        doc ("For type \"MultiPoint\", the \"coordinates\" member is an array of " ++
+             "positions.") $
+        record [
+          "coordinates">: list $ geoj "Point",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "MultiPolygon" $
+        doc ("For type \"MultiPolygon\", the \"coordinates\" member is an array of " ++
+             "Polygon coordinate arrays.") $
+        record [
+          "coordinates">: list $ geoj "Polygon",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "Object" $
+        doc ("A GeoJSON object represents a Geometry, Feature, or collection of " ++
+             "Features.") $
+        union [
+          "geometry">: geoj "Geometry",
+          "feature">: geoj "Feature",
+          "featureCollection">: geoj "FeatureCollection"],
+
+      def "Point" $
+        doc ("For type \"Point\", the \"coordinates\" member is a single position.") $
+        record [
+          "coordinates">: geoj "Position",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "Polygon" $
+        doc ("For type \"Polygon\", the \"coordinates\" member MUST be an array of " ++
+             "linear ring coordinate arrays.\n" ++
+             "For Polygons with more than one of these rings, the first MUST be " ++
+             "the exterior ring, and any others MUST be interior rings.  The " ++
+             "exterior ring bounds the surface, and the interior rings (if " ++
+             "present) bound holes within the surface.") $
+        record [
+          "coordinates">: list $ geoj "Position",
+          "bbox">: optional $ geoj "BoundingBox"],
+
+      def "Position" $
+        doc ("A position is an array of numbers.  There MUST be two or more " ++
+             "elements.  The first two elements are longitude and latitude, or " ++
+             "easting and northing, precisely in that order and using decimal " ++
+             "numbers.  Altitude or elevation MAY be included as an optional third " ++
+             "element.") $
+        record [
+          "latitude">: bigfloat,
+          "longitude">: bigfloat,
+          "altitude">: optional bigfloat]]
---
+++ hydra-extensions/src/main/haskell/Hydra/Models/StacItems.hs
@@ -0,0 +1,156 @@
+module Hydra.Models.StacItems where
+
+import Hydra.Compute
+import Hydra.Core
+import Hydra.Module
+import Hydra.Impl.Haskell.Dsl.Types as Types
+import Hydra.Impl.Haskell.Dsl.Standard
+import Hydra.Impl.Haskell.Sources.Compute
+import Hydra.Impl.Haskell.Sources.Core
+
+import Hydra.Models.GeoJson
+import Hydra.Models.IanaRelations
+
+
+stacModule :: Module Meta
+stacModule = Module ns elements [geoJsonModule, ianaRelationsModule] $
+    Just ("A model for SpatioTemporal Asset Catalog (STAC) Items. " ++
+          "See https://github.com/radiantearth/stac-spec/blob/master/item-spec/item-spec.md")
+  where
+    ns = Namespace "org/stacspec/items"
+    def = datatype ns
+
+    geoj = nsref $ moduleNamespace geoJsonModule
+    ianarel = nsref $ moduleNamespace ianaRelationsModule
+    stac = nominal . qualify ns . Name
+
+    elements = [
+
+      def "Asset" $
+        doc ("An Asset is an object that contains a URI to data associated with the Item that can be downloaded " ++
+             "or streamed. It is allowed to add additional fields.") $
+        record [
+          "href">:
+            doc "URI to the asset object. Relative and absolute URI are both allowed." $
+            stac "Uri",
+          "title">:
+            doc "The displayed title for clients and users." $
+            optional string,
+          "description">:
+            doc ("A description of the Asset providing additional details, such as how it was processed or " ++
+                 "created. CommonMark 0.29 syntax MAY be used for rich text representation.") $
+            optional string,
+          "type">:
+            doc ("Media type of the asset. See the common media types in the best practice doc for commonly " ++
+                 "used asset types.") $
+            optional $ stac "MediaType",
+          "roles">:
+            doc "The semantic roles of the asset, similar to the use of rel in links." $
+            list $ stac "Role"],
+
+      def "Item" $
+        doc ("This object describes a STAC Item. The fields id, type, bbox, geometry and properties are inherited " ++
+             "from GeoJSON.") $
+        record [
+          "feature">:
+            geoj "Feature",
+          "stacVersion">:
+            doc "The STAC version the Item implements" $
+            stac "StacVersion",
+          "stacExtensions">:
+            doc "A list of extensions the Item implements" $
+            list $ stac "Url",
+          "links">:
+            doc ("List of link objects to resources and related URLs. A link with the rel set to self is strongly " ++
+                 "recommended.") $
+            list $ stac "Link",
+          "assets">:
+            doc "Dictionary of asset objects that can be downloaded, each with a unique key." $
+            Types.map string (stac "Asset"),
+          "collection">:
+            doc ("The id of the STAC Collection this Item references to (see collection relation type). This field " ++
+                 "is required if such a relation type is present and is not allowed otherwise. This field provides " ++
+                 "an easy way for a user to search for any Items that belong in a specified Collection. Must be a " ++
+                 "non-empty string.") $
+            optional $ geoj "Id"],
+
+      def "Link" $
+        doc ("This object describes a relationship with another entity. Data providers are advised to be liberal " ++
+             "with the links section, to describe things like the Catalog an Item is in, related Items, parent or " ++
+             "child Items (modeled in different ways, like an 'acquisition' or derived data). It is allowed to add " ++
+             "additional fields such as a title and type.") $
+        record [
+          "href">:
+            doc "The actual link in the format of an URL. Relative and absolute links are both allowed." $
+            stac "Url",
+          "rel">:
+            doc ("Relationship between the current document and the linked document. See chapter \"Relation types\" " ++
+                 "for more information.") $
+            stac "RelationType",
+          "type">:
+            doc "Media type of the referenced entity." $
+            optional $ stac "MediaType",
+          "title">:
+            doc "A human readable title to be used in rendered displays of the link." $
+            optional string],
+
+      def "MediaType" string,
+
+      def "RelationType" $
+        doc ("STAC Items use a variety of rel types in the link object, to describe the exact nature of the link " ++
+             "between this Item and the entity it is linking to. It is recommended to use the official IANA Link " ++
+             "Relation Types where possible. The following table explains places where STAC use custom rel types " ++
+             "are used with Items. This happens where there is not a clear official option, or where STAC uses an " ++
+             "official type but adds additional meaning for the STAC context.") $
+        union [
+          "iana">: ianarel "LinkRelationType",
+          "stac">: stac "StacRelationType",
+          "other">: string],
+
+      def "Role" $
+        doc ("The roles field is used to describe the purpose of each asset. It is recommended to include one for " ++
+             "every asset, to give users a sense of why they might want to make use of the asset. There are some " ++
+             "emerging standards that enable clients to take particular action when they encounter particular " ++
+             "roles, listed below. But implementors are encouraged to come up with their own terms to describe the " ++
+             "role.") $
+        union [
+          "thumbnail">:
+            doc ("An asset that represents a thumbnail of the Item, typically a true color image (for Items with " ++
+                 "assets in the visible wavelengths), lower-resolution (typically smaller 600x600 pixels), and " ++
+                 "typically a JPEG or PNG (suitable for display in a web browser). Multiple assets may have this " ++
+                 "purpose, but it recommended that the type and roles be unique tuples. For example, Sentinel-2 L2A " ++
+                 "provides thumbnail images in both JPEG and JPEG2000 formats, and would be distinguished by their " ++
+                 "media types.") unit,
+          "overview">:
+            doc ("An asset that represents a possibly larger view than the thumbnail of the Item, for example, a " ++
+                 "true color composite of multi-band data.") unit,
+          "data">:
+            doc ("The data itself. This is a suggestion for a common role for data files to be used in case data " ++
+                 "providers don't come up with their own names and semantics.") unit,
+          "metadata">:
+            doc "A metadata sidecar file describing the data in this Item, for example the Landsat-8 MTL file." unit,
+          "other">:
+            string],
+
+      def "StacRelationType" $
+        union [
+          "self">:
+            doc ("STRONGLY RECOMMENDED. Absolute URL to the Item if it is available at a public URL. This is " ++
+                 "particularly useful when in a download package that includes metadata, so that the downstream " ++
+                 "user can know where the data has come from.") unit,
+          "root">:
+            doc "URL to the root STAC entity (Catalog or Collection)." unit,
+          "parent">:
+            doc ("URL to the parent STAC entity (Catalog or Collection).") unit,
+          "collection">:
+            doc ("STRONGLY RECOMMENDED. URL to a Collection. Absolute URLs should be used whenever possible. " ++
+                 "The referenced Collection is STRONGLY RECOMMENDED to implement the same STAC version as the Item. " ++
+                 "A link with this rel type is required if the collection field in properties is present.") unit,
+          "derivedFrom">:
+            doc ("URL to a STAC Item that was used as input data in the creation of this Item.") unit],
+
+      def "StacVersion" string,
+
+      def "Uri" string,
+
+      def "Url" string]
