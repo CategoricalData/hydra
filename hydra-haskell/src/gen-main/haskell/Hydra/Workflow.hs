30f4b9c578fd8b0d188305998f508c6de40d7756
Joshua Shinavier <josh@fortytwo.net>
2023-02-22 12:26:34 -0800

    Elevate the workflow module into the Hydra kernel, and make it available in Java

---
+++ hydra-haskell/src/gen-main/haskell/Hydra/Workflow.hs
@@ -0,0 +1,66 @@
+-- | A model for Hydra transformation workflows
+
+module Hydra.Workflow where
+
+import qualified Hydra.Compute as Compute
+import qualified Hydra.Core as Core
+import qualified Hydra.Module as Module
+import Data.List
+import Data.Map
+import Data.Set
+
+-- | The specification of a Hydra schema, provided as a set of modules and a distinguished type
+data HydraSchemaSpec = 
+  HydraSchemaSpec {
+    -- | The modules to include in the schema graph
+    hydraSchemaSpecModules :: [Module.Module Compute.Meta],
+    -- | The name of the top-level type; all data which passes through the workflow will be instances of this type
+    hydraSchemaSpecTypeName :: Core.Name}
+  deriving (Eq, Ord, Read, Show)
+
+_HydraSchemaSpec = (Core.Name "hydra/workflow.HydraSchemaSpec")
+
+_HydraSchemaSpec_modules = (Core.FieldName "modules")
+
+_HydraSchemaSpec_typeName = (Core.FieldName "typeName")
+
+-- | The specification of a schema at the source end of a workflow
+data SchemaSpec = 
+  -- | A native Hydra schema
+  SchemaSpecHydra HydraSchemaSpec |
+  -- | A schema provided as a file, available at the given file path
+  SchemaSpecFile String |
+  -- | A schema which will be provided within the workflow
+  SchemaSpecProvided 
+  deriving (Eq, Ord, Read, Show)
+
+_SchemaSpec = (Core.Name "hydra/workflow.SchemaSpec")
+
+_SchemaSpec_hydra = (Core.FieldName "hydra")
+
+_SchemaSpec_file = (Core.FieldName "file")
+
+_SchemaSpec_provided = (Core.FieldName "provided")
+
+-- | The specification of a workflow which takes a schema specification, reads data from a directory, and writes data to another directory
+data TransformWorkflow = 
+  TransformWorkflow {
+    -- | A descriptive name for the workflow
+    transformWorkflowName :: String,
+    -- | The schema specification
+    transformWorkflowSchemaSpec :: SchemaSpec,
+    -- | The source directory
+    transformWorkflowSrcDir :: String,
+    -- | The destination directory
+    transformWorkflowDestDir :: String}
+  deriving (Eq, Ord, Read, Show)
+
+_TransformWorkflow = (Core.Name "hydra/workflow.TransformWorkflow")
+
+_TransformWorkflow_name = (Core.FieldName "name")
+
+_TransformWorkflow_schemaSpec = (Core.FieldName "schemaSpec")
+
+_TransformWorkflow_srcDir = (Core.FieldName "srcDir")
+
+_TransformWorkflow_destDir = (Core.FieldName "destDir")
---
+++ hydra-java/src/gen-main/java/hydra/workflow/SchemaSpec.java
@@ -0,0 +1,134 @@
+package hydra.workflow;
+
+/**
+ * The specification of a schema at the source end of a workflow
+ */
+public abstract class SchemaSpec {
+  public static final hydra.core.Name NAME = new hydra.core.Name("hydra/workflow.SchemaSpec");
+  
+  private SchemaSpec () {
+  
+  }
+  
+  public abstract <R> R accept(Visitor<R> visitor) ;
+  
+  public interface Visitor<R> {
+    R visit(Hydra instance) ;
+    
+    R visit(File instance) ;
+    
+    R visit(Provided instance) ;
+  }
+  
+  public interface PartialVisitor<R> extends Visitor<R> {
+    default R otherwise(SchemaSpec instance) {
+      throw new IllegalStateException("Non-exhaustive patterns when matching: " + (instance));
+    }
+    
+    default R visit(Hydra instance) {
+      return otherwise((instance));
+    }
+    
+    default R visit(File instance) {
+      return otherwise((instance));
+    }
+    
+    default R visit(Provided instance) {
+      return otherwise((instance));
+    }
+  }
+  
+  /**
+   * A native Hydra schema
+   */
+  public static final class Hydra extends hydra.workflow.SchemaSpec {
+    /**
+     * A native Hydra schema
+     */
+    public final hydra.workflow.HydraSchemaSpec value;
+    
+    public Hydra (hydra.workflow.HydraSchemaSpec value) {
+      this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      if (!(other instanceof Hydra)) {
+        return false;
+      }
+      Hydra o = (Hydra) (other);
+      return value.equals(o.value);
+    }
+    
+    @Override
+    public int hashCode() {
+      return 2 * value.hashCode();
+    }
+    
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visit(this);
+    }
+  }
+  
+  /**
+   * A schema provided as a file, available at the given file path
+   */
+  public static final class File extends hydra.workflow.SchemaSpec {
+    /**
+     * A schema provided as a file, available at the given file path
+     */
+    public final String value;
+    
+    public File (String value) {
+      this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      if (!(other instanceof File)) {
+        return false;
+      }
+      File o = (File) (other);
+      return value.equals(o.value);
+    }
+    
+    @Override
+    public int hashCode() {
+      return 2 * value.hashCode();
+    }
+    
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visit(this);
+    }
+  }
+  
+  /**
+   * A schema which will be provided within the workflow
+   */
+  public static final class Provided extends hydra.workflow.SchemaSpec {
+    public Provided () {
+    
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      if (!(other instanceof Provided)) {
+        return false;
+      }
+      Provided o = (Provided) (other);
+      return true;
+    }
+    
+    @Override
+    public int hashCode() {
+      return 0;
+    }
+    
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visit(this);
+    }
+  }
+}
