42429bbefbeb78628964941bd58f4e590edaeee9
Joshua Shinavier <josh@fortytwo.net>
2023-02-28 16:53:36 -0800

    Experimental bundling of Java primitive function implementations with their metadata

--- hydra-java/src/test/java/hydra/lib/StringsTest.java
+++ hydra-java/src/test/java/hydra/lib/StringsTest.java
@@ -5,53 +5,54 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static hydra.lib.strings.Strings.*;
+// import static hydra.dsl.prims.Strings.*;
+import hydra.lib.strings.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringsTest {
     @Test
     public void catIsCorrect() {
-        assertEquals("onetwothree", cat(Arrays.asList("one", "two", "three")));
-        assertEquals("one", cat(Arrays.asList("", "one", "", "")));
-        assertEquals("", cat(Collections.emptyList()));
+        assertEquals("onetwothree", Cat.apply(Arrays.asList("one", "two", "three")));
+        assertEquals("one", Cat.apply(Arrays.asList("", "one", "", "")));
+        assertEquals("", Cat.apply(Collections.emptyList()));
     }
 
     @Test
     public void lengthIsCorrect() {
-        assertEquals(0, length(""));
-        assertEquals(1, length("a"));
-        assertEquals(3, length("one"));
+        assertEquals(0, Length.apply(""));
+        assertEquals(1, Length.apply("a"));
+        assertEquals(3, Length.apply("one"));
     }
 
     @Test
     public void splitOnIsCorrect() {
-        assertEquals(Arrays.asList("Mi", "i", "ippi"), splitOn("ss", "Mississippi"));
-        assertEquals(Arrays.asList("", ""), splitOn("Mississippi", "Mississippi"));
+        assertEquals(Arrays.asList("Mi", "i", "ippi"), SplitOn.apply("ss", "Mississippi"));
+        assertEquals(Arrays.asList("", ""), SplitOn.apply("Mississippi", "Mississippi"));
 
-        assertEquals(Arrays.asList("one", "two", "three"), splitOn(" ", "one two three"));
-        assertEquals(Arrays.asList("", "one", "two", "three", ""), splitOn(" ", " one two three "));
-        assertEquals(Arrays.asList("", "", "one", "two", "three", ""), splitOn(" ", "  one two three "));
-        assertEquals(Arrays.asList("", "one two three "), splitOn("  ", "  one two three "));
+        assertEquals(Arrays.asList("one", "two", "three"), SplitOn.apply(" ", "one two three"));
+        assertEquals(Arrays.asList("", "one", "two", "three", ""), SplitOn.apply(" ", " one two three "));
+        assertEquals(Arrays.asList("", "", "one", "two", "three", ""), SplitOn.apply(" ", "  one two three "));
+        assertEquals(Arrays.asList("", "one two three "), SplitOn.apply("  ", "  one two three "));
 
-        assertEquals(Arrays.asList("", "a"), splitOn("aa", "aaa"));
+        assertEquals(Arrays.asList("", "a"), SplitOn.apply("aa", "aaa"));
 
-        assertEquals(Collections.singletonList(""), splitOn("a", ""));
+        assertEquals(Collections.singletonList(""), SplitOn.apply("a", ""));
 
-        assertEquals(Arrays.asList("", "a", "b", "c"), splitOn("", "abc"));
-        assertEquals(Collections.singletonList(""), splitOn("", ""));
+        assertEquals(Arrays.asList("", "a", "b", "c"), SplitOn.apply("", "abc"));
+        assertEquals(Collections.singletonList(""), SplitOn.apply("", ""));
     }
 
     @Test
     // TODO: test the behavior of toLower for extended characters. Currently, the implementation just inherits Java's String.toLower()
     public void toLowerIsCorrect() {
-        assertEquals("one two three", toLower("One TWO threE"));
-        assertEquals("abc123", toLower("AbC123"));
+        assertEquals("one two three", ToLower.apply("One TWO threE"));
+        assertEquals("abc123", ToLower.apply("AbC123"));
     }
 
     @Test
     // TODO: test the behavior of toUpper for extended characters. Currently, the implementation just inherits Java's String.toUpper()
     public void toUpperIsCorrect() {
-        assertEquals("ONE TWO THREE", toUpper("One TWO threE"));
-        assertEquals("ABC123", toUpper("AbC123"));
+        assertEquals("ONE TWO THREE", ToUpper.apply("One TWO threE"));
+        assertEquals("ABC123", ToUpper.apply("AbC123"));
     }
 }
