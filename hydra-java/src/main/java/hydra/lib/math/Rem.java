4250c4feaa301c889b0a3687843b2392466882c5
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 13:34:55 -0800

    [hydra-java] Implement partial application for math lib
    This commit allows for partial application of the primitive functions in the math library. In other words, currying.
    
    You can call `apply` with any number of arguments (up to the maximum defined for a function), and receive one of the following return types:
    - If you provided all arguments, then you simply get the function's defined return value.
    - If you provided a subset of arguments, then you get a function that takes the rest of the arguments and returns the final value.

--- hydra-java/src/main/java/hydra/lib/math/Rem.java
+++ hydra-java/src/main/java/hydra/lib/math/Rem.java
@@ -3,13 +3,19 @@ package hydra.lib.math;
 import hydra.core.Name;
 import hydra.util.PrimitiveFunction;
 
+import java.util.function.Function;
+
 public class Rem<A> extends PrimitiveFunction<A> {
     public Name name() {
         return new Name("hydra/lib/math.rem");
     }
 
-    public static Integer apply(Integer dividend, Integer divisor) {
+    public static Function<Integer, Integer> apply(Integer dividend) {
         // % in Java is a mathematical remainder, not modulus
-        return (dividend % divisor);
+        return (divisor) -> dividend % divisor;
+    }
+
+    public static Integer apply(Integer dividend, Integer divisor) {
+        return Rem.apply(dividend).apply(divisor);
     }
 }
