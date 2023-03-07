c7e7d73c19c17ff14535f2e7e0f3206d6a80d863
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 15:19:52 -0800

    [hydra-java] Implement partial application for sets library

--- hydra-java/src/main/java/hydra/lib/sets/Map.java
+++ hydra-java/src/main/java/hydra/lib/sets/Map.java
@@ -12,7 +12,11 @@ public class Map<A> extends PrimitiveFunction<A> {
         return new Name("hydra/lib/sets.map");
     }
 
+    public static <X, Y> Function<Set<X>, Set<Y>> apply(Function<X, Y> mapping) {
+        return (arg) -> arg.stream().map(mapping).collect(Collectors.toSet());
+    }
+
     public static <X, Y> Set<Y> apply(Function<X, Y> mapping, Set<X> arg) {
-        return arg.stream().map(mapping).collect(Collectors.toSet());
+        return Map.apply(mapping).apply(arg);
     }
 }
