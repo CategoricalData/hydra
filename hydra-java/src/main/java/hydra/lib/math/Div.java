c3ae0cfe555a5ed1bde003b9fe51e44ac9055563
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 16:18:28 -0800

    Revert "[hydra-java] Implement partial application for math lib"
    This reverts commit 4250c4feaa301c889b0a3687843b2392466882c5.

--- hydra-java/src/main/java/hydra/lib/math/Div.java
+++ hydra-java/src/main/java/hydra/lib/math/Div.java
@@ -3,18 +3,12 @@ package hydra.lib.math;
 import hydra.core.Name;
 import hydra.util.PrimitiveFunction;
 
-import java.util.function.Function;
-
 public class Div<A> extends PrimitiveFunction<A> {
     public Name name() {
         return new Name("hydra/lib/math.div");
     }
 
-    public static Function<Integer, Integer> apply(Integer dividend) {
-        return (divisor) -> dividend / divisor;
-    }
-
     public static Integer apply(Integer dividend, Integer divisor) {
-        return Div.apply(dividend).apply(divisor);
+        return (dividend / divisor);
     }
 }
