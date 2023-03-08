ecf109ffd1b6b1f254dfc192c2610c7ce6ad9fd3
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 16:22:52 -0800

    [hydra-java] Update math lib partial function application style

--- hydra-java/src/main/java/hydra/lib/math/Add.java
+++ hydra-java/src/main/java/hydra/lib/math/Add.java
@@ -3,9 +3,15 @@ package hydra.lib.math;
 import hydra.core.Name;
 import hydra.util.PrimitiveFunction;
 
+import java.util.function.Function;
+
 public class Add<A> extends PrimitiveFunction<A> {
     public Name name() {
         return new Name("hydra/lib/math.add");
+    }
+
+    public static Function<Integer, Integer> apply(Integer augend) {
+        return (addend) -> apply(augend, addend);
     }
 
     public static Integer apply(Integer augend, Integer addend) {
