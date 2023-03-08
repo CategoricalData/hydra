2c8d92b33dda56ad8d71ad3f772a2421c1d33fec
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 16:16:03 -0800

    [hydra-java] Update sets partial function application style

--- hydra-java/src/main/java/hydra/lib/sets/Contains.java
+++ hydra-java/src/main/java/hydra/lib/sets/Contains.java
@@ -3,11 +3,16 @@ package hydra.lib.sets;
 import hydra.core.Name;
 import hydra.util.PrimitiveFunction;
 
+import java.util.function.Function;
 import java.util.Set;
 
 public class Contains<A> extends PrimitiveFunction<A> {
     public Name name() {
         return new Name("hydra/lib/sets.contains");
+    }
+
+    public static <X> Function<Set<X>, Boolean> apply(X elem) {
+        return (arg) -> apply(elem, arg);
     }
 
     public static <X> Boolean apply(X elem, Set<X> arg) {
