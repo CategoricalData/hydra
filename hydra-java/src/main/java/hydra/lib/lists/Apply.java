3e9178c91a4bd553b26991153bd7919f585ac728
aman-dureja <dureja.aman.97@gmail.com>
2023-03-07 16:10:06 -0800

    [hydra-java] Implement partial function application for lists library

--- hydra-java/src/main/java/hydra/lib/lists/Apply.java
+++ hydra-java/src/main/java/hydra/lib/lists/Apply.java
@@ -3,14 +3,18 @@ package hydra.lib.lists;
 import hydra.core.Name;
 import hydra.util.PrimitiveFunction;
 
+import java.util.function.Function;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.function.Function;
 
 
 public class Apply<A> extends PrimitiveFunction<A> {
     public Name name() {
         return new Name("hydra/lib/lists.apply");
+    }
+
+    public static <X, Y> Function<List<X>, List<Y>> apply(List<Function<X, Y>> functions) {
+        return (args) -> apply(functions, args);
     }
 
     public static <X, Y> List<Y> apply(List<Function<X, Y>> functions, List<X> args) {
