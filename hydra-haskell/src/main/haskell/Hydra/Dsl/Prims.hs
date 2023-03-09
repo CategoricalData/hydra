54aa4b51d1210bb3ac901e5600a6df998d108c1a
Joshua Shinavier <josh@fortytwo.net>
2023-03-09 14:38:53 -0800

    Small improvement to primitives DSL in Haskell

--- hydra-haskell/src/main/haskell/Hydra/Dsl/Prims.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Prims.hs
@@ -1,5 +1,6 @@
--- | A DSL for constructing primitive function definitions
+{-# LANGUAGE FlexibleInstances #-} -- for IsString with nontrivial parameters
 
+-- | A DSL for constructing primitive function definitions
 module Hydra.Dsl.Prims where
 
 import Hydra.Kernel
@@ -10,20 +11,10 @@ import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Maybe as Y
---import Data.String(IsString(..))
+import Hydra.Rewriting (removeTermAnnotations)
+import Data.String(IsString(..))
 
-
---instance IsString (TermCoder a (Term a)) where fromString = variable
-
-binaryPrimitive :: Name -> TermCoder a x -> TermCoder a y -> TermCoder a z -> (x -> y -> z) -> Primitive a
-binaryPrimitive name input1 input2 output compute = Primitive name ft impl
-  where
-    ft = TypeFunction $ FunctionType (termCoderType input1) (Types.function (termCoderType input2) (termCoderType output))
-    impl args = do
-      Terms.expectNArgs 2 args
-      arg1 <- coderEncode (termCoderCoder input1) (args !! 0)
-      arg2 <- coderEncode (termCoderCoder input2) (args !! 1)
-      coderDecode (termCoderCoder output) $ compute arg1 arg2
+instance IsString (TermCoder a (Term a)) where fromString = variable
 
 boolean :: Show a => TermCoder a Bool
 boolean = TermCoder Types.boolean $ Coder encode decode
@@ -38,10 +29,12 @@ flow states values = TermCoder (Types.wrap _Flow Types.@@ (termCoderType states)
     encode _ = fail $ "cannot currently encode flows from terms"
     decode _ = fail $ "cannot decode flows to terms"
 
-function :: TermCoder a x -> TermCoder a y -> TermCoder a (x -> y)
+function :: (Ord a, Show a) => TermCoder a x -> TermCoder a y -> TermCoder a (x -> y)
 function dom cod = TermCoder (Types.function (termCoderType dom) (termCoderType cod)) $ Coder encode decode
   where
-    encode _ = fail $ "cannot currently encode functions from terms"
+    encode term = case stripTerm term of
+      TermApplication _ -> fail $ "cannot currently encode application terms in primitives"
+      _ -> fail $ "cannot encode: " ++ show (removeTermAnnotations term)
     decode _ = fail $ "cannot decode functions to terms"
 
 int32 :: Show a => TermCoder a Int
@@ -67,11 +60,6 @@ map keys values = TermCoder (Types.map (termCoderType keys) (termCoderType value
           ve <- (coderDecode $ termCoderCoder values) v
           return (ke, ve)
 
-nullaryPrimitive :: Name -> TermCoder a x -> x -> Primitive a
-nullaryPrimitive name output value = Primitive name (termCoderType output) impl
-  where
-    impl _ = coderDecode (termCoderCoder output) value
-
 optional :: Show a => TermCoder a x -> TermCoder a (Y.Maybe x)
 optional mel = TermCoder (Types.optional $ termCoderType mel) $ Coder encode decode
   where
@@ -79,6 +67,30 @@ optional mel = TermCoder (Types.optional $ termCoderType mel) $ Coder encode dec
     decode mv = Terms.optional <$> case mv of
       Nothing -> pure Nothing
       Just v -> Just <$> (coderDecode $ termCoderCoder mel) v
+
+prim0 :: Name -> TermCoder a x -> x -> Primitive a
+prim0 name output value = Primitive name (termCoderType output) impl
+  where
+    impl _ = coderDecode (termCoderCoder output) value
+
+prim1 :: Name -> TermCoder a x -> TermCoder a y -> (x -> y) -> Primitive a
+prim1 name input1 output compute = Primitive name ft impl
+  where
+    ft = TypeFunction $ FunctionType (termCoderType input1) $ termCoderType output
+    impl args = do
+      Terms.expectNArgs 1 args
+      arg1 <- coderEncode (termCoderCoder input1) (args !! 0)
+      coderDecode (termCoderCoder output) $ compute arg1
+
+prim2 :: Name -> TermCoder a x -> TermCoder a y -> TermCoder a z -> (x -> y -> z) -> Primitive a
+prim2 name input1 input2 output compute = Primitive name ft impl
+  where
+    ft = TypeFunction $ FunctionType (termCoderType input1) (Types.function (termCoderType input2) (termCoderType output))
+    impl args = do
+      Terms.expectNArgs 2 args
+      arg1 <- coderEncode (termCoderCoder input1) (args !! 0)
+      arg2 <- coderEncode (termCoderCoder input2) (args !! 1)
+      coderDecode (termCoderCoder output) $ compute arg1 arg2
 
 set :: (Ord x, Ord a, Show a) => TermCoder a x -> TermCoder a (S.Set x)
 set els = TermCoder (Types.set $ termCoderType els) $ Coder encode decode
@@ -91,15 +103,6 @@ string = TermCoder Types.string $ Coder encode decode
   where
     encode = Terms.expectString
     decode = pure . Terms.string
-
-unaryPrimitive :: Name -> TermCoder a x -> TermCoder a y -> (x -> y) -> Primitive a
-unaryPrimitive name input1 output compute = Primitive name ft impl
-  where
-    ft = TypeFunction $ FunctionType (termCoderType input1) $ termCoderType output
-    impl args = do
-      Terms.expectNArgs 1 args
-      arg1 <- coderEncode (termCoderCoder input1) (args !! 0)
-      coderDecode (termCoderCoder output) $ compute arg1
 
 variable :: String -> TermCoder a (Term a)
 variable v = TermCoder (Types.variable v) $ Coder encode decode
