84bbe5d88cce66e0fe7957f02ddbec093977d4b4
Joshua Shinavier <josh@fortytwo.net>
2023-03-14 01:05:49 -0700

    Provide intermediate Java implementations for additional primitives

--- hydra-java/src/main/java/hydra/lib/strings/Length.java
+++ hydra-java/src/main/java/hydra/lib/strings/Length.java
@@ -1,9 +1,17 @@
 package hydra.lib.strings;
 
+import hydra.compute.Flow;
 import hydra.core.Name;
+import hydra.core.Term;
 import hydra.core.Type;
+import hydra.dsl.Expect;
+import hydra.dsl.Terms;
+import hydra.graph.Graph;
 import hydra.tools.PrimitiveFunction;
+import java.util.List;
+import java.util.function.Function;
 
+import static hydra.Flows.map;
 import static hydra.dsl.Types.*;
 
 public class Length<A> extends PrimitiveFunction<A> {
@@ -14,6 +22,11 @@ public class Length<A> extends PrimitiveFunction<A> {
     @Override
     public Type<A> type() {
         return function(string(), int32());
+    }
+
+    @Override
+    protected Function<List<Term<A>>, Flow<Graph<A>, Term<A>>> implementation() {
+        return args -> map(Expect.string(args.get(0)), s -> Terms.int32(apply(s)));
     }
 
     public static int apply(String s) {
