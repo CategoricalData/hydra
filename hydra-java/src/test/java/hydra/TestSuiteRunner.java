efafc594440ef347e1385685de2002a5ac61d278
Joshua Shinavier <josh@fortytwo.net>
2023-03-16 05:23:45 -0700

    Add parameterized test runner for language-agnostic test suite in Java

--- hydra-java/src/test/java/hydra/TestSuiteRunner.java
+++ hydra-java/src/test/java/hydra/TestSuiteRunner.java
@@ -8,8 +8,12 @@ import hydra.graph.Graph;
 import hydra.test.testSuite.TestSuite;
 import hydra.testing.TestCase;
 import hydra.testing.TestGroup;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static hydra.Flows.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,48 +23,51 @@ import static org.junit.jupiter.api.Assertions.*;
  * Java executor for Hydra's language-agnostic test suite
  */
 public class TestSuiteRunner extends HydraTestBase {
-    @Test
-    public void runAllTestCases() {
-        TestGroup<Kv> allTests = TestSuite.allTests;
-        runTestGroup(emptyGraph(), allTests, 0);
-    }
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void runTestCase(String name, Term<Kv> input, Term<Kv> output) {
+        Graph<Kv> graph = emptyGraph();
+        String suffix = " (" + name + ")";
 
-    private static <A> void runTestCase(Graph<A> graph, TestCase<A> testCase, int depth, int idx) {
-        System.out.println(testCaseDescription(testCase, depth, idx));
-
-        Flow<Graph<A>, Term<A>> reduced = reduce(testCase.input);
-        FlowState<Graph<A>, Term<A>> result = reduced.value.apply(graph).apply(EMPTY_TRACE);
+        Flow<Graph<Kv>, Term<Kv>> reduced = reduce(input);
+        FlowState<Graph<Kv>, Term<Kv>> result = reduced.value.apply(graph).apply(EMPTY_TRACE);
         if (result.value.isPresent()) {
-            assertEquals(testCase.output, result.value.get(), "Original term does not reduce to expected term");
+            assertEquals(output, result.value.get(), "Original term does not reduce to expected term" + suffix);
         } else if (result.trace.messages.isEmpty()){
-            Assertions.fail("Reduction failed");
+            Assertions.fail("Reduction failed" + suffix);
         } else {
-            Assertions.fail("Reduction failed: " + result.trace.messages.get(0));
+            Assertions.fail("Reduction failed: " + result.trace.messages.get(0) + suffix);
         }
     }
 
-    private static <A> void runTestGroup(Graph<A> graph, TestGroup<A> testGroup, int depth) {
-        System.out.println(testGroupDescription(testGroup, depth));
+    private static List<Arguments> provideTestCases() {
+        List<Arguments> args = new ArrayList<>();
+        addTestGroup(TestSuite.allTests, null, args);
+        return args;
+    }
 
+    private static <A> void addTestCase(TestCase<A> testCase, int idx, String prefix, List<Arguments> args) {
+        args.add(Arguments.of(testCaseDescription(testCase, prefix, idx), testCase.input, testCase.output));
+    }
+
+    private static <A> void addTestGroup(TestGroup<A> testGroup, String prefix, List<Arguments> args) {
+        String newPrefix = testGroupDescription(testGroup, prefix);
         int idx = 0;
         for (TestCase<A> testCase : testGroup.cases) {
-            runTestCase(graph, testCase, depth + 1, ++idx);
+            addTestCase(testCase, ++idx, newPrefix, args);
         }
-
         for (TestGroup<A> subgroup : testGroup.subgroups) {
-            runTestGroup(graph, subgroup, depth + 1);
+            addTestGroup(subgroup, newPrefix, args);
         }
     }
 
-    private static <A> String testCaseDescription(TestCase<A> testCase, int depth, int idx) {
-        return indent(depth) + testCase.description.orElse("test #" + idx);
+    private static <A> String testCaseDescription(TestCase<A> testCase, String prefix, int idx) {
+        String name = testCase.description.orElse("test #" + idx);
+        return prefix == null ? name : prefix + " > " + name;
     }
 
-    private static <A> String testGroupDescription(TestGroup<A> testGroup, int depth) {
-        return indent(depth) + testGroup.name + testGroup.description.map(s -> " (" + s + ")").orElse("");
-    }
-
-    private static String indent(int depth) {
-        return "    ".repeat(Math.max(0, depth));
+    private static <A> String testGroupDescription(TestGroup<A> testGroup, String prefix) {
+        String name = testGroup.name + testGroup.description.map(s -> " (" + s + ")").orElse("");
+        return prefix == null ? name : prefix + " > " + name;
     }
 }
