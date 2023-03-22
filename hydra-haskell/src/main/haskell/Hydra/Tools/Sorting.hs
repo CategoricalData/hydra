d8b5fcf75674fad296147c6c7642d572b7e9ca77
Joshua Shinavier <josh@fortytwo.net>
2023-03-22 15:41:44 -0700

    Use Data.Graph for a better topological sort

--- hydra-haskell/src/main/haskell/Hydra/Tools/Sorting.hs
+++ hydra-haskell/src/main/haskell/Hydra/Tools/Sorting.hs
@@ -5,16 +5,16 @@ module Hydra.Tools.Sorting where
 import qualified Data.List as L
 import qualified Data.Bifunctor as BF
 
+import qualified Data.Graph as G
+
 
 -- | Sort a directed acyclic graph (DAG) based on an adjacency list
 --   Note: assumes that the input is in fact a DAG; the ordering is incomplete in the presence of cycles.
-topologicalSort :: Eq a => [(a, [a])] -> Maybe [a]
-topologicalSort pairs = if L.length result < L.length pairs
-    then Nothing
-    else Just result
+topologicalSort :: Ord a => [(a, [a])] -> Maybe [a]
+topologicalSort pairs = Just (getKey <$> G.topSort g)
   where
-    result = foldl makePrecede [] pairs
-    makePrecede ts (x, xs) = L.nub $
-      case L.elemIndex x ts of
-        Just i -> uncurry (++) $ BF.first (++ xs) $ splitAt i ts
-        _ -> ts ++ xs ++ [x]
+    (g, nodeFromVertex, _) = G.graphFromEdges (toEdge <$> pairs)
+    toEdge (key, keys) = ((), key, keys)
+    getKey v = n
+      where
+        (_, n, _) = nodeFromVertex v
