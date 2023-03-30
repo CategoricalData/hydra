22f4d3a75fc3a85ebb20fede835db10a0a62643b
Joshua Shinavier <josh@fortytwo.net>
2023-03-29 19:40:33 -0700

    Update Java

--- hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/Schema.java
+++ hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/Schema.java
@@ -3,15 +3,15 @@ package hydra.langs.tinkerpop.mappings;
 public class Schema<S, A, T, V, E, P> {
   public static final hydra.core.Name NAME = new hydra.core.Name("hydra/langs/tinkerpop/mappings.Schema");
   
-  public final hydra.compute.Coder<S, S, V, hydra.core.Term<A>> vertexIds;
+  public final hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds;
   
-  public final hydra.compute.Coder<S, S, E, hydra.core.Term<A>> edgeIds;
+  public final hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds;
   
-  public final hydra.compute.Coder<S, S, T, hydra.core.Type<A>> propertyTypes;
+  public final hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes;
   
-  public final hydra.compute.Coder<S, S, P, hydra.core.Term<A>> propertyValues;
+  public final hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues;
   
-  public Schema (hydra.compute.Coder<S, S, V, hydra.core.Term<A>> vertexIds, hydra.compute.Coder<S, S, E, hydra.core.Term<A>> edgeIds, hydra.compute.Coder<S, S, T, hydra.core.Type<A>> propertyTypes, hydra.compute.Coder<S, S, P, hydra.core.Term<A>> propertyValues) {
+  public Schema (hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds, hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds, hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes, hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues) {
     this.vertexIds = vertexIds;
     this.edgeIds = edgeIds;
     this.propertyTypes = propertyTypes;
@@ -32,19 +32,19 @@ public class Schema<S, A, T, V, E, P> {
     return 2 * vertexIds.hashCode() + 3 * edgeIds.hashCode() + 5 * propertyTypes.hashCode() + 7 * propertyValues.hashCode();
   }
   
-  public Schema withVertexIds(hydra.compute.Coder<S, S, V, hydra.core.Term<A>> vertexIds) {
+  public Schema withVertexIds(hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds) {
     return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues);
   }
   
-  public Schema withEdgeIds(hydra.compute.Coder<S, S, E, hydra.core.Term<A>> edgeIds) {
+  public Schema withEdgeIds(hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds) {
     return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues);
   }
   
-  public Schema withPropertyTypes(hydra.compute.Coder<S, S, T, hydra.core.Type<A>> propertyTypes) {
+  public Schema withPropertyTypes(hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes) {
     return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues);
   }
   
-  public Schema withPropertyValues(hydra.compute.Coder<S, S, P, hydra.core.Term<A>> propertyValues) {
+  public Schema withPropertyValues(hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues) {
     return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues);
   }
 }
