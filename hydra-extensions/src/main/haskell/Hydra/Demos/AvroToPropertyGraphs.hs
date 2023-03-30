47cc1937eff1b79efa2ed18c0940328c5838452f
Joshua Shinavier <josh@fortytwo.net>
2023-03-30 13:36:50 -0700

    Add an Avro-to-PG demo (WIP)

---
+++ hydra-extensions/src/main/haskell/Hydra/Demos/AvroToPropertyGraphs.hs
@@ -0,0 +1,23 @@
+module Hydra.Demos.AvroToPropertyGraphs where
+
+import Hydra.Kernel
+import Hydra.Tools.AvroWorkflows
+
+import qualified System.FilePath as FP
+import System.Directory
+
+
+{-
+import Hydra.Tools.AvroWorkflows
+import Hydra.Demos.AvroToPropertyGraphs
+
+transformAirplaneInfo propertyGraphLastMile
+-}
+
+transformAirplaneInfo :: LastMile a -> IO ()
+transformAirplaneInfo lastMile = do
+  pwd <- getCurrentDirectory
+  executeAvroTransformWorkflow lastMile $ TransformWorkflow "airplane info"
+    (SchemaSpecFile $ FP.combine pwd "src/test/avro/AirplaneInfo.avsc")
+    (FP.combine pwd "src/test/json")
+    "/tmp/avro-pg-demo/output"
