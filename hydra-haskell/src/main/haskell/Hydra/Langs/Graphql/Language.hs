e33030a044d4198fb12d39e022668104a909dc2f
Joshua Shinavier <josh@fortytwo.net>
2023-04-04 11:29:43 -0700

    Adjust the GraphQL language constraints for the four built-in scalar types. Exclude all union types other than enums.

--- hydra-haskell/src/main/haskell/Hydra/Langs/Graphql/Language.hs
+++ hydra-haskell/src/main/haskell/Hydra/Langs/Graphql/Language.hs
@@ -2,6 +2,7 @@ module Hydra.Langs.Graphql.Language where
 
 import Hydra.Kernel
 
+import qualified Data.List as L
 import qualified Data.Set as S
 
 
@@ -10,21 +11,21 @@ graphqlLanguage = Language (LanguageName "hydra/langs/graphql") $ LanguageConstr
   -- Note: this language is for schemas and data only; support for queries may be added later
   languageConstraintsEliminationVariants = S.empty,
   languageConstraintsLiteralVariants = S.fromList [
-    LiteralVariantBoolean,
-    LiteralVariantFloat,
-    LiteralVariantInteger,
-    LiteralVariantString],
+    LiteralVariantBoolean, -- Boolean
+    LiteralVariantFloat,   -- Float
+    LiteralVariantInteger, -- Int
+    LiteralVariantString], -- String
   languageConstraintsFloatTypes = S.fromList [
-    FloatTypeBigfloat],
+    FloatTypeFloat64], -- Float
   languageConstraintsFunctionVariants = S.empty,
   languageConstraintsIntegerTypes = S.fromList [
-    IntegerTypeBigint],
+    IntegerTypeInt32], -- Int
   languageConstraintsTermVariants = S.fromList [
     TermVariantList,
     TermVariantLiteral,
     TermVariantOptional,
     TermVariantRecord,
-    TermVariantUnion],
+    TermVariantUnion], -- Unions are supported only in the form of enums
   languageConstraintsTypeVariants = S.fromList [
     TypeVariantApplication,
     TypeVariantLambda,
@@ -33,6 +34,14 @@ graphqlLanguage = Language (LanguageName "hydra/langs/graphql") $ LanguageConstr
     TypeVariantWrap,
     TypeVariantOptional,
     TypeVariantRecord,
-    TypeVariantUnion,
+    TypeVariantUnion, -- Unions are supported only in the form of enums
     TypeVariantVariable],
-  languageConstraintsTypes = const True }
+  languageConstraintsTypes = \typ -> case stripType typ of
+    -- If it is a union type, make sure it can be treated as an enum.
+    TypeUnion rt -> L.foldl (\b f -> b && isEnumField f) True $ rowTypeFields rt
+      where
+        isUnitType t = case stripType t of
+          TypeRecord (RowType name _ fields) -> L.null fields || name == _UnitType
+          _ -> False
+        isEnumField = isUnitType . fieldTypeType
+    _ -> True}
