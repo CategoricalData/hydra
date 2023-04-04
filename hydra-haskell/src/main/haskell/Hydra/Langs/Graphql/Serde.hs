f0bafe8f751dbe216f7a3a8c38c13547af50b48b
Joshua Shinavier <josh@fortytwo.net>
2023-04-04 10:16:54 -0700

    Add the scaffolding for a GraphQL coder

---
+++ hydra-haskell/src/main/haskell/Hydra/Langs/Graphql/Serde.hs
@@ -0,0 +1,13 @@
+module Hydra.Langs.Graphql.Serde where
+
+import Hydra.Tools.Script
+import Hydra.Tools.Formatting
+import qualified Hydra.Ast as CT
+import qualified Hydra.Langs.Graphql.Syntax as G
+
+import qualified Data.List as L
+import qualified Data.Maybe as Y
+
+
+exprDocument :: G.Document -> CT.Expr
+exprDocument d = cst "TODO"
