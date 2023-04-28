a9bad19c3341451aa77b9e9f1d59eb130ecca469
Joshua Shinavier <josh@fortytwo.net>
2023-04-28 11:04:25 -0700

    Update generated Java, manually fixing some issues

--- hydra-java/src/gen-main/java/hydra/workflow/LastMile.java
+++ hydra-java/src/gen-main/java/hydra/workflow/LastMile.java
@@ -3,25 +3,25 @@ package hydra.workflow;
 /**
  * The last mile of a transformation, which encodes and serializes terms to a file
  */
-public class LastMile<A> {
+public class LastMile<S, A> {
   public static final hydra.core.Name NAME = new hydra.core.Name("hydra/workflow.LastMile");
   
   /**
    * An encoder for terms to a list of output objects
    */
-  public final java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.List<A>>>>>> encoder;
+  public final java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.List<A>>>>>> encoder;
   
   /**
    * A function which serializes a list of output objects to a string representation
    */
-  public final java.util.function.Function<java.util.List<A>, String> serializer;
+  public final java.util.function.Function<java.util.List<A>, hydra.compute.Flow<S, String>> serializer;
   
   /**
    * A file extension for the generated file(s)
    */
   public final String fileExtension;
   
-  public LastMile (java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.List<A>>>>>> encoder, java.util.function.Function<java.util.List<A>, String> serializer, String fileExtension) {
+  public LastMile (java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.List<A>>>>>> encoder, java.util.function.Function<java.util.List<A>, hydra.compute.Flow<S, String>> serializer, String fileExtension) {
     this.encoder = encoder;
     this.serializer = serializer;
     this.fileExtension = fileExtension;
@@ -41,11 +41,11 @@ public class LastMile<A> {
     return 2 * encoder.hashCode() + 3 * serializer.hashCode() + 5 * fileExtension.hashCode();
   }
   
-  public LastMile withEncoder(java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<hydra.graph.Graph<hydra.compute.Kv>, java.util.List<A>>>>>> encoder) {
+  public LastMile withEncoder(java.util.function.Function<hydra.core.Type<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.function.Function<hydra.core.Term<hydra.compute.Kv>, java.util.function.Function<hydra.graph.Graph<hydra.compute.Kv>, hydra.compute.Flow<S, java.util.List<A>>>>>> encoder) {
     return new LastMile(encoder, serializer, fileExtension);
   }
   
-  public LastMile withSerializer(java.util.function.Function<java.util.List<A>, String> serializer) {
+  public LastMile withSerializer(java.util.function.Function<java.util.List<A>, hydra.compute.Flow<S, String>> serializer) {
     return new LastMile(encoder, serializer, fileExtension);
   }
   
