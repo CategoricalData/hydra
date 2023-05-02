17af4b45743a94ab95205fdafb324a4dde58418f
Joshua Shinavier <josh@fortytwo.net>
2023-05-02 15:45:57 -0700

    Add a topological sort function for graphs with cycles (which sorts strongly connected components, currently using Data.Graph)

--- hydra-haskell/src/main/haskell/Hydra/Tools/Sorting.hs
+++ hydra-haskell/src/main/haskell/Hydra/Tools/Sorting.hs
@@ -1,6 +1,9 @@
 -- | Utilities for sorting
 
-module Hydra.Tools.Sorting where
+module Hydra.Tools.Sorting (
+  topologicalSort,
+  topologicalSortComponents,
+) where
 
 import qualified Data.List as L
 import qualified Data.Bifunctor as BF
@@ -11,10 +14,26 @@ import qualified Data.Graph as G
 -- | Sort a directed acyclic graph (DAG) based on an adjacency list
 --   Note: assumes that the input is in fact a DAG; the ordering is incomplete in the presence of cycles.
 topologicalSort :: Ord a => [(a, [a])] -> Maybe [a]
-topologicalSort pairs = Just (getKey <$> G.topSort g)
+topologicalSort pairs = Just (getKey nodeFromVertex <$> G.topSort g)
+  where
+    (g, nodeFromVertex) = initGraph pairs
+
+-- | Find the strongly connected components of a graph, in (reverse) topological order
+topologicalSortComponents :: Ord a => [(a, [a])] -> [[a]]
+topologicalSortComponents pairs = (fmap (getKey nodeFromVertex) . treeToList) <$> G.scc g
+  where
+    (g, nodeFromVertex) = initGraph pairs
+
+treeToList :: G.Tree a -> [a]
+treeToList (G.Node root subforest) = root:(L.concat (treeToList <$> subforest))
+
+getKey :: (G.Vertex -> ((), a, [a])) -> G.Vertex -> a
+getKey nodeFromVertex v = n
+  where
+    (_, n, _) = nodeFromVertex v
+
+initGraph :: Ord a => [(a, [a])] -> (G.Graph, G.Vertex -> ((), a, [a]))
+initGraph pairs = (g, nodeFromVertex)
   where
     (g, nodeFromVertex, _) = G.graphFromEdges (toEdge <$> pairs)
     toEdge (key, keys) = ((), key, keys)
-    getKey v = n
-      where
-        (_, n, _) = nodeFromVertex v
