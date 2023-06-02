7d644ea087fd9a61d78081b188074e7be6e9934a
Joshua Shinavier <josh@fortytwo.net>
2023-06-02 15:51:06 -0700

    Update generated Java

--- hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/AnnotationSchema.java
+++ hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/AnnotationSchema.java
@@ -14,9 +14,9 @@ public class AnnotationSchema {
   
   public final String edgeId;
   
-  public final String key;
+  public final String propertyKey;
   
-  public final String value;
+  public final String propertyValue;
   
   public final String outVertex;
   
@@ -36,13 +36,13 @@ public class AnnotationSchema {
   
   public final String ignore;
   
-  public AnnotationSchema (String vertexLabel, String edgeLabel, String vertexId, String edgeId, String key, String value, String outVertex, String outVertexLabel, String inVertex, String inVertexLabel, String outEdge, String outEdgeLabel, String inEdge, String inEdgeLabel, String ignore) {
+  public AnnotationSchema (String vertexLabel, String edgeLabel, String vertexId, String edgeId, String propertyKey, String propertyValue, String outVertex, String outVertexLabel, String inVertex, String inVertexLabel, String outEdge, String outEdgeLabel, String inEdge, String inEdgeLabel, String ignore) {
     this.vertexLabel = vertexLabel;
     this.edgeLabel = edgeLabel;
     this.vertexId = vertexId;
     this.edgeId = edgeId;
-    this.key = key;
-    this.value = value;
+    this.propertyKey = propertyKey;
+    this.propertyValue = propertyValue;
     this.outVertex = outVertex;
     this.outVertexLabel = outVertexLabel;
     this.inVertex = inVertex;
@@ -60,71 +60,71 @@ public class AnnotationSchema {
       return false;
     }
     AnnotationSchema o = (AnnotationSchema) (other);
-    return vertexLabel.equals(o.vertexLabel) && edgeLabel.equals(o.edgeLabel) && vertexId.equals(o.vertexId) && edgeId.equals(o.edgeId) && key.equals(o.key) && value.equals(o.value) && outVertex.equals(o.outVertex) && outVertexLabel.equals(o.outVertexLabel) && inVertex.equals(o.inVertex) && inVertexLabel.equals(o.inVertexLabel) && outEdge.equals(o.outEdge) && outEdgeLabel.equals(o.outEdgeLabel) && inEdge.equals(o.inEdge) && inEdgeLabel.equals(o.inEdgeLabel) && ignore.equals(o.ignore);
+    return vertexLabel.equals(o.vertexLabel) && edgeLabel.equals(o.edgeLabel) && vertexId.equals(o.vertexId) && edgeId.equals(o.edgeId) && propertyKey.equals(o.propertyKey) && propertyValue.equals(o.propertyValue) && outVertex.equals(o.outVertex) && outVertexLabel.equals(o.outVertexLabel) && inVertex.equals(o.inVertex) && inVertexLabel.equals(o.inVertexLabel) && outEdge.equals(o.outEdge) && outEdgeLabel.equals(o.outEdgeLabel) && inEdge.equals(o.inEdge) && inEdgeLabel.equals(o.inEdgeLabel) && ignore.equals(o.ignore);
   }
   
   @Override
   public int hashCode() {
-    return 2 * vertexLabel.hashCode() + 3 * edgeLabel.hashCode() + 5 * vertexId.hashCode() + 7 * edgeId.hashCode() + 11 * key.hashCode() + 13 * value.hashCode() + 17 * outVertex.hashCode() + 19 * outVertexLabel.hashCode() + 23 * inVertex.hashCode() + 29 * inVertexLabel.hashCode() + 31 * outEdge.hashCode() + 37 * outEdgeLabel.hashCode() + 41 * inEdge.hashCode() + 43 * inEdgeLabel.hashCode() + 47 * ignore.hashCode();
+    return 2 * vertexLabel.hashCode() + 3 * edgeLabel.hashCode() + 5 * vertexId.hashCode() + 7 * edgeId.hashCode() + 11 * propertyKey.hashCode() + 13 * propertyValue.hashCode() + 17 * outVertex.hashCode() + 19 * outVertexLabel.hashCode() + 23 * inVertex.hashCode() + 29 * inVertexLabel.hashCode() + 31 * outEdge.hashCode() + 37 * outEdgeLabel.hashCode() + 41 * inEdge.hashCode() + 43 * inEdgeLabel.hashCode() + 47 * ignore.hashCode();
   }
   
   public AnnotationSchema withVertexLabel(String vertexLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withEdgeLabel(String edgeLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withVertexId(String vertexId) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withEdgeId(String edgeId) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
-  public AnnotationSchema withKey(String key) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+  public AnnotationSchema withPropertyKey(String propertyKey) {
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
-  public AnnotationSchema withValue(String value) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+  public AnnotationSchema withPropertyValue(String propertyValue) {
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withOutVertex(String outVertex) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withOutVertexLabel(String outVertexLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withInVertex(String inVertex) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withInVertexLabel(String inVertexLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withOutEdge(String outEdge) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withOutEdgeLabel(String outEdgeLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withInEdge(String inEdge) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withInEdgeLabel(String inEdgeLabel) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
   
   public AnnotationSchema withIgnore(String ignore) {
-    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, key, value, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
+    return new AnnotationSchema(vertexLabel, edgeLabel, vertexId, edgeId, propertyKey, propertyValue, outVertex, outVertexLabel, inVertex, inVertexLabel, outEdge, outEdgeLabel, inEdge, inEdgeLabel, ignore);
   }
 }
--- hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/Schema.java
+++ hydra-java/src/gen-main/java/hydra/langs/tinkerpop/mappings/Schema.java
@@ -16,12 +16,18 @@ public class Schema<S, A, T, V, E, P> {
   
   public final hydra.langs.tinkerpop.mappings.AnnotationSchema annotations;
   
-  public Schema (hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds, hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds, hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes, hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues, hydra.langs.tinkerpop.mappings.AnnotationSchema annotations) {
+  public final V defaultVertexId;
+  
+  public final E defaultEdgeId;
+  
+  public Schema (hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds, hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds, hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes, hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues, hydra.langs.tinkerpop.mappings.AnnotationSchema annotations, V defaultVertexId, E defaultEdgeId) {
     this.vertexIds = vertexIds;
     this.edgeIds = edgeIds;
     this.propertyTypes = propertyTypes;
     this.propertyValues = propertyValues;
     this.annotations = annotations;
+    this.defaultVertexId = defaultVertexId;
+    this.defaultEdgeId = defaultEdgeId;
   }
   
   @Override
@@ -30,31 +36,39 @@ public class Schema<S, A, T, V, E, P> {
       return false;
     }
     Schema o = (Schema) (other);
-    return vertexIds.equals(o.vertexIds) && edgeIds.equals(o.edgeIds) && propertyTypes.equals(o.propertyTypes) && propertyValues.equals(o.propertyValues) && annotations.equals(o.annotations);
+    return vertexIds.equals(o.vertexIds) && edgeIds.equals(o.edgeIds) && propertyTypes.equals(o.propertyTypes) && propertyValues.equals(o.propertyValues) && annotations.equals(o.annotations) && defaultVertexId.equals(o.defaultVertexId) && defaultEdgeId.equals(o.defaultEdgeId);
   }
   
   @Override
   public int hashCode() {
-    return 2 * vertexIds.hashCode() + 3 * edgeIds.hashCode() + 5 * propertyTypes.hashCode() + 7 * propertyValues.hashCode() + 11 * annotations.hashCode();
+    return 2 * vertexIds.hashCode() + 3 * edgeIds.hashCode() + 5 * propertyTypes.hashCode() + 7 * propertyValues.hashCode() + 11 * annotations.hashCode() + 13 * defaultVertexId.hashCode() + 17 * defaultEdgeId.hashCode();
   }
   
   public Schema withVertexIds(hydra.compute.Coder<S, S, hydra.core.Term<A>, V> vertexIds) {
-    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations);
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
   }
   
   public Schema withEdgeIds(hydra.compute.Coder<S, S, hydra.core.Term<A>, E> edgeIds) {
-    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations);
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
   }
   
   public Schema withPropertyTypes(hydra.compute.Coder<S, S, hydra.core.Type<A>, T> propertyTypes) {
-    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations);
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
   }
   
   public Schema withPropertyValues(hydra.compute.Coder<S, S, hydra.core.Term<A>, P> propertyValues) {
-    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations);
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
   }
   
   public Schema withAnnotations(hydra.langs.tinkerpop.mappings.AnnotationSchema annotations) {
-    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations);
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
+  }
+  
+  public Schema withDefaultVertexId(V defaultVertexId) {
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
+  }
+  
+  public Schema withDefaultEdgeId(E defaultEdgeId) {
+    return new Schema(vertexIds, edgeIds, propertyTypes, propertyValues, annotations, defaultVertexId, defaultEdgeId);
   }
 }
