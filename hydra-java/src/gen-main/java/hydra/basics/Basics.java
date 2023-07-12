8dabac280684dac397bf8b9bc29ebab9ee3e6aac
Joshua Shinavier <josh@fortytwo.net>
2023-07-12 16:14:43 -0700

    Complete the case statements for literal equality, and generate Java

--- hydra-java/src/gen-main/java/hydra/basics/Basics.java
+++ hydra-java/src/gen-main/java/hydra/basics/Basics.java
@@ -541,6 +541,303 @@ public interface Basics {
     new hydra.mantle.TypeVariant.Union(),
     new hydra.mantle.TypeVariant.Variable());
   
+  static java.util.function.Function<hydra.core.FloatValue, Boolean> floatEqual(hydra.core.FloatValue v1) {
+    return ((v1)).accept(new hydra.core.FloatValue.Visitor<>() {
+      @Override
+      public java.util.function.Function<hydra.core.FloatValue, Boolean> visit(hydra.core.FloatValue.Bigfloat instance) {
+        return (java.util.function.Function<hydra.core.FloatValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.FloatValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.FloatValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.FloatValue.Bigfloat instance) {
+            return hydra.lib.literals.EqualBigfloat.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.FloatValue, Boolean> visit(hydra.core.FloatValue.Float32 instance) {
+        return (java.util.function.Function<hydra.core.FloatValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.FloatValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.FloatValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.FloatValue.Float32 instance) {
+            return hydra.lib.literals.EqualFloat32.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.FloatValue, Boolean> visit(hydra.core.FloatValue.Float64 instance) {
+        return (java.util.function.Function<hydra.core.FloatValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.FloatValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.FloatValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.FloatValue.Float64 instance) {
+            return hydra.lib.literals.EqualFloat64.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+    });
+  }
+  
+  static java.util.function.Function<hydra.core.IntegerValue, Boolean> integerEqual(hydra.core.IntegerValue v1) {
+    return ((v1)).accept(new hydra.core.IntegerValue.Visitor<>() {
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Bigint instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Bigint instance) {
+            return hydra.lib.literals.EqualBigint.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Int8 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Int8 instance) {
+            return hydra.lib.literals.EqualInt8.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Int16 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Int16 instance) {
+            return hydra.lib.literals.EqualInt16.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Int32 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Int32 instance) {
+            return hydra.lib.literals.EqualInt32.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Int64 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Int64 instance) {
+            return hydra.lib.literals.EqualInt64.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Uint8 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Uint8 instance) {
+            return hydra.lib.literals.EqualUint8.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Uint16 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Uint16 instance) {
+            return hydra.lib.literals.EqualUint16.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Uint32 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Uint32 instance) {
+            return hydra.lib.literals.EqualUint32.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.IntegerValue, Boolean> visit(hydra.core.IntegerValue.Uint64 instance) {
+        return (java.util.function.Function<hydra.core.IntegerValue, Boolean>) (v1 -> ((v1)).accept(new hydra.core.IntegerValue.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.IntegerValue instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.IntegerValue.Uint64 instance) {
+            return hydra.lib.literals.EqualUint64.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+    });
+  }
+  
+  static java.util.function.Function<hydra.core.Literal, Boolean> literalEqual(hydra.core.Literal v1) {
+    return ((v1)).accept(new hydra.core.Literal.Visitor<>() {
+      @Override
+      public java.util.function.Function<hydra.core.Literal, Boolean> visit(hydra.core.Literal.Binary instance) {
+        return (java.util.function.Function<hydra.core.Literal, Boolean>) (v1 -> ((v1)).accept(new hydra.core.Literal.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.Literal instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.Literal.Binary instance) {
+            return hydra.lib.literals.EqualBinary.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Literal, Boolean> visit(hydra.core.Literal.Boolean_ instance) {
+        return (java.util.function.Function<hydra.core.Literal, Boolean>) (v1 -> ((v1)).accept(new hydra.core.Literal.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.Literal instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.Literal.Boolean_ instance) {
+            return hydra.lib.literals.EqualBoolean.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Literal, Boolean> visit(hydra.core.Literal.Float_ instance) {
+        return (java.util.function.Function<hydra.core.Literal, Boolean>) (v1 -> ((v1)).accept(new hydra.core.Literal.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.Literal instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.Literal.Float_ instance) {
+            return (hydra.basics.Basics.floatEqual((instance.value))).apply((instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Literal, Boolean> visit(hydra.core.Literal.Integer_ instance) {
+        return (java.util.function.Function<hydra.core.Literal, Boolean>) (v1 -> ((v1)).accept(new hydra.core.Literal.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.Literal instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.Literal.Integer_ instance) {
+            return (hydra.basics.Basics.integerEqual((instance.value))).apply((instance.value));
+          }
+        }));
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Literal, Boolean> visit(hydra.core.Literal.String_ instance) {
+        return (java.util.function.Function<hydra.core.Literal, Boolean>) (v1 -> ((v1)).accept(new hydra.core.Literal.PartialVisitor<>() {
+          @Override
+          public Boolean otherwise(hydra.core.Literal instance) {
+            return false;
+          }
+          
+          @Override
+          public Boolean visit(hydra.core.Literal.String_ instance) {
+            return hydra.lib.literals.EqualString.apply(
+              (instance.value),
+              (instance.value));
+          }
+        }));
+      }
+    });
+  }
+  
   static <A, X> java.util.function.Function<X, X> skipAnnotations(java.util.function.Function<X, java.util.Optional<hydra.core.Annotated<X, A>>> getAnn) {
     java.util.concurrent.atomic.AtomicReference<java.util.function.Function<X, X>> skip = new java.util.concurrent.atomic.AtomicReference<>();
     skip.set((java.util.function.Function<X, X>) (t1 -> ((((getAnn)).apply((t1))).map((java.util.function.Function<hydra.core.Annotated<X, A>, X>) (ann -> (skip.get()).apply(((ann)).subject)))).orElse((t1))));
@@ -573,6 +870,90 @@ public interface Basics {
         return java.util.Optional.of((instance.value));
       }
     })))).apply((x));
+  }
+  
+  static <A> java.util.function.Function<hydra.core.Term<A>, Boolean> termEqual(hydra.core.Term<A> v1) {
+    return ((v1)).accept(new hydra.core.Term.Visitor<>() {
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Annotated<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Application<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Function<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Let<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.List<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Literal<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Map<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Optional<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Product<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Record<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Set<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Stream<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Sum<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Union<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Variable<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+      
+      @Override
+      public java.util.function.Function<hydra.core.Term<A>, Boolean> visit(hydra.core.Term.Wrap<A> instance) {
+        return (java.util.function.Function<hydra.core.Term<A>, Boolean>) (ignored -> false);
+      }
+    });
   }
   
   static hydra.core.Name unqualifyName(hydra.module.QualifiedName qname) {
