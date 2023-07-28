a6c0425f3727424c922f71986212393d824d73ba
Joshua Shinavier <josh@fortytwo.net>
2023-07-28 16:03:53 -0700

    Move literal equality primitives into hydra/lib/equality

--- hydra-haskell/src/main/haskell/Hydra/Lib/Literals.hs
+++ hydra-haskell/src/main/haskell/Hydra/Lib/Literals.hs
@@ -2,8 +2,6 @@
 
 module Hydra.Lib.Literals where
 
-import Data.Int
-
 
 -- TODO: expose as a primitive
 bigfloatToBigint :: Double -> Integer
@@ -12,51 +10,6 @@ bigfloatToBigint = round
 -- TODO: expose as a primitive
 bigintToBigfloat :: Integer -> Double
 bigintToBigfloat = fromIntegral
-
-equalBinary :: String -> String -> Bool
-equalBinary = (==)
-
-equalBoolean :: Bool -> Bool -> Bool
-equalBoolean = (==)
-
-equalBigfloat :: Double -> Double -> Bool
-equalBigfloat = (==)
-
-equalFloat32 :: Float -> Float -> Bool
-equalFloat32 = (==)
-
-equalFloat64 :: Double -> Double -> Bool
-equalFloat64 = (==)
-
-equalBigint :: Integer -> Integer -> Bool
-equalBigint = (==)
-
-equalInt8 :: Int8 -> Int8 -> Bool
-equalInt8 = (==)
-
-equalInt16 :: Int16 -> Int16 -> Bool
-equalInt16 = (==)
-
-equalInt32 :: Int -> Int -> Bool
-equalInt32 = (==)
-
-equalInt64 :: Int64 -> Int64 -> Bool
-equalInt64 = (==)
-
-equalUint8 :: Int16 -> Int16 -> Bool
-equalUint8 = (==)
-
-equalUint16 :: Int -> Int -> Bool
-equalUint16 = (==)
-
-equalUint32 :: Int64 -> Int64 -> Bool
-equalUint32 = (==)
-
-equalUint64 :: Integer -> Integer -> Bool
-equalUint64 = (==)
-
-equalString :: String -> String -> Bool
-equalString = (==)
 
 showInt32 :: Int -> String
 showInt32 = show
