d268f04224f8157a5929737817ab40d8fdc8f5b4
Joshua Shinavier <josh@fortytwo.net>
2023-08-09 11:59:59 -0700

    Propagate all tier-0 modules into Java

---
+++ hydra-java/src/gen-main/java/hydra/constants/Constants.java
@@ -0,0 +1,12 @@
+package hydra.constants;
+
+/**
+ * A module for tier-0 constants.
+ */
+public interface Constants {
+  String ignoredVariable = "_";
+  
+  hydra.core.Name placeholderName = new hydra.core.Name("Placeholder");
+  
+  Integer maxTraceDepth = 50;
+}
---
+++ hydra-java/src/gen-main/java/hydra/mantle/Either.java
@@ -0,0 +1,88 @@
+package hydra.mantle;
+
+/**
+ * A disjoint union between a 'left' type and a 'right' type
+ */
+public abstract class Either<A, B> {
+  public static final hydra.core.Name NAME = new hydra.core.Name("hydra/mantle.Either");
+  
+  private Either () {
+  
+  }
+  
+  public abstract <R> R accept(Visitor<A, B, R> visitor) ;
+  
+  public interface Visitor<A, B, R> {
+    R visit(Left<A, B> instance) ;
+    
+    R visit(Right<A, B> instance) ;
+  }
+  
+  public interface PartialVisitor<A, B, R> extends Visitor<A, B, R> {
+    default R otherwise(Either<A, B> instance) {
+      throw new IllegalStateException("Non-exhaustive patterns when matching: " + (instance));
+    }
+    
+    default R visit(Left<A, B> instance) {
+      return otherwise((instance));
+    }
+    
+    default R visit(Right<A, B> instance) {
+      return otherwise((instance));
+    }
+  }
+  
+  public static final class Left<A, B> extends hydra.mantle.Either<A, B> {
+    public final A value;
+    
+    public Left (A value) {
+      this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      if (!(other instanceof Left)) {
+        return false;
+      }
+      Left o = (Left) (other);
+      return value.equals(o.value);
+    }
+    
+    @Override
+    public int hashCode() {
+      return 2 * value.hashCode();
+    }
+    
+    @Override
+    public <R> R accept(Visitor<A, B, R> visitor) {
+      return visitor.visit(this);
+    }
+  }
+  
+  public static final class Right<A, B> extends hydra.mantle.Either<A, B> {
+    public final B value;
+    
+    public Right (B value) {
+      this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+      if (!(other instanceof Right)) {
+        return false;
+      }
+      Right o = (Right) (other);
+      return value.equals(o.value);
+    }
+    
+    @Override
+    public int hashCode() {
+      return 2 * value.hashCode();
+    }
+    
+    @Override
+    public <R> R accept(Visitor<A, B, R> visitor) {
+      return visitor.visit(this);
+    }
+  }
+}
