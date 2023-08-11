04d7252eac0e8ab22fd8aa60d1e90ab64642fefa
Joshua Shinavier <josh@fortytwo.net>
2023-08-10 20:24:45 -0700

    Update generated Java for the TinkerPop PG and mappings models

--- hydra-java/src/gen-main/java/hydra/langs/tinkerpop/propertyGraph/VertexType.java
+++ hydra-java/src/gen-main/java/hydra/langs/tinkerpop/propertyGraph/VertexType.java
@@ -3,15 +3,18 @@ package hydra.langs.tinkerpop.propertyGraph;
 /**
  * The type of a vertex
  */
-public class VertexType<T> {
+public class VertexType<V, P> {
   public static final hydra.core.Name NAME = new hydra.core.Name("hydra/langs/tinkerpop/propertyGraph.VertexType");
   
   public final hydra.langs.tinkerpop.propertyGraph.VertexLabel label;
   
-  public final java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, T> properties;
+  public final V id;
   
-  public VertexType (hydra.langs.tinkerpop.propertyGraph.VertexLabel label, java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, T> properties) {
+  public final java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, P> properties;
+  
+  public VertexType (hydra.langs.tinkerpop.propertyGraph.VertexLabel label, V id, java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, P> properties) {
     this.label = label;
+    this.id = id;
     this.properties = properties;
   }
   
@@ -21,19 +24,23 @@ public class VertexType<T> {
       return false;
     }
     VertexType o = (VertexType) (other);
-    return label.equals(o.label) && properties.equals(o.properties);
+    return label.equals(o.label) && id.equals(o.id) && properties.equals(o.properties);
   }
   
   @Override
   public int hashCode() {
-    return 2 * label.hashCode() + 3 * properties.hashCode();
+    return 2 * label.hashCode() + 3 * id.hashCode() + 5 * properties.hashCode();
   }
   
   public VertexType withLabel(hydra.langs.tinkerpop.propertyGraph.VertexLabel label) {
-    return new VertexType(label, properties);
+    return new VertexType(label, id, properties);
   }
   
-  public VertexType withProperties(java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, T> properties) {
-    return new VertexType(label, properties);
+  public VertexType withId(V id) {
+    return new VertexType(label, id, properties);
+  }
+  
+  public VertexType withProperties(java.util.Map<hydra.langs.tinkerpop.propertyGraph.PropertyKey, P> properties) {
+    return new VertexType(label, id, properties);
   }
 }
