6ab8e3e5831b113a0084cd946f228eec03073de8
Joshua Shinavier <josh@fortytwo.net>
2023-08-16 15:29:11 -0700

    Add generated hydra/tier3 module in Java

---
+++ hydra-java/src/gen-main/java/hydra/tier3/Tier3.java
@@ -0,0 +1,30 @@
+package hydra.tier3;
+
+/**
+ * A module for miscellaneous tier-3 functions and constants.
+ */
+public interface Tier3 {
+  static String traceSummary(hydra.compute.Trace t) {
+    java.util.function.Function<hydra.core.Tuple.Tuple2<String, hydra.core.Term<hydra.compute.Kv>>, String> toLine = (java.util.function.Function<hydra.core.Tuple.Tuple2<String, hydra.core.Term<hydra.compute.Kv>>, String>) (pair -> hydra.lib.strings.Cat.apply(java.util.Arrays.asList(
+      hydra.lib.strings.Cat.apply(java.util.Arrays.asList(
+        hydra.lib.strings.Cat.apply(java.util.Arrays.asList(
+          "\t",
+          ((pair)).object1)),
+        ": ")),
+      hydra.lib.io.ShowTerm.apply(((pair)).object2))));
+    java.util.List<String> messageLines = hydra.lib.lists.Nub.apply(((t)).messages);
+    java.util.List<String> keyvalLines = hydra.lib.logic.IfElse.apply(
+      java.util.Arrays.asList(),
+      hydra.lib.lists.Cons.apply(
+        "key/value pairs: ",
+        hydra.lib.lists.Map.apply(
+          (toLine),
+          hydra.lib.maps.ToList.apply(((t)).other))),
+      hydra.lib.maps.IsEmpty.apply(((t)).other));
+    return hydra.lib.strings.Intercalate.apply(
+      "\n",
+      hydra.lib.lists.Concat2.apply(
+        (messageLines),
+        (keyvalLines)));
+  }
+}
