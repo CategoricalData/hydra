2634e99561e82ff998e0e4a6904d8aa8d94f2ddb
Joshua Shinavier <josh@fortytwo.net>
2023-08-15 23:32:29 -0700

    Add hydra/lib/maps.remove primitive in Java, for symmetry with Haskell

--- hydra-java/src/main/java/hydra/lib/maps/Remove.java
+++ hydra-java/src/main/java/hydra/lib/maps/Remove.java
@@ -19,36 +19,31 @@ import static hydra.dsl.Types.function;
 import static hydra.dsl.Types.lambda;
 import static hydra.dsl.Types.map;
 
-public class Insert<A> extends PrimitiveFunction<A> {
+public class Remove<A> extends PrimitiveFunction<A> {
     public Name name() {
-        return new Name("hydra/lib/maps.insert");
+        return new Name("hydra/lib/maps.remove");
     }
 
     @Override
     public Type<A> type() {
-        return lambda("k", "v", function("k", "v", map("k", "v"), map("k", "v")));
+        return lambda("k", "v", function("k", map("k", "v"), map("k", "v")));
     }
 
     @Override
     protected Function<List<Term<A>>, Flow<Graph<A>, Term<A>>> implementation() {
         return args -> {
             Term<A> key = args.get(0);
-            Term<A> value = args.get(1);
-            return Flows.map(Expect.map(Flows::pure, Flows::pure, args.get(2)), before -> Terms.map(apply(key, value, before)));
+            return Flows.map(Expect.map(Flows::pure, Flows::pure, args.get(2)), before -> Terms.map(apply(key, before)));
         };
     }
 
-    public static <K, V> Function<V, Function<Map<K, V>, Map<K, V>>> apply(K k) {
-        return v -> apply(k, v);
+    public static <K, V> Function<Map<K, V>, Map<K, V>> apply(K k) {
+        return before -> apply(k, before);
     }
 
-    public static <K, V> Function<Map<K, V>, Map<K, V>> apply(K k, V v) {
-        return before -> apply(k, v, before);
-    }
-
-    public static <K, V> Map<K, V> apply(K k, V v, Map<K, V> before) {
+    public static <K, V> Map<K, V> apply(K k, Map<K, V> before) {
         Map<K, V> after = new HashMap<>(before);
-        after.put(k, v);
+        after.remove(k);
         return after;
     }
 }
