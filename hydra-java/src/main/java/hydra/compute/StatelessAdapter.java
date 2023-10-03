e7a8cc9a3a08f1d8ea3c62c07b09208600ea0a9a
Joshua Shinavier <josh@fortytwo.net>
2023-10-03 14:44:33 -0700

    Minor convenience functions for avoiding big type signatures in Java 8

--- hydra-java/src/main/java/hydra/compute/StatelessAdapter.java
+++ hydra-java/src/main/java/hydra/compute/StatelessAdapter.java
@@ -4,7 +4,11 @@ package hydra.compute;
  * A convenience class for stateless adapters; we simply use Void as the state type
  */
 public class StatelessAdapter<T1, T2, V1, V2> extends Adapter<Void, Void, T1, T2, V1, V2> {
-    public StatelessAdapter(boolean isLossy, T1 source, T2 target, StatelessCoder<V1, V2> coder) {
+    public StatelessAdapter(boolean isLossy, T1 source, T2 target, Coder<Void, Void, V1, V2> coder) {
         super(isLossy, source, target, coder);
+    }
+
+    public static <T1, T2, V1, V2> StatelessAdapter<T1, T2, V1, V2> of(boolean isLossy, T1 source, T2 target, Coder<Void, Void, V1, V2> coder) {
+        return new StatelessAdapter<T1, T2, V1, V2>(isLossy, source, target, coder);
     }
 }
