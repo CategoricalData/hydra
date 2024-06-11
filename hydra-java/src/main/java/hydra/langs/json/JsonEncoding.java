d8babcea79bbcf4f4034971b78c2075af2b8eeba
joshsh <josh@fortytwo.net>
2024-06-10 17:15:50 -0700

    Add a JSON encoding class in Java

---
+++ hydra-java/src/main/java/hydra/langs/json/JsonEncoding.java
@@ -0,0 +1,90 @@
+package hydra.langs.json;
+
+import hydra.json.Value;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+/**
+ * Encoding functions for Hydra's native JSON values
+ */
+public abstract class JsonEncoding {
+
+  protected static Value toJson(boolean b) {
+    return new Value.Boolean_(b);
+  }
+
+  protected static Value toJson(double d) {
+    return new Value.Number_(d);
+  }
+
+  protected static Value toJson(String value) {
+    return new Value.String_(value);
+  }
+
+  protected static <L> Value toJson(List<L> list, Function<L, Value> mapping) {
+    return new Value.Array(list.stream().map(mapping).collect(Collectors.toList()));
+  }
+
+  protected static <L> Value toJson(Optional<L> opt, Function<L, Value> mapping) {
+    return opt.map(mapping).orElse(null);
+  }
+
+  protected static Value toJson(List<String> strings) {
+    return toJson(strings, JsonEncoding::toJson);
+  }
+
+  protected static Value unit() {
+    return new Value.Object_(Collections.emptyMap());
+  }
+
+  protected static Value unitVariant(String key) {
+    return variant(key, unit());
+  }
+
+  protected static Value variant(String key, Value value) {
+    return new ObjectBuilder().put(key, value).build();
+  }
+
+  public static class ObjectBuilder {
+    private final Map<String, Value> map = new HashMap<>();
+
+    public ObjectBuilder put(String key, String value) {
+      return put(key, new Value.String_(value));
+    }
+
+    public ObjectBuilder put(String key, Value value) {
+      if (value != null) {
+        map.put(key, value);
+      }
+      return this;
+    }
+
+    public <L> ObjectBuilder put(String key, L value, Function<L, Value> mapping) {
+      return put(key, mapping.apply(value));
+    }
+
+    public <L> ObjectBuilder putList(String key, List<L> value, Function<L, Value> mapping) {
+      return put(key, toJson(value, mapping));
+    }
+
+    public <L> ObjectBuilder putOpt(String key, Optional<L> value, Function<L, Value> mapping) {
+      return put(key, toJson(value, mapping));
+    }
+
+    public <L> ObjectBuilder putOptSet(String key, Optional<Set<L>> values, Function<L, Value> mapping) {
+      return put(key, toJson(new ArrayList<>(values.orElse(Collections.emptySet())), mapping));
+    }
+
+    public Value build() {
+      return new Value.Object_(map);
+    }
+  }
+}
