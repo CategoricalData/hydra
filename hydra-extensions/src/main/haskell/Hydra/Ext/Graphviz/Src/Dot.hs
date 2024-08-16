8e1530e38bdf3f6e5db760c9b62f638d06769d37
Joshua Shinavier <josh@fortytwo.net>
2024-08-15 22:16:02 -0700

    Add a Graphviz DOT model. Addresses #136.

---
+++ hydra-extensions/src/main/haskell/Hydra/Ext/Graphviz/Src/Dot.hs
@@ -0,0 +1,105 @@
+module Hydra.Ext.Graphviz.Src.Dot where
+
+-- Standard Tier-0 imports
+import qualified Data.List             as L
+import qualified Data.Map              as M
+import qualified Data.Set              as S
+import qualified Data.Maybe            as Y
+import           Hydra.Dsl.Annotations
+import           Hydra.Dsl.Bootstrap
+import qualified Hydra.Dsl.Terms       as Terms
+import           Hydra.Dsl.Types       as Types
+import           Hydra.Sources.Core
+
+
+dotModule :: Module
+dotModule = Module ns elements [] [hydraCoreModule] $
+    Just ("A model from the Graphviz DOT graph description language."
+      ++ " Based on the grammar at https://graphviz.org/doc/info/lang.html")
+  where
+    ns = Namespace "hydra/ext/graphviz/dot"
+    def = datatype ns
+    dot = typeref ns
+
+    elements = [
+
+      def "Id" string,
+      
+--graph	:	[ strict ] (graph | digraph) [ ID ] '{' stmt_list '}'
+      def "Graph" $
+        record [
+          "strict">: boolean,
+          "directed">: boolean,
+          "id">: optional $ dot "Id",
+          "statements">: list $ dot "Stmt"],
+
+--stmt_list	:	[ stmt [ ';' ] stmt_list ]
+--stmt	:	node_stmt
+--      |	edge_stmt
+--      |	attr_stmt
+--      |	ID '=' ID
+--      |	subgraph
+      def "Stmt" $
+        union [
+          "node">: dot "NodeStmt",
+          "edge">: dot "EdgeStmt",
+          "attr">: dot "AttrStmt",
+          "equals">: dot "EqualityPair",
+          "subgraph">: dot "Subgraph"],
+      def "EqualityPair" $
+        record [
+          "left">: dot "Id",
+          "right">: dot "Id"],
+
+--attr_stmt	:	(graph | node | edge) attr_list
+--attr_list	:	'[' [ a_list ] ']' [ attr_list ]
+--a_list	:	ID '=' ID [ (';' | ',') ] [ a_list ]
+      def "AttrStmt" $
+        record [
+          "type">: dot "AttrType",
+          "attributes">: nonemptyList $ list $ dot "EqualityPair"],
+      def "AttrType" $
+        enum ["graph", "node", "edge"],
+        
+--edge_stmt	:	(node_id | subgraph) edgeRHS [ attr_list ]
+--edgeRHS	:	edgeop (node_id | subgraph) [ edgeRHS ]
+      def "EdgeStmt" $
+        record [
+          "left">: dot "NodeOrSubgraph",
+          "right">: dot "NodeOrSubgraph",
+          "attributes">: list $ dot "EqualityPair"],
+      def "NodeOrSubgraph" $
+        union [
+          "node">: dot "NodeId",
+          "subgraph">: dot "Subgraph"],
+
+--node_stmt	:	node_id [ attr_list ]
+      def "NodeStmt" $
+        record [
+          "id">: dot "NodeId",
+          "attributes">: list $ list $ dot "EqualityPair"],
+
+--node_id	:	ID [ port ]
+      def "NodeId" $
+        record [
+          "id">: dot "Id",
+          "port">: optional $ dot "Port"],
+
+--port	:	':' ID [ ':' compass_pt ]
+--      |	':' compass_pt
+      def "Port" $
+        record [
+          "id">: optional $ dot "Id",
+          "position">: optional $ dot "CompassPt"],
+
+--subgraph	:	[ subgraph [ ID ] ] '{' stmt_list '}'
+       def "Subgraph" $
+         record [
+           "subgraphId">: optional $ dot "SubgraphId",
+           "statements">: list $ dot "Stmt"],
+       def "SubgraphId" $
+         optional $ dot "Id",
+
+--compass_pt	:	(n | ne | e | se | s | sw | w | nw | c | _)
+      def "CompassPt" $
+        enum ["n", "ne", "e", "se", "s", "sw", "w", "nw", "c", "none"]]
