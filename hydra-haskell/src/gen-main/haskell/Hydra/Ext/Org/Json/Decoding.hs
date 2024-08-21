83f54a0b992116b219b2f3370b47272a455eac18
joshsh <josh@fortytwo.net>
2024-08-20 21:02:47 -0700

    Move hydra/ext/json/decoding to hydra/ext/org/json/decoding

--- hydra-haskell/src/gen-main/haskell/Hydra/Ext/Org/Json/Decoding.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Ext/Org/Json/Decoding.hs
@@ -1,6 +1,6 @@
 -- | Decoding functions for JSON data
 
-module Hydra.Ext.Json.Decoding where
+module Hydra.Ext.Org.Json.Decoding where
 
 import qualified Hydra.Compute as Compute
 import qualified Hydra.Json as Json
@@ -14,27 +14,27 @@ import Data.Set as S
 
 decodeString :: (Json.Value -> Compute.Flow s String)
 decodeString x = case x of
-  Json.ValueString v320 -> (Flows.pure v320)
+  Json.ValueString v0 -> (Flows.pure v0)
   _ -> (Flows.fail "expected a string")
 
 decodeNumber :: (Json.Value -> Compute.Flow s Double)
 decodeNumber x = case x of
-  Json.ValueNumber v321 -> (Flows.pure v321)
+  Json.ValueNumber v1 -> (Flows.pure v1)
   _ -> (Flows.fail "expected a number")
 
 decodeBoolean :: (Json.Value -> Compute.Flow s Bool)
 decodeBoolean x = case x of
-  Json.ValueBoolean v322 -> (Flows.pure v322)
+  Json.ValueBoolean v2 -> (Flows.pure v2)
   _ -> (Flows.fail "expected a boolean")
 
 decodeArray :: ((Json.Value -> Compute.Flow s a) -> Json.Value -> Compute.Flow s [a])
 decodeArray decodeElem x = case x of
-  Json.ValueArray v323 -> (Flows.mapList decodeElem v323)
+  Json.ValueArray v3 -> (Flows.mapList decodeElem v3)
   _ -> (Flows.fail "expected an array")
 
 decodeObject :: (Json.Value -> Compute.Flow s (Map String Json.Value))
 decodeObject x = case x of
-  Json.ValueObject v324 -> (Flows.pure v324)
+  Json.ValueObject v4 -> (Flows.pure v4)
   _ -> (Flows.fail "expected an object")
 
 decodeField :: ((Json.Value -> Compute.Flow s a) -> String -> Map String Json.Value -> Compute.Flow s a)
@@ -42,9 +42,9 @@ decodeField decodeValue name m = (Flows.bind (decodeOptionalField decodeValue na
   Nothing -> (Flows.fail (Strings.cat [
     "missing field: ",
     name]))
-  Just v325 -> (Flows.pure v325)))
+  Just v5 -> (Flows.pure v5)))
 
 decodeOptionalField :: ((Json.Value -> Compute.Flow s a) -> String -> Map String Json.Value -> Compute.Flow s (Maybe a))
 decodeOptionalField decodeValue name m = ((\x -> case x of
   Nothing -> (Flows.pure Nothing)
-  Just v326 -> (Flows.map (\x -> Just x) (decodeValue v326))) (Maps.lookup name m))
+  Just v6 -> (Flows.map (\x -> Just x) (decodeValue v6))) (Maps.lookup name m))
