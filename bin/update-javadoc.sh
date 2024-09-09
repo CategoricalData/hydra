a91544be2c9a8a25e5ad24599cde1198d96774d4
Joshua Shinavier <josh@fortytwo.net>
2024-09-09 11:56:47 -0700

    Fix a problem with the JavaDoc generation script. Add README links to JavaDocs

--- bin/update-javadoc.sh
+++ bin/update-javadoc.sh
@@ -4,16 +4,16 @@ echo "Updating JavaDoc HTML"
 
 ./gradlew javadoc
 
-rm -r ../docs/hydra-ext/javadoc
-rm -r ../docs/hydra-java/javadoc
+rm -r docs/hydra-ext/javadoc
+rm -r docs/hydra-java/javadoc
 
-mkdir -p ../docs/hydra-ext/javadoc
-mkdir -p ../docs/hydra-java/javadoc
+mkdir -p docs/hydra-ext/javadoc
+mkdir -p docs/hydra-java/javadoc
 
-cp -r hydra-ext/build/docs/javadoc/ ../docs/hydra-ext/javadoc/
-cp -r hydra-java/build/docs/javadoc/ ../docs/hydra-java/javadoc/
+cp -r hydra-ext/build/docs/javadoc/ docs/hydra-ext/javadoc/
+cp -r hydra-java/build/docs/javadoc/ docs/hydra-java/javadoc/
 
 git add docs/hydra-ext/javadoc
 git add docs/hydra-java/javadoc
 
-echo "Done. Now commit any new files, push them to the 'main' branch and also to 'docs' branch for GitHub Pages."
+echo "Done. Now merge the changes into the 'main' branch, and also into the 'docs' branch for GitHub Pages."
