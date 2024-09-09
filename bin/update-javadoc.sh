7daed992a5ba7e11e77db09c37d072871f94e8c0
Joshua Shinavier <josh@fortytwo.net>
2024-09-09 11:44:44 -0700

    Update JavaDoc generation script

--- bin/update-javadoc.sh
+++ bin/update-javadoc.sh
@@ -2,13 +2,18 @@
 
 echo "Updating JavaDoc HTML"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd ${SCRIPT_DIR}/../hydra-java
 ./gradlew javadoc
+
+rm -r ../docs/hydra-ext/javadoc
 rm -r ../docs/hydra-java/javadoc
-cp -r build/docs/javadoc/ ../docs/hydra-java/javadoc/
-cd -
+
+mkdir -p ../docs/hydra-ext/javadoc
+mkdir -p ../docs/hydra-java/javadoc
+
+cp -r hydra-ext/build/docs/javadoc/ ../docs/hydra-ext/javadoc/
+cp -r hydra-java/build/docs/javadoc/ ../docs/hydra-java/javadoc/
+
+git add docs/hydra-ext/javadoc
+git add docs/hydra-java/javadoc
 
 echo "Done. Now commit any new files, push them to the 'main' branch and also to 'docs' branch for GitHub Pages."
-
