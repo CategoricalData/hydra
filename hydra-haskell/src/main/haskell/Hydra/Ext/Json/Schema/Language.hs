215237124a58335ba4d983cd097f8be38f851e73
joshsh <josh@fortytwo.net>
2024-10-29 14:17:56 -0700

    Complete JSON Schema serde and refine the coder. Resolves #141. Further refinements will be made.

--- hydra-haskell/src/main/haskell/Hydra/Ext/Json/Schema/Language.hs
+++ hydra-haskell/src/main/haskell/Hydra/Ext/Json/Schema/Language.hs
@@ -13,10 +13,17 @@ jsonSchemaLanguage :: Language
 jsonSchemaLanguage = Language (LanguageName "hydra/ext/json/schema") $ LanguageConstraints {
   languageConstraintsEliminationVariants = S.empty,
   languageConstraintsLiteralVariants = S.fromList [
-    LiteralVariantBoolean, LiteralVariantInteger, LiteralVariantString],
-  languageConstraintsFloatTypes = S.empty,
+    LiteralVariantBoolean, LiteralVariantFloat, LiteralVariantInteger, LiteralVariantString],
+  languageConstraintsFloatTypes = S.fromList [FloatTypeBigfloat],
   languageConstraintsFunctionVariants = S.empty,
   languageConstraintsIntegerTypes = S.fromList [IntegerTypeBigint],
   languageConstraintsTermVariants = S.fromList Basics.termVariants,
-  languageConstraintsTypeVariants = S.fromList Basics.typeVariants,
+  languageConstraintsTypeVariants = S.fromList [
+    TypeVariantAnnotated,
+    TypeVariantList,
+    TypeVariantLiteral,
+    TypeVariantMap,
+    TypeVariantOptional,
+    TypeVariantRecord,
+    TypeVariantVariable],
   languageConstraintsTypes = \typ -> True }
