1c640979a40b952851011c5a289e2a16199c38ad
joshsh <josh@fortytwo.net>
2025-01-07 16:48:15 -0800

    Python coder support for record types, and supporting constructs

--- hydra-haskell/src/main/haskell/Hydra/Ext/Python/Utils.hs
+++ hydra-haskell/src/main/haskell/Hydra/Ext/Python/Utils.hs
@@ -3,8 +3,14 @@ module Hydra.Ext.Python.Utils where
 import qualified Hydra.Ext.Python.Syntax as Py
 
 
+pyAssignmentToPyStatement :: Py.Assignment -> Py.Statement
+pyAssignmentToPyStatement a = Py.StatementSimple [Py.SimpleStatementAssignment a]
+
 pyNameToPyExpression :: Py.Name -> Py.Expression
 pyNameToPyExpression =  pyPrimaryToPyExpression . pyNameToPyPrimary
+
+pyNameToPyNamedExpression :: Py.Name -> Py.NamedExpression
+pyNameToPyNamedExpression = Py.NamedExpressionSimple . pyNameToPyExpression
 
 pyNameToPyPrimary :: Py.Name -> Py.Primary
 pyNameToPyPrimary = Py.PrimarySimple . Py.AtomName
@@ -16,12 +22,17 @@ pyPrimaryToPyExpression prim = Py.ExpressionSimple $ Py.Disjunction [Py.Conjunct
       Py.Power (Py.AwaitPrimary False prim) Nothing)) []]]
 
 functionCall :: Py.Primary -> [Py.Expression] -> Py.Expression
-functionCall func args = pyPrimaryToPyExpression $ Py.PrimaryCompound $ Py.PrimaryWithRhs func $
+functionCall func args = pyPrimaryToPyExpression $ primaryWithRhs func $
   Py.PrimaryRhsCall $ Py.Args (Py.PosArgExpression <$> args) [] []
+
+primaryWithRhs :: Py.Primary -> Py.PrimaryRhs -> Py.Primary
+primaryWithRhs prim rhs = Py.PrimaryCompound $ Py.PrimaryWithRhs prim rhs
+
+primaryWithSlice :: Py.Primary -> Py.Slice -> Py.Primary
+primaryWithSlice prim slice = primaryWithRhs prim $ Py.PrimaryRhsSlices $ Py.Slices slice []
 
 simpleStatementNoComment :: Py.SimpleStatement -> Py.StatementWithComment
 simpleStatementNoComment s = Py.StatementWithComment (Py.StatementSimple [s]) Nothing
-
 
 stringToPyExpression :: String -> Py.Expression
 stringToPyExpression = pyPrimaryToPyExpression . Py.PrimarySimple . Py.AtomString
