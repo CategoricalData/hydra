063a46891c21de14fc31eab77eae2b563a3e66f9
Joshua Shinavier <josh@fortytwo.net>
2025-01-19 20:57:43 -0800

    Add minimal Types DSL in Python, and allow generated sources to depend on it.

---
+++ hydra-python/src/main/python/hydra/dsl/types.py
@@ -0,0 +1,9 @@
+from typing import TypeVar, Generic
+from dataclasses import dataclass
+
+T = TypeVar("T")
+
+@dataclass
+class Variant(Generic[T]):
+    """A helper class for variants (alternatives) of Hydra union types in Python."""
+    value: T
