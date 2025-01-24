1b680a10157f5b466932e44d92151ca4e2f2c481
mwbrulhardt <mwbrulhardt@gmail.com>
2025-01-24 18:23:19 -0500

    Build is working and pytest can be run now.

---
+++ hydra-python/tests/conftest.py
@@ -0,0 +1,8 @@
+import subprocess
+
+from pytest import Session
+
+def pytest_sessionstart(session: Session):
+    """Install the package before running tests"""
+    subprocess.run(["uv", "build"], check=True)
+    subprocess.run(["uv", "pip", "install", "dist/hydra-0.9.0-py3-none-any.whl"], check=True)
