7e8b02f1634940eabe84abf073798e69d799eddd
Joshua Shinavier <josh@fortytwo.net>
2025-02-10 14:51:19 -0800

    Promote a few more inference test cases; refine inference test helpers

--- hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference.hs
@@ -2,16 +2,21 @@ module Hydra.Sources.Tier3.Test.Inference (inferenceTests) where
 
 import Hydra.Kernel
 import Hydra.Dsl.Tests
-import Hydra.Dsl.Terms
 import Hydra.Dsl.ShorthandTypes
+import Hydra.Dsl.Terms as Terms
 import qualified Hydra.Dsl.Types as Types
 
 
 inferenceTests :: TestGroup
-inferenceTests = TestGroup "hydra/lib/lists primitives" Nothing groups []
+inferenceTests = TestGroup "inference tests" Nothing groups []
   where
     groups = [
-      applicationTerms]
+      applicationTerms,
+      functionTerms]
+
+expectMono i term typ = infTest ("#" ++ show i) term $ Types.mono typ
+
+expectPoly i term params typ = infTest ("#" ++ show i) term $ Types.poly params typ
 
 infTest :: String -> Term -> TypeScheme -> TestCaseWithMetadata
 infTest name term ts = TestCaseWithMetadata name (TestCaseInference $ InferenceTestCase term ts) Nothing []
@@ -19,10 +24,34 @@ infTest name term ts = TestCaseWithMetadata name (TestCaseInference $ InferenceT
 ---
 
 applicationTerms :: TestGroup
-applicationTerms = TestGroup "application terms" (Just "Check a few hand-picked application terms") [] [
-    infTest "check lambda applications"
+applicationTerms = TestGroup "Application terms" (Just "Check a few hand-picked application terms") [] [
+    expectMono 1
       ((lambda "x" $ var "x") @@ string "foo")
-      (tMono tString),
-    infTest "Check mixed expressions with lambdas, constants, and primitive functions"
+      tString,
+    expectMono 2
       (lambda "x" $ primitive _math_sub @@ (primitive _math_add @@ var "x" @@ var "x") @@ int32 1)
-      (tMono $ tFun tInt32 tInt32)]
+      (tFun tInt32 tInt32)]
+
+functionTerms :: TestGroup
+functionTerms = TestGroup "Function terms" (Just "Check a few hand-picked function terms") subgroups []
+  where
+    foldAdd = Terms.fold $ primitive _math_add
+    subgroups = [
+      TestGroup "Check lambdas" Nothing [] [
+        expectPoly 1
+          (lambda "x" (var "x"))
+          ["t0"] (Types.function (Types.var "t0") (Types.var "t0")),
+        expectPoly 2
+          (lambda "x" (int16 137))
+          ["t0"] (Types.function (Types.var "t0") Types.int16)],
+
+      TestGroup "Check list eliminations" Nothing [] [
+        expectMono 1
+          foldAdd
+          (Types.functionN [Types.int32, Types.list Types.int32, Types.int32]),
+        expectMono 2
+          (apply foldAdd $ int32 0)
+          (Types.function (Types.list Types.int32) Types.int32),
+        expectMono 3
+          (apply (apply foldAdd $ int32 0) (list (int32 <$> [1, 2, 3, 4, 5])))
+          Types.int32]]
--- hydra-haskell/src/test/haskell/Hydra/Inference/InferenceSpec.hs
+++ hydra-haskell/src/test/haskell/Hydra/Inference/InferenceSpec.hs
@@ -49,26 +49,6 @@ expectTypeAnnotation path term etyp = shouldSucceedWith atyp etyp
 checkFunctionTerms :: H.SpecWith ()
 checkFunctionTerms = H.describe "Check a few hand-picked function terms" $ do
 
-    H.it "Check lambdas" $ do
-      expectPolytype
-        (lambda "x" (var "x"))
-        ["t0"] (Types.function (Types.var "t0") (Types.var "t0"))
-      expectPolytype
-        (lambda "x" (int16 137))
-        ["t0"] (Types.function (Types.var "t0") Types.int16)
-
-    H.it "Check list eliminations" $ do
-      let fun = Terms.fold $ primitive _math_add
-      expectMonotype
-        fun
-        (Types.functionN [Types.int32, Types.list Types.int32, Types.int32])
-      expectMonotype
-        (apply fun $ int32 0)
-        (Types.function (Types.list Types.int32) Types.int32)
-      expectMonotype
-        (apply (apply fun $ int32 0) (list (int32 <$> [1, 2, 3, 4, 5])))
-        Types.int32
-
     H.it "Check projections" $ do
       expectMonotype
         (project testTypePersonName (Name "firstName"))
