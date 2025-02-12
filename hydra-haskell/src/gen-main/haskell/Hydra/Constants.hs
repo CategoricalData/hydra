6fe1381ad92bb5151d01d07fdcd7f1556daf7180
Joshua Shinavier <josh@fortytwo.net>
2025-02-12 06:31:10 -0800

    Promote annotation key constants into the kernel

--- hydra-haskell/src/gen-main/haskell/Hydra/Constants.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Constants.hs
@@ -14,8 +14,23 @@ ignoredVariable = "_"
 key_classes :: Core.Name
 key_classes = (Core.Name "classes")
 
+key_deprecated :: Core.Name
+key_deprecated = (Core.Name "_deprecated")
+
 key_description :: Core.Name
 key_description = (Core.Name "description")
+
+key_exclude :: Core.Name
+key_exclude = (Core.Name "exclude")
+
+key_maxLength :: Core.Name
+key_maxLength = (Core.Name "_maxLength")
+
+key_minLength :: Core.Name
+key_minLength = (Core.Name "_minLength")
+
+key_preserveFieldName :: Core.Name
+key_preserveFieldName = (Core.Name "_preserveFieldName")
 
 key_type :: Core.Name
 key_type = (Core.Name "type")
