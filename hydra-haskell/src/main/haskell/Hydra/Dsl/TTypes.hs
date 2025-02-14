a331a236cda76ed13a918c30a7b1c4ee60e3f6d7
Joshua Shinavier <josh@fortytwo.net>
2025-02-13 21:56:26 -0800

    Introduce term-encoded test groups into the test kernel

--- hydra-haskell/src/main/haskell/Hydra/Dsl/TTypes.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/TTypes.hs
@@ -6,8 +6,8 @@ module Hydra.Dsl.TTypes (
 ) where
 
 import Hydra.Kernel
-import Hydra.Dsl.Base as Base
-import qualified Hydra.Dsl.Core as Core
+import qualified Hydra.Dsl.Base as Base
+import Hydra.Dsl.Core as Core hiding (name, unName)
 import Hydra.Dsl.TBase
 
 import qualified Data.Map as M
@@ -15,25 +15,36 @@ import qualified Data.Maybe as Y
 
 
 boolean :: TTerm Type
-boolean = Core.typeLiteral Core.literalTypeBoolean
+boolean = typeLiteral literalTypeBoolean
 
 field :: String -> TTerm Type -> TTerm FieldType
-field s = Core.fieldType (name s)
+field s = fieldType (name s)
 
 floatType :: FloatType -> TTerm FloatType
-floatType t = unitVariant _FloatType $ case t of
+floatType t = Base.unitVariant _FloatType $ case t of
   FloatTypeBigfloat -> _FloatType_bigfloat
   FloatTypeFloat32 -> _FloatType_float32
   FloatTypeFloat64 -> _FloatType_float64
 
 float32 :: TTerm Type
-float32 = Core.typeLiteral $ Core.literalTypeFloat $ floatType FloatTypeFloat32
+float32 = typeLiteral $ literalTypeFloat $ floatType FloatTypeFloat32
+
+function :: TTerm Type -> TTerm Type -> TTerm Type
+function dom cod = typeFunction $ functionType dom cod
+
+functionN :: [TTerm Type] -> TTerm Type
+functionN types = case types of
+  [t] -> t
+  t:ts -> function t $ functionN ts
+
+int16 :: TTerm Type
+int16 = typeLiteral $ literalTypeInteger integerTypeInt16
 
 int32 :: TTerm Type
-int32 = Core.typeLiteral $ Core.literalTypeInteger Core.integerTypeInt32
+int32 = typeLiteral $ literalTypeInteger integerTypeInt32
 
 integerType :: IntegerType -> TTerm IntegerType
-integerType t = unitVariant _IntegerType $ case t of
+integerType t = Base.unitVariant _IntegerType $ case t of
   IntegerTypeBigint -> _IntegerType_bigint
   IntegerTypeInt8 -> _IntegerType_int8
   IntegerTypeInt16 -> _IntegerType_int16
@@ -45,38 +56,41 @@ integerType t = unitVariant _IntegerType $ case t of
   IntegerTypeUint64 -> _IntegerType_uint64
 
 lambda :: String -> TTerm Type -> TTerm Type
-lambda var body = Core.typeLambda $ Core.lambdaType (name var) body
+lambda var body = typeLambda $ lambdaType (name var) body
+
+list :: TTerm Type -> TTerm Type
+list = typeList
 
 mono :: TTerm Type -> TTerm TypeScheme
 mono t = Base.record _TypeScheme [
-  _TypeScheme_variables>>: list [],
-  _TypeScheme_type>>: t]
+  Base.field _TypeScheme_variables $ Base.list [],
+  Base.field _TypeScheme_type t]
 
 optional :: TTerm Type -> TTerm Type
-optional = Core.typeOptional
+optional = typeOptional
 
-poly :: TTerm [Name] -> TTerm Type -> TTerm TypeScheme
+poly :: [String] -> TTerm Type -> TTerm TypeScheme
 poly params t = Base.record _TypeScheme [
-  _TypeScheme_variables>>: params,
-  _TypeScheme_type>>: t]
+  Base.field _TypeScheme_variables (Base.list (name <$> params)),
+  Base.field _TypeScheme_type t]
 
 record :: TTerm Name -> [TTerm FieldType] -> TTerm Type
-record name fields = Core.typeRecord $ Core.rowType name $ list fields
+record name fields = typeRecord $ rowType name $ Base.list fields
 
 string :: TTerm Type
-string = Core.typeLiteral Core.literalTypeString
+string = typeLiteral literalTypeString
 
 apply :: TTerm Type -> TTerm Type -> TTerm Type
-apply l r = Core.typeApplication $ Core.applicationType l r
+apply l r = typeApplication $ applicationType l r
 
 uint64 :: TTerm Type
-uint64 = Core.typeLiteral $ Core.literalTypeInteger Core.integerTypeUint64
+uint64 = typeLiteral $ literalTypeInteger integerTypeUint64
 
 union :: TTerm Name -> [TTerm FieldType] -> TTerm Type
-union name fields = Core.typeUnion $ Core.rowType name $ list fields
+union name fields = typeUnion $ rowType name $ Base.list fields
 
 unit :: TTerm Type
-unit = Core.typeRecord $ Core.rowType (Core.name _Unit) $ list []
+unit = typeRecord $ rowType (Base.wrap _Name $ Base.string $ unName _Unit) $ Base.list []
 
 var :: String -> TTerm Type
-var = Core.typeVariable . name
+var = typeVariable . name
--- hydra-haskell/src/main/haskell/Hydra/Dsl/Testing.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Testing.hs
@@ -5,36 +5,78 @@ import Hydra.Kernel
 import Hydra.Testing
 import Hydra.Dsl.Base as Base
 import qualified Hydra.Dsl.Core as Core
+import Hydra.Dsl.TTerms
+import qualified Hydra.Dsl.Terms as Terms
+import qualified Hydra.Dsl.TTypes as T
 
 import qualified Data.Map as M
 
 
+tgroup :: String -> Maybe String -> [TTerm TestGroup] -> [TTerm TestCaseWithMetadata] -> TTerm TestGroup
+tgroup name mdesc subgroups cases = testGroup (Base.string name) (opt (Base.string <$> mdesc)) (Base.list subgroups) (Base.list cases)
+
+infTest :: String -> TTerm Term -> TTerm TypeScheme -> TTerm TestCaseWithMetadata
+infTest name term ts = testCaseWithMetadata (Base.string name)
+  (testCaseInference $ inferenceTestCase term ts) nothing (Base.list [])
+
+expectMono i term typ = infTest ("#" ++ show i) term $ T.mono typ
+expectPoly i term params typ = infTest ("#" ++ show i) term $ T.poly params typ
+
+--
+
+encodeGroup (TestGroup name desc groups cases) = Terms.record _TestGroup [
+  Field _TestGroup_name $ Terms.string name,
+  Field _TestGroup_description $ Terms.optional (Terms.string <$> desc),
+  Field _TestGroup_subgroups $ Terms.list (encodeGroup <$> groups),
+  Field _TestGroup_cases $ Terms.list (encodeCaseWithMetadata <$> cases)]
+encodeCaseWithMetadata (TestCaseWithMetadata name tcase mdesc tags) = Terms.record _TestCaseWithMetadata [
+  Field _TestCaseWithMetadata_name $ Terms.string name,
+  Field _TestCaseWithMetadata_case $ encodeCase tcase,
+  Field _TestCaseWithMetadata_description $ Terms.optional (Terms.string <$> mdesc),
+  Field _TestCaseWithMetadata_tags $ Terms.list (Terms.string . unTag <$> tags)]
+encodeCase tcase = case tcase of
+  TestCaseCaseConversion ccase -> Terms.variant _TestCase _TestCase_caseConversion $ encodeCaseConversionTestCase ccase
+  TestCaseEvaluation ecase -> Terms.variant _TestCase _TestCase_evaluation $ encodeEvaluationTestCase ecase
+  TestCaseInference icase -> Terms.variant _TestCase _TestCase_inference $ encodeInferenceTestCase icase
+encodeCaseConvention c = Terms.unitVariant _CaseConvention $ case c of
+  CaseConventionLowerSnake -> _CaseConvention_lowerSnake
+  CaseConventionUpperSnake -> _CaseConvention_upperSnake
+  CaseConventionCamel -> _CaseConvention_camel
+  CaseConventionPascal -> _CaseConvention_pascal
+encodeCaseConversionTestCase (CaseConversionTestCase fromConvention toConvention fromString toString) = Terms.record _CaseConversionTestCase [
+  Field _CaseConversionTestCase_fromConvention $ encodeCaseConvention fromConvention,
+  Field _CaseConversionTestCase_toConvention $ encodeCaseConvention toConvention,
+  Field _CaseConversionTestCase_fromString $ Terms.string fromString,
+  Field _CaseConversionTestCase_toString $ Terms.string toString]
+encodeEvaluationTestCase (EvaluationTestCase style input output) = Terms.record _EvaluationTestCase [
+  Field _EvaluationTestCase_evaluationStyle $ Terms.variant _EvaluationStyle (case style of
+    EvaluationStyleEager -> _EvaluationStyle_eager
+    EvaluationStyleLazy -> _EvaluationStyle_lazy) Terms.unit,
+  Field _EvaluationTestCase_input $ coreEncodeTerm input,
+  Field _EvaluationTestCase_output $ coreEncodeTerm output]
+encodeInferenceTestCase (InferenceTestCase input output) = Terms.record _InferenceTestCase [
+  Field _InferenceTestCase_input $ coreEncodeTerm input,
+  Field _InferenceTestCase_output $ coreEncodeTypeScheme output]
+
+---
+
 inferenceTestCase :: TTerm Term -> TTerm TypeScheme -> TTerm InferenceTestCase
-inferenceTestCase input output = record _InferenceTestCase [
+inferenceTestCase input output = Base.record _InferenceTestCase [
   _InferenceTestCase_input>>: input,
   _InferenceTestCase_output>>: output]
-
-{-
-data InferenceTestCase =
-  InferenceTestCase {
-    inferenceTestCaseInput :: Core.Term,
-    inferenceTestCaseOutput :: Core.TypeScheme}
-  deriving (Eq, Ord, Read, Show)
-
--}
 
 testCaseInference :: TTerm InferenceTestCase -> TTerm TestCase
 testCaseInference = variant _TestCase _TestCase_inference
 
 testCaseWithMetadata :: TTerm String -> TTerm TestCase -> TTerm (Maybe String) -> TTerm [Tag] -> TTerm TestCaseWithMetadata
-testCaseWithMetadata name tcase description tags = record _TestCaseWithMetadata [
+testCaseWithMetadata name tcase description tags = Base.record _TestCaseWithMetadata [
   _TestCaseWithMetadata_name>>: name,
   _TestCaseWithMetadata_case>>: tcase,
   _TestCaseWithMetadata_description>>: description,
   _TestCaseWithMetadata_tags>>: tags]
 
 testGroup :: TTerm String -> TTerm (Maybe String) -> TTerm [TestGroup] -> TTerm [TestCaseWithMetadata] -> TTerm TestGroup
-testGroup name description subgroups cases = record _TestGroup [
+testGroup name description subgroups cases = Base.record _TestGroup [
   _TestGroup_name>>: name,
   _TestGroup_description>>: description,
   _TestGroup_subgroups>>: subgroups,
