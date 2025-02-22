f3a43b74e70b9d56df94b5e3c840cbc8d7a8935c
Joshua Shinavier <josh@fortytwo.net>
2025-02-21 16:47:42 -0800

    Small changes to inference tests

--- hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference.hs
@@ -21,18 +21,18 @@ import Prelude hiding (map, product, sum)
 
 inferenceTests :: TTerm TestGroup
 inferenceTests = supergroup "inference tests" [
-    applicationTerms,
-    functionTerms,
-    individualTerms,
-    letTerms,
-    listTerms,
-    primitiveTerms,
-    productTerms,
-    sumTerms,
-    wrappedTerms]
+    testGroupForApplicationTerms,
+    testGroupForFunctionTerms,
+    testGroupForIndividualTerms,
+    testGroupForLetTerms,
+    testGroupForListTerms,
+    testGroupForPrimitiveTerms,
+    testGroupForProductTerms,
+    testGroupForSumTerms,
+    testGroupForWrapTerms]
 
-applicationTerms :: TTerm TestGroup
-applicationTerms = subgroup "Application terms" [
+testGroupForApplicationTerms :: TTerm TestGroup
+testGroupForApplicationTerms = subgroup "Application terms" [
     expectMono 1
       ((lambda "x" $ var "x") @@ string "foo")
       T.string,
@@ -40,9 +40,9 @@ applicationTerms = subgroup "Application terms" [
       (lambda "x" $ primitive _math_sub @@ (primitive _math_add @@ var "x" @@ var "x") @@ int32 1)
       (T.function T.int32 T.int32)]
 
-functionTerms :: TTerm TestGroup
-functionTerms = supergroup "Function terms" [
-    subgroup "Check lambdas" [
+testGroupForFunctionTerms :: TTerm TestGroup
+testGroupForFunctionTerms = supergroup "Function terms" [
+    subgroup "Lambdas" [
       expectPoly 1
         (lambda "x" $ var "x")
         ["t0"] (T.function (T.var "t0") (T.var "t0")),
@@ -50,7 +50,7 @@ functionTerms = supergroup "Function terms" [
         (lambda "x" $ int16 137)
         ["t0"] (T.function (T.var "t0") T.int16)],
 
-    subgroup "Check list eliminations" [
+    subgroup "List eliminations" [
       expectMono 1
         foldAdd
         (T.functionN [T.int32, T.list T.int32, T.int32]),
@@ -61,12 +61,12 @@ functionTerms = supergroup "Function terms" [
         (apply (apply foldAdd $ int32 0) (list (int32 <$> [1, 2, 3, 4, 5])))
         T.int32],
 
-    subgroup "Check projections" [
+    subgroup "Projections" [
       expectMono 1
         (project (ref testTypePersonNameDef) (name "firstName"))
         (T.function (ref testTypePersonDef) T.string)],
 
-    subgroup "Check case statements" [
+    subgroup "Case statements" [
       expectMono 1
         (match (ref testTypeFoobarValueNameDef) nothing [
           "bool">: constant true,
@@ -76,9 +76,9 @@ functionTerms = supergroup "Function terms" [
   where
     foldAdd = fold $ primitive _math_add
 
-individualTerms :: TTerm TestGroup
-individualTerms = supergroup "Individual terms" [
-    subgroup "Check literal values" [
+testGroupForIndividualTerms :: TTerm TestGroup
+testGroupForIndividualTerms = supergroup "Individual terms" [
+    subgroup "Literal values" [
       expectMono 1
         (int32 42)
         T.int32,
@@ -92,7 +92,7 @@ individualTerms = supergroup "Individual terms" [
         (float64 42.0)
         T.float64],
 
-    subgroup "Check let terms" [
+    subgroup "Let terms" [
       expectPoly 1
         (let1 "x" (float32 42.0) $ lambdas ["y", "z"] $ var "x")
         ["t0", "t1"] (T.function (T.var "t0") (T.function (T.var "t1") T.float32)),
@@ -107,7 +107,7 @@ individualTerms = supergroup "Individual terms" [
         (T.functionN [
           T.functionN [T.int32, T.boolean, T.boolean], T.int32, T.boolean, T.boolean])],
 
-    subgroup "Check optionals" [
+    subgroup "Optionals" [
       expectMono 1
         (optional $ just $ int32 42)
         (T.optional T.int32),
@@ -115,7 +115,7 @@ individualTerms = supergroup "Individual terms" [
         (optional nothing)
         ["t0"] (T.optional $ T.var "t0")],
 
-    subgroup "Check products" [
+    subgroup "Products" [
       expectMono 1
         (product [])
         (T.product []),
@@ -123,7 +123,7 @@ individualTerms = supergroup "Individual terms" [
         (pair (int32 42) (string "foo"))
         (T.product [T.int32, T.string])],
 
-    subgroup "Check records" [
+    subgroup "Records" [
       expectMono 1
         (record (ref testTypeLatLonNameDef) [
           "lat">: float32 37.7749,
@@ -155,12 +155,12 @@ individualTerms = supergroup "Individual terms" [
             "lat">: T.var "t0",
             "lon">: T.var "t0"]))],
 
-    subgroup "Check unions" [
+    subgroup "Unions" [
       expectMono 1
         (inject (ref testTypeTimestampNameDef) "unixTimeMillis" $ uint64 1638200308368)
         (ref testTypeTimestampDef)],
 
-    subgroup "Check sets" [
+    subgroup "Sets" [
       expectMono 1
         (set [true])
         (T.set T.boolean),
@@ -168,7 +168,7 @@ individualTerms = supergroup "Individual terms" [
         (set [set []])
         ["t0"] (T.set $ T.set $ T.var "t0")],
 
-    subgroup "Check maps" [
+    subgroup "Maps" [
       expectMono 1
         (mapTermCheat [
           (Terms.string "firstName", Terms.string "Arthur"),
@@ -183,7 +183,7 @@ individualTerms = supergroup "Individual terms" [
         ["t0"] (T.function (T.var "t0") (T.function (T.var "t0") (T.map (T.var "t0") T.float64)))]]
 
 --     -- TODO: add a case for a recursive nominal type -- e.g. MyList := () + (int, Mylist)
---     subgroup "Check nominal (newtype) terms" [
+--     subgroup "Nominal (newtype) terms" [
 --       expectMono
 --         testDataArthur
 --         (T.wrap "Person")
@@ -198,89 +198,89 @@ individualTerms = supergroup "Individual terms" [
     mapTermCheat :: [(Term, Term)] -> TTerm Term
     mapTermCheat = TTerm . coreEncodeTerm . Terms.mapTerm . M.fromList
 
-letTerms :: TTerm TestGroup
-letTerms = supergroup "Let terms" [
-    subgroup "Check empty let" [
+testGroupForLetTerms :: TTerm TestGroup
+testGroupForLetTerms = supergroup "Let terms" [
+    subgroup "Empty let" [
       expectMono 1
         (lets [] $ int32 42)
         T.int32],
 
-    subgroup "Check trivial let" [
+    subgroup "Trivial let" [
       expectMono 2
         (lets [
             "foo">: int32 42]
           $ var "foo")
         T.int32]]
 
-listTerms :: TTerm TestGroup
-listTerms = supergroup "List terms" [
-    subgroup "Check list of strings" [
+testGroupForListTerms :: TTerm TestGroup
+testGroupForListTerms = supergroup "List terms" [
+    subgroup "List of strings" [
       expectMono 1
         (list [string "foo", string "bar"])
         (T.list T.string)],
-    subgroup "Check list of lists of strings" [
+    subgroup "List of lists of strings" [
       expectMono 1
         (list [list [string "foo"], list []])
         (T.list $ T.list T.string)],
-    subgroup "Check empty list" [
+    subgroup "Empty list" [
       expectPoly 1
         (list [])
         ["t0"] (T.list $ T.var "t0")],
-    subgroup "Check list containing an empty list" [
+    subgroup "List containing an empty list" [
       expectPoly 1
         (list [list []])
         ["t0"] (T.list $ T.list $ T.var "t0")],
-    subgroup "Check lambda producing a list of integers" [
+    subgroup "Lambda producing a list of integers" [
       expectMono 1
         (lambda "x" (list [var "x", int32 42]))
         (T.function T.int32 $ T.list T.int32)],
-    subgroup "Check list with bound variables" [
+    subgroup "List with bound variables" [
       expectMono 1
         (lambda "x" (list [var "x", string "foo", var "x"]))
         (T.function T.string (T.list T.string))]]
 
-primitiveTerms :: TTerm TestGroup
-primitiveTerms = supergroup "Primitive terms" [
-    subgroup "Check monomorphic primitive functions" [
+testGroupForPrimitiveTerms :: TTerm TestGroup
+testGroupForPrimitiveTerms = supergroup "Primitive terms" [
+    subgroup "Monomorphic primitive functions" [
       expectMono 1
         (primitive $ Name "hydra.lib.strings.length")
         (T.function T.string T.int32),
       expectMono 2
         (primitive _math_sub)
         (T.function T.int32 (T.function T.int32 T.int32))],
-    subgroup "Check polymorphic primitive functions" [
+    subgroup "Polymorphic primitive functions" [
       expectPoly 1
         (lambda "els" (apply (primitive _lists_length) (apply (primitive _lists_concat) $ var "els")))
         ["t0"] (T.function (T.list $ T.list $ T.var "t0") T.int32)]]
 
-productTerms :: TTerm TestGroup
-productTerms = supergroup "Product terms" [
-    subgroup "Check empty product" [
+testGroupForProductTerms :: TTerm TestGroup
+testGroupForProductTerms = supergroup "Product terms" [
+    subgroup "Empty product" [
       expectMono 1
         (product [])
         (T.product [])],
-    subgroup "Check non-empty, monotyped products" [
+    subgroup "Non-empty, monotyped products" [
       expectMono 1
         (product [string "foo", int32 42])
         (T.product [T.string, T.int32]),
       expectMono 2
         (product [string "foo", list [float32 42.0, float32 137.0]])
         (T.product [T.string, T.list T.float32])],
-    subgroup "Check polytyped products" [
+    subgroup "Polytyped products" [
       expectPoly 1
         (product [list [], string "foo"])
         ["t0"] (T.product [T.list $ T.var "t0", T.string])]]
 
-sumTerms :: TTerm TestGroup
-sumTerms = supergroup "Sum terms" [
-    subgroup "Check singleton sum terms" [
+testGroupForSumTerms :: TTerm TestGroup
+testGroupForSumTerms = supergroup "Sum terms" [
+    subgroup "Singleton sum terms" [
       expectMono 1
         (sum 0 1 $ string "foo")
         (T.sum [T.string]),
       expectPoly 2
         (sum 0 1 $ list [])
         ["t0"] (T.sum [T.list $ T.var "t0"])],
-    subgroup "Check non-singleton sum terms" [
+    subgroup "Non-singleton sum terms" [
       expectPoly 1
         (sum 0 2 $ string "foo")
         ["t0"] (T.sum [T.string, T.var "t0"]),
@@ -288,16 +288,16 @@ sumTerms = supergroup "Sum terms" [
         (sum 1 2 $ string "foo")
         ["t0"] (T.sum [T.var "t0", T.string])]]
 
-wrappedTerms :: TTerm TestGroup
-wrappedTerms = supergroup "Wrapped terms" [
-    subgroup "Check nominal introductions" [
+testGroupForWrapTerms :: TTerm TestGroup
+testGroupForWrapTerms = supergroup "Wrap terms" [
+    subgroup "Wrap introductions" [
       expectMono 1
         (wrap (ref testTypeStringAliasNameDef) $ string "foo")
         (ref testTypeStringAliasDef),
       expectMono 2
         (lambda "v" $ wrap (ref testTypeStringAliasNameDef) $ var "v")
         (T.function T.string (ref testTypeStringAliasDef))],
-    subgroup "Check nominal eliminations" [
+    subgroup "Wrap eliminations" [
       expectMono 1
         (apply (unwrap (ref testTypeStringAliasNameDef)) (wrap (ref testTypeStringAliasNameDef) $ string "foo"))
         T.string,
