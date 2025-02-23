ed4228d9fc8551d7805fcc86184d80a80b8094d8
Joshua Shinavier <josh@fortytwo.net>
2025-02-22 23:46:50 -0800

    Finish promoting all inference tests into the test kernel. Added runners for all three inference engines. Resolves #148.

--- hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference/AlgebraicTypes.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference/AlgebraicTypes.hs
@@ -32,13 +32,13 @@ algebraicTypesTests = supergroup "Algebraic terms" [
 
 testGroupForFolds :: TTerm TestGroup
 testGroupForFolds = subgroup "List eliminations (folds)" [
-    expectMono 1
+    expectMono 1 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       foldAdd
       (T.functionN [T.int32, T.list T.int32, T.int32]),
-    expectMono 2
+    expectMono 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (foldAdd @@ int32 0)
       (T.function (T.list T.int32) T.int32),
-    expectMono 3
+    expectMono 3 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (foldAdd @@ int32 0 @@ (list (int32 <$> [1, 2, 3, 4, 5])))
       T.int32]
   where
@@ -47,114 +47,114 @@ testGroupForFolds = subgroup "List eliminations (folds)" [
 testGroupForLists :: TTerm TestGroup
 testGroupForLists = supergroup "List terms" [
     subgroup "List of strings" [
-      expectMono 1
+      expectMono 1 []
         (list [string "foo", string "bar"])
         (T.list T.string)],
     subgroup "List of lists of strings" [
-      expectMono 1
+      expectMono 1 []
         (list [list [string "foo"], list []])
         (T.list $ T.list T.string)],
     subgroup "Empty list" [
-      expectPoly 1
+      expectPoly 1 []
         (list [])
         ["t0"] (T.list $ T.var "t0")],
     subgroup "List containing an empty list" [
-      expectPoly 1
+      expectPoly 1 []
         (list [list []])
         ["t0"] (T.list $ T.list $ T.var "t0")],
     subgroup "Lambda producing a polymorphic list" [
-      expectPoly 1
+      expectPoly 1 []
         (lambda "x" (list [var "x"]))
         ["t0"] (T.function (T.var "t0") (T.list $ T.var "t0"))],
     subgroup "Lambda producing a list of integers" [
-      expectMono 1
+      expectMono 1 []
         (lambda "x" (list [var "x", int32 42]))
         (T.function T.int32 $ T.list T.int32)],
     subgroup "List with repeated variables" [
-      expectMono 1
+      expectMono 1 []
         (lambda "x" (list [var "x", string "foo", var "x"]))
         (T.function T.string (T.list T.string))]]
 
 testGroupForMaps :: TTerm TestGroup
 testGroupForMaps = subgroup "Map terms" [
-    expectMono 1
+    expectMono 1 [tag_disabledForAlgorithmWInference]
       (mapTermCheat [
         (Terms.string "firstName", Terms.string "Arthur"),
         (Terms.string "lastName", Terms.string "Dent")])
       (T.map T.string T.string),
-    expectPoly 2
+    expectPoly 2 [tag_disabledForAlgorithmWInference]
       (mapTermCheat [])
       ["t0", "t1"] (T.map (T.var "t0") (T.var "t1"))]
 
 testGroupForOptionals :: TTerm TestGroup
 testGroupForOptionals = subgroup "Optional terms" [
-    expectMono 1
+    expectMono 1 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (optional $ just $ int32 42)
       (T.optional T.int32),
-    expectPoly 2
+    expectPoly 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (optional nothing)
       ["t0"] (T.optional $ T.var "t0")]
 
 testGroupForProducts :: TTerm TestGroup
 testGroupForProducts = supergroup "Product terms" [
     subgroup "Empty products" [
-      expectMono 1
+      expectMono 1 []
         (product [])
         (T.product [])],
 
     subgroup "Non-empty, monotyped products" [
-      expectMono 1
+      expectMono 1 []
         (product [string "foo", int32 42])
         (T.product [T.string, T.int32]),
-      expectMono 2
+      expectMono 2 []
         (product [string "foo", list [float32 42.0, float32 137.0]])
         (T.product [T.string, T.list T.float32]),
-      expectMono 3
+      expectMono 3 []
         (product [string "foo", int32 42, list [float32 42.0, float32 137.0]])
         (T.product [T.string, T.int32, T.list T.float32])],
 
     subgroup "Polytyped products" [
-      expectPoly 1
+      expectPoly 1 []
         (product [list [], string "foo"])
         ["t0"] (T.product [T.list $ T.var "t0", T.string]),
-      expectPoly 2
+      expectPoly 2 []
         (product [int32 42, string "foo", list []])
         ["t0"] (T.product [T.int32, T.string, T.list $ T.var "t0"])],
 
     subgroup "Pairs" [
-      expectMono 1
+      expectMono 1 []
         (pair (int32 42) (string "foo"))
         (T.pair T.int32 T.string),
-      expectPoly 2
+      expectPoly 2 []
         (pair (list []) (string "foo"))
         ["t0"] (T.pair (T.list $ T.var "t0") T.string),
-      expectPoly 3
+      expectPoly 3 []
         (pair (list []) (list []))
         ["t0", "t1"] (T.pair (T.list $ T.var "t0") (T.list $ T.var "t1"))]]
 
 testGroupForSets :: TTerm TestGroup
 testGroupForSets = subgroup "Set terms" [
-    expectMono 1
+    expectMono 1 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (set [true])
       (T.set T.boolean),
-    expectPoly 2
+    expectPoly 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
       (set [set []])
       ["t0"] (T.set $ T.set $ T.var "t0")]
 
 testGroupForSums :: TTerm TestGroup
 testGroupForSums = supergroup "Sum terms" [
     subgroup "Singleton sum terms" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (sum 0 1 (string "foo"))
         (T.sum [T.string]),
-      expectPoly 2
+      expectPoly 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (sum 0 1 (list []))
         ["t0"] (T.sum [T.list $ T.var "t0"])],
 
     subgroup "Non-singleton sum terms" [
-      expectPoly 1
+      expectPoly 1 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (sum 0 2 (string "foo"))
         ["t0"] (T.sum [T.string, T.var "t0"]),
-      expectPoly 2
+      expectPoly 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (sum 1 2 (string "foo"))
         ["t0"] (T.sum [T.var "t0", T.string])]]
--- hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference/Fundamentals.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Tier3/Test/Inference/Fundamentals.hs
@@ -25,16 +25,16 @@ fundamentalsTests = supergroup "Fundamentals" [
   testGroupForLambdas,
   testGroupForLet,
   testGroupForLiterals,
---  testGroupForPathologicalTerms, -- TODO: restore these
+  testGroupForPathologicalTerms,
   testGroupForPolymorphism,
   testGroupForPrimitives]
 
 testGroupForLambdas :: TTerm TestGroup
 testGroupForLambdas = subgroup "Lambdas" [
-    expectPoly 1
+    expectPoly 1 []
       (lambda "x" $ var "x")
       ["t0"] (T.function (T.var "t0") (T.var "t0")),
-    expectPoly 2
+    expectPoly 2 []
       (lambda "x" $ int16 137)
       ["t0"] (T.function (T.var "t0") T.int16)]
 
@@ -42,111 +42,82 @@ testGroupForLet :: TTerm TestGroup
 testGroupForLet = supergroup "Let" [
 
     subgroup "Simple" [
-
---  H.it "test #0" $ do
---    expectPolytype
---      (let1 "x" (float32 42.0) (lambda "y" (lambda "z" (var "x"))))
---      ["t0", "t1"] (Types.function (Types.var "t0") (Types.function (Types.var "t1") Types.float32))
-
-      expectPoly 1
+      expectPoly 1  []
         (let1 "x" (float32 42.0) (lambda "y" (lambda "z" (var "x"))))
         ["t0", "t1"] (T.function (T.var "t0") (T.function (T.var "t1") T.float32))],
-
---  H.it "Empty let" $ do
---    expectType
---      ((int32 42) `with` [])
---      Types.int32
-
     subgroup "Empty let" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (lets [] $ int32 42)
         T.int32],
-
---  H.it "Trivial let" $ do
---    expectType
---      (var "foo" `with` [
---        "foo">: int32 42])
---      Types.int32
-
     subgroup "Trivial let" [
-      expectMono 1
+      expectMono 1  []
         (lets [
-          "foo" >: int32 42]
+          "foo">: int32 42]
           $ var "foo")
         T.int32],
-
---  H.it "Multiple references to a let-bound term" $
---    expectType
---      (list [var "foo", var "bar", var "foo"] `with` [
---        "foo">: int32 42,
---        "bar">: int32 137])
---      (Types.list Types.int32)
-
     subgroup "Multiple references to a let-bound term" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (lets [
-          "foo" >: int32 42,
-          "bar" >: int32 137]
+          "foo">: int32 42,
+          "bar">: int32 137]
           $ list [var "foo", var "bar", var "foo"])
         (T.list T.int32)],
 
---  H.describe "Let-polymorphism" $ do
-
     subgroup "Let-polymorphism" [
-      expectPoly 1
+      expectPoly 1 []
         (lets [
-          "id" >: lambda "x" $ var "x"]
+          "id">: lambda "x" $ var "x"]
           $ lambda "x" $ var "id" @@ (var "id" @@ var "x"))
-        ["t0"] (T.function (T.var "t0") (T.var "t0"))],
---      expectMono 2
---        (lets [
---          "id" >: lambda "x" $ var "x"]
---          $ var "id" @@ (list [var "id" @@ int32 42]))
---        (T.list T.int32),
---      expectPoly 3
---        (lets [
---          "id" >: lambda "x" $ var "x"]
---          $ lambda "x" (var "id" @@ (list [var "id" @@ var "x"])))
---        ["t0"] (T.function (T.var "t0") (T.list $ T.var "t0")),
---      expectMono 4
---        (lets [
---          "id" >: lambda "x" $ var "x"]
---          $ pair (var "id" @@ int32 42) (var "id" @@ string "foo"))
---        (T.pair T.int32 T.string),
---      expectMono 5
---        (lets [
---          "list" >: lambda "x" $ list [var "x"]]
---          $ pair (var "list" @@ int32 42) (var "list" @@ string "foo"))
---        (T.pair (T.list T.int32) (T.list T.string)),
---      expectPoly 6
---        (lets [
---          "singleton" >: lambda "x" $ list [var "x"],
---          "f" >: lambda "x" $ lambda "y" $ primitive _lists_cons
---            @@ (pair (var "singleton" @@ var "x") (var "singleton" @@ var "y"))
---            @@ (var "g" @@ var "x" @@ var "y"),
---          "g" >: lambda "x" $ lambda "y" $ var "f" @@ int32 42 @@ var "y"]
---          $ var "f")
---        (T.list $ T.pair T.int32 (T.var "t0"))]
+        ["t0"] (T.function (T.var "t0") (T.var "t0")),
+      expectMono 2 [tag_disabled]
+        (lets [
+          "id">: lambda "x" $ var "x"]
+          $ var "id" @@ (list [var "id" @@ int32 42]))
+        (T.list T.int32),
+      expectPoly 3 [tag_disabled]
+        (lets [
+          "id">: lambda "x" $ var "x"]
+          $ lambda "x" (var "id" @@ (list [var "id" @@ var "x"])))
+        ["t0"] (T.function (T.var "t0") (T.list $ T.var "t0")),
+      expectMono 4 [tag_disabled]
+        (lets [
+          "id">: lambda "x" $ var "x"]
+          $ pair (var "id" @@ int32 42) (var "id" @@ string "foo"))
+        (T.pair T.int32 T.string),
+      expectMono 5 [tag_disabled]
+        (lets [
+          "list">: lambda "x" $ list [var "x"]]
+          $ pair (var "list" @@ int32 42) (var "list" @@ string "foo"))
+        (T.pair (T.list T.int32) (T.list T.string)),
+      expectPoly 6 [tag_disabled]
+        (lets [
+          "singleton">: lambda "x" $ list [var "x"],
+          "f">: lambda "x" $ lambda "y" $ primitive _lists_cons
+            @@ (pair (var "singleton" @@ var "x") (var "singleton" @@ var "y"))
+            @@ (var "g" @@ var "x" @@ var "y"),
+          "g">: lambda "x" $ lambda "y" $ var "f" @@ int32 42 @@ var "y"]
+          $ var "f")
+        ["t0"] (T.list $ T.pair T.int32 (T.var "t0"))],
 
 --  H.describe "Recursive and mutually recursive let (@wisnesky's test cases)" $ do
     subgroup "Recursive and mutually recursive let (@wisnesky's test cases)" []]
 --      expectPoly 1
 --        (lets [
---          "f" >: lambda "x" $ lambda "y" (var "f" @@ int32 0 @@ var "x")]
+--          "f">: lambda "x" $ lambda "y" (var "f" @@ int32 0 @@ var "x")]
 --          $ var "f")
 --        ["t0"] (T.function T.int32 (T.function T.int32 (T.var "t0"))),
 --      expectPoly 2
 --        (lets [
---          "f" >: var "g",
---          "g" >: var "f"]
+--          "f">: var "g",
+--          "g">: var "f"]
 --          $ pair (var "f") (var "g"))
 --        -- Note: GHC finds (a, b) rather than (a, a)
 --        -- Try: :t (let (f, g) = (g, f) in (f, g))
 --        ["t0"] (T.pair (T.var "t0") (T.var "t0")),
 --      expectPoly 3
 --        (lets [
---          "f" >: lambda "x" $ lambda "y" (var "g" @@ int32 0 @@ var "x"),
---          "g" >: lambda "u" $ lambda "v" (var "f" @@ var "v" @@ int32 0)]
+--          "f">: lambda "x" $ lambda "y" (var "g" @@ int32 0 @@ var "x"),
+--          "g">: lambda "u" $ lambda "v" (var "f" @@ var "v" @@ int32 0)]
 --          $ pair (var "f") (var "g"))
 --        ["t0", "t1"] (T.pair
 --          (T.function (T.var "v0") (T.function T.int32 (T.var "t1")))
@@ -154,7 +125,7 @@ testGroupForLet = supergroup "Let" [
 --      expectPoly 4
 --        -- letrec + = (\x . (\y . (S (+ (P x) y)))) in (+ (S (S 0)) (S 0))
 --        (lets [
---          "plus" >: lambda "x" $ lambda "y" (s @@ (var "plus" @@ (p @@ var "x") @@ var "y"))]
+--          "plus">: lambda "x" $ lambda "y" (s @@ (var "plus" @@ (p @@ var "x") @@ var "y"))]
 --          $ var "plus" @@ (s @@ (s @@ int32 0)) @@ (s @@ int32 0))
 --        ["t0"] (T.function T.int32 $ T.function (T.var "t0") T.int32),
 --      expectMono 5
@@ -162,8 +133,8 @@ testGroupForLet = supergroup "Let" [
 --        --     f = (\p0. (pair (id p0) (id p0)))
 --        --     in 0
 --        (lets [
---          "id" >: lambda "z" $ var "z",
---          "f" >: lambda "p0" $ pair (var "id" @@ var "p0") (var "id" @@ var "p0")]
+--          "id">: lambda "z" $ var "z",
+--          "f">: lambda "p0" $ pair (var "id" @@ var "p0") (var "id" @@ var "p0")]
 --          $ int32 0)
 --        T.int32]]
   where
@@ -172,16 +143,16 @@ testGroupForLet = supergroup "Let" [
 
 testGroupForLiterals :: TTerm TestGroup
 testGroupForLiterals = subgroup "Literals" [
-    expectMono 1
+    expectMono 1 []
       (int32 42)
       T.int32,
-    expectMono 2
+    expectMono 2 []
       (string "foo")
       T.string,
-    expectMono 3
+    expectMono 3 []
       false
       T.boolean,
-    expectMono 4
+    expectMono 4 []
       (float64 42.0)
       T.float64]
 
@@ -189,22 +160,22 @@ testGroupForPathologicalTerms :: TTerm TestGroup
 testGroupForPathologicalTerms = supergroup "Pathological terms" [
 
     subgroup "Infinite lists" [
-      expectMono 1
+      expectMono 1 [tag_disabled]
         (lets [
           "self">: primitive _lists_cons @@ int32 42 @@ var "self"]
           $ var "self")
         (T.list T.int32),
-      expectPoly 2
+      expectPoly 2  [tag_disabled]
         (lambda "x" $ lets [
           "self">: primitive _lists_cons @@ var "x" @@ var "self"]
           $ var "self")
         ["t0"] (T.function (T.var "t0") (T.list $ T.var "t0")),
-      expectPoly 3
+      expectPoly 3  [tag_disabled]
         (lets [
           "self">: lambda "e" $ primitive _lists_cons @@ var "e" @@ (var "self" @@ var "e")]
           $ lambda "x" $ var "self" @@ var "x")
         ["t0"] (T.function T.int32 (T.list T.int32)),
-      expectMono 4
+      expectMono 4  [tag_disabled]
         (lets [
           "build">: lambda "x" $ primitive _lists_cons @@ var "x" @@ (var "build" @@
             (primitive _math_add @@ var "x" @@ int32 1))]
@@ -219,56 +190,56 @@ testGroupForPolymorphism :: TTerm TestGroup
 testGroupForPolymorphism = supergroup "Polymorphism" [
 
     subgroup "Simple lists and optionals" [
-      expectPoly 1
+      expectPoly 1 []
         (list [])
         ["t0"] (T.list (T.var "t0")),
-      expectPoly 2
+      expectPoly 2 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (optional nothing)
         ["t0"] (T.optional (T.var "t0")),
-      expectMono 3
+      expectMono 3 [tag_disabledForAlgorithmWInference, tag_disabledForAltInference]
         (optional $ just $ int32 42)
         (T.optional T.int32)],
 
     subgroup "Lambdas, lists, and products" [
-      expectPoly 1
+      expectPoly 1 []
         (lambda "x" $ var "x")
         ["t0"] (T.function (T.var "t0") (T.var "t0")),
-      expectPoly 2
+      expectPoly 2 []
         (lambda "x" $ pair (var "x") (var "x"))
         ["t0"] (T.function (T.var "t0") (T.pair (T.var "t0") (T.var "t0"))),
-      expectPoly 3
+      expectPoly 3 []
         (lambda "x" $ list [var "x"])
         ["t0"] (T.function (T.var "t0") (T.list $ T.var "t0")),
-      expectPoly 4
+      expectPoly 4 []
         (list [lambda "x" $ var "x", lambda "y" $ var "y"])
         ["t0"] (T.list (T.function (T.var "t0") (T.var "t0"))),
-      expectPoly 5
+      expectPoly 5 []
         (list [lambda "x" $ lambda "y" $ pair (var "y") (var "x")])
         ["t0", "t1"] (T.list (T.function (T.var "t0") (T.function (T.var "t1") (T.pair (T.var "t1") (T.var "t0")))))],
 
     subgroup "Lambdas and application" [
-      expectMono 1
+      expectMono 1 []
         ((lambda "x" $ var "x") @@ string "foo")
         T.string],
 
     subgroup "Primitives and application" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (primitive _lists_concat @@ list [list [int32 42], list []])
         (T.list T.int32)],
 
     subgroup "Lambdas and primitives" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (primitive _math_add)
         (T.functionN [T.int32, T.int32, T.int32]),
-      expectMono 2
+      expectMono 2 [tag_disabledForAltInference]
         (lambda "x" (primitive _math_add @@ var "x"))
         (T.functionN [T.int32, T.int32, T.int32]),
-      expectMono 3
+      expectMono 3 [tag_disabledForAltInference]
         (lambda "x" (primitive _math_add @@ var "x" @@ var "x"))
         (T.function T.int32 T.int32)],
 
     subgroup "Mixed expressions with lambdas, constants, and primitive functions" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (lambda "x" $ (primitive _math_sub @@ (primitive _math_add @@ var "x" @@ var "x") @@ int32 1))
         (T.function T.int32 T.int32)]]
 
@@ -276,37 +247,37 @@ testGroupForPrimitives :: TTerm TestGroup
 testGroupForPrimitives = supergroup "Primitives" [
 
     subgroup "Monomorphic primitive functions" [
-      expectMono 1
+      expectMono 1 [tag_disabledForAltInference]
         (primitive $ _strings_length)
         (T.function T.string T.int32),
-      expectMono 2
+      expectMono 2 [tag_disabledForAltInference]
         (primitive _math_sub)
         (T.functionN [T.int32, T.int32, T.int32])],
 
     subgroup "Polymorphic primitive functions" [
-      expectPoly 1
+      expectPoly 1 [tag_disabledForAltInference]
         (lambda "el" (primitive _lists_length @@ (list [var "el"])))
         ["t0"] (T.function (T.var "t0") T.int32),
-      expectMono 2
+      expectMono 2 [tag_disabledForAltInference]
         (lambda "el" (primitive _lists_length @@ (list [int32 42, var "el"])))
         (T.function T.int32 T.int32),
-      expectPoly 3
+      expectPoly 3 [tag_disabledForAltInference]
         (primitive _lists_concat)
         ["t0"] (T.function (T.list $ T.list $ T.var "t0") (T.list $ T.var "t0")),
-      expectPoly 4
+      expectPoly 4 [tag_disabledForAltInference]
         (lambda "lists" (primitive _lists_concat @@ var "lists"))
         ["t0"] (T.function (T.list $ T.list $ T.var "t0") (T.list $ T.var "t0")),
-      expectPoly 5
+      expectPoly 5 [tag_disabledForAltInference]
         (lambda "lists" (primitive _lists_length @@ (primitive _lists_concat @@ var "lists")))
         ["t0"] (T.function (T.list $ T.list $ T.var "t0") T.int32),
-      expectPoly 6
+      expectPoly 6 [tag_disabledForAltInference]
         (lambda "list" (primitive _lists_length @@ (primitive _lists_concat @@ list [var "list", list []])))
         ["t0"] (T.function (T.list $ T.var "t0") T.int32),
-      expectPoly 7
+      expectPoly 7 [tag_disabledForAltInference]
         (lambda "list" (primitive _math_add
           @@ int32 1
           @@ (primitive _lists_length @@ (primitive _lists_concat @@ list [var "list", list []]))))
         ["t0"] (T.function (T.list $ T.var "t0") T.int32),
-      expectPoly 8
+      expectPoly 8 [tag_disabledForAltInference]
         (lambda "lists" (primitive _lists_length @@ (primitive _lists_concat @@ var "lists")))
         ["t0"] (T.function (T.list $ T.list $ T.var "t0") T.int32)]]
