aa8259782d8c7cc61a53c9eb7537ad94323704aa
Joshua Shinavier <josh@fortytwo.net>
2025-02-28 08:38:20 -0800

    Update generated Haskell (test) for the updated naming logic

--- hydra-haskell/src/gen-test/haskell/Hydra/Test/TestGraph.hs
+++ hydra-haskell/src/gen-test/haskell/Hydra/Test/TestGraph.hs
@@ -5,10 +5,10 @@ module Hydra.Test.TestGraph where
 import qualified Hydra.Core as Core
 import qualified Hydra.Graph as Graph
 import qualified Hydra.Module as Module
-import Data.Int
-import Data.List as L
-import Data.Map as M
-import Data.Set as S
+import qualified Data.Int as I
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 testTypeLatLonName :: Core.Name
 testTypeLatLonName = (Core.Name "LatLon")
