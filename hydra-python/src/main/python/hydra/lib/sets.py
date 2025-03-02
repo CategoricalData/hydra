43135e763ae7491de5aeb31d6bbc93a95b9dd8f2
mwbrulhardt <mwbrulhardt@gmail.com>
2025-03-02 17:52:07 +0000

    Added in the FrozenDict type and the frozenlist alias.

--- hydra-python/src/main/python/hydra/lib/sets.py
+++ hydra-python/src/main/python/hydra/lib/sets.py
@@ -4,66 +4,66 @@ from collections.abc import Callable
 from typing import Any
 
 
-def contains[A](x: A, s: set[A]) -> bool:
+def empty[A]():
+    """Create an empty set."""
+    return frozenset[A]()
+
+
+def contains[A](x: A, s: frozenset[A]) -> bool:
     """Check if an element is in a set."""
     return x in s
 
 
-def difference[A](s1: set[A], s2: set[A]) -> set[A]:
+def difference[A](s1: frozenset[A], s2: frozenset[A]) -> frozenset[A]:
     """Compute the difference of two sets."""
     return s1 - s2
 
 
-def empty[A]():
-    """Create an empty set."""
-    return set[A]()
-
-
-def from_list[A](xs: list[A]) -> set[A]:
+def from_list[A](xs: list[A]) -> frozenset[A]:
     """Create a set from a list."""
-    return set(xs)
+    return frozenset(xs)
 
 
-def insert[A](x: A, s: set[A]) -> set[A]:
+def insert[A](x: A, s: frozenset[A]) -> frozenset[A]:
     """Insert an element into a set."""
     return s | {x}
 
 
-def intersection[A](s1: set[A], s2: set[A]) -> set[A]:
+def intersection[A](s1: frozenset[A], s2: frozenset[A]) -> frozenset[A]:
     """Compute the intersection of two sets."""
     return s1 & s2
 
 
-def is_empty(s: set[Any]) -> bool:
+def is_empty(s: frozenset[Any]) -> bool:
     """Check if a set is empty."""
     return len(s) == 0
 
 
-def map[A, B](f: Callable[[A], B], s: set[A]) -> set[B]:
+def map[A, B](f: Callable[[A], B], s: frozenset[A]) -> frozenset[B]:
     """Map a function over a set."""
-    return set(f(x) for x in s)
+    return frozenset(f(x) for x in s)
 
 
-def remove[A](x: A, s: set[A]) -> set[A]:
+def remove[A](x: A, s: frozenset[A]) -> frozenset[A]:
     """Remove an element from a set."""
     return s - {x}
 
 
-def singleton[A](x: A) -> set[A]:
+def singleton[A](x: A) -> frozenset[A]:
     """Create a singleton set."""
-    return {x}
+    return frozenset[A]({x})
 
 
-def size(s: set[Any]) -> int:
+def size(s: frozenset[Any]) -> int:
     """Get the size of a set."""
     return len(s)
 
 
-def to_list[A](s: set[A]) -> list[A]:
+def to_list[A](s: frozenset[A]) -> list[A]:
     """Convert a set to a list."""
     return list(s)
 
 
-def union[A](s1: set[A], s2: set[A]) -> set[A]:
+def union[A](s1: frozenset[A], s2: frozenset[A]) -> frozenset[A]:
     """Compute the union of two sets."""
     return s1 | s2
