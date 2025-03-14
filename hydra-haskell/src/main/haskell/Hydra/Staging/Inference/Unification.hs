f7ab3c0ea950814be2a3e50c29f90621cd26bb8d
Joshua Shinavier <josh@fortytwo.net>
2025-03-14 16:24:29 -0700

    Completely remove the legacy inference implementation, replacing it with 'alt inference'

--- hydra-haskell/src/main/haskell/Hydra/Staging/Inference/Unification.hs
+++ hydra-haskell/src/main/haskell/Hydra/Staging/Inference/Unification.hs
@@ -1,111 +1,141 @@
--- | Hindley-Milner style type unification
+-- | Type unification using an adaptation Robinson's algorithm
 
-module Hydra.Staging.Inference.Unification (
-  solveConstraints
-) where
+module Hydra.Staging.Inference.Unification where
 
-import Hydra.Variants
-import Hydra.Strip
+import Hydra.Coders
 import Hydra.Compute
 import Hydra.Core
-import Hydra.Staging.Lexical
-import Hydra.Mantle
-import Hydra.Printing
-import Hydra.Staging.Rewriting
-import Hydra.Staging.Inference.Substitution
-import Hydra.Rewriting
-import Hydra.Dsl.Types as Types
+import Hydra.Lib.Flows as Flows
 import Hydra.Lib.Io
+import Hydra.Inference
+import Hydra.Rewriting
+import Hydra.Strip
+import Hydra.Staging.Inference.Substitution
 
-import qualified Data.List as L
-import qualified Data.Map as M
-import qualified Data.Set as S
+import qualified Data.List     as L
+import qualified Data.Map      as M
+import qualified Data.Maybe    as Y
 
 
--- Note: type variables in Hydra are allowed to bind to type expressions which contain the variable;
---       i.e. type recursion by name is allowed.
-bind :: Name -> Type -> Flow s Subst
-bind name typ = do
-  if typ == TypeVariable name
-  then return M.empty
-  else if variableOccursInType name typ
---     then fail $ "infinite type for " ++ unName name ++ ": " ++ show typ
-    then return M.empty
-    else return $ M.singleton name typ
-
-solveConstraints :: [TypeConstraint] -> Flow s Subst
-solveConstraints cs = unificationSolver M.empty cs
-
-unificationSolver :: Subst -> [TypeConstraint] -> Flow s Subst
-unificationSolver su cs = case cs of
-  [] -> return su
-  ((TypeConstraint t1 t2 _):rest) -> do
-    su1  <- unify t1 t2
-    unificationSolver
-      (composeSubst su1 su)
-      ((\(TypeConstraint t1 t2 ctx) -> (TypeConstraint (substituteInType su1 t1) (substituteInType su1 t2) ctx)) <$> rest)
-
-unify :: Type -> Type -> Flow s Subst
-unify ltyp rtyp = do
---     withTrace ("unify " ++ show ltyp ++ " with " ++ show rtyp) $
-     case (stripType ltyp, stripType rtyp) of
-       -- Symmetric patterns
-      (TypeApplication (ApplicationType lhs1 rhs1), TypeApplication (ApplicationType lhs2 rhs2)) ->
-        unifyMany [lhs1, rhs1] [lhs2, rhs2]
-      (TypeFunction (FunctionType dom1 cod1), TypeFunction (FunctionType dom2 cod2)) ->
-        unifyMany [dom1, cod1] [dom2, cod2]
-      (TypeList lt1, TypeList lt2) -> unify lt1 lt2
-      (TypeLiteral lt1, TypeLiteral lt2) -> verify "different literal types" $ lt1 == lt2
-      (TypeMap (MapType k1 v1), TypeMap (MapType k2 v2)) -> unifyMany [k1, v1] [k2, v2]
-      (TypeOptional ot1, TypeOptional ot2) -> unify ot1 ot2
-      (TypeProduct types1, TypeProduct types2) -> unifyMany types1 types2
-      (TypeRecord rt1, TypeRecord rt2) -> do
-        verify "different record type names" (rowTypeTypeName rt1 == rowTypeTypeName rt2)
-        verify "different number of record fields" (L.length (rowTypeFields rt1) == L.length (rowTypeFields rt2))
-        unifyMany (fieldTypeType <$> rowTypeFields rt1) (fieldTypeType <$> rowTypeFields rt2)
-      (TypeSet st1, TypeSet st2) -> unify st1 st2
-      (TypeUnion rt1, TypeUnion rt2) -> verify "different union type names" (rowTypeTypeName rt1 == rowTypeTypeName rt2)
-      (TypeLambda (LambdaType (Name v1) body1), TypeLambda (LambdaType (Name v2) body2)) ->
-        unifyMany [Types.var v1, body1] [Types.var v2, body2]
-      (TypeSum types1, TypeSum types2) -> unifyMany types1 types2
-      (TypeWrap n1, TypeWrap n2) -> verify "different wrapper type names" $ n1 == n2
-
-      -- Asymmetric patterns
-      (TypeVariable v1, TypeVariable v2) -> bindWeakest v1 v2
-      (TypeVariable v, t2) -> bind v t2
-      (t1, TypeVariable v) -> bind v t1
-
-      -- TODO; temporary "slop", e.g. (record "RowType" ...) is allowed to unify with (wrap "RowType" @ "a")
-      (TypeApplication (ApplicationType lhs rhs), t2) -> unify lhs t2
-      (t1, TypeApplication (ApplicationType lhs rhs)) -> unify t1 lhs
-      (TypeLambda (LambdaType _ body), t2) -> unify body t2
-      (t1, TypeLambda (LambdaType _ body)) -> unify t1 body
-      -- TODO; temporary "slop", e.g. (record "RowType" ...) is allowed to unify with (wrap "RowType")
-      (TypeWrap _, _) -> return M.empty -- TODO
-      (_, TypeWrap name) -> return M.empty -- TODO
-
-      (l, r) -> fail $ "unification of " ++ show (typeVariant l) ++ " with " ++ show (typeVariant r) ++
-        ":\n  " ++ showType l ++
-        "\n  " ++ showType r
-  where
-    verify reason b = if b then return M.empty else failUnification reason
-    failUnification reason = fail $ "could not unify types (reason: " ++ reason ++ "):\n\t"
-      ++ showType (stripType ltyp) ++ "\n\t"
-      ++ showType (stripType rtyp) ++ "\n"
---     failUnification = fail $ "could not unify type " ++ describeType (stripType ltyp) ++ " with " ++ describeType (stripType rtyp)
-    bindWeakest v1 v2 = if isWeak v1
-        then bind v1 (TypeVariable v2)
-        else bind v2 (TypeVariable v1)
-      where
-        isWeak v = L.head (unName v) == 't' -- TODO: use a convention like _xxx for temporarily variables, then normalize and replace them
-
-unifyMany :: [Type] -> [Type] -> Flow s Subst
-unifyMany [] [] = return M.empty
-unifyMany (t1 : ts1) (t2 : ts2) =
-  do su1 <- unify t1 t2
-     su2 <- unifyMany (substituteInType su1 <$> ts1) (substituteInType su1 <$> ts2)
-     return (composeSubst su2 su1)
-unifyMany t1 t2 = fail $ "unification mismatch between " ++ show t1 ++ " and " ++ show t2
-
+-- | Determine whether a type variable appears within a type expression.
+--   No distinction is made between free and bound type variables.
 variableOccursInType :: Name -> Type -> Bool
-variableOccursInType a t = S.member a $ freeVariablesInType t
+variableOccursInType var = foldOverType TraversalOrderPre tryType False
+  where
+    tryType b typ = case typ of
+      TypeVariable v -> b || v == var
+      _ -> b
+
+{-
+Robinson's algorithm, following https://www.cs.cornell.edu/courses/cs6110/2017sp/lectures/lec23.pdf
+Specifically this is an implementation of the following rules:
+ * Unify({(x, t)} ∪ E) = {t/x} Unify(E{t/x}) if x ∉ FV(t)
+ * Unify(∅) = I (the identity substitution x ↦ x)
+ * Unify({(x, x)} ∪ E) = Unify(E)
+ * Unify({(f(s1, ..., sn), f(t1, ..., tn))} ∪ E) = Unify({(s1, t1), ..., (sn, tn)} ∪ E)
+-}
+unifyTypeConstraints :: M.Map Name TypeScheme -> [TypeConstraint] -> Flow s TypeSubst
+unifyTypeConstraints schemaTypes constraints = case constraints of
+  [] -> Flows.pure emptyTypeSubst
+  ((TypeConstraint left right comment):rest) -> do
+      result <- case sleft of
+       TypeVariable name -> case sright of
+           TypeVariable name2 -> if name == name2
+             then unifyTypeConstraints schemaTypes rest
+             -- Avoid replacing schema type references with temporary type variables.
+             else if Y.isJust (M.lookup name schemaTypes)
+             then if Y.isJust (M.lookup name2 schemaTypes)
+               then Flows.fail $ "attempted to unify schema names " ++ unName name ++ " and " ++ unName name2
+               else bind name2 sleft
+             else bind name sright
+           _ -> tryBinding name sright
+       _ -> case sright of
+         TypeVariable name -> tryBinding name sleft
+         _ -> do
+           constraints2 <- joinTypes sleft sright comment
+           unifyTypeConstraints schemaTypes $ constraints2 ++ rest
+      return result
+    where
+      sleft = stripType left
+      sright = stripType right
+      -- TODO: this occurrence check is expensive; consider delaying it until the time of substitution
+      tryBinding v t = if variableOccursInType v t
+        then Flows.fail $ "Variable " ++ unName v ++ " appears free in type " ++ showType t
+          ++ " (" ++ comment ++ ")"
+        else bind v t
+      bind v t = composeTypeSubst subst <$> unifyTypeConstraints schemaTypes (substituteInConstraints subst rest)
+        where
+          subst = singletonTypeSubst v t
+
+unifyTypeLists :: M.Map Name TypeScheme -> [Type] -> [Type] -> String -> Flow s TypeSubst
+unifyTypeLists schemaTypes l r comment = unifyTypeConstraints schemaTypes $ L.zipWith toConstraint l r
+  where
+    toConstraint l r = TypeConstraint l r comment
+
+unifyTypes :: M.Map Name TypeScheme -> Type -> Type -> String -> Flow s TypeSubst
+unifyTypes schemaTypes l r comment = unifyTypeConstraints schemaTypes [TypeConstraint l r comment]
+
+joinTypes :: Type -> Type -> String -> Flow s [TypeConstraint]
+joinTypes left right comment = case sleft of
+    TypeApplication (ApplicationType lhs1 rhs1) -> case sright of
+      TypeApplication (ApplicationType lhs2 rhs2) -> Flows.pure [
+        joinOne lhs1 lhs2,
+        joinOne rhs1 rhs2]
+      _ -> cannotUnify
+    TypeFunction (FunctionType domleft codleft) -> case sright of
+      TypeFunction (FunctionType domright codright) -> Flows.pure [
+        joinOne domleft domright,
+        joinOne codleft codright]
+      _ -> cannotUnify
+    TypeList eleft -> case sright of
+      TypeList eright -> Flows.pure [joinOne eleft eright]
+      _ -> cannotUnify
+    TypeLiteral ltleft -> assertEqual
+    TypeMap (MapType kleft vleft) -> case sright of
+      TypeMap (MapType kright vright) -> Flows.pure [
+        joinOne kleft kright,
+        joinOne vleft vright]
+      _ -> cannotUnify
+    TypeOptional eleft -> case sright of
+      TypeOptional eright -> Flows.pure [joinOne eleft eright]
+      _ -> cannotUnify
+    TypeProduct lefts -> case sright of
+      TypeProduct rights -> joinList lefts rights
+      _ -> cannotUnify
+    TypeRecord rtleft -> case sright of
+      TypeRecord rtright -> joinRowTypes rtleft rtright
+      _ -> cannotUnify
+    TypeSet eleft -> case sright of
+      TypeSet eright -> Flows.pure [joinOne eleft eright]
+      _ -> cannotUnify
+    TypeSum lefts -> case sright of
+      TypeSum rights -> joinList lefts rights
+      _ -> cannotUnify
+    TypeUnion rtleft -> case sright of
+      TypeUnion rtright -> joinRowTypes rtleft rtright
+      _ -> cannotUnify
+    TypeWrap (WrappedType nameLeft eleft) -> case sright of
+      TypeWrap (WrappedType nameRight eright) -> if nameLeft /= nameRight
+        then cannotUnify
+        else Flows.pure [joinOne eleft eright]
+      _ -> cannotUnify
+    -- TypeAnnotated, TypeLambda, TypeVariable should not appear here
+    _ -> cannotUnify
+  where
+    sleft = stripType left
+    sright = stripType right
+    joinOne l r = TypeConstraint l r $ "join types; " ++ comment
+    cannotUnify = Flows.fail $ "Cannot unify " ++ showType sleft ++ " with " ++ showType sright
+    assertEqual = if sleft == sright
+      then Flows.pure []
+      else cannotUnify
+    joinList lefts rights = if L.length lefts == L.length rights
+      then Flows.pure $ L.zipWith joinOne lefts rights
+      else cannotUnify
+    fieldNames = fmap fieldTypeName
+    fieldTypes = fmap fieldTypeType
+    joinRowTypes (RowType tnameLeft fieldsLeft) (RowType tnameRight fieldsRight) = if tnameLeft /= tnameRight
+      then cannotUnify
+      else if (fieldNames fieldsLeft) /= (fieldNames fieldsRight)
+      then cannotUnify
+      else joinList (fieldTypes fieldsLeft) (fieldTypes fieldsRight)
