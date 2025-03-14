56e0b21e13ca0c2f249dc75a2f4d0712da8d5f34
Joshua Shinavier <josh@fortytwo.net>
2025-03-14 05:16:16 -0700

    Add some new utilities to hydra.sorting

--- hydra-haskell/src/main/haskell/Hydra/Staging/Sorting.hs
+++ hydra-haskell/src/main/haskell/Hydra/Staging/Sorting.hs
@@ -1,19 +1,38 @@
 -- | Utilities for sorting
 
 module Hydra.Staging.Sorting (
+  OrderingIsomorphism(..),
+  createOrderingIsomorphism,
   topologicalSort,
   topologicalSortComponents,
   initGraph
 ) where
 
-import qualified Data.List as L
-import qualified Data.Bifunctor as BF
-
 import qualified Data.Graph as G
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Maybe as Y
 
+
+-- Note: useful, but currently unused
+data OrderingIsomorphism a = OrderingIsomorphism {
+  orderingIsomorphismEncode :: [a] -> [a],
+  orderingIsomorphismDecode :: [a] -> [a]}
+
+-- Note: useful, but currently unused
+createOrderingIsomorphism :: Ord a => [a] -> [a] -> OrderingIsomorphism b
+createOrderingIsomorphism sourceOrd targetOrd = OrderingIsomorphism sourceToTargetMapping targetToSourceMapping
+  where
+    sourceToTargetMapping els = Y.catMaybes $ fmap (\n -> M.lookup n mp) targetOrd
+      where
+        mp = M.fromList $ L.zip sourceOrd els
+    targetToSourceMapping els = Y.catMaybes $ fmap (\n -> M.lookup n mp) sourceOrd
+      where
+        mp = M.fromList $ L.zip targetOrd els
 
 -- | Sort a directed acyclic graph (DAG) based on an adjacency list
---   Yields a list of nontrivial strongly connected components if the graph has cycles
+--   Yields a list of nontrivial strongly connected components if the graph has cycles, otherwise a simple list.
 topologicalSort :: Ord a => [(a, [a])] -> Either [[a]] [a]
 topologicalSort pairs = if L.null withCycles
     then Right $ L.concat sccs
