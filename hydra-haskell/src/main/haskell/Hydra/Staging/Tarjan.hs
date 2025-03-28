0efb1a240d1e2b77ab53f1a9017d7d74aa59fbd2
Joshua Shinavier <josh@fortytwo.net>
2025-03-28 08:50:32 -0700

    Use only Hydra-friendly collection types in Tarjan's algo, rather than Haskell-specific Data.IntMap and Data.IntSet

--- hydra-haskell/src/main/haskell/Hydra/Staging/Tarjan.hs
+++ hydra-haskell/src/main/haskell/Hydra/Staging/Tarjan.hs
@@ -8,25 +8,23 @@ module Hydra.Staging.Tarjan (
   stronglyConnectedComponents
 ) where
 
-import qualified Data.IntMap.Strict as IMS
-import qualified Data.IntSet as DS
+import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Control.Monad.State.Strict as CMS
 import qualified Control.Monad as CM
-import qualified Data.List            as L
-import qualified Data.Map.Strict      as MS
-import qualified Data.Maybe           as Y
-
+import qualified Data.List as L
+import qualified Data.Maybe as Y
 
 type Vertex = Int
-type Graph = IMS.IntMap [Vertex]
+type Graph  = M.Map Vertex [Vertex]
 
 data TarjanState = TarjanState
-  { indexCounter :: !Int           -- next available index
-  , indices      :: IMS.IntMap Int -- vertex -> index
-  , lowlinks     :: IMS.IntMap Int -- vertex -> lowest index reachable
-  , stack        :: [Vertex]       -- current DFS stack (vertices in reverse order)
-  , onStack      :: DS.IntSet      -- quick lookup for vertices on the stack
-  , sccs         :: [[Vertex]]     -- accumulated strongly connected components
+  { indexCounter :: !Int            -- next available index
+  , indices      :: M.Map Vertex Int -- vertex -> index
+  , lowlinks     :: M.Map Vertex Int -- vertex -> lowest index reachable
+  , stack        :: [Vertex]         -- current DFS stack (vertices in reverse order)
+  , onStack      :: S.Set Vertex    -- quick lookup for vertices on the stack
+  , sccs         :: [[Vertex]]       -- accumulated strongly connected components
   }
 
 -- | Given a list of adjacency lists represented as (key, [key]) pairs,
@@ -39,27 +37,27 @@ adjacencyListsToGraph edges0 = (graph, vertexToKey)
   where
     sortedEdges = L.sortBy (\(k1, _) (k2, _) -> compare k1 k2) edges0
     indexedEdges = zip [0..] sortedEdges
-    keyToVertex = MS.fromList [ (k, v) | (v, (k, _)) <- indexedEdges ]
-    vertexMap = IMS.fromList [ (v, k) | (v, (k, _)) <- indexedEdges ]
-    graph = IMS.fromList
-              [ (v, Y.mapMaybe (\k -> MS.lookup k keyToVertex) neighbors)
-              | (v, (_, neighbors)) <- indexedEdges ]
-    vertexToKey v = vertexMap IMS.! v
+    keyToVertex = M.fromList [ (k, v) | (v, (k, _)) <- indexedEdges ]
+    vertexMap   = M.fromList [ (v, k) | (v, (k, _)) <- indexedEdges ]
+    graph       = M.fromList
+                    [ (v, Y.mapMaybe (\k -> M.lookup k keyToVertex) neighbors)
+                    | (v, (_, neighbors)) <- indexedEdges ]
+    vertexToKey v = vertexMap M.! v
 
 initialState :: TarjanState
-initialState = TarjanState 0 IMS.empty IMS.empty [] DS.empty []
+initialState = TarjanState 0 M.empty M.empty [] S.empty []
 
 -- | Pop vertices off the stack until the given vertex is reached,
--- collecting the current strongly connected component.
+--   collecting the current strongly connected component.
 popStackUntil :: Vertex -> CMS.State TarjanState [Vertex]
 popStackUntil v = go []
   where
     go acc = do
       st <- CMS.get
       case stack st of
-        [] -> error "popStackUntil: empty stack"
+        []     -> error "popStackUntil: empty stack"
         (x:xs) -> do
-          CMS.modify $ \s -> s { stack = xs, onStack = DS.delete x (onStack s) }
+          CMS.modify $ \s -> s { stack = xs, onStack = S.delete x (onStack s) }
           let acc' = x : acc
           if x == v
             then return (reverse acc')
@@ -71,41 +69,41 @@ strongConnect graph v = do
   st <- CMS.get
   let !i = indexCounter st
   CMS.modify $ \s -> s { indexCounter = i + 1
-                   , indices      = IMS.insert v i (indices s)
-                   , lowlinks     = IMS.insert v i (lowlinks s)
-                   , stack        = v : stack s
-                   , onStack      = DS.insert v (onStack s)
-                   }
-  let neighbors = IMS.findWithDefault [] v graph
+                       , indices      = M.insert v i (indices s)
+                       , lowlinks     = M.insert v i (lowlinks s)
+                       , stack        = v : stack s
+                       , onStack      = S.insert v (onStack s)
+                       }
+  let neighbors = M.findWithDefault [] v graph
   CM.forM_ neighbors $ \w -> do
     st' <- CMS.get
-    if not (IMS.member w (indices st'))
+    if not (M.member w (indices st'))
       then do
          strongConnect graph w
          stAfter <- CMS.get
-         let low_v = IMS.findWithDefault maxBound v (lowlinks stAfter)
-             low_w = IMS.findWithDefault maxBound w (lowlinks stAfter)
-         CMS.modify $ \s -> s { lowlinks = IMS.insert v (min low_v low_w) (lowlinks s) }
-      else if DS.member w (onStack st')
+         let low_v = M.findWithDefault maxBound v (lowlinks stAfter)
+             low_w = M.findWithDefault maxBound w (lowlinks stAfter)
+         CMS.modify $ \s -> s { lowlinks = M.insert v (min low_v low_w) (lowlinks s) }
+      else if S.member w (onStack st')
         then do
-         let low_v = IMS.findWithDefault maxBound v (lowlinks st')
-             idx_w = IMS.findWithDefault maxBound w (indices st')
-         CMS.modify $ \s -> s { lowlinks = IMS.insert v (min low_v idx_w) (lowlinks s) }
+         let low_v = M.findWithDefault maxBound v (lowlinks st')
+             idx_w = M.findWithDefault maxBound w (indices st')
+         CMS.modify $ \s -> s { lowlinks = M.insert v (min low_v idx_w) (lowlinks s) }
         else return ()
   stFinal <- CMS.get
-  let low_v = IMS.findWithDefault maxBound v (lowlinks stFinal)
-      idx_v = IMS.findWithDefault maxBound v (indices stFinal)
+  let low_v = M.findWithDefault maxBound v (lowlinks stFinal)
+      idx_v = M.findWithDefault maxBound v (indices stFinal)
   CM.when (low_v == idx_v) $ do
     comp <- popStackUntil v
     CMS.modify $ \s -> s { sccs = comp : sccs s }
 
 -- | Compute the strongly connected components of the given graph.
--- The components are returned in reverse topological order.
+--   The components are returned in reverse topological order.
 stronglyConnectedComponents :: Graph -> [[Vertex]]
 stronglyConnectedComponents graph = reverse $ fmap L.sort $ sccs finalState
   where
-    verts = IMS.keys graph
+    verts = M.keys graph
     finalState = CMS.execState (mapM_ (\v -> do
-                      visited <- CMS.gets (IMS.member v . indices)
+                      visited <- CMS.gets (M.member v . indices)
                       if not visited then strongConnect graph v else return ()
                     ) verts) initialState
