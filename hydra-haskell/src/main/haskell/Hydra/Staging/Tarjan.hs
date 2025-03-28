02e7c510146dfaa8c24f90a91baa2a78137c9312
Joshua Shinavier <josh@fortytwo.net>
2025-03-28 12:22:37 -0700

    Use the Flow monad in Tarjan's algo rather than Control.Monad.State. hydra.sorting is ready to be promoted. Resolves #167.

--- hydra-haskell/src/main/haskell/Hydra/Staging/Tarjan.hs
+++ hydra-haskell/src/main/haskell/Hydra/Staging/Tarjan.hs
@@ -8,9 +8,12 @@ module Hydra.Staging.Tarjan (
   stronglyConnectedComponents
 ) where
 
+import Hydra.Compute
+import Hydra.Flows
+import Hydra.Errors
+
 import qualified Data.Map as M
 import qualified Data.Set as S
-import qualified Control.Monad.State.Strict as CMS
 import qualified Control.Monad as CM
 import qualified Data.List as L
 import qualified Data.Maybe as Y
@@ -28,7 +31,7 @@ data TarjanState = TarjanState
   }
 
 -- | Given a list of adjacency lists represented as (key, [key]) pairs,
---   constructs a graph along with a function mapping each vertex (an Int)
+--   construct a graph along with a function mapping each vertex (an Int)
 --   back to its original key.
 adjacencyListsToGraph :: Ord key
   => [(key, [key])]
@@ -49,26 +52,26 @@ initialState = TarjanState 0 M.empty M.empty [] S.empty []
 
 -- | Pop vertices off the stack until the given vertex is reached,
 --   collecting the current strongly connected component.
-popStackUntil :: Vertex -> CMS.State TarjanState [Vertex]
+popStackUntil :: Vertex -> Flow TarjanState [Vertex]
 popStackUntil v = go []
   where
     go acc = do
-      st <- CMS.get
+      st <- getState
       case stack st of
         []     -> error "popStackUntil: empty stack"
         (x:xs) -> do
-          CMS.modify $ \s -> s { stack = xs, onStack = S.delete x (onStack s) }
+          modify $ \s -> s { stack = xs, onStack = S.delete x (onStack s) }
           let acc' = x : acc
           if x == v
             then return (reverse acc')
             else go acc'
 
 -- | Visit a vertex and recursively explore its successors.
-strongConnect :: Graph -> Vertex -> CMS.State TarjanState ()
+strongConnect :: Graph -> Vertex -> Flow TarjanState ()
 strongConnect graph v = do
-  st <- CMS.get
+  st <- getState
   let !i = indexCounter st
-  CMS.modify $ \s -> s { indexCounter = i + 1
+  modify $ \s -> s { indexCounter = i + 1
                        , indices      = M.insert v i (indices s)
                        , lowlinks     = M.insert v i (lowlinks s)
                        , stack        = v : stack s
@@ -76,26 +79,26 @@ strongConnect graph v = do
                        }
   let neighbors = M.findWithDefault [] v graph
   CM.forM_ neighbors $ \w -> do
-    st' <- CMS.get
+    st' <- getState
     if not (M.member w (indices st'))
       then do
          strongConnect graph w
-         stAfter <- CMS.get
+         stAfter <- getState
          let low_v = M.findWithDefault maxBound v (lowlinks stAfter)
              low_w = M.findWithDefault maxBound w (lowlinks stAfter)
-         CMS.modify $ \s -> s { lowlinks = M.insert v (min low_v low_w) (lowlinks s) }
+         modify $ \s -> s { lowlinks = M.insert v (min low_v low_w) (lowlinks s) }
       else if S.member w (onStack st')
         then do
          let low_v = M.findWithDefault maxBound v (lowlinks st')
              idx_w = M.findWithDefault maxBound w (indices st')
-         CMS.modify $ \s -> s { lowlinks = M.insert v (min low_v idx_w) (lowlinks s) }
+         modify $ \s -> s { lowlinks = M.insert v (min low_v idx_w) (lowlinks s) }
         else return ()
-  stFinal <- CMS.get
+  stFinal <- getState
   let low_v = M.findWithDefault maxBound v (lowlinks stFinal)
       idx_v = M.findWithDefault maxBound v (indices stFinal)
   CM.when (low_v == idx_v) $ do
     comp <- popStackUntil v
-    CMS.modify $ \s -> s { sccs = comp : sccs s }
+    modify $ \s -> s { sccs = comp : sccs s }
 
 -- | Compute the strongly connected components of the given graph.
 --   The components are returned in reverse topological order.
@@ -103,7 +106,15 @@ stronglyConnectedComponents :: Graph -> [[Vertex]]
 stronglyConnectedComponents graph = reverse $ fmap L.sort $ sccs finalState
   where
     verts = M.keys graph
-    finalState = CMS.execState (mapM_ (\v -> do
-                      visited <- CMS.gets (M.member v . indices)
+    finalState = exec (mapM_ (\v -> do
+                      visited <- (M.member v . indices) <$> getState
                       if not visited then strongConnect graph v else return ()
                     ) verts) initialState
+
+-- TODO: add to hydra.flows
+
+exec :: Flow s a -> s -> s
+exec f s0 = flowStateState $ unFlow f s0 emptyTrace
+
+modify :: (s -> s) -> Flow s ()
+modify f = (f <$> getState) >>= putState
