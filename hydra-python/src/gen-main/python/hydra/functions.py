50bcc97ec48ce0582b7c7152e440d7c8bc97d278
Joshua Shinavier <josh@fortytwo.net>
2025-03-29 10:35:38 -0700

    Manually correct some issues in generated Python sources. Coder enhancements needed.

--- hydra-python/src/gen-main/python/hydra/functions.py
+++ hydra-python/src/gen-main/python/hydra/functions.py
@@ -1,5 +1,9 @@
 """Placeholder for utilities dealing with Hydra functions."""
 
 from __future__ import annotations
+from typing import TypeVar
 
-id = lambda any_: any_
+T0 = TypeVar("T0")
+
+def id(any_: T0) -> T0:
+  return any_
