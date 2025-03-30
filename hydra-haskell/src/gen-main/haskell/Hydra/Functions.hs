bc20dfed2144aed77aca8e8484b715cd94206e01
Joshua Shinavier <josh@fortytwo.net>
2025-03-30 04:22:47 -0700

    Promote the optionalToList helper function.

--- hydra-haskell/src/gen-main/haskell/Hydra/Functions.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Functions.hs
@@ -1,7 +1,9 @@
--- | Placeholder for utilities dealing with Hydra functions.
+-- | Various general-purpose helper functions.
 
 module Hydra.Functions where
 
+import qualified Hydra.Lib.Lists as Lists
+import qualified Hydra.Lib.Optionals as Optionals
 import qualified Data.Int as I
 import qualified Data.List as L
 import qualified Data.Map as M
@@ -9,3 +11,6 @@ import qualified Data.Set as S
 
 id_ :: (t0 -> t0)
 id_ any_ = any_
+
+optionalToList :: (Maybe t0 -> [t0])
+optionalToList mx = (Optionals.maybe [] Lists.pure mx)
