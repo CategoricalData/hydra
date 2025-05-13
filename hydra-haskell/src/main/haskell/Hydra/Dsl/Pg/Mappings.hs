91c6ed7e5eebf42b26e264a9ab6da03433a110bc
Joshua Shinavier <josh@fortytwo.net>
2025-05-13 11:22:46 -0700

    Add a DSL for property graph mappings (in addition to the DSL for property graph schemas).

---
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Pg/Mappings.hs
@@ -0,0 +1,35 @@
+module Hydra.Dsl.Pg.Mappings where
+
+import Hydra.Core
+import Hydra.Phantoms
+import Hydra.Pg.Model
+import Hydra.Dsl.Base
+
+import qualified Data.Map as M
+
+
+-- | A graph which does not assume that vertex or edge ids are unique.
+--   This is useful in mappings because the id specifications for vertices and/or edges may be non-unique.
+data LazyGraph v = LazyGraph
+  { lazyGraphVertices :: [Vertex v]
+  , lazyGraphEdges :: [Edge v]}
+
+columnValue :: String -> String -> TTerm (a -> b)
+columnValue sourceTable sourceColumn = project (Name sourceTable) (Name sourceColumn)
+
+edge :: String -> TTerm e -> TTerm vo -> TTerm vi -> [Property Term] -> Edge Term
+edge label (TTerm idValue) (TTerm outValue) (TTerm inValue) propertiesList = Edge (EdgeLabel label) idValue outValue inValue $
+  M.fromList $ fmap (\(Property k v) -> (k, v)) propertiesList
+
+graph :: [Vertex v] -> [Edge v] -> LazyGraph v
+graph = LazyGraph
+
+property :: String -> TTerm v -> Property Term
+property key (TTerm value) = Property (PropertyKey key) value
+
+simpleEdge :: String -> TTerm vo -> TTerm vi -> [Property Term] -> Edge Term
+simpleEdge label = edge label unit
+
+vertex :: String -> TTerm v -> [Property Term] -> Vertex Term
+vertex label (TTerm idValue) propertiesList = Vertex (VertexLabel label) idValue $
+  M.fromList $ fmap (\(Property k v) -> (k, v)) propertiesList
--- hydra-haskell/src/main/haskell/Hydra/Dsl/Pg/Schemas.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Pg/Schemas.hs
@@ -1,6 +1,7 @@
-module Hydra.Dsl.Pg where
+module Hydra.Dsl.Pg.Schemas where
 
 import Hydra.Pg.Model
+import Hydra.Core
 import qualified Hydra.Dsl.Types as Types
 
 import qualified Data.Map as M
@@ -10,8 +11,13 @@ edge :: String -> v -> v -> v -> [Property v] -> Edge v
 edge label idValue outValue inValue propertiesList = Edge (EdgeLabel label) idValue outValue inValue $
   M.fromList $ fmap (\(Property k v) -> (k, v)) propertiesList
 
-edgeType :: String -> t -> VertexLabel -> VertexLabel -> [PropertyType t] -> EdgeType t
-edgeType label id outLabel inLabel properties = EdgeType (EdgeLabel label) id outLabel inLabel properties
+edgeType :: String -> t -> String -> String -> [PropertyType t] -> EdgeType t
+edgeType label id outLabel inLabel properties = EdgeType (EdgeLabel label) id (VertexLabel outLabel) (VertexLabel inLabel) properties
+
+graph :: Ord v => [Vertex v] -> [Edge v] -> Graph v
+graph vertices edges = Graph
+  (M.fromList $ fmap (\v -> (vertexId v, v)) vertices)
+  (M.fromList $ fmap (\e -> (edgeId e, e)) edges)
 
 property :: String -> v -> Property v
 property key value = Property (PropertyKey key) value
@@ -21,6 +27,14 @@ propertyType key typ = PropertyType (PropertyKey key) typ False
 
 required :: PropertyType t -> PropertyType t
 required (PropertyType key typ _) = PropertyType key typ True
+
+schema :: [VertexType t] -> [EdgeType t] -> GraphSchema t
+schema vtypes etypes = GraphSchema
+  (M.fromList $ fmap (\vt -> (vertexTypeLabel vt, vt)) vtypes)
+  (M.fromList $ fmap (\et -> (edgeTypeLabel et, et)) etypes)
+
+simpleEdgeType :: String -> String -> String -> [PropertyType Type] -> EdgeType Type
+simpleEdgeType label = edgeType label Types.unit
 
 vertex :: String -> v -> [Property v] -> Vertex v
 vertex label idValue propertiesList = Vertex (VertexLabel label) idValue $
