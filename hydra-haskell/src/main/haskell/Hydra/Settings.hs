b9ffd53dfe49b54f99b5911a92c338b973ef0746
Joshua Shinavier <josh@fortytwo.net>
2025-06-20 05:13:20 -0700

    Add the Hydra.Settings module in Haskell

---
+++ hydra-haskell/src/main/haskell/Hydra/Settings.hs
@@ -0,0 +1,5 @@
+-- | The one non-generated Hydra kernel module. It contains constants which are set individually in each Hydra implementation.
+
+module Hydra.Settings where
+
+hydraDebug = True :: Bool
