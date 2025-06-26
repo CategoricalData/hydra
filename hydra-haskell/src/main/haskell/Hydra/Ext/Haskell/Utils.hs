72b52ab573c34c0e91b224431ef080d60c3fe64b
Joshua Shinavier <josh@fortytwo.net>
2025-06-25 18:38:55 -0700

    Prepare the Haskell coder for promotion (toward #179)

--- hydra-haskell/src/main/haskell/Hydra/Ext/Haskell/Utils.hs
+++ hydra-haskell/src/main/haskell/Hydra/Ext/Haskell/Utils.hs
@@ -1,22 +1,29 @@
 module Hydra.Ext.Haskell.Utils where
 
-import Hydra.Kernel
-import Hydra.Adapters
-import Hydra.Ext.Haskell.Language
+import qualified Hydra.Compute as Compute
+import qualified Hydra.Core as Core
 import qualified Hydra.Ext.Haskell.Ast as H
+import qualified Hydra.Ext.Haskell.Language as HaskellLanguage
+import qualified Hydra.Formatting as Formatting
+import qualified Hydra.Graph as Graph
 import qualified Hydra.Lib.Strings as Strings
+import qualified Hydra.Module as Module
+import qualified Hydra.Qnames as Qnames
+import qualified Hydra.Schemas as Schemas
+import qualified Hydra.Strip as Strip
 
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
 
 
-type HaskellNamespaces = Namespaces H.ModuleName
+type HaskellNamespaces = Module.Namespaces H.ModuleName
 
+applicationPattern :: H.Name -> [H.Pattern] -> H.Pattern
 applicationPattern name args = H.PatternApplication $ H.ApplicationPattern name args
 
-elementReference :: HaskellNamespaces -> Name -> H.Name
-elementReference (Namespaces (gname, H.ModuleName gmod) namespaces) name = case (qualifiedNameNamespace qname) of
+elementReference :: HaskellNamespaces -> Core.Name -> H.Name
+elementReference (Module.Namespaces (gname, H.ModuleName gmod) namespaces) name = case (Module.qualifiedNameNamespace qname) of
     Nothing -> simpleName local
     Just ns -> case M.lookup ns namespaces of
       Nothing -> simpleName local
@@ -24,10 +31,10 @@ elementReference (Namespaces (gname, H.ModuleName gmod) namespaces) name = case 
          then simpleName escLocal
          else rawName $ a ++ "." ++ sanitizeHaskellName local
   where
-    qname = qualifyName name
-    local = qualifiedNameLocal qname
+    qname = Qnames.qualifyName name
+    local = Module.qualifiedNameLocal qname
     escLocal = sanitizeHaskellName local
-    simple = simpleName $ qualifiedNameLocal qname
+    simple = simpleName $ Module.qualifiedNameLocal qname
 
 hsapp :: H.Expression -> H.Expression -> H.Expression
 hsapp l r = H.ExpressionApplication $ H.ApplicationExpression l r
@@ -41,33 +48,33 @@ hslit = H.ExpressionLiteral
 hsvar :: String -> H.Expression
 hsvar s = H.ExpressionVariable $ rawName s
 
-namespacesForModule :: Module -> Flow Graph HaskellNamespaces
+namespacesForModule :: Module.Module -> Compute.Flow Graph.Graph HaskellNamespaces
 namespacesForModule mod = do
-    nss <- moduleDependencyNamespaces True True True True mod
-    return $ Namespaces focusPair $ fst $ L.foldl addPair (M.empty, S.empty) (toPair <$> S.toList nss)
+    nss <- Schemas.moduleDependencyNamespaces True True True True mod
+    return $ Module.Namespaces focusPair $ fst $ L.foldl addPair (M.empty, S.empty) (toPair <$> S.toList nss)
   where
-    ns = moduleNamespace mod
+    ns = Module.moduleNamespace mod
     focusPair = toPair ns
-    toModuleName (Namespace n) = H.ModuleName $ capitalize $ L.last $ Strings.splitOn "." n
+    toModuleName (Module.Namespace n) = H.ModuleName $ Formatting.capitalize $ L.last $ Strings.splitOn "." n
     toPair name = (name, toModuleName name)
     addPair (m, s) (name, alias@(H.ModuleName aliasStr)) = if S.member alias s
       then addPair (m, s) (name, H.ModuleName $ aliasStr ++ "_")
       else (M.insert name alias m, S.insert alias s)
 
-newtypeAccessorName :: Name -> String
-newtypeAccessorName name = "un" ++ localNameOf name
+newtypeAccessorName :: Core.Name -> String
+newtypeAccessorName name = "un" ++ Qnames.localNameOf name
 
 rawName :: String -> H.Name
 rawName n = H.NameNormal $ H.QualifiedName [] $ H.NamePart n
 
-recordFieldReference :: HaskellNamespaces -> Name -> Name -> H.Name
-recordFieldReference namespaces sname (Name fname) = elementReference namespaces $
-    unqualifyName $ QualifiedName (qualifiedNameNamespace $ qualifyName sname) nm
+recordFieldReference :: HaskellNamespaces -> Core.Name -> Core.Name -> H.Name
+recordFieldReference namespaces sname (Core.Name fname) = elementReference namespaces $
+    Qnames.unqualifyName $ Module.QualifiedName (Module.qualifiedNameNamespace $ Qnames.qualifyName sname) nm
   where
-    nm = decapitalize (typeNameForRecord sname) ++ capitalize fname
+    nm = Formatting.decapitalize (typeNameForRecord sname) ++ Formatting.capitalize fname
 
 sanitizeHaskellName :: String -> String
-sanitizeHaskellName = sanitizeWithUnderscores reservedWords
+sanitizeHaskellName = Formatting.sanitizeWithUnderscores HaskellLanguage.reservedWords
 
 simpleName :: String -> H.Name
 simpleName = rawName . sanitizeHaskellName
@@ -84,19 +91,19 @@ toTypeApplication = app . L.reverse
       [e] -> e
       (h:r) -> H.TypeApplication $ H.ApplicationType (app r) h
 
-typeNameForRecord :: Name -> String
-typeNameForRecord (Name sname) = L.last (Strings.splitOn "." sname)
+typeNameForRecord :: Core.Name -> String
+typeNameForRecord (Core.Name sname) = L.last (Strings.splitOn "." sname)
 
-unionFieldReference :: HaskellNamespaces -> Name -> Name -> H.Name
-unionFieldReference namespaces sname (Name fname) = elementReference namespaces $
-    unqualifyName $ QualifiedName ns nm
+unionFieldReference :: HaskellNamespaces -> Core.Name -> Core.Name -> H.Name
+unionFieldReference namespaces sname (Core.Name fname) = elementReference namespaces $
+    Qnames.unqualifyName $ Module.QualifiedName ns nm
   where
-    ns = qualifiedNameNamespace $ qualifyName sname
-    nm = capitalize (typeNameForRecord sname) ++ capitalize fname
+    ns = Module.qualifiedNameNamespace $ Qnames.qualifyName sname
+    nm = Formatting.capitalize (typeNameForRecord sname) ++ Formatting.capitalize fname
 
-unpackForallType :: Graph -> Type -> ([Name], Type)
-unpackForallType cx t = case stripType t of
-  TypeForall (ForallType v tbody) -> (v:vars, t')
+unpackForallType :: Graph.Graph -> Core.Type -> ([Core.Name], Core.Type)
+unpackForallType cx t = case Strip.stripType t of
+  Core.TypeForall (Core.ForallType v tbody) -> (v:vars, t')
     where
       (vars, t') = unpackForallType cx tbody
   _ -> ([], t)
