c6ba82813ac8d0175ce281c25e7e381717bbc5b0
Joshua Shinavier <josh@fortytwo.net>
2025-06-26 22:22:53 -0700

    Add a number of new primitives: hydra.lib.strings.charAt, hydra.lib.optionals.cases, and hydra.lib.equality.[lt,gt,lte,gte]floatxx for floating point comparison

--- hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Equality.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Equality.hs
@@ -70,14 +70,38 @@ equalUint64 = primitive2 _equality_equalUint64
 identity :: TTerm x -> TTerm x
 identity = primitive1 _equality_identity
 
+gtFloat32 :: TTerm Float -> TTerm Float -> TTerm Bool
+gtFloat32 = primitive2 _equality_gtFloat32
+
+gtFloat64 :: TTerm Double -> TTerm Double -> TTerm Bool
+gtFloat64 = primitive2 _equality_gtFloat64
+
 gtInt32 :: TTerm Int -> TTerm Int -> TTerm Bool
 gtInt32 = primitive2 _equality_gtInt32
+
+gteFloat32 :: TTerm Float -> TTerm Float -> TTerm Bool
+gteFloat32 = primitive2 _equality_gteFloat32
+
+gteFloat64 :: TTerm Double -> TTerm Double -> TTerm Bool
+gteFloat64 = primitive2 _equality_gteFloat64
 
 gteInt32 :: TTerm Int -> TTerm Int -> TTerm Bool
 gteInt32 = primitive2 _equality_gteInt32
 
+ltFloat32 :: TTerm Float -> TTerm Float -> TTerm Bool
+ltFloat32 = primitive2 _equality_ltFloat32
+
+ltFloat64 :: TTerm Double -> TTerm Double -> TTerm Bool
+ltFloat64 = primitive2 _equality_ltFloat64
+
 ltInt32 :: TTerm Int -> TTerm Int -> TTerm Bool
 ltInt32 = primitive2 _equality_ltInt32
+
+lteFloat32 :: TTerm Float -> TTerm Float -> TTerm Bool
+lteFloat32 = primitive2 _equality_lteFloat32
+
+lteFloat64 :: TTerm Double -> TTerm Double -> TTerm Bool
+lteFloat64 = primitive2 _equality_lteFloat64
 
 lteInt32 :: TTerm Int -> TTerm Int -> TTerm Bool
 lteInt32 = primitive2 _equality_lteInt32
