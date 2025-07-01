5ad9acf16bb6f0ff46b889fcdd6ae944b943338e
Joshua Shinavier <josh@fortytwo.net>
2025-07-01 11:32:43 -0700

    Move all of the hard-coded Hydra.Ext modules from hydra-haskell to hydra-ext, with the exception of the JSON and YAML coders

--- hydra-ext/src/main/haskell/Hydra/Extensions.hs
+++ hydra-ext/src/main/haskell/Hydra/Extensions.hs
@@ -1,18 +1,16 @@
+-- | Entry point for "extension" models and coders which are not included in hydra-haskell
+
 module Hydra.Extensions where
 
 import Hydra.Kernel
+import Hydra.Codegen
 
+import Hydra.Ext.Cpp.Coder
 import Hydra.Ext.Delta.Src.Parquet
+import Hydra.Ext.Graphql.Coder
 import Hydra.Ext.Graphviz.Src.Dot
+import Hydra.Ext.Java.Coder
 import Hydra.Ext.Kusto.Src.Kql
-import Hydra.Ext.Owl.Src.Syntax
-import Hydra.Ext.Parquet.Src.Format
-import Hydra.Ext.Shex.Src.Syntax
-import Hydra.Ext.Sql.Src.Ansi
-import Hydra.Ext.Tinkerpop.Src.Features
-import Hydra.Ext.Tinkerpop.Src.Gremlin
-import Hydra.Ext.Xml.Src.Schema
-
 import Hydra.Ext.Other.Atlas
 import Hydra.Ext.Other.AzureDtld
 import Hydra.Ext.Other.Coq
@@ -22,6 +20,24 @@ import Hydra.Ext.Other.IanaRelations
 import Hydra.Ext.Other.Osv
 import Hydra.Ext.Other.PathAlgebra
 import Hydra.Ext.Other.StacItems
+import Hydra.Ext.Owl.Src.Syntax
+import Hydra.Ext.Parquet.Src.Format
+import Hydra.Ext.Pegasus.Coder
+import Hydra.Ext.Protobuf.Coder
+import Hydra.Ext.Python.Coder
+import Hydra.Ext.Scala.Coder
+import Hydra.Ext.Shex.Src.Syntax
+import Hydra.Ext.Sql.Src.Ansi
+import Hydra.Ext.Tinkerpop.Src.Features
+import Hydra.Ext.Tinkerpop.Src.Gremlin
+import Hydra.Ext.Xml.Src.Schema
+
+import qualified Control.Monad as CM
+import qualified System.FilePath as FP
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified System.Directory as SD
+import qualified Data.Maybe as Y
 
 
 hydraExtModules :: [Module]
@@ -45,3 +61,25 @@ hydraExtModules = [
   stacModule,
   tinkerpopFeaturesModule,
   xmlSchemaModule]
+
+writeCpp :: FP.FilePath -> [Module] -> IO ()
+writeCpp = generateSources moduleToCpp
+
+writeGraphql :: FP.FilePath -> [Module] -> IO ()
+writeGraphql = generateSources moduleToGraphql
+
+writeJava :: FP.FilePath -> [Module] -> IO ()
+writeJava = generateSources moduleToJava
+
+writePdl :: FP.FilePath -> [Module] -> IO ()
+writePdl = generateSources moduleToPdl
+
+writeProtobuf :: FP.FilePath -> [Module] -> IO ()
+writeProtobuf = generateSources moduleToProtobuf
+
+writePython :: FP.FilePath -> [Module] -> IO ()
+writePython = generateSources moduleToPython
+
+writeScala :: FP.FilePath -> [Module] -> IO ()
+writeScala = generateSources moduleToScala
+
