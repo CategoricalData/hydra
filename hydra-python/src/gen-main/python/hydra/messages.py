96cc46f5514d08f9bcc3bd7016d709b544e0c4a2
Joshua Shinavier <josh@fortytwo.net>
2025-08-14 21:28:01 -0700

    Update some term-level modules in Python

--- hydra-python/src/gen-main/python/hydra/messages.py
+++
@@ -1,5 +0,0 @@
-"""A collection of standard error and warning messages."""
-
-from __future__ import annotations
-
-warning_auto_generated_file = "Note: this is an automatically generated file. Do not edit."
