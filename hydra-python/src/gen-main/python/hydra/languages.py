5b5dfc1458c5e3d39e8339616fc83dacf194b93f
Joshua Shinavier <josh@fortytwo.net>
2025-08-17 08:36:30 -0700

    Aspirational fixes to hydra.languages in Python. Code generation needs to catch up.

--- hydra-python/src/gen-main/python/hydra/languages.py
+++ hydra-python/src/gen-main/python/hydra/languages.py
@@ -9,13 +9,13 @@ import hydra.variants
 def hydra_language() -> hydra.coders.Language:
     """Language constraints for Hydra Core, i.e. no constraints."""
     
-    elimination_variants = hydra.lib.sets.from_list("hydra.variants.elimination_variants")
-    literal_variants = hydra.lib.sets.from_list("hydra.variants.literal_variants")
-    float_types = hydra.lib.sets.from_list("hydra.variants.float_types")
-    function_variants = hydra.lib.sets.from_list("hydra.variants.function_variants")
-    integer_types = hydra.lib.sets.from_list("hydra.variants.integer_types")
-    term_variants = hydra.lib.sets.from_list("hydra.variants.term_variants")
-    type_variants = hydra.lib.sets.from_list("hydra.variants.type_variants")
-    def types(_) -> bool:
+    elimination_variants = hydra.lib.sets.from_list(hydra.variants.elimination_variants)
+    literal_variants = hydra.lib.sets.from_list(hydra.variants.literal_variants)
+    float_types = hydra.lib.sets.from_list(hydra.variants.float_types)
+    function_variants = hydra.lib.sets.from_list(hydra.variants.function_variants)
+    integer_types = hydra.lib.sets.from_list(hydra.variants.integer_types)
+    term_variants = hydra.lib.sets.from_list(hydra.variants.term_variants)
+    type_variants = hydra.lib.sets.from_list(hydra.variants.type_variants)
+    def types(_: hydra.core.Type) -> bool:
         return True
     return hydra.coders.Language(hydra.coders.LanguageName("hydra.core"), hydra.coders.LanguageConstraints(elimination_variants, literal_variants, float_types, function_variants, integer_types, term_variants, type_variants, types))
