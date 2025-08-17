8fdedaad7099d3fa685e328533827d61fd24407a
Joshua Shinavier <josh@fortytwo.net>
2025-08-17 08:31:28 -0700

    Add hydra.languages in Python

---
+++ hydra-python/src/gen-main/python/hydra/languages.py
@@ -0,0 +1,21 @@
+"""Language constraints for Hydra Core."""
+
+from __future__ import annotations
+import hydra.coders
+import hydra.core
+import hydra.lib.sets
+import hydra.variants
+
+def hydra_language() -> hydra.coders.Language:
+    """Language constraints for Hydra Core, i.e. no constraints."""
+    
+    elimination_variants = hydra.lib.sets.from_list("hydra.variants.elimination_variants")
+    literal_variants = hydra.lib.sets.from_list("hydra.variants.literal_variants")
+    float_types = hydra.lib.sets.from_list("hydra.variants.float_types")
+    function_variants = hydra.lib.sets.from_list("hydra.variants.function_variants")
+    integer_types = hydra.lib.sets.from_list("hydra.variants.integer_types")
+    term_variants = hydra.lib.sets.from_list("hydra.variants.term_variants")
+    type_variants = hydra.lib.sets.from_list("hydra.variants.type_variants")
+    def types(_) -> bool:
+        return True
+    return hydra.coders.Language(hydra.coders.LanguageName("hydra.core"), hydra.coders.LanguageConstraints(elimination_variants, literal_variants, float_types, function_variants, integer_types, term_variants, type_variants, types))
