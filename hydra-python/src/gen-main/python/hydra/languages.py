92f8acaaea63d64054a84a658f163fcf2825585b
Joshua Shinavier <josh@fortytwo.net>
2025-08-27 07:10:39 -0700

    Generate hydra.languages into Python; no more cheating

--- hydra-python/src/gen-main/python/hydra/languages.py
+++ hydra-python/src/gen-main/python/hydra/languages.py
@@ -16,6 +16,8 @@ def hydra_language() -> hydra.coders.Language:
     integer_types = hydra.lib.sets.from_list(hydra.variants.integer_types)
     term_variants = hydra.lib.sets.from_list(hydra.variants.term_variants)
     type_variants = hydra.lib.sets.from_list(hydra.variants.type_variants)
-    def types(_: hydra.core.Type) -> bool:
-        return True
+    def types(t: hydra.core.Type) -> bool:
+        match t:
+            case _:
+                return True
     return hydra.coders.Language(hydra.coders.LanguageName("hydra.core"), hydra.coders.LanguageConstraints(elimination_variants, literal_variants, float_types, function_variants, integer_types, term_variants, type_variants, types))
