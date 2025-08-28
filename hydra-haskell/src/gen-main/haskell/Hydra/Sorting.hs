b077487887fbbc7e8f76aff5a0889ecb07a56481
Joshua Shinavier <josh@fortytwo.net>
2025-08-28 09:32:01 -0700

    No-op updating of hydra.sorting syntax

--- hydra-haskell/src/gen-main/haskell/Hydra/Sorting.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Sorting.hs
@@ -19,24 +19,29 @@ createOrderingIsomorphism :: (Ord t0) => ([t0] -> [t0] -> Topology.OrderingIsomo
 createOrderingIsomorphism sourceOrd targetOrd =  
   let sourceToTargetMapping = (\els ->  
           let mp = (Maps.fromList (Lists.zip sourceOrd els))
-          in (Optionals.cat (Lists.map (\n -> Maps.lookup n mp) targetOrd))) 
-      targetToSourceMapping = (\els ->  
-              let mp = (Maps.fromList (Lists.zip targetOrd els))
-              in (Optionals.cat (Lists.map (\n -> Maps.lookup n mp) sourceOrd)))
-  in Topology.OrderingIsomorphism {
-    Topology.orderingIsomorphismEncode = sourceToTargetMapping,
-    Topology.orderingIsomorphismDecode = targetToSourceMapping}
+          in (Optionals.cat (Lists.map (\n -> Maps.lookup n mp) targetOrd)))
+  in  
+    let targetToSourceMapping = (\els ->  
+            let mp = (Maps.fromList (Lists.zip targetOrd els))
+            in (Optionals.cat (Lists.map (\n -> Maps.lookup n mp) sourceOrd)))
+    in Topology.OrderingIsomorphism {
+      Topology.orderingIsomorphismEncode = sourceToTargetMapping,
+      Topology.orderingIsomorphismDecode = targetToSourceMapping}
 
 topologicalSort :: (Ord t0) => ([(t0, [t0])] -> Mantle.Either [[t0]] [t0])
 topologicalSort pairs =  
-  let sccs = (topologicalSortComponents pairs) 
-      isCycle = (\scc -> Logic.not (Lists.null (Lists.tail scc)))
-      withCycles = (Lists.filter isCycle sccs)
-  in (Logic.ifElse (Lists.null withCycles) (Mantle.EitherRight (Lists.concat sccs)) (Mantle.EitherLeft withCycles))
+  let sccs = (topologicalSortComponents pairs)
+  in  
+    let isCycle = (\scc -> Logic.not (Lists.null (Lists.tail scc)))
+    in  
+      let withCycles = (Lists.filter isCycle sccs)
+      in (Logic.ifElse (Lists.null withCycles) (Mantle.EitherRight (Lists.concat sccs)) (Mantle.EitherLeft withCycles))
 
 topologicalSortComponents :: (Ord t0) => ([(t0, [t0])] -> [[t0]])
 topologicalSortComponents pairs =  
-  let graphResult = (Tarjan.adjacencyListsToGraph pairs) 
-      g = (fst graphResult)
-      getKey = (snd graphResult)
-  in (Lists.map (\component -> Lists.map getKey component) (Tarjan.stronglyConnectedComponents g))
+  let graphResult = (Tarjan.adjacencyListsToGraph pairs)
+  in  
+    let g = (fst graphResult)
+    in  
+      let getKey = (snd graphResult)
+      in (Lists.map (\comp -> Lists.map getKey comp) (Tarjan.stronglyConnectedComponents g))
