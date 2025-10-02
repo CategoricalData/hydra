6b3e31953785b8b9f23dcb45c5c9f06d56de14fc
Joshua Shinavier <josh@fortytwo.net>
2025-10-02 14:12:05 -0700

    Replace the hydra.lib.* DSLs in Python with TODOs/placeholders; these DSLs need to construct terms, not Python-native objects

--- hydra-python/src/main/python/hydra/dsl/lib/chars.py
+++ hydra-python/src/main/python/hydra/dsl/lib/chars.py
@@ -1,40 +1,4 @@
-"""DSL for character functions."""
+"""Phantom-typed term DSL for the hydra.lib.chars library."""
 
-from hydra.tools import primitive
-
-
-@primitive()
-def is_alpha_num(c: int) -> bool:
-    """Check if a character is alphanumeric."""
-    return chr(c).isalnum()
-
-
-@primitive()
-def is_lower(c: int) -> bool:
-    """Check if a character is lowercase."""
-    return chr(c).islower()
-
-
-@primitive()
-def is_space(c: int) -> bool:
-    """Check if a character is a whitespace character."""
-    return chr(c).isspace()
-
-
-@primitive()
-def is_upper(c: int) -> bool:
-    """Check if a character is uppercase."""
-    return chr(c).isupper()
-
-
-@primitive()
-def to_lower(c: int) -> int:
-    """Convert a character to lowercase."""
-    return ord(chr(c).lower())
-
-
-@primitive()
-def to_upper(c: int) -> int:
-    """Convert a character to uppercase."""
-    return ord(chr(c).upper())
+# TODO
 
--- hydra-python/src/main/python/hydra/dsl/lib/strings.py
+++ hydra-python/src/main/python/hydra/dsl/lib/strings.py
@@ -1,33 +1,3 @@
-"""Primitive functions for the Hydra strings DSL."""
+"""Phantom-typed term DSL for the hydra.lib.strings library."""
 
-from hydra.dsl.python import frozenlist
-from hydra.tools import primitive
-
-
-@primitive()
-def to_lower(s: str) -> str:
-    """Convert a string to lowercase."""
-    return s.lower()
-
-@primitive()
-def to_upper(s: str) -> str:
-    """Convert a string to uppercase."""
-    return s.upper()
-
-
-@primitive()
-def length(s: str) -> int:
-    """Get the length of a string."""
-    return len(s)
-
-
-@primitive()
-def cat(strings: frozenlist[str]) -> str:
-    """Concatenate a list of strings."""
-    return "".join(strings)
-
-
-# TODO: Get primitive to handle multiple intputs
-def split_on(delimiter: str, x: str) -> frozenlist[str]:
-    """Split a string on a delimiter."""
-    return tuple(x.split(delimiter))
+# TODO
