4f35c922f09d425ccc2e10ba7c5aeb31f41f7c34
Joshua Shinavier <josh@fortytwo.net>
2025-10-19 08:30:04 -0700

    Update DSL syntax in hydra.monads

--- hydra-haskell/src/gen-main/haskell/Hydra/Monads.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Monads.hs
@@ -53,17 +53,20 @@ fromFlow def cx f = (Optionals.maybe def (\xmo -> xmo) (Compute.flowStateValue (
 
 getState :: (Compute.Flow t0 t0)
 getState = (Compute.Flow (\s0 -> \t0 ->  
-  let fs1 = (Compute.unFlow (pure ()) s0 t0) 
-      v = (Compute.flowStateValue fs1)
-      s = (Compute.flowStateState fs1)
-      t = (Compute.flowStateTrace fs1)
-  in (Optionals.maybe (Compute.FlowState {
-    Compute.flowStateValue = Nothing,
-    Compute.flowStateState = s,
-    Compute.flowStateTrace = t}) (\_ -> Compute.FlowState {
-    Compute.flowStateValue = (Just s),
-    Compute.flowStateState = s,
-    Compute.flowStateTrace = t}) v)))
+  let fs1 = (Compute.unFlow (pure ()) s0 t0)
+  in  
+    let v = (Compute.flowStateValue fs1)
+    in  
+      let s = (Compute.flowStateState fs1)
+      in  
+        let t = (Compute.flowStateTrace fs1)
+        in (Optionals.maybe (Compute.FlowState {
+          Compute.flowStateValue = Nothing,
+          Compute.flowStateState = s,
+          Compute.flowStateTrace = t}) (\_ -> Compute.FlowState {
+          Compute.flowStateValue = (Just s),
+          Compute.flowStateState = s,
+          Compute.flowStateTrace = t}) v)))
 
 map :: ((t0 -> t1) -> Compute.Flow t2 t0 -> Compute.Flow t2 t1)
 map f f1 = (Compute.Flow (\s0 -> \t0 ->  
@@ -81,16 +84,17 @@ mutateTrace mutate restore f = (Compute.Flow (\s0 -> \t0 ->
   let forLeft = (\msg -> Compute.FlowState {
           Compute.flowStateValue = Nothing,
           Compute.flowStateState = s0,
-          Compute.flowStateTrace = (pushError msg t0)}) 
-      forRight = (\t1 ->  
-              let f2 = (Compute.unFlow f s0 t1)
-              in Compute.FlowState {
-                Compute.flowStateValue = (Compute.flowStateValue f2),
-                Compute.flowStateState = (Compute.flowStateState f2),
-                Compute.flowStateTrace = (restore t0 (Compute.flowStateTrace f2))})
-  in ((\x -> case x of
-    Mantle.EitherLeft v1 -> (forLeft v1)
-    Mantle.EitherRight v1 -> (forRight v1)) (mutate t0))))
+          Compute.flowStateTrace = (pushError msg t0)})
+  in  
+    let forRight = (\t1 ->  
+            let f2 = (Compute.unFlow f s0 t1)
+            in Compute.FlowState {
+              Compute.flowStateValue = (Compute.flowStateValue f2),
+              Compute.flowStateState = (Compute.flowStateState f2),
+              Compute.flowStateTrace = (restore t0 (Compute.flowStateTrace f2))})
+    in ((\x -> case x of
+      Mantle.EitherLeft v1 -> (forLeft v1)
+      Mantle.EitherRight v1 -> (forRight v1)) (mutate t0))))
 
 optionalToList :: (Maybe t0 -> [t0])
 optionalToList mx = (Optionals.maybe [] Lists.pure mx)
@@ -104,26 +108,28 @@ pure xp = (Compute.Flow (\s -> \t -> Compute.FlowState {
 -- | Push an error message
 pushError :: (String -> Compute.Trace -> Compute.Trace)
 pushError msg t =  
-  let condenseRepeats =  
+  let condenseRepeats = (\ys ->  
           let condenseGroup = (\xs ->  
-                  let x = (Lists.head xs) 
-                      n = (Lists.length xs)
-                  in (Logic.ifElse (Equality.equal n 1) x (Strings.cat [
-                    x,
-                    " (x",
-                    Literals.showInt32 n,
-                    ")"])))
-          in (\ys -> Lists.map condenseGroup (Lists.group ys)) 
-      errorMsg = (Strings.cat [
-              "Error: ",
-              msg,
-              " (",
-              Strings.intercalate " > " (condenseRepeats (Lists.reverse (Compute.traceStack t))),
-              ")"])
-  in Compute.Trace {
-    Compute.traceStack = (Compute.traceStack t),
-    Compute.traceMessages = (Lists.cons errorMsg (Compute.traceMessages t)),
-    Compute.traceOther = (Compute.traceOther t)}
+                  let x = (Lists.head xs)
+                  in  
+                    let n = (Lists.length xs)
+                    in (Logic.ifElse (Equality.equal n 1) x (Strings.cat [
+                      x,
+                      " (x",
+                      Literals.showInt32 n,
+                      ")"])))
+          in (Lists.map condenseGroup (Lists.group ys)))
+  in  
+    let errorMsg = (Strings.cat [
+            "Error: ",
+            msg,
+            " (",
+            Strings.intercalate " > " (condenseRepeats (Lists.reverse (Compute.traceStack t))),
+            ")"])
+    in Compute.Trace {
+      Compute.traceStack = (Compute.traceStack t),
+      Compute.traceMessages = (Lists.cons errorMsg (Compute.traceMessages t)),
+      Compute.traceOther = (Compute.traceOther t)}
 
 putState :: (t0 -> Compute.Flow t0 ())
 putState cx = (Compute.Flow (\s0 -> \t0 ->  
@@ -136,16 +142,18 @@ putState cx = (Compute.Flow (\s0 -> \t0 ->
 -- | Summarize a trace as a string
 traceSummary :: (Compute.Trace -> String)
 traceSummary t =  
-  let messageLines = (Lists.nub (Compute.traceMessages t)) 
-      keyvalLines = (Logic.ifElse (Maps.null (Compute.traceOther t)) [] (Lists.cons "key/value pairs: " (Lists.map toLine (Maps.toList (Compute.traceOther t)))))
-      toLine = (\pair -> Strings.cat [
+  let messageLines = (Lists.nub (Compute.traceMessages t))
+  in  
+    let toLine = (\pair -> Strings.cat [
+            Strings.cat [
               Strings.cat [
-                Strings.cat [
-                  "\t",
-                  (Core.unName (fst pair))],
-                ": "],
-              (Core_.term (snd pair))])
-  in (Strings.intercalate "\n" (Lists.concat2 messageLines keyvalLines))
+                "\t",
+                (Core.unName (fst pair))],
+              ": "],
+            (Core_.term (snd pair))])
+    in  
+      let keyvalLines = (Logic.ifElse (Maps.null (Compute.traceOther t)) [] (Lists.cons "key/value pairs: " (Lists.map toLine (Maps.toList (Compute.traceOther t)))))
+      in (Strings.intercalate "\n" (Lists.concat2 messageLines keyvalLines))
 
 unexpected :: (String -> String -> Compute.Flow t0 t1)
 unexpected expected actual = (fail (Strings.cat [
@@ -158,29 +166,31 @@ unexpected expected actual = (fail (Strings.cat [
 
 warn :: (String -> Compute.Flow t0 t1 -> Compute.Flow t0 t1)
 warn msg b = (Compute.Flow (\s0 -> \t0 ->  
-  let f1 = (Compute.unFlow b s0 t0) 
-      addMessage = (\t -> Compute.Trace {
-              Compute.traceStack = (Compute.traceStack t),
-              Compute.traceMessages = (Lists.cons (Strings.cat [
-                "Warning: ",
-                msg]) (Compute.traceMessages t)),
-              Compute.traceOther = (Compute.traceOther t)})
-  in Compute.FlowState {
-    Compute.flowStateValue = (Compute.flowStateValue f1),
-    Compute.flowStateState = (Compute.flowStateState f1),
-    Compute.flowStateTrace = (addMessage (Compute.flowStateTrace f1))}))
+  let f1 = (Compute.unFlow b s0 t0)
+  in  
+    let addMessage = (\t -> Compute.Trace {
+            Compute.traceStack = (Compute.traceStack t),
+            Compute.traceMessages = (Lists.cons (Strings.cat [
+              "Warning: ",
+              msg]) (Compute.traceMessages t)),
+            Compute.traceOther = (Compute.traceOther t)})
+    in Compute.FlowState {
+      Compute.flowStateValue = (Compute.flowStateValue f1),
+      Compute.flowStateState = (Compute.flowStateState f1),
+      Compute.flowStateTrace = (addMessage (Compute.flowStateTrace f1))}))
 
 withFlag :: (Core.Name -> Compute.Flow t0 t1 -> Compute.Flow t0 t1)
-withFlag flag =  
+withFlag flag f =  
   let mutate = (\t -> Mantle.EitherRight (Compute.Trace {
           Compute.traceStack = (Compute.traceStack t),
           Compute.traceMessages = (Compute.traceMessages t),
-          Compute.traceOther = (Maps.insert flag (Core.TermLiteral (Core.LiteralBoolean True)) (Compute.traceOther t))})) 
-      restore = (\ignored -> \t1 -> Compute.Trace {
-              Compute.traceStack = (Compute.traceStack t1),
-              Compute.traceMessages = (Compute.traceMessages t1),
-              Compute.traceOther = (Maps.remove flag (Compute.traceOther t1))})
-  in (mutateTrace mutate restore)
+          Compute.traceOther = (Maps.insert flag (Core.TermLiteral (Core.LiteralBoolean True)) (Compute.traceOther t))}))
+  in  
+    let restore = (\ignored -> \t1 -> Compute.Trace {
+            Compute.traceStack = (Compute.traceStack t1),
+            Compute.traceMessages = (Compute.traceMessages t1),
+            Compute.traceOther = (Maps.remove flag (Compute.traceOther t1))})
+    in (mutateTrace mutate restore f)
 
 withState :: (t0 -> Compute.Flow t0 t1 -> Compute.Flow t2 t1)
 withState cx0 f = (Compute.Flow (\cx1 -> \t1 ->  
@@ -191,13 +201,14 @@ withState cx0 f = (Compute.Flow (\cx1 -> \t1 ->
     Compute.flowStateTrace = (Compute.flowStateTrace f1)}))
 
 withTrace :: (String -> Compute.Flow t0 t1 -> Compute.Flow t0 t1)
-withTrace msg =  
+withTrace msg f =  
   let mutate = (\t -> Logic.ifElse (Equality.gte (Lists.length (Compute.traceStack t)) Constants.maxTraceDepth) (Mantle.EitherLeft "maximum trace depth exceeded. This may indicate an infinite loop") (Mantle.EitherRight (Compute.Trace {
           Compute.traceStack = (Lists.cons msg (Compute.traceStack t)),
           Compute.traceMessages = (Compute.traceMessages t),
-          Compute.traceOther = (Compute.traceOther t)}))) 
-      restore = (\t0 -> \t1 -> Compute.Trace {
-              Compute.traceStack = (Compute.traceStack t0),
-              Compute.traceMessages = (Compute.traceMessages t1),
-              Compute.traceOther = (Compute.traceOther t1)})
-  in (mutateTrace mutate restore)
+          Compute.traceOther = (Compute.traceOther t)})))
+  in  
+    let restore = (\t0 -> \t1 -> Compute.Trace {
+            Compute.traceStack = (Compute.traceStack t0),
+            Compute.traceMessages = (Compute.traceMessages t1),
+            Compute.traceOther = (Compute.traceOther t1)})
+    in (mutateTrace mutate restore f)
