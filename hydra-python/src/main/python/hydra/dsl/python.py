dfb4f2b2967fedf6d8c4b8160dedc45bf14027c3
Joshua Shinavier <josh@fortytwo.net>
2025-10-20 20:40:50 -0700

    Define a Maybe type (with variants Nothing and Just) in the Python DSL

--- hydra-python/src/main/python/hydra/dsl/python.py
+++ hydra-python/src/main/python/hydra/dsl/python.py
@@ -2,26 +2,42 @@
 
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from collections.abc import Callable, Iterator, Mapping
+# from abc import ABC
+from collections.abc import Iterator, Mapping
 from dataclasses import dataclass, field
-from typing import Any, Generic, TypeVar, cast, override
+#from typing import Any, Generic, TypeVar, cast, override
+from typing import Any, Generic, TypeVar, cast, final
 
 T = TypeVar("T")
 K = TypeVar("K")
 V = TypeVar("V")
 
-
 A = TypeVar("A")
 B = TypeVar("B")
 C = TypeVar("C")
 
-M = TypeVar("M", bound="Monad[Any]")
+# M = TypeVar("M", bound="Monad[Any]")
 
 S = TypeVar("S")
 
 
 type frozenlist[T] = tuple[T, ...]
+
+
+@final
+@dataclass(frozen=True)
+class Just(Generic[T]):
+    value: T
+
+@final
+class Nothing:
+    __slots__ = ()
+    def __repr__(self) -> str:
+        return "Nothing"
+
+NOTHING = Nothing()
+
+type Maybe[T] = Just[T] | Nothing
 
 
 @dataclass(frozen=True, unsafe_hash=True, eq=True, order=True)
@@ -94,146 +110,152 @@ class FrozenDict(Mapping[K, V]):
         return f"FrozenDict({dict(self.data)})"
 
 
-class Monad(ABC, Generic[A]):
-    """A monad is a type that supports a bind operation."""
-
-    @classmethod
-    @abstractmethod
-    def pure(cls: type[M], value: A) -> Monad[A]:
-        """Construct a monad with a value."""
-        raise NotImplementedError("Not implemented")
-
-    @abstractmethod
-    def bind(self, f: Callable[[A], Monad[B]]) -> Monad[B]:
-        """Bind a function to a monad."""
-        raise NotImplementedError("Not implemented")
-
-    def then(self, f: Callable[[A], B]) -> Monad[B]:
-        """Compose a function with a monad."""
-        return self.lift(f)(self)
-
-    @classmethod
-    def map(cls: type[M], f: Callable[[A], B], ma: Monad[A]) -> Monad[B]:
-        """Map a function over a monadic value."""
-        return ma.bind(lambda x: cast(Monad[B], cls).pure(f(x)))
-
-    @classmethod
-    def join(cls: type[M], mma: Monad[Monad[A]]) -> Monad[A]:
-        """Flatten a nested monad."""
-        return mma.bind(lambda ma: ma)
-
-    @classmethod
-    def apply(cls, mf: Monad[Callable[[A], B]], ma: Monad[A]) -> Monad[B]:
-        """Apply a monadic function to a monadic value (Applicative functor operation)."""
-        return mf.bind(lambda f: ma.bind(lambda a: cast(Monad[B], cls).pure(f(a))))
-
-    @staticmethod
-    def compose(
-        f: Callable[[A], Monad[B]], g: Callable[[B], Monad[C]]
-    ) -> Callable[[A], Monad[C]]:
-        """Compose two monadic functions (Kleisli composition)."""
-        return lambda x: f(x).bind(g)
-
-    @classmethod
-    def lift(cls, f: Callable[[A], B]) -> Callable[[Monad[A]], Monad[B]]:
-        """Lift a function to a monadic function."""
-        return lambda ma: cls.map(f, ma)
-
-
-class MonadPlus(Monad[A], ABC):
-    """A monad that supports a plus operation."""
-
-    @classmethod
-    @abstractmethod
-    def empty(cls) -> MonadPlus[A]:
-        """Construct an empty monad."""
-        raise NotImplementedError("Not implemented")
-
-    @abstractmethod
-    def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
-        """Combine two monads."""
-        raise NotImplementedError("Not implemented")
-
-    def __add__(self, other: MonadPlus[A]) -> MonadPlus[A]:
-        """Combine two monads."""
-        return self.plus(other)
+# Currently unused
+# class Monad(ABC, Generic[A]):
+#     """A monad is a type that supports a bind operation."""
+#
+#     @classmethod
+#     @abstractmethod
+#     def pure(cls: type[M], value: A) -> Monad[A]:
+#         """Construct a monad with a value."""
+#         raise NotImplementedError("Not implemented")
+#
+#     @abstractmethod
+#     def bind(self, f: Callable[[A], Monad[B]]) -> Monad[B]:
+#         """Bind a function to a monad."""
+#         raise NotImplementedError("Not implemented")
+#
+#     def then(self, f: Callable[[A], B]) -> Monad[B]:
+#         """Compose a function with a monad."""
+#         return self.lift(f)(self)
+#
+#     @classmethod
+#     def map(cls: type[M], f: Callable[[A], B], ma: Monad[A]) -> Monad[B]:
+#         """Map a function over a monadic value."""
+#         return ma.bind(lambda x: cast(Monad[B], cls).pure(f(x)))
+#
+#     @classmethod
+#     def join(cls: type[M], mma: Monad[Monad[A]]) -> Monad[A]:
+#         """Flatten a nested monad."""
+#         return mma.bind(lambda ma: ma)
+#
+#     @classmethod
+#     def apply(cls, mf: Monad[Callable[[A], B]], ma: Monad[A]) -> Monad[B]:
+#         """Apply a monadic function to a monadic value (Applicative functor operation)."""
+#         return mf.bind(lambda f: ma.bind(lambda a: cast(Monad[B], cls).pure(f(a))))
+#
+#     @staticmethod
+#     def compose(
+#         f: Callable[[A], Monad[B]], g: Callable[[B], Monad[C]]
+#     ) -> Callable[[A], Monad[C]]:
+#         """Compose two monadic functions (Kleisli composition)."""
+#         return lambda x: f(x).bind(g)
+#
+#     @classmethod
+#     def lift(cls, f: Callable[[A], B]) -> Callable[[Monad[A]], Monad[B]]:
+#         """Lift a function to a monadic function."""
+#         return lambda ma: cls.map(f, ma)
 
 
-@dataclass
-class Maybe(MonadPlus[A]):
-    """A monad that represents an optional value."""
-
-    value: A | None
-
-    @classmethod
-    @override
-    def pure(cls, value: A) -> Maybe[A]:
-        return cls(value)
-
-    @override
-    def bind(self, f: Callable[[A], Monad[B]]) -> Monad[B]:
-        return f(self.value) if self.value is not None else Maybe(None)
-
-    @classmethod
-    @override
-    def empty(cls) -> Maybe[A]:
-        return cls(None)
-
-    @override
-    def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
-        other = cast(Maybe[A], other)
-        return Maybe[A](self.value if self.value is not None else other.value)
+# Currently unused
+# class MonadPlus(Monad[A], ABC):
+#     """A monad that supports a plus operation."""
+#
+#     @classmethod
+#     @abstractmethod
+#     def empty(cls) -> MonadPlus[A]:
+#         """Construct an empty monad."""
+#         raise NotImplementedError("Not implemented")
+#
+#     @abstractmethod
+#     def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
+#         """Combine two monads."""
+#         raise NotImplementedError("Not implemented")
+#
+#     def __add__(self, other: MonadPlus[A]) -> MonadPlus[A]:
+#         """Combine two monads."""
+#         return self.plus(other)
 
 
-@dataclass
-class Either(Generic[A, B], MonadPlus[A]):
-    """A monad that represents an either value."""
+# Currently unused, and conflicts with the previous definition of Maybe
+# @dataclass
+# class Maybe(MonadPlus[A]):
+#     """A monad that represents an optional value."""
+#
+#     value: A | None
+#
+#     @classmethod
+#     @override
+#     def pure(cls, value: A) -> Maybe[A]:
+#         return cls(value)
+#
+#     @override
+#     def bind(self, f: Callable[[A], Monad[B]]) -> Monad[B]:
+#         return f(self.value) if self.value is not None else Maybe(None)
+#
+#     @classmethod
+#     @override
+#     def empty(cls) -> Maybe[A]:
+#         return cls(None)
+#
+#     @override
+#     def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
+#         other = cast(Maybe[A], other)
+#         return Maybe[A](self.value if self.value is not None else other.value)
 
 
-@dataclass
-class List(MonadPlus[A]):
-    """A monad that represents a list."""
-
-    value: list[A]
-
-    @classmethod
-    @override
-    def pure(cls, value: A) -> List[A]:
-        return List[A]([value])
-
-    @override
-    def bind(self, f: Callable[[A], Monad[B]]) -> List[B]:
-        return List[B]([b for a in self.value for b in cast(List[B], f(a)).value])
-
-    @classmethod
-    @override
-    def empty(cls) -> List[A]:
-        return List[A]([])
-
-    @override
-    def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
-        other = cast(List[A], other)
-        return List[A](self.value + other.value)
+# Currently unused
+# @dataclass
+# class Either(Generic[A, B], MonadPlus[A]):
+#     """A monad that represents an either value."""
 
 
-@dataclass
-class State(Monad[A], Generic[A, S]):
-    """A monad that represents a stateful computation."""
+# Currently unused
+# @dataclass
+# class List(MonadPlus[A]):
+#     """A monad that represents a list."""
+#
+#     value: list[A]
+#
+#     @classmethod
+#     @override
+#     def pure(cls, value: A) -> List[A]:
+#         return List[A]([value])
+#
+#     @override
+#     def bind(self, f: Callable[[A], Monad[B]]) -> List[B]:
+#         return List[B]([b for a in self.value for b in cast(List[B], f(a)).value])
+#
+#     @classmethod
+#     @override
+#     def empty(cls) -> List[A]:
+#         return List[A]([])
+#
+#     @override
+#     def plus(self, other: MonadPlus[A]) -> MonadPlus[A]:
+#         other = cast(List[A], other)
+#         return List[A](self.value + other.value)
 
-    run: Callable[[S], tuple[S, A]]
 
-    @classmethod
-    @override
-    def pure(cls, value: A) -> State[A, S]:
-        def run(s: S) -> tuple[S, A]:
-            return (s, value)
-
-        return State[A, S](run)
-
-    @override
-    def bind(self, f: Callable[[A], Monad[B]]) -> State[B, S]:
-        def run(s: S) -> tuple[S, B]:
-            s, a = self.run(s)
-            return cast(State[B, S], f(a)).run(s)
-
-        return State[B, S](run)
+# Currently unused
+# @dataclass
+# class State(Monad[A], Generic[A, S]):
+#     """A monad that represents a stateful computation."""
+#
+#     run: Callable[[S], tuple[S, A]]
+#
+#     @classmethod
+#     @override
+#     def pure(cls, value: A) -> State[A, S]:
+#         def run(s: S) -> tuple[S, A]:
+#             return (s, value)
+#
+#         return State[A, S](run)
+#
+#     @override
+#     def bind(self, f: Callable[[A], Monad[B]]) -> State[B, S]:
+#         def run(s: S) -> tuple[S, B]:
+#             s, a = self.run(s)
+#             return cast(State[B, S], f(a)).run(s)
+#
+#         return State[B, S](run)
