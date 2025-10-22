54f19e26244e603c402f8547efedfe1082296408
Joshua Shinavier <josh@fortytwo.net>
2025-10-21 21:47:57 -0700

    Add generated hydra.templates in Python

--- hydra-haskell/src/gen-main/haskell/Hydra/Templates.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Templates.hs
@@ -23,61 +23,73 @@ import qualified Data.Set as S
 graphToSchema :: (Graph.Graph -> Compute.Flow Graph.Graph (M.Map Core.Name Core.Type))
 graphToSchema g =  
   let toPair = (\nameAndEl ->  
-          let name = (fst nameAndEl) 
-              el = (snd nameAndEl)
-          in (Flows.bind (Core_.type_ (Core.bindingTerm el)) (\t -> Flows.pure (name, t))))
+          let name = (fst nameAndEl)
+          in  
+            let el = (snd nameAndEl)
+            in (Flows.bind (Core_.type_ (Core.bindingTerm el)) (\t -> Flows.pure (name, t))))
   in (Flows.bind (Flows.mapList toPair (Maps.toList (Graph.graphElements g))) (\pairs -> Flows.pure (Maps.fromList pairs)))
 
 instantiateTemplate :: (Bool -> M.Map Core.Name Core.Type -> Core.Type -> Compute.Flow t0 Core.Term)
 instantiateTemplate minimal schema t =  
-  let inst = (instantiateTemplate minimal schema) 
-      noPoly = (Flows.fail "Polymorphic and function types are not currently supported")
-  in ((\x -> case x of
-    Core.TypeAnnotated v1 -> (inst (Core.annotatedTypeBody v1))
-    Core.TypeApplication _ -> noPoly
-    Core.TypeFunction _ -> noPoly
-    Core.TypeForall _ -> noPoly
-    Core.TypeList v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermList [])) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermList [
-      e]))))
-    Core.TypeLiteral v1 -> (Flows.pure (Core.TermLiteral ((\x -> case x of
-      Core.LiteralTypeBinary -> (Core.LiteralString "")
-      Core.LiteralTypeBoolean -> (Core.LiteralBoolean False)
-      Core.LiteralTypeInteger v2 -> (Core.LiteralInteger ((\x -> case x of
-        Core.IntegerTypeBigint -> (Core.IntegerValueBigint 0)
-        Core.IntegerTypeInt8 -> (Core.IntegerValueInt8 0)
-        Core.IntegerTypeInt16 -> (Core.IntegerValueInt16 0)
-        Core.IntegerTypeInt32 -> (Core.IntegerValueInt32 0)
-        Core.IntegerTypeInt64 -> (Core.IntegerValueInt64 0)
-        Core.IntegerTypeUint8 -> (Core.IntegerValueUint8 0)
-        Core.IntegerTypeUint16 -> (Core.IntegerValueUint16 0)
-        Core.IntegerTypeUint32 -> (Core.IntegerValueUint32 0)
-        Core.IntegerTypeUint64 -> (Core.IntegerValueUint64 0)) v2))
-      Core.LiteralTypeFloat v2 -> (Core.LiteralFloat ((\x -> case x of
-        Core.FloatTypeBigfloat -> (Core.FloatValueBigfloat 0.0)
-        Core.FloatTypeFloat32 -> (Core.FloatValueFloat32 0.0)
-        Core.FloatTypeFloat64 -> (Core.FloatValueFloat64 0.0)) v2))
-      Core.LiteralTypeString -> (Core.LiteralString "")) v1)))
-    Core.TypeMap v1 ->  
-      let kt = (Core.mapTypeKeys v1) 
-          vt = (Core.mapTypeValues v1)
-      in (Logic.ifElse minimal (Flows.pure (Core.TermMap Maps.empty)) (Flows.bind (inst kt) (\ke -> Flows.bind (inst vt) (\ve -> Flows.pure (Core.TermMap (Maps.singleton ke ve))))))
-    Core.TypeOptional v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermOptional Nothing)) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermOptional (Just e)))))
-    Core.TypeProduct v1 -> (Flows.bind (Flows.mapList inst v1) (\es -> Flows.pure (Core.TermProduct es)))
-    Core.TypeRecord v1 ->  
-      let tname = (Core.rowTypeTypeName v1) 
-          fields = (Core.rowTypeFields v1)
-          toField = (\ft -> Flows.bind (inst (Core.fieldTypeType ft)) (\e -> Flows.pure (Core.Field {
-                  Core.fieldName = (Core.fieldTypeName ft),
-                  Core.fieldTerm = e})))
-      in (Flows.bind (Flows.mapList toField fields) (\dfields -> Flows.pure (Core.TermRecord (Core.Record {
-        Core.recordTypeName = tname,
-        Core.recordFields = dfields}))))
-    Core.TypeSet v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermSet Sets.empty)) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermSet (Sets.fromList [
-      e])))))
-    Core.TypeVariable v1 -> (Optionals.maybe (Flows.fail (Strings.cat2 "Type variable " (Strings.cat2 (Core__.term (Core.TermVariable v1)) " not found in schema"))) inst (Maps.lookup v1 schema))
-    Core.TypeWrap v1 ->  
-      let tname = (Core.wrappedTypeTypeName v1) 
-          t_ = (Core.wrappedTypeBody v1)
-      in (Flows.bind (inst t_) (\e -> Flows.pure (Core.TermWrap (Core.WrappedTerm {
-        Core.wrappedTermTypeName = tname,
-        Core.wrappedTermBody = e}))))) t)
+  let inst = (instantiateTemplate minimal schema)
+  in  
+    let noPoly = (Flows.fail "Polymorphic and function types are not currently supported")
+    in  
+      let forFloat = (\ft -> (\x -> case x of
+              Core.FloatTypeBigfloat -> (Core.FloatValueBigfloat 0.0)
+              Core.FloatTypeFloat32 -> (Core.FloatValueFloat32 0.0)
+              Core.FloatTypeFloat64 -> (Core.FloatValueFloat64 0.0)) ft)
+      in  
+        let forInteger = (\it -> (\x -> case x of
+                Core.IntegerTypeBigint -> (Core.IntegerValueBigint 0)
+                Core.IntegerTypeInt8 -> (Core.IntegerValueInt8 0)
+                Core.IntegerTypeInt16 -> (Core.IntegerValueInt16 0)
+                Core.IntegerTypeInt32 -> (Core.IntegerValueInt32 0)
+                Core.IntegerTypeInt64 -> (Core.IntegerValueInt64 0)
+                Core.IntegerTypeUint8 -> (Core.IntegerValueUint8 0)
+                Core.IntegerTypeUint16 -> (Core.IntegerValueUint16 0)
+                Core.IntegerTypeUint32 -> (Core.IntegerValueUint32 0)
+                Core.IntegerTypeUint64 -> (Core.IntegerValueUint64 0)) it)
+        in  
+          let forLiteral = (\lt -> (\x -> case x of
+                  Core.LiteralTypeBinary -> (Core.LiteralString "")
+                  Core.LiteralTypeBoolean -> (Core.LiteralBoolean False)
+                  Core.LiteralTypeInteger v1 -> (Core.LiteralInteger (forInteger v1))
+                  Core.LiteralTypeFloat v1 -> (Core.LiteralFloat (forFloat v1))
+                  Core.LiteralTypeString -> (Core.LiteralString "")) lt)
+          in ((\x -> case x of
+            Core.TypeAnnotated v1 -> (inst (Core.annotatedTypeBody v1))
+            Core.TypeApplication _ -> noPoly
+            Core.TypeFunction _ -> noPoly
+            Core.TypeForall _ -> noPoly
+            Core.TypeList v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermList [])) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermList [
+              e]))))
+            Core.TypeLiteral v1 -> (Flows.pure (Core.TermLiteral (forLiteral v1)))
+            Core.TypeMap v1 ->  
+              let kt = (Core.mapTypeKeys v1)
+              in  
+                let vt = (Core.mapTypeValues v1)
+                in (Logic.ifElse minimal (Flows.pure (Core.TermMap Maps.empty)) (Flows.bind (inst kt) (\ke -> Flows.bind (inst vt) (\ve -> Flows.pure (Core.TermMap (Maps.singleton ke ve))))))
+            Core.TypeOptional v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermOptional Nothing)) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermOptional (Just e)))))
+            Core.TypeProduct v1 -> (Flows.bind (Flows.mapList inst v1) (\es -> Flows.pure (Core.TermProduct es)))
+            Core.TypeRecord v1 ->  
+              let tname = (Core.rowTypeTypeName v1)
+              in  
+                let fields = (Core.rowTypeFields v1)
+                in  
+                  let toField = (\ft -> Flows.bind (inst (Core.fieldTypeType ft)) (\e -> Flows.pure (Core.Field {
+                          Core.fieldName = (Core.fieldTypeName ft),
+                          Core.fieldTerm = e})))
+                  in (Flows.bind (Flows.mapList toField fields) (\dfields -> Flows.pure (Core.TermRecord (Core.Record {
+                    Core.recordTypeName = tname,
+                    Core.recordFields = dfields}))))
+            Core.TypeSet v1 -> (Logic.ifElse minimal (Flows.pure (Core.TermSet Sets.empty)) (Flows.bind (inst v1) (\e -> Flows.pure (Core.TermSet (Sets.fromList [
+              e])))))
+            Core.TypeVariable v1 -> (Optionals.maybe (Flows.fail (Strings.cat2 "Type variable " (Strings.cat2 (Core__.term (Core.TermVariable v1)) " not found in schema"))) inst (Maps.lookup v1 schema))
+            Core.TypeWrap v1 ->  
+              let tname = (Core.wrappedTypeTypeName v1)
+              in  
+                let t_ = (Core.wrappedTypeBody v1)
+                in (Flows.bind (inst t_) (\e -> Flows.pure (Core.TermWrap (Core.WrappedTerm {
+                  Core.wrappedTermTypeName = tname,
+                  Core.wrappedTermBody = e}))))) t)
--- hydra-haskell/src/main/haskell/Hydra/Sources/Kernel/Terms/Templates.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Kernel/Terms/Templates.hs
@@ -61,94 +61,95 @@ define = definitionInModule module_
 graphToSchemaDef :: TBinding (Graph -> Flow Graph (M.Map Name Type))
 graphToSchemaDef = define "graphToSchema" $
   doc "Create a graph schema from a graph which contains nothing but encoded type definitions" $
-  lambda "g" $ lets [
-    "toPair">: lambda "nameAndEl" $ lets [
-      "name">: first $ var "nameAndEl",
-      "el">: second $ var "nameAndEl"]
-      $ Flows.bind (ref DecodeCore.typeDef @@ (Core.bindingTerm $ var "el")) $
-        lambda "t" $ Flows.pure $ pair (var "name") (var "t")]
-    $ Flows.bind (Flows.mapList (var "toPair") $ Maps.toList $ Graph.graphElements $ var "g") $
-      lambda "pairs" $ Flows.pure $ Maps.fromList $ var "pairs"
+  "g" ~>
+  "toPair" <~ ("nameAndEl" ~>
+    "name" <~ first (var "nameAndEl") $
+    "el" <~ second (var "nameAndEl") $
+    Flows.bind (ref DecodeCore.typeDef @@ (Core.bindingTerm (var "el"))) (
+      "t" ~> Flows.pure (pair (var "name") (var "t")))) $
+  Flows.bind (Flows.mapList (var "toPair") (Maps.toList (Graph.graphElements (var "g")))) (
+    "pairs" ~> Flows.pure (Maps.fromList (var "pairs")))
 
 instantiateTemplateDef :: TBinding (Bool -> M.Map Name Type -> Type -> Flow s Term)
 instantiateTemplateDef = define "instantiateTemplate" $
   doc ("Given a graph schema and a nonrecursive type, instantiate it with default values."
     <> " If the minimal flag is set, the smallest possible term is produced; otherwise, exactly one subterm"
     <> " is produced for constructors which do not otherwise require one, e.g. in lists and optionals") $
-  lambdas ["minimal", "schema", "t"] $ lets [
-    "inst">: ref instantiateTemplateDef @@ var "minimal" @@ var "schema",
-    "noPoly">: Flows.fail $ string "Polymorphic and function types are not currently supported"]
-    $ match _Type Nothing [
-      _Type_annotated>>: lambda "at" $ var "inst" @@ (Core.annotatedTypeBody $ var "at"),
-      _Type_application>>: constant $ var "noPoly",
-      _Type_function>>: constant $ var "noPoly",
-      _Type_forall>>: constant $ var "noPoly",
-      _Type_list>>: lambda "et" $ Logic.ifElse (var "minimal")
-        (Flows.pure $ Core.termList $ list [])
-        (Flows.bind (var "inst" @@ var "et") $
-          lambda "e" $ Flows.pure $ Core.termList $ list [var "e"]),
-      _Type_literal>>: lambda "lt" $ Flows.pure $ Core.termLiteral $
-        cases _LiteralType (var "lt") Nothing [
-          _LiteralType_binary>>: constant $ Core.literalString $ string "",
-          _LiteralType_boolean>>: constant $ Core.literalBoolean false,
-          _LiteralType_integer>>: lambda "it" $ Core.literalInteger $
-            match _IntegerType Nothing [
-              _IntegerType_bigint>>: constant $ Core.integerValueBigint $ bigint 0,
-              _IntegerType_int8>>: constant $ Core.integerValueInt8 $ int8 0,
-              _IntegerType_int16>>: constant $ Core.integerValueInt16 $ int16 0,
-              _IntegerType_int32>>: constant $ Core.integerValueInt32 $ int32 0,
-              _IntegerType_int64>>: constant $ Core.integerValueInt64 $ int64 0,
-              _IntegerType_uint8>>: constant $ Core.integerValueUint8 $ uint8 0,
-              _IntegerType_uint16>>: constant $ Core.integerValueUint16 $ uint16 0,
-              _IntegerType_uint32>>: constant $ Core.integerValueUint32 $ uint32 0,
-              _IntegerType_uint64>>: constant $ Core.integerValueUint64 $ uint64 0] @@ var "it",
-          _LiteralType_float>>: lambda "ft" $ Core.literalFloat $
-            cases _FloatType (var "ft") Nothing [
-              _FloatType_bigfloat>>: constant $ Core.floatValueBigfloat $ bigfloat 0.0,
-              _FloatType_float32>>: constant $ Core.floatValueFloat32 $ float32 0.0,
-              _FloatType_float64>>: constant $ Core.floatValueFloat64 $ float64 0.0],
-          _LiteralType_string>>: constant $ Core.literalString $ string ""],
-      _Type_map>>: lambda "mt" $ lets [
-        "kt">: Core.mapTypeKeys $ var "mt",
-        "vt">: Core.mapTypeValues $ var "mt"]
-        $ Logic.ifElse (var "minimal")
-          (Flows.pure $ Core.termMap Maps.empty)
-          (Flows.bind (var "inst" @@ var "kt") $
-            lambda "ke" $
-              Flows.bind (var "inst" @@ var "vt") $
-                lambda "ve" $ Flows.pure $ Core.termMap $ Maps.singleton (var "ke") (var "ve")),
-      _Type_optional>>: lambda "ot" $ Logic.ifElse (var "minimal")
-        (Flows.pure $ Core.termOptional nothing)
-        (Flows.bind (var "inst" @@ var "ot") $
-          lambda "e" $ Flows.pure $ Core.termOptional $ just $ var "e"),
-      _Type_product>>: lambda "types" $
-        Flows.bind (Flows.mapList (var "inst") (var "types")) $
-          lambda "es" $ Flows.pure $ Core.termProduct $ var "es",
-      _Type_record>>: lambda "rt" $ lets [
-        "tname">: Core.rowTypeTypeName $ var "rt",
-        "fields">: Core.rowTypeFields $ var "rt",
-        "toField">: lambda "ft" $
-          Flows.bind (var "inst" @@ (Core.fieldTypeType $ var "ft")) $
-            lambda "e" $ Flows.pure $ Core.field (Core.fieldTypeName $ var "ft") (var "e")]
-        $ Flows.bind (Flows.mapList (var "toField") (var "fields")) $
-          lambda "dfields" $ Flows.pure $ Core.termRecord $ Core.record (var "tname") (var "dfields"),
-      _Type_set>>: lambda "et" $ Logic.ifElse (var "minimal")
-        (Flows.pure $ Core.termSet Sets.empty)
-        (Flows.bind (var "inst" @@ var "et") $
-          lambda "e" $ Flows.pure $ Core.termSet $ Sets.fromList $ list [var "e"]),
-      -- TODO: _Type_sum
-      -- TODO: _Type_union
-      -- TODO: _Type_unit>>: constant $ Flows.pure Core.termUnit,
-      _Type_variable>>: lambda "tname" $
-        Optionals.maybe
-          (Flows.fail $ Strings.cat2 (string "Type variable ") $ Strings.cat2 (ref ShowCore.termDef @@ (Core.termVariable $ var "tname")) (string " not found in schema"))
-          (var "inst")
-          (Maps.lookup (var "tname") (var "schema")),
-      _Type_wrap>>: lambda "wt" $ lets [
-        "tname">: Core.wrappedTypeTypeName $ var "wt",
-        "t'">: Core.wrappedTypeBody $ var "wt"]
-        $ Flows.bind (var "inst" @@ var "t'") $
-          lambda "e" $ Flows.pure $ Core.termWrap $ Core.wrappedTerm (var "tname") (var "e")] @@ var "t"
+  "minimal" ~> "schema" ~> "t" ~>
+  "inst" <~ ref instantiateTemplateDef @@ var "minimal" @@ var "schema" $
+  "noPoly" <~ Flows.fail (string "Polymorphic and function types are not currently supported") $
+  "forFloat" <~ ("ft" ~> cases _FloatType (var "ft")
+    Nothing [
+    _FloatType_bigfloat>>: constant (Core.floatValueBigfloat (bigfloat 0.0)),
+    _FloatType_float32>>: constant (Core.floatValueFloat32 (float32 0.0)),
+    _FloatType_float64>>: constant (Core.floatValueFloat64 (float64 0.0))]) $
+  "forInteger" <~ ("it" ~> cases _IntegerType (var "it")
+    Nothing [
+    _IntegerType_bigint>>: constant (Core.integerValueBigint (bigint 0)),
+    _IntegerType_int8>>: constant (Core.integerValueInt8 (int8 0)),
+    _IntegerType_int16>>: constant (Core.integerValueInt16 (int16 0)),
+    _IntegerType_int32>>: constant (Core.integerValueInt32 (int32 0)),
+    _IntegerType_int64>>: constant (Core.integerValueInt64 (int64 0)),
+    _IntegerType_uint8>>: constant (Core.integerValueUint8 (uint8 0)),
+    _IntegerType_uint16>>: constant (Core.integerValueUint16 (uint16 0)),
+    _IntegerType_uint32>>: constant (Core.integerValueUint32 (uint32 0)),
+    _IntegerType_uint64>>: constant (Core.integerValueUint64 (uint64 0))]) $
+  "forLiteral" <~ ("lt" ~> cases _LiteralType (var "lt")
+    Nothing [
+    _LiteralType_binary>>: constant (Core.literalString (string "")),
+    _LiteralType_boolean>>: constant (Core.literalBoolean false),
+    _LiteralType_integer>>: "it" ~> Core.literalInteger (var "forInteger" @@ var "it"),
+    _LiteralType_float>>: "ft" ~> Core.literalFloat (var "forFloat" @@ var "ft"),
+    _LiteralType_string>>: constant (Core.literalString (string ""))]) $
+  cases _Type (var "t")
+    Nothing [
+    _Type_annotated>>: "at" ~> var "inst" @@ (Core.annotatedTypeBody (var "at")),
+    _Type_application>>: constant (var "noPoly"),
+    _Type_function>>: constant (var "noPoly"),
+    _Type_forall>>: constant (var "noPoly"),
+    _Type_list>>: "et" ~> Logic.ifElse (var "minimal")
+      (Flows.pure (Core.termList (list [])))
+      (Flows.bind (var "inst" @@ var "et") (
+        "e" ~> Flows.pure (Core.termList (list [var "e"])))),
+    _Type_literal>>: "lt" ~> Flows.pure (Core.termLiteral (var "forLiteral" @@ var "lt")),
+    _Type_map>>: "mt" ~>
+      "kt" <~ Core.mapTypeKeys (var "mt") $
+      "vt" <~ Core.mapTypeValues (var "mt") $
+      Logic.ifElse (var "minimal")
+        (Flows.pure (Core.termMap Maps.empty))
+        (Flows.bind (var "inst" @@ var "kt") (
+          "ke" ~>
+          Flows.bind (var "inst" @@ var "vt") (
+            "ve" ~> Flows.pure (Core.termMap (Maps.singleton (var "ke") (var "ve")))))),
+    _Type_optional>>: "ot" ~> Logic.ifElse (var "minimal")
+      (Flows.pure (Core.termOptional nothing))
+      (Flows.bind (var "inst" @@ var "ot") (
+        "e" ~> Flows.pure (Core.termOptional (just (var "e"))))),
+    _Type_product>>: "types" ~>
+      Flows.bind (Flows.mapList (var "inst") (var "types")) (
+        "es" ~> Flows.pure (Core.termProduct (var "es"))),
+    _Type_record>>: "rt" ~>
+      "tname" <~ Core.rowTypeTypeName (var "rt") $
+      "fields" <~ Core.rowTypeFields (var "rt") $
+      "toField" <~ ("ft" ~>
+        Flows.bind (var "inst" @@ (Core.fieldTypeType (var "ft"))) (
+          "e" ~> Flows.pure (Core.field (Core.fieldTypeName (var "ft")) (var "e")))) $
+      Flows.bind (Flows.mapList (var "toField") (var "fields")) (
+        "dfields" ~> Flows.pure (Core.termRecord (Core.record (var "tname") (var "dfields")))),
+    _Type_set>>: "et" ~> Logic.ifElse (var "minimal")
+      (Flows.pure (Core.termSet Sets.empty))
+      (Flows.bind (var "inst" @@ var "et") (
+        "e" ~> Flows.pure (Core.termSet (Sets.fromList (list [var "e"]))))),
+    _Type_variable>>: "tname" ~>
+      Optionals.maybe
+        (Flows.fail (Strings.cat2 (string "Type variable ") (Strings.cat2 (ref ShowCore.termDef @@ (Core.termVariable (var "tname"))) (string " not found in schema"))))
+        (var "inst")
+        (Maps.lookup (var "tname") (var "schema")),
+    _Type_wrap>>: "wt" ~>
+      "tname" <~ Core.wrappedTypeTypeName (var "wt") $
+      "t'" <~ Core.wrappedTypeBody (var "wt") $
+      Flows.bind (var "inst" @@ var "t'") (
+        "e" ~> Flows.pure (Core.termWrap (Core.wrappedTerm (var "tname") (var "e"))))]
 
 {-
 
