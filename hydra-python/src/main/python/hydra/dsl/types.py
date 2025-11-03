6a745411a525405ce0a52280cb0153a4c6aa1778
Joshua Shinavier <josh@fortytwo.net>
2025-11-02 23:09:19 -0800

    Update and expand the Types DSL in Python

--- hydra-python/src/main/python/hydra/dsl/types.py
+++ hydra-python/src/main/python/hydra/dsl/types.py
@@ -3,6 +3,7 @@
 from collections.abc import Sequence
 from functools import reduce
 
+import hydra.constants
 import hydra.dsl.literal_types as lt
 from hydra.core import (
     AnnotatedType,
@@ -32,237 +33,243 @@ from hydra.core import (
     TypeSet,
     TypeSum,
     TypeUnion,
+    TypeUnit,
     TypeVariable,
     TypeWrap,
     WrappedType,
 )
 from hydra.dsl.python import FrozenDict
 
-PLACEHOLDER_NAME = Name("placeholder")
 
-
-def boolean() -> Type:
-    """Construct a boolean type."""
-    return literal(lt.boolean())
-
-
-def binary() -> Type:
-    """Construct a binary type."""
-    return literal(lt.binary())
-
-
-def string() -> Type:
-    """Construct a string type."""
-    return literal(lt.string())
-
-
-def integer(itype: IntegerType) -> Type:
-    """Construct an integer type."""
-    return literal(lt.integer(itype))
-
-
-def uint8() -> Type:
-    """Construct a uint8 type."""
-    return literal(lt.uint8())
-
-
-def uint16() -> Type:
-    """Construct a uint16 type."""
-    return literal(lt.uint16())
-
-
-def uint32() -> Type:
-    """Construct a uint32 type."""
-    return literal(lt.uint32())
-
-
-def uint64() -> Type:
-    """Construct a uint64 type."""
-    return literal(lt.uint64())
-
-
-def int8() -> Type:
-    """Construct an int8 type."""
-    return literal(lt.int8())
-
-
-def int16() -> Type:
-    """Construct an int16 type."""
-    return literal(lt.int16())
-
-
-def int32() -> Type:
-    """Construct an int32 type."""
-    return literal(lt.int32())
-
-
-def int64() -> Type:
-    """Construct an int64 type."""
-    return literal(lt.int64())
-
-
-def non_negative_int32() -> Type:
-    """Construct a non-negative int32 type."""
-    return int32()
-
-
-def bigint() -> Type:
-    """Construct a bigint type."""
-    return literal(lt.bigint())
-
-
-def float_(ftype: FloatType) -> Type:
-    """Construct a float type."""
-    return literal(lt.float_(ftype))
-
-
-def float32() -> Type:
-    """Construct a float32 type."""
-    return literal(lt.float32())
-
-
-def float64() -> Type:
-    """Construct a float64 type."""
-    return literal(lt.float64())
-
-
-def bigfloat() -> Type:
-    """Construct a bigfloat type."""
-    return literal(lt.bigfloat())
+# Operators - TODO: find Python equivalents for these special syntax forms
+# (-->) :: Type -> Type -> Type  -- function type constructor operator
+# (@@) :: Type -> Type -> Type   -- type application operator
+# (>:) :: String -> Type -> FieldType  -- field definition operator
 
 
 def annot(ann: dict[str, Term], t: Type) -> Type:
-    """Construct an annotated type."""
+    """Attach an annotation to a type."""
     return TypeAnnotated(
         AnnotatedType(t, FrozenDict({Name(k): v for k, v in ann.items()}))
     )
 
 
 def apply(lhs: Type, rhs: Type) -> Type:
-    """Construct an application type."""
+    """Apply a type to a type argument."""
     return TypeApplication(ApplicationType(lhs, rhs))
 
 
-def apply_n(ts: list[Type]) -> Type:
-    """Construct an application type."""
+def applys(t: Type, ts: Sequence[Type]) -> Type:
+    """Apply a type to multiple type arguments."""
+    return reduce(apply, ts, t)
+
+
+def apply_many(ts: Sequence[Type]) -> Type:
+    """Apply a type to multiple type arguments."""
     return reduce(apply, ts[1:], ts[0])
 
 
-def set_(t: Type) -> Type:
-    """Construct a set type."""
-    return TypeSet(t)
+def var(name: str) -> Type:
+    """Create a type variable with the given name."""
+    return TypeVariable(Name(name))
 
 
-def sum_(ts: Sequence[Type]) -> Type:
-    """Construct a sum type."""
-    return TypeSum(tuple(ts))
+def function(dom: Type, cod: Type) -> Type:
+    """Create a function type."""
+    return TypeFunction(FunctionType(dom, cod))
 
 
-def union(fields: Sequence[FieldType]) -> Type:
-    """Construct a union type."""
-    return TypeUnion(RowType(PLACEHOLDER_NAME, tuple(fields)))
+def function_many(ts: Sequence[Type]) -> Type:
+    """Create an n-ary function type."""
+    r = list(reversed(ts))
+    return reduce(lambda cod, dom: function(dom, cod), r[1:], r[0])
 
 
-def unit() -> Type:
-    """Construct a unit type."""
-    return record([field(PLACEHOLDER_NAME, unit())])
+def forall(v: str, body: Type) -> Type:
+    """Create a universally quantified type (polymorphic type) with a single variable."""
+    return TypeForall(ForallType(Name(v), body))
 
 
-def enum(names: list[str]) -> Type:
-    """Construct an enum type."""
-    return union([field(Name(n), unit()) for n in names])
-
-
-def field(n: Name, t: Type) -> FieldType:
-    """Construct a field type."""
-    return FieldType(n, t)
-
-
-def fields_to_map(fields: list[FieldType]) -> dict[Name, Type]:
-    """Construct a map type from a list of fields."""
-    return {f.name: f.type for f in fields}
-
-
-def forall(v: Name, b: Type) -> Type:
-    """Construct a unary forall type."""
-    return TypeForall(ForallType(v, b))
-
-
-def foralls(vs: list[str], body: Type) -> Type:
-    """Construct a multi-ary forall type."""
-    return reduce(lambda acc, v: forall(Name(v), acc), reversed(vs), body)
-
-
-def function(a: Type, b: Type) -> Type:
-    """Construct a function type."""
-    return TypeFunction(FunctionType(a, b))
-
-
-def function_n(ts: list[Type]) -> Type:
-    """Construct a function type."""
-    return reduce(lambda acc, t: function(t, acc), reversed(ts), ts[0])
-
-
-def list_(t: Type) -> Type:
-    """Construct a list type."""
-    return TypeList(t)
-
-
-def literal(t: LiteralType) -> Type:
-    """Construct a literal type."""
-    return TypeLiteral(t)
-
-
-def map_(k: Type, v: Type) -> Type:
-    """Construct a map type."""
-    return TypeMap(MapType(k, v))
+def foralls(vs: Sequence[str], body: Type) -> Type:
+    """Universal quantification with multiple variables."""
+    return reduce(lambda acc, v: forall(v, acc), reversed(vs), body)
 
 
 def mono(t: Type) -> TypeScheme:
-    """Construct a type scheme."""
+    """Create a monomorphic type scheme."""
     return TypeScheme((), t)
 
 
-def optional(t: Type) -> Type:
-    """Construct an optional type."""
-    return TypeOptional(t)
-
-
-def pair(a: Type, b: Type) -> Type:
-    """Construct a pair type."""
-    return product([a, b])
-
-
 def poly(vs: Sequence[str], t: Type) -> TypeScheme:
-    """Construct a type scheme."""
+    """Create a polymorphic type scheme with explicit type variables."""
     return TypeScheme(tuple(Name(v) for v in vs), t)
 
 
-def product(ts: Sequence[Type]) -> Type:
-    """Construct a product type."""
-    return TypeProduct(tuple(ts))
+def bigfloat() -> Type:
+    """Arbitrary-precision floating point type."""
+    return literal(lt.bigfloat())
+
+
+def bigint() -> Type:
+    """Arbitrary-precision integer type."""
+    return literal(lt.bigint())
+
+
+def binary() -> Type:
+    """Binary data type."""
+    return literal(lt.binary())
+
+
+def boolean() -> Type:
+    """Boolean type."""
+    return literal(lt.boolean())
+
+
+def float32() -> Type:
+    """32-bit floating point type."""
+    return literal(lt.float32())
+
+
+def float64() -> Type:
+    """64-bit floating point type."""
+    return literal(lt.float64())
+
+
+def float_(ftype: FloatType) -> Type:
+    """Create a floating point type with the specified precision."""
+    return literal(lt.float_(ftype))
+
+
+def int8() -> Type:
+    """8-bit signed integer type."""
+    return literal(lt.int8())
+
+
+def int16() -> Type:
+    """16-bit signed integer type."""
+    return literal(lt.int16())
+
+
+def int32() -> Type:
+    """32-bit signed integer type."""
+    return literal(lt.int32())
+
+
+def int64() -> Type:
+    """64-bit signed integer type."""
+    return literal(lt.int64())
+
+
+def integer(itype: IntegerType) -> Type:
+    """Create an integer type with the specified bit width."""
+    return literal(lt.integer(itype))
+
+
+def literal(t: LiteralType) -> Type:
+    """Literal primitive type."""
+    return TypeLiteral(t)
+
+
+def non_negative_int32() -> Type:
+    """Non-negative 32-bit integer type."""
+    return int32()
+
+
+def string() -> Type:
+    """String type."""
+    return literal(lt.string())
+
+
+def uint8() -> Type:
+    """8-bit unsigned integer type."""
+    return literal(lt.uint8())
+
+
+def uint16() -> Type:
+    """16-bit unsigned integer type."""
+    return literal(lt.uint16())
+
+
+def uint32() -> Type:
+    """32-bit unsigned integer type."""
+    return literal(lt.uint32())
+
+
+def uint64() -> Type:
+    """64-bit unsigned integer type."""
+    return literal(lt.uint64())
+
+
+def list_(t: Type) -> Type:
+    """List type."""
+    return TypeList(t)
+
+
+def map_(k: Type, v: Type) -> Type:
+    """Map/dictionary type with key and value types."""
+    return TypeMap(MapType(k, v))
+
+
+def optional(t: Type) -> Type:
+    """Optional (nullable) type."""
+    return TypeOptional(t)
+
+
+def set_(t: Type) -> Type:
+    """Set type."""
+    return TypeSet(t)
+
+
+def enum(names: Sequence[str]) -> Type:
+    """Create an enum type with the given variant names."""
+    return union([field(n, unit()) for n in names])
+
+
+def field(fn: str, t: Type) -> FieldType:
+    """Create a field with the given name and type."""
+    return FieldType(Name(fn), t)
 
 
 def record(fields: Sequence[FieldType]) -> Type:
-    """Construct a record type."""
-    return TypeRecord(RowType(PLACEHOLDER_NAME, tuple(fields)))
+    """Create a record type with the given fields and the default type name."""
+    return record_with_name(hydra.constants.placeholder_name, fields)
 
 
-def scheme(variables: Sequence[str], body: Type) -> TypeScheme:
-    """Construct a type scheme."""
-    return TypeScheme(tuple(Name(v) for v in variables), body)
+def record_with_name(tname: Name, fields: Sequence[FieldType]) -> Type:
+    """Create a record type with the given fields and a provided type name."""
+    return TypeRecord(RowType(tname, tuple(fields)))
 
 
-def var(n: Name) -> Type:
-    """Construct a variable type."""
-    return TypeVariable(n)
+def unit() -> Type:
+    """Unit type."""
+    return TypeUnit(None)
 
 
-def wrap_with_name(name: Name, t: Type) -> Type:
-    """Construct a wrapped type."""
-    return TypeWrap(WrappedType(name, t))
+def union(fields: Sequence[FieldType]) -> Type:
+    """Create a union type with the given variants and the default type name."""
+    return TypeUnion(RowType(hydra.constants.placeholder_name, tuple(fields)))
 
 
 def wrap(t: Type) -> Type:
-    """Construct a wrapped type."""
-    return wrap_with_name(PLACEHOLDER_NAME, t)
+    """Create a wrapped type (newtype) with a provided base type and the default type name."""
+    return wrap_with_name(hydra.constants.placeholder_name, t)
+
+
+def wrap_with_name(name: Name, t: Type) -> Type:
+    """Create a wrapped type (newtype) with a provided base type and type name."""
+    return TypeWrap(WrappedType(name, t))
+
+
+def pair(a: Type, b: Type) -> Type:
+    """Create a pair (2-tuple) type."""
+    return TypeProduct((a, b))
+
+
+def product(ts: Sequence[Type]) -> Type:
+    """Create a product type (tuple) with multiple components."""
+    return TypeProduct(tuple(ts))
+
+
+def sum_(ts: Sequence[Type]) -> Type:
+    """Create a sum type (disjoint union) with multiple variants."""
+    return TypeSum(tuple(ts))
