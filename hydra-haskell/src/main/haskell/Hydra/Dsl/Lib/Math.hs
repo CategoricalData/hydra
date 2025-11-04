1e2e97c5dfb875a7beae900a2b091cc25fd86842
Joshua Shinavier <josh@fortytwo.net>
2025-11-03 23:10:34 -0800

    Add 'even' and 'odd' for symmetry with Prelude

--- hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Math.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Math.hs
@@ -8,38 +8,44 @@ import qualified Hydra.Dsl.Terms as Terms
 import Hydra.Sources.Libraries
 
 
-abs :: TTerm a -> TTerm a
+abs :: Num a => TTerm a -> TTerm a
 abs = primitive1 _math_abs
 
-add :: TTerm a -> TTerm a -> TTerm a
+add :: Num a => TTerm a -> TTerm a -> TTerm a
 add = primitive2 _math_add
 
-div :: TTerm a -> TTerm a -> TTerm a
+div :: Integral a => TTerm a -> TTerm a -> TTerm a
 div = primitive2 _math_div
 
-mod :: TTerm a -> TTerm a -> TTerm a
+even :: Integral a => TTerm a -> TTerm Bool
+even = primitive1 _math_even
+
+mod :: Integral a => TTerm a -> TTerm a -> TTerm a
 mod = primitive2 _math_mod
 
-mul :: TTerm a -> TTerm a -> TTerm a
+mul :: Num a => TTerm a -> TTerm a -> TTerm a
 mul = primitive2 _math_mul
 
-negate :: TTerm a -> TTerm a
+negate :: Num a => TTerm a -> TTerm a
 negate = primitive1 _math_negate
 
-pred :: TTerm a -> TTerm a
+odd :: Integral a => TTerm a -> TTerm Bool
+odd = primitive1 _math_odd
+
+pred :: Enum a => TTerm a -> TTerm a
 pred = primitive1 _math_pred
 
-range :: TTerm a -> TTerm a -> TTerm [a]
+range :: Enum a => TTerm a -> TTerm a -> TTerm [a]
 range start end = primitive2 _math_range start end
 
-rem :: TTerm a -> TTerm a -> TTerm a
+rem :: Integral a => TTerm a -> TTerm a -> TTerm a
 rem = primitive2 _math_rem
 
-signum :: TTerm a -> TTerm a
+signum :: Num a => TTerm a -> TTerm a
 signum = primitive1 _math_signum
 
-sub :: TTerm a -> TTerm a -> TTerm a
+sub :: Num a => TTerm a -> TTerm a -> TTerm a
 sub = primitive2 _math_sub
 
-succ :: TTerm a -> TTerm a
+succ :: Enum a => TTerm a -> TTerm a
 succ = primitive1 _math_succ
