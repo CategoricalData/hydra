799f4da426247600963d71a058906894cbd85314
Joshua Shinavier <josh@fortytwo.net>
2025-11-03 22:27:28 -0800

    Add 'abs', 'pred', 'signum', and 'succ' functions for symmetry with Prelude

--- hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Math.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Lib/Math.hs
@@ -8,6 +8,9 @@ import qualified Hydra.Dsl.Terms as Terms
 import Hydra.Dsl.Phantoms
 
 
+abs :: TTerm a -> TTerm a
+abs = primitive1 _math_abs
+
 add :: TTerm a -> TTerm a -> TTerm a
 add = primitive2 _math_add
 
@@ -23,11 +26,20 @@ mul = primitive2 _math_mul
 neg :: TTerm a -> TTerm a
 neg = primitive1 _math_neg
 
+pred :: TTerm a -> TTerm a
+pred = primitive1 _math_pred
+
 range :: TTerm a -> TTerm a -> TTerm [a]
 range start end = primitive2 _math_range start end
 
 rem :: TTerm a -> TTerm a -> TTerm a
 rem = primitive2 _math_rem
 
+signum :: TTerm a -> TTerm a
+signum = primitive1 _math_signum
+
 sub :: TTerm a -> TTerm a -> TTerm a
 sub = primitive2 _math_sub
+
+succ :: TTerm a -> TTerm a
+succ = primitive1 _math_succ
