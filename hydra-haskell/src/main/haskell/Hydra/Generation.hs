07d144f929cdf92d63daaa0cc474ae53bd90eaf9
Joshua Shinavier <josh@fortytwo.net>
2025-11-12 14:45:39 -0800

    Add a utility for generating hydra-lexicon.txt, the API overview which is used for LLM-assisted Hydra code generation

--- hydra-haskell/src/main/haskell/Hydra/Generation.hs
+++ hydra-haskell/src/main/haskell/Hydra/Generation.hs
@@ -9,6 +9,10 @@ import Hydra.Ext.Haskell.Coder
 import Hydra.Ext.Org.Json.Coder
 import Hydra.Staging.Yaml.Modules
 import Hydra.Sources.Libraries
+import qualified Hydra.Decode.Core as DecodeCore
+import qualified Hydra.Inference as Inference
+import qualified Hydra.Show.Core as ShowCore
+import qualified Hydra.Sources.All as Sources
 
 import qualified Control.Monad as CM
 import qualified System.FilePath as FP
@@ -128,3 +132,66 @@ writeHaskell = generateSources moduleToHaskell
 
 writeYaml :: FP.FilePath -> [Module] -> IO ()
 writeYaml = generateSources moduleToYaml
+
+-- | Generate the lexicon content from a graph
+generateLexicon :: Graph -> Flow Graph String
+generateLexicon graph = do
+  let bindings = M.elems $ graphElements graph
+      primitives = M.elems $ graphPrimitives graph
+      (typeBindings, termBindings) = L.partition isNativeType bindings
+      sortedPrimitives = L.sortBy comparePrimitiveNames primitives
+      sortedTypes = L.sortBy compareBindingNames typeBindings
+      sortedTerms = L.sortBy compareBindingNames termBindings
+  typeLines <- CM.mapM formatTypeBinding sortedTypes
+  let termLines = fmap formatTermBinding sortedTerms
+      primitiveLines = fmap formatPrimitive sortedPrimitives
+  return $ "Primitives:\n" ++ unlines primitiveLines ++
+    "\nTypes:\n" ++ unlines typeLines ++
+    "\nTerms:\n" ++ unlines termLines
+  where
+    compareBindingNames a b = compare (bindingName a) (bindingName b)
+    comparePrimitiveNames a b = compare (primitiveName a) (primitiveName b)
+
+-- | Format a type binding for the lexicon
+formatTypeBinding :: Binding -> Flow Graph String
+formatTypeBinding binding = do
+  let name = unName $ bindingName binding
+  typ <- DecodeCore.type_ (bindingTerm binding)
+  let typeStr = ShowCore.type_ typ
+  return $ "  " ++ name ++ " = " ++ typeStr
+
+-- | Format a term binding for the lexicon
+formatTermBinding :: Binding -> String
+formatTermBinding binding =
+  let name = unName $ bindingName binding
+      typeStr = case bindingType binding of
+        Just scheme -> ShowCore.typeScheme scheme
+        Nothing -> "?"
+  in "  " ++ name ++ " : " ++ typeStr
+
+-- | Format a primitive for the lexicon
+formatPrimitive :: Primitive -> String
+formatPrimitive prim =
+  let name = unName $ primitiveName prim
+      scheme = primitiveType prim
+      typeStr = ShowCore.typeScheme scheme
+  in "  " ++ name ++ " : " ++ typeStr
+
+-- | Generate and write the lexicon file
+writeLexicon :: FilePath -> IO ()
+writeLexicon path = do
+  let g0 = modulesToGraph Sources.kernelModules
+  mg1 <- runFlow g0 (Inference.inferGraphTypes g0)
+  case mg1 of
+    Nothing -> fail "Type inference failed"
+    Just g1 -> do
+      mcontent <- runFlow g1 (generateLexicon g1)
+      case mcontent of
+        Nothing -> fail "Lexicon generation failed"
+        Just content -> do
+          writeFile path content
+          putStrLn $ "Lexicon written to " ++ path
+
+-- | Generate the lexicon to the standard location
+writeLexiconToStandardPath :: IO ()
+writeLexiconToStandardPath = writeLexicon "../docs/hydra-lexicon.txt"
