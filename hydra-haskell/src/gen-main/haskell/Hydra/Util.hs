5b75e1755c8a169254e36f08f2af1e640a0ba345
Joshua Shinavier <josh@fortytwo.net>
2025-11-12 10:21:51 -0800

    Create the hydra.meta and hydra.util modules (part of #217)

---
+++ hydra-haskell/src/gen-main/haskell/Hydra/Util.hs
@@ -0,0 +1,57 @@
+-- | General-purpose utility types used across Hydra.
+
+module Hydra.Util where
+
+import qualified Hydra.Core as Core
+import Prelude hiding  (Enum, Ordering, fail, map, pure, sum)
+import qualified Data.Int as I
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+-- | A naming convention for symbols, such as camelCase or snake_case
+data CaseConvention = 
+  CaseConventionCamel  |
+  CaseConventionPascal  |
+  CaseConventionLowerSnake  |
+  CaseConventionUpperSnake 
+  deriving (Eq, Ord, Read, Show)
+
+_CaseConvention = (Core.Name "hydra.util.CaseConvention")
+
+_CaseConvention_camel = (Core.Name "camel")
+
+_CaseConvention_pascal = (Core.Name "pascal")
+
+_CaseConvention_lowerSnake = (Core.Name "lowerSnake")
+
+_CaseConvention_upperSnake = (Core.Name "upperSnake")
+
+-- | An equality judgement: less than, equal to, or greater than
+data Comparison = 
+  ComparisonLessThan  |
+  ComparisonEqualTo  |
+  ComparisonGreaterThan 
+  deriving (Eq, Ord, Read, Show)
+
+_Comparison = (Core.Name "hydra.util.Comparison")
+
+_Comparison_lessThan = (Core.Name "lessThan")
+
+_Comparison_equalTo = (Core.Name "equalTo")
+
+_Comparison_greaterThan = (Core.Name "greaterThan")
+
+-- | Numeric precision: arbitrary precision, or precision to a specified number of bits
+data Precision = 
+  -- | Arbitrary precision
+  PrecisionArbitrary  |
+  -- | Precision to a specified number of bits
+  PrecisionBits Int
+  deriving (Eq, Ord, Read, Show)
+
+_Precision = (Core.Name "hydra.util.Precision")
+
+_Precision_arbitrary = (Core.Name "arbitrary")
+
+_Precision_bits = (Core.Name "bits")
---
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Kernel/Types/Util.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hydra.Sources.Kernel.Types.Util where
+
+-- Standard type-level kernel imports
+import           Hydra.Kernel
+import           Hydra.Dsl.Annotations
+import           Hydra.Dsl.Bootstrap
+import qualified Hydra.Dsl.Terms                 as Terms
+import           Hydra.Dsl.Types                 as Types
+import qualified Hydra.Sources.Kernel.Types.Core as Core
+import qualified Data.List                       as L
+import qualified Data.Map                        as M
+import qualified Data.Set                        as S
+import qualified Data.Maybe                      as Y
+
+
+module_ :: Module
+module_ = Module ns elements [Core.module_] [Core.module_] $
+    Just ("General-purpose utility types used across Hydra.")
+  where
+    ns = Namespace "hydra.util"
+    core = typeref $ moduleNamespace Core.module_
+    util = typeref ns
+    def = datatype ns
+
+    elements = [
+
+      def "CaseConvention" $
+        doc "A naming convention for symbols, such as camelCase or snake_case" $
+        Types.enum ["camel", "pascal", "lowerSnake", "upperSnake"],
+
+      def "Comparison" $
+        doc "An equality judgement: less than, equal to, or greater than" $
+        enum [
+          "lessThan",
+          "equalTo",
+          "greaterThan"],
+
+      def "Precision" $
+        doc "Numeric precision: arbitrary precision, or precision to a specified number of bits" $
+        union [
+          "arbitrary">:
+            doc "Arbitrary precision" $
+            unit,
+          "bits">:
+            doc "Precision to a specified number of bits" $
+            int32]]
