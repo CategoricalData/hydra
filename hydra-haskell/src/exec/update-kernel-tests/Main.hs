af4741d15d8eb1585bb2375d6e32d4484ae12376
Joshua Shinavier <josh@fortytwo.net>
2025-11-24 19:05:29 -0800

    Add a script for updating generated kernel test cases

---
+++ hydra-haskell/src/exec/update-kernel-tests/Main.hs
@@ -0,0 +1,30 @@
+#!/usr/bin/env stack
+{- stack script
+   --resolver lts-22.28
+   --package hydra
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Hydra.Generation
+import Hydra.Sources.All
+
+main :: IO ()
+main = do
+  putStrLn "=== Generate Hydra kernel tests ==="
+  putStrLn ""
+  putStrLn "Generating kernel test modules to src/gen-test/haskell..."
+  putStrLn ""
+
+  writeHaskell "src/gen-test/haskell" testModules
+
+  putStrLn ""
+  putStrLn "=== Done! ==="
+  putStrLn ""
+  putStrLn "To view the generated test modules:"
+  putStrLn "  ls -R src/gen-test/haskell"
+  putStrLn ""
+  putStrLn "To run the tests:"
+  putStrLn "  stack test"
