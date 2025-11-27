029a71d7b9f3c4b8fe06df48642dcd2c249e2d88
Joshua Shinavier <josh@fortytwo.net>
2025-11-27 13:04:45 -0800

    Helper for generating generation tests from ghci

---
+++ hydra-haskell/src/test/haskell/Hydra/UpdateGenerationTests.hs
@@ -0,0 +1,32 @@
+module Hydra.UpdateGenerationTests where
+
+import Hydra.Staging.Testing.Generation.Generate
+import Hydra.Staging.Testing.Generation.HaskellCodec (haskellTestGenerator)
+import qualified Hydra.Sources.Test.TestSuite as TestSuite
+import qualified Hydra.Test.TestSuite as GenTests
+
+
+updateGenerationTestsHaskell :: IO ()
+updateGenerationTestsHaskell = do
+  putStrLn "=== Generate Hydra generation tests ==="
+  putStrLn ""
+
+  -- Automatically build the lookup function from module and test group hierarchies
+  let lookupFn = createTestGroupLookup TestSuite.module_ GenTests.allTests
+
+  -- Generate generation tests to src/gen-test/haskell
+  let outputDir = "src/gen-test/haskell"
+
+  putStrLn $ "Generating tests into: " ++ outputDir
+  putStrLn ""
+
+  generateGenerationTestSuite haskellTestGenerator outputDir TestSuite.module_ lookupFn
+
+  putStrLn ""
+  putStrLn "=== Done! ==="
+  putStrLn ""
+  putStrLn "To view the generated tests:"
+  putStrLn $ "  ls -R " ++ outputDir
+  putStrLn ""
+  putStrLn "To run the generated tests:"
+  putStrLn "  stack test"
