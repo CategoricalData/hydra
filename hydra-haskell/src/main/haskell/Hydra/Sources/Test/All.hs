4306dc226cc735f8b01e2cd0f2ad2ce9db4065ab
Joshua Shinavier <josh@fortytwo.net>
2025-11-30 14:26:23 -0800

    Fix dependencies for the common test suite

--- hydra-haskell/src/main/haskell/Hydra/Sources/Test/All.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Test/All.hs
@@ -6,7 +6,9 @@ import qualified Hydra.Sources.Test.TestGraph as TestGraph
 import qualified Hydra.Sources.Test.TestSuite as TestSuite
 import qualified Hydra.Sources.Test.TestTerms as TestTerms
 import qualified Hydra.Sources.Test.TestTypes as TestTypes
+import qualified Hydra.Lib.Strings as Strings
 
+import qualified Data.List as L
 import qualified Data.Map as M
 
 
@@ -15,9 +17,14 @@ testModules = [
   TestGraph.module_,
   TestTerms.module_,
   TestTypes.module_]
-  ++ moduleAndDependencies TestSuite.module_
+  ++ testModuleAndDependencies TestSuite.module_
 
 moduleAndDependencies :: Module -> [Module]
 moduleAndDependencies modl = M.elems $ add M.empty modl
   where
     add m md = M.insert (moduleNamespace md) md m `M.union` M.unions (fmap (add m) (moduleTermDependencies md))
+
+testModuleAndDependencies :: Module -> [Module]
+testModuleAndDependencies modl = L.filter isTestModule (moduleTermDependencies modl)
+  where
+     isTestModule md = L.take 11 (unNamespace $ moduleNamespace md) == "hydra.test."
