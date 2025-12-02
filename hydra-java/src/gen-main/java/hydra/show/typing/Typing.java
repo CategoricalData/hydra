e3d7a1cb2f80a9366834676bdfab31c1574cb6a4
Joshua Shinavier <josh@fortytwo.net>
2025-12-02 08:16:58 -0800

    Update generated terms modules in Java

--- hydra-java/src/gen-main/java/hydra/show/typing/Typing.java
+++ hydra-java/src/gen-main/java/hydra/show/typing/Typing.java
@@ -7,8 +7,8 @@ package hydra.show.typing;
  */
 public interface Typing {
   static String typeConstraint(hydra.typing.TypeConstraint tc) {
-    hydra.core.Type ltyp = ((hydra.typing.TypeConstraint) ((tc))).left;
-    hydra.core.Type rtyp = ((hydra.typing.TypeConstraint) ((tc))).right;
+    hydra.core.Type ltyp = ((tc)).left;
+    hydra.core.Type rtyp = ((tc)).right;
     return hydra.lib.strings.Cat.apply(java.util.List.of(
       hydra.show.core.Core.type((ltyp)),
       "\u2261",
@@ -18,9 +18,9 @@ public interface Typing {
   static String typeSubst(hydra.typing.TypeSubst ts) {
     java.util.Map<hydra.core.Name, hydra.core.Type> subst = ((ts)).value;
     java.util.List<hydra.util.Tuple.Tuple2<hydra.core.Name, hydra.core.Type>> pairs = hydra.lib.maps.ToList.apply((subst));
-    java.util.function.Function<hydra.util.Tuple.Tuple2<hydra.core.Name, hydra.core.Type>, String> showPair = (java.util.function.Function<hydra.util.Tuple.Tuple2<hydra.core.Name, hydra.core.Type>, String>) (tuple2 -> {
-      String name = (((tuple2)).object1).value;
-      hydra.core.Type typ = ((tuple2)).object2;
+    java.util.function.Function<hydra.util.Tuple.Tuple2<hydra.core.Name, hydra.core.Type>, String> showPair = (java.util.function.Function<hydra.util.Tuple.Tuple2<hydra.core.Name, hydra.core.Type>, String>) (pair -> {
+      String name = (hydra.lib.pairs.First.apply((pair))).value;
+      hydra.core.Type typ = hydra.lib.pairs.Second.apply((pair));
       return hydra.lib.strings.Cat.apply(java.util.List.of(
         (name),
         "\u21A6",
