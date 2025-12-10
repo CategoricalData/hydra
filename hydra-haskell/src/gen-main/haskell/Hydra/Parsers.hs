d8da0635396651384c30f3df1ac9af1c7f6cc2d2
Joshua Shinavier <josh@fortytwo.net>
2025-12-10 13:21:58 -0800

    Minor updates to the new hydra.parsers module for Python compatibility

--- hydra-haskell/src/gen-main/haskell/Hydra/Parsers.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Parsers.hs
@@ -16,41 +16,44 @@ import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-pure :: (t0 -> Parsing.Parser t0)
-pure a = (Parsing.Parser (\input -> Parsing.ParseResultSuccess (Parsing.ParseSuccess {
-  Parsing.parseSuccessValue = a,
-  Parsing.parseSuccessRemainder = input})))
+alt :: (Parsing.Parser t0 -> Parsing.Parser t0 -> Parsing.Parser t0)
+alt p1 p2 =  
+  let parse = (\input -> (\x -> case x of
+          Parsing.ParseResultSuccess v1 -> (Parsing.ParseResultSuccess v1)
+          Parsing.ParseResultFailure v1 -> (Logic.ifElse (Equality.equal (Parsing.parseErrorRemainder v1) input) (Parsing.unParser p2 input) (Parsing.ParseResultFailure v1))) (Parsing.unParser p1 input))
+  in (Parsing.Parser parse)
 
-fail :: (String -> Parsing.Parser t0)
-fail msg = (Parsing.Parser (\input -> Parsing.ParseResultFailure (Parsing.ParseError {
-  Parsing.parseErrorMessage = msg,
-  Parsing.parseErrorRemainder = input})))
-
-bind :: (Parsing.Parser t0 -> (t0 -> Parsing.Parser t1) -> Parsing.Parser t1)
-bind pa f = (Parsing.Parser (\input -> (\x -> case x of
-  Parsing.ParseResultSuccess v1 -> (Parsing.unParser (f (Parsing.parseSuccessValue v1)) (Parsing.parseSuccessRemainder v1))
-  Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pa input)))
-
-map :: ((t0 -> t1) -> Parsing.Parser t0 -> Parsing.Parser t1)
-map f pa = (Parsing.Parser (\input -> (\x -> case x of
-  Parsing.ParseResultSuccess v1 -> (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
-    Parsing.parseSuccessValue = (f (Parsing.parseSuccessValue v1)),
-    Parsing.parseSuccessRemainder = (Parsing.parseSuccessRemainder v1)}))
-  Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pa input)))
+-- | Parse any single character (codepoint)
+anyChar :: (Parsing.Parser Int)
+anyChar = (satisfy (\_ -> True))
 
 apply :: (Parsing.Parser (t0 -> t1) -> Parsing.Parser t0 -> Parsing.Parser t1)
-apply pf pa = (Parsing.Parser (\input -> (\x -> case x of
-  Parsing.ParseResultSuccess v1 -> ((\x -> case x of
-    Parsing.ParseResultSuccess v2 -> (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
-      Parsing.parseSuccessValue = (Parsing.parseSuccessValue v1 (Parsing.parseSuccessValue v2)),
-      Parsing.parseSuccessRemainder = (Parsing.parseSuccessRemainder v2)}))
-    Parsing.ParseResultFailure v2 -> (Parsing.ParseResultFailure v2)) (Parsing.unParser pa (Parsing.parseSuccessRemainder v1)))
-  Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pf input)))
+apply pf pa =  
+  let parse = (\input -> (\x -> case x of
+          Parsing.ParseResultSuccess v1 -> ((\x -> case x of
+            Parsing.ParseResultSuccess v2 -> (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
+              Parsing.parseSuccessValue = (Parsing.parseSuccessValue v1 (Parsing.parseSuccessValue v2)),
+              Parsing.parseSuccessRemainder = (Parsing.parseSuccessRemainder v2)}))
+            Parsing.ParseResultFailure v2 -> (Parsing.ParseResultFailure v2)) (Parsing.unParser pa (Parsing.parseSuccessRemainder v1)))
+          Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pf input))
+  in (Parsing.Parser parse)
 
-alt :: (Parsing.Parser t0 -> Parsing.Parser t0 -> Parsing.Parser t0)
-alt p1 p2 = (Parsing.Parser (\input -> (\x -> case x of
-  Parsing.ParseResultSuccess v1 -> (Parsing.ParseResultSuccess v1)
-  Parsing.ParseResultFailure v1 -> (Logic.ifElse (Equality.equal (Parsing.parseErrorRemainder v1) input) (Parsing.unParser p2 input) (Parsing.ParseResultFailure v1))) (Parsing.unParser p1 input)))
+between :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser t2 -> Parsing.Parser t2)
+between open close p = (bind open (\_ -> bind p (\x -> bind close (\_ -> pure x))))
+
+bind :: (Parsing.Parser t0 -> (t0 -> Parsing.Parser t1) -> Parsing.Parser t1)
+bind pa f =  
+  let parse = (\input -> (\x -> case x of
+          Parsing.ParseResultSuccess v1 -> (Parsing.unParser (f (Parsing.parseSuccessValue v1)) (Parsing.parseSuccessRemainder v1))
+          Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pa input))
+  in (Parsing.Parser parse)
+
+-- | Parse a specific character (codepoint)
+char :: (Int -> Parsing.Parser Int)
+char c = (satisfy (\x -> Equality.equal x c))
+
+choice :: ([Parsing.Parser t0] -> Parsing.Parser t0)
+choice ps = (Lists.foldl alt (fail "no choice matched") ps)
 
 -- | A parser that succeeds only at the end of input
 eof :: (Parsing.Parser ())
@@ -60,25 +63,60 @@ eof = (Parsing.Parser (\input -> Logic.ifElse (Equality.equal input "") (Parsing
   Parsing.parseErrorMessage = "expected end of input",
   Parsing.parseErrorRemainder = input}))))
 
+fail :: (String -> Parsing.Parser t0)
+fail msg = (Parsing.Parser (\input -> Parsing.ParseResultFailure (Parsing.ParseError {
+  Parsing.parseErrorMessage = msg,
+  Parsing.parseErrorRemainder = input})))
+
+many :: (Parsing.Parser t0 -> Parsing.Parser [t0])
+many p = (alt (some p) (pure []))
+
+map :: ((t0 -> t1) -> Parsing.Parser t0 -> Parsing.Parser t1)
+map f pa =  
+  let parse = (\input -> (\x -> case x of
+          Parsing.ParseResultSuccess v1 -> (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
+            Parsing.parseSuccessValue = (f (Parsing.parseSuccessValue v1)),
+            Parsing.parseSuccessRemainder = (Parsing.parseSuccessRemainder v1)}))
+          Parsing.ParseResultFailure v1 -> (Parsing.ParseResultFailure v1)) (Parsing.unParser pa input))
+  in (Parsing.Parser parse)
+
+optional :: (Parsing.Parser t0 -> Parsing.Parser (Maybe t0))
+optional p = (alt (map Maybes.pure p) (pure Nothing))
+
+pure :: (t0 -> Parsing.Parser t0)
+pure a = (Parsing.Parser (\input -> Parsing.ParseResultSuccess (Parsing.ParseSuccess {
+  Parsing.parseSuccessValue = a,
+  Parsing.parseSuccessRemainder = input})))
+
+runParser :: (Parsing.Parser t0 -> String -> Parsing.ParseResult t0)
+runParser p input = (Parsing.unParser p input)
+
 -- | Parse a character (codepoint) that satisfies the given predicate
 satisfy :: ((Int -> Bool) -> Parsing.Parser Int)
-satisfy pred = (Parsing.Parser (\input -> Logic.ifElse (Strings.null input) (Parsing.ParseResultFailure (Parsing.ParseError {
-  Parsing.parseErrorMessage = "unexpected end of input",
-  Parsing.parseErrorRemainder = input})) ( 
-  let codes = (Strings.toList input)
-  in  
-    let c = (Lists.head codes)
-    in  
-      let rest = (Strings.fromList (Lists.tail codes))
-      in (Logic.ifElse (pred c) (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
-        Parsing.parseSuccessValue = c,
-        Parsing.parseSuccessRemainder = rest})) (Parsing.ParseResultFailure (Parsing.ParseError {
-        Parsing.parseErrorMessage = "character did not satisfy predicate",
-        Parsing.parseErrorRemainder = input}))))))
+satisfy pred =  
+  let parse = (\input ->  
+          let codes = (Strings.toList input)
+          in  
+            let c = (Lists.head codes)
+            in  
+              let rest = (Strings.fromList (Lists.tail codes))
+              in (Logic.ifElse (Strings.null input) (Parsing.ParseResultFailure (Parsing.ParseError {
+                Parsing.parseErrorMessage = "unexpected end of input",
+                Parsing.parseErrorRemainder = input})) (Logic.ifElse (pred c) (Parsing.ParseResultSuccess (Parsing.ParseSuccess {
+                Parsing.parseSuccessValue = c,
+                Parsing.parseSuccessRemainder = rest})) (Parsing.ParseResultFailure (Parsing.ParseError {
+                Parsing.parseErrorMessage = "character did not satisfy predicate",
+                Parsing.parseErrorRemainder = input})))))
+  in (Parsing.Parser parse)
 
--- | Parse a specific character (codepoint)
-char :: (Int -> Parsing.Parser Int)
-char c = (satisfy (\x -> Equality.equal x c))
+sepBy :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser [t0])
+sepBy p sep = (alt (sepBy1 p sep) (pure []))
+
+sepBy1 :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser [t0])
+sepBy1 p sep = (bind p (\x -> bind (many (bind sep (\_ -> p))) (\xs -> pure (Lists.cons x xs))))
+
+some :: (Parsing.Parser t0 -> Parsing.Parser [t0])
+some p = (bind p (\x -> bind (many p) (\xs -> pure (Lists.cons x xs))))
 
 -- | Parse a specific string
 string :: (String -> Parsing.Parser String)
@@ -95,31 +133,3 @@ string str = (Parsing.Parser (\input ->
           Parsing.parseSuccessRemainder = (Strings.fromList (Lists.drop strLen inputCodes))})) (Parsing.ParseResultFailure (Parsing.ParseError {
           Parsing.parseErrorMessage = (Strings.cat2 "expected: " str),
           Parsing.parseErrorRemainder = input})))))
-
--- | Parse any single character (codepoint)
-anyChar :: (Parsing.Parser Int)
-anyChar = (satisfy (\_ -> True))
-
-many :: (Parsing.Parser t0 -> Parsing.Parser [t0])
-many p = (alt (some p) (pure []))
-
-some :: (Parsing.Parser t0 -> Parsing.Parser [t0])
-some p = (bind p (\x -> bind (many p) (\xs -> pure (Lists.cons x xs))))
-
-optional :: (Parsing.Parser t0 -> Parsing.Parser (Maybe t0))
-optional p = (alt (map Maybes.pure p) (pure Nothing))
-
-between :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser t2 -> Parsing.Parser t2)
-between open close p = (bind open (\_ -> bind p (\x -> bind close (\_ -> pure x))))
-
-sepBy :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser [t0])
-sepBy p sep = (alt (sepBy1 p sep) (pure []))
-
-sepBy1 :: (Parsing.Parser t0 -> Parsing.Parser t1 -> Parsing.Parser [t0])
-sepBy1 p sep = (bind p (\x -> bind (many (bind sep (\_ -> p))) (\xs -> pure (Lists.cons x xs))))
-
-choice :: ([Parsing.Parser t0] -> Parsing.Parser t0)
-choice ps = (Lists.foldl alt (fail "no choice matched") ps)
-
-runParser :: (Parsing.Parser t0 -> String -> Parsing.ParseResult t0)
-runParser p input = (Parsing.unParser p input)
