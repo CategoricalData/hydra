f499a1c7c26cfca451e42fd8bd775746b36e8698
Joshua Shinavier <josh@fortytwo.net>
2026-01-15 16:17:13 -0800

    Configure Hydra-Python to distinguish src/gen-main from src/main

--- hydra-python/src/test/python/test_generated_code.py
+++ hydra-python/src/test/python/test_generated_code.py
@@ -1,51 +1,51 @@
 """
 hydra-haskell {
 
-writePython "../hydra-python/src/main/python" kernelTypesModules
+writePython "../hydra-python/src/gen-main/python" kernelTypesModules
 
 writePython "../hydra-python/src/gen-test/python" testModules
 
 :set +m
 
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Adapt.Literals.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Adapt.Modules.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Adapt.Simple.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Adapt.Terms.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Adapt.Utils.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Decode.Core.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Describe.Core.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Describe.Mantle.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Encode.Core.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Extract.Core.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Extract.Mantle.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Show.Accessors.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Show.Core.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Show.Graph.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Show.Mantle.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Show.Typing.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Annotations.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Arity.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Checking.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Constants.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Decoding.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Formatting.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Grammars.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Inference.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Languages.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Lexical.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Literals.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Monads.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Names.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Reduction.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Rewriting.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Schemas.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Serialization.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Sorting.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Substitution.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Tarjan.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Templates.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Unification.module_]
-writePython "../hydra-python/src/main/python" [Hydra.Sources.Kernel.Terms.Variants.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Adapt.Literals.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Adapt.Modules.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Adapt.Simple.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Adapt.Terms.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Adapt.Utils.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Decode.Core.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Describe.Core.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Describe.Mantle.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Encode.Core.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Extract.Core.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Extract.Mantle.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Show.Accessors.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Show.Core.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Show.Graph.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Show.Mantle.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Show.Typing.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Annotations.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Arity.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Checking.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Constants.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Decoding.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Formatting.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Grammars.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Inference.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Languages.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Lexical.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Literals.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Monads.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Names.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Reduction.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Rewriting.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Schemas.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Serialization.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Sorting.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Substitution.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Tarjan.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Templates.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Unification.module_]
+writePython "../hydra-python/src/gen-main/python" [Hydra.Sources.Kernel.Terms.Variants.module_]
 
 }
 
@@ -73,67 +73,67 @@ pyright src/main/python/hydra.lib.maybes.py
 pyright src/main/python/hydra/lib/sets.py
 pyright src/main/python/hydra/lib/strings.py
 
-# Kernel types
-pyright src/main/python/hydra/accessors.py
-pyright src/main/python/hydra/ast.py
-pyright src/main/python/hydra/coders.py
-pyright src/main/python/hydra/compute.py
-pyright src/main/python/hydra/constraints.py
-pyright src/main/python/hydra/core.py
-pyright src/main/python/hydra/grammar.py
-pyright src/main/python/hydra/graph.py
-pyright src/main/python/hydra/json.py
-pyright src/main/python/hydra/mantle.py
-pyright src/main/python/hydra/module.py
-pyright src/main/python/hydra/phantoms.py
-pyright src/main/python/hydra/query.py
-pyright src/main/python/hydra/relational.py
-pyright src/main/python/hydra/tabular.py
-pyright src/main/python/hydra/testing.py
-pyright src/main/python/hydra/topology.py
-pyright src/main/python/hydra/typing.py
-pyright src/main/python/hydra/workflow.py
+# Kernel types (generated)
+pyright src/gen-main/python/hydra/accessors.py
+pyright src/gen-main/python/hydra/ast.py
+pyright src/gen-main/python/hydra/coders.py
+pyright src/gen-main/python/hydra/compute.py
+pyright src/gen-main/python/hydra/constraints.py
+pyright src/gen-main/python/hydra/core.py
+pyright src/gen-main/python/hydra/grammar.py
+pyright src/gen-main/python/hydra/graph.py
+pyright src/gen-main/python/hydra/json.py
+pyright src/gen-main/python/hydra/mantle.py
+pyright src/gen-main/python/hydra/module.py
+pyright src/gen-main/python/hydra/phantoms.py
+pyright src/gen-main/python/hydra/query.py
+pyright src/gen-main/python/hydra/relational.py
+pyright src/gen-main/python/hydra/tabular.py
+pyright src/gen-main/python/hydra/testing.py
+pyright src/gen-main/python/hydra/topology.py
+pyright src/gen-main/python/hydra/typing.py
+pyright src/gen-main/python/hydra/workflow.py
 
-# Kernel terms
-pyright src/main/python/hydra/adapt/literals.py
-#pyright src/main/python/hydra/adapt/modules.py # TODO
-pyright src/main/python/hydra/adapt/simple.py
-#pyright src/main/python/hydra/adapt/terms.py # TODO
-#pyright src/main/python/hydra/adapt/utils.py # TODO
-pyright src/main/python/hydra/decode/core.py
-pyright src/main/python/hydra/describe/core.py
-pyright src/main/python/hydra/describe/mantle.py
-pyright src/main/python/hydra/encode/core.py
-pyright src/main/python/hydra/extract/core.py
-pyright src/main/python/hydra/extract/mantle.py
-pyright src/main/python/hydra/show/accessors.py
-pyright src/main/python/hydra/show/core.py
-pyright src/main/python/hydra/show/graph.py
-pyright src/main/python/hydra/show/mantle.py
-pyright src/main/python/hydra/show/typing.py
-pyright src/main/python/hydra/annotations.py
-pyright src/main/python/hydra/arity.py
-pyright src/main/python/hydra/checking.py
-pyright src/main/python/hydra/constants.py
-#pyright src/main/python/hydra/decoding.py # TODO
-pyright src/main/python/hydra/formatting.py
-pyright src/main/python/hydra/grammars.py
-pyright src/main/python/hydra/inference.py
-pyright src/main/python/hydra/languages.py
-pyright src/main/python/hydra/lexical.py
-pyright src/main/python/hydra/literals.py
-pyright src/main/python/hydra/monads.py
-pyright src/main/python/hydra/names.py
-pyright src/main/python/hydra/reduction.py
-pyright src/main/python/hydra/rewriting.py
-pyright src/main/python/hydra/schemas.py
-pyright src/main/python/hydra/serialization.py
-pyright src/main/python/hydra/sorting.py
-pyright src/main/python/hydra/substitution.py
-pyright src/main/python/hydra/tarjan.py
-pyright src/main/python/hydra/templates.py
-pyright src/main/python/hydra/unification.py
-pyright src/main/python/hydra/variants.py
+# Kernel terms (generated)
+pyright src/gen-main/python/hydra/adapt/literals.py
+#pyright src/gen-main/python/hydra/adapt/modules.py # TODO
+pyright src/gen-main/python/hydra/adapt/simple.py
+#pyright src/gen-main/python/hydra/adapt/terms.py # TODO
+#pyright src/gen-main/python/hydra/adapt/utils.py # TODO
+pyright src/gen-main/python/hydra/decode/core.py
+pyright src/gen-main/python/hydra/describe/core.py
+pyright src/gen-main/python/hydra/describe/mantle.py
+pyright src/gen-main/python/hydra/encode/core.py
+pyright src/gen-main/python/hydra/extract/core.py
+pyright src/gen-main/python/hydra/extract/mantle.py
+pyright src/gen-main/python/hydra/show/accessors.py
+pyright src/gen-main/python/hydra/show/core.py
+pyright src/gen-main/python/hydra/show/graph.py
+pyright src/gen-main/python/hydra/show/mantle.py
+pyright src/gen-main/python/hydra/show/typing.py
+pyright src/gen-main/python/hydra/annotations.py
+pyright src/gen-main/python/hydra/arity.py
+pyright src/gen-main/python/hydra/checking.py
+pyright src/gen-main/python/hydra/constants.py
+#pyright src/gen-main/python/hydra/decoding.py # TODO
+pyright src/gen-main/python/hydra/formatting.py
+pyright src/gen-main/python/hydra/grammars.py
+pyright src/gen-main/python/hydra/inference.py
+pyright src/gen-main/python/hydra/languages.py
+pyright src/gen-main/python/hydra/lexical.py
+pyright src/gen-main/python/hydra/literals.py
+pyright src/gen-main/python/hydra/monads.py
+pyright src/gen-main/python/hydra/names.py
+pyright src/gen-main/python/hydra/reduction.py
+pyright src/gen-main/python/hydra/rewriting.py
+pyright src/gen-main/python/hydra/schemas.py
+pyright src/gen-main/python/hydra/serialization.py
+pyright src/gen-main/python/hydra/sorting.py
+pyright src/gen-main/python/hydra/substitution.py
+pyright src/gen-main/python/hydra/tarjan.py
+pyright src/gen-main/python/hydra/templates.py
+pyright src/gen-main/python/hydra/unification.py
+pyright src/gen-main/python/hydra/variants.py
 }
 """
 
