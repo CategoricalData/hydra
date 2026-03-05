71e3784ec8d21fcfe834e1b446dabd86185d5584
Joshua Shinavier <josh@fortytwo.net>
2026-03-04 21:45:02 -0800

    Adjust Haskell executables for Flow --> Either

--- hydra-haskell/src/exec/update-json-main/Main.hs
+++ hydra-haskell/src/exec/update-json-main/Main.hs
@@ -3,19 +3,21 @@ module Main where
 import Hydra.Generation (writeModulesJson)
 import Hydra.Sources.All (mainModules)
 import Hydra.Sources.Eval.Lib.All (evalLibModules)
+import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import Control.Exception (catch, SomeException)
 
 
 main :: IO ()
 main = do
+  outputDir <- parseOutputDir "src/gen-main/json"
   let allMods = mainModules ++ evalLibModules
   putStrLn "=== Generate Hydra main modules JSON ==="
   putStrLn ""
   putStrLn $ "Generating " ++ show (length allMods) ++ " main + eval lib modules to JSON..."
   putStrLn ""
 
-  result <- catch (writeModulesJson True "src/gen-main/json" allMods allMods >> return True)
+  result <- catch (writeModulesJson True outputDir allMods allMods >> return True)
                   (\e -> do
                     putStrLn $ "Error: " ++ show (e :: SomeException)
                     return False)
@@ -25,8 +27,17 @@ main = do
       putStrLn ""
       putStrLn "=== Done! ==="
       putStrLn ""
-      putStrLn "Generated files are in: src/gen-main/json/"
+      putStrLn $ "Generated files are in: " ++ outputDir ++ "/"
     else do
       putStrLn ""
       putStrLn "=== FAILED ==="
       exitFailure
+
+parseOutputDir :: String -> IO String
+parseOutputDir defaultDir = do
+  args <- getArgs
+  return $ go args
+  where
+    go ("--output-dir" : dir : _) = dir
+    go (_ : rest) = go rest
+    go [] = defaultDir
--- hydra-haskell/src/exec/update-json-test/Main.hs
+++ hydra-haskell/src/exec/update-json-test/Main.hs
@@ -3,19 +3,21 @@ module Main where
 import Hydra.Generation (writeModulesJson)
 import Hydra.Sources.All (mainModules)
 import Hydra.Sources.Test.All (testModules)
+import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import Control.Exception (catch, SomeException)
 
 
 main :: IO ()
 main = do
+  outputDir <- parseOutputDir "src/gen-test/json"
   putStrLn "=== Generate Hydra test modules JSON ==="
   putStrLn ""
   putStrLn $ "Generating " ++ show (length testModules) ++ " test modules to JSON..."
   putStrLn ""
 
   let universeModules = mainModules ++ testModules
-  result <- catch (writeModulesJson True "src/gen-test/json" universeModules testModules >> return True)
+  result <- catch (writeModulesJson True outputDir universeModules testModules >> return True)
                   (\e -> do
                     putStrLn $ "Error: " ++ show (e :: SomeException)
                     return False)
@@ -25,8 +27,17 @@ main = do
       putStrLn ""
       putStrLn "=== Done! ==="
       putStrLn ""
-      putStrLn "Generated files are in: src/gen-test/json/"
+      putStrLn $ "Generated files are in: " ++ outputDir ++ "/"
     else do
       putStrLn ""
       putStrLn "=== FAILED ==="
       exitFailure
+
+parseOutputDir :: String -> IO String
+parseOutputDir defaultDir = do
+  args <- getArgs
+  return $ go args
+  where
+    go ("--output-dir" : dir : _) = dir
+    go (_ : rest) = go rest
+    go [] = defaultDir
