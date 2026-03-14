7ef4ebeab706aa41ea943a4fdddd5bd966661c4d
Joshua Shinavier <josh@fortytwo.net>
2026-03-14 07:14:57 -0700

    Promote the transform module for generation tests

---
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Test/Transform.hs
@@ -0,0 +1,287 @@
+-- | Transform test cases for code generation, filtering to tests that can be compiled to target languages
+
+module Hydra.Sources.Test.Transform where
+
+-- Standard imports for term-level sources outside of the kernel
+import Hydra.Kernel hiding (transformModule)
+import Hydra.Sources.Libraries
+import           Hydra.Dsl.Meta.Lib.Strings                as Strings
+import           Hydra.Dsl.Meta.Phantoms                   as Phantoms
+import qualified Hydra.Dsl.Annotations                     as Annotations
+import qualified Hydra.Dsl.Bootstrap                       as Bootstrap
+import qualified Hydra.Dsl.Grammars                        as Grammars
+import qualified Hydra.Dsl.LiteralTypes                    as LiteralTypes
+import qualified Hydra.Dsl.Literals                        as Literals
+import qualified Hydra.Dsl.Meta.Accessors                  as Accessors
+import qualified Hydra.Dsl.Meta.Ast                        as Ast
+import qualified Hydra.Dsl.Meta.Base                       as MetaBase
+import qualified Hydra.Dsl.Meta.Coders                     as Coders
+import qualified Hydra.Dsl.Meta.Compute                    as Compute
+import qualified Hydra.Dsl.Meta.Context                    as Ctx
+import qualified Hydra.Dsl.Meta.Core                       as Core
+import qualified Hydra.Dsl.Meta.Error                      as Error
+import qualified Hydra.Dsl.Meta.Grammar                    as Grammar
+import qualified Hydra.Dsl.Meta.Graph                      as Graph
+import qualified Hydra.Dsl.Meta.Json                       as Json
+import qualified Hydra.Dsl.Meta.Lib.Chars                  as Chars
+import qualified Hydra.Dsl.Meta.Lib.Eithers                as Eithers
+import qualified Hydra.Dsl.Meta.Lib.Equality               as Equality
+import qualified Hydra.Dsl.Meta.Lib.Lists                  as Lists
+import qualified Hydra.Dsl.Meta.Lib.Literals               as Literals
+import qualified Hydra.Dsl.Meta.Lib.Logic                  as Logic
+import qualified Hydra.Dsl.Meta.Lib.Maps                   as Maps
+import qualified Hydra.Dsl.Meta.Lib.Math                   as Math
+import qualified Hydra.Dsl.Meta.Lib.Maybes                 as Maybes
+import qualified Hydra.Dsl.Meta.Lib.Pairs                  as Pairs
+import qualified Hydra.Dsl.Meta.Lib.Sets                   as Sets
+import qualified Hydra.Dsl.Meta.Module                     as Module
+import qualified Hydra.Dsl.Meta.Terms                      as MetaTerms
+import qualified Hydra.Dsl.Meta.Testing                    as Testing
+import qualified Hydra.Dsl.Meta.Topology                   as Topology
+import qualified Hydra.Dsl.Meta.Types                      as MetaTypes
+import qualified Hydra.Dsl.Meta.Typing                     as Typing
+import qualified Hydra.Dsl.Meta.Util                       as Util
+import qualified Hydra.Dsl.Meta.Variants                   as Variants
+import qualified Hydra.Dsl.Prims                           as Prims
+import qualified Hydra.Dsl.Tabular                         as Tabular
+import qualified Hydra.Dsl.Terms                           as Terms
+import qualified Hydra.Dsl.Tests                           as Tests
+import qualified Hydra.Dsl.Types                           as Types
+import qualified Hydra.Sources.Kernel.Terms.Formatting     as Formatting
+import qualified Hydra.Sources.Kernel.Terms.Sorting        as Sorting
+import qualified Hydra.Sources.Kernel.Types.All            as KernelTypes
+import           Prelude hiding ((++))
+import qualified Data.Int                                  as I
+import qualified Data.List                                 as L
+import qualified Data.Map                                  as M
+import qualified Data.Set                                  as S
+import qualified Data.Maybe                                as Y
+
+
+define :: String -> TTerm a -> TBinding a
+define = definitionInModule module_
+
+
+ns :: Namespace
+ns = Namespace "hydra.test.transform"
+
+
+module_ :: Module
+module_ = Module ns elements
+    [Formatting.ns, Sorting.ns]
+    KernelTypes.kernelTypesNamespaces $
+    Just "Transform test cases for code generation, filtering to tests that can be compiled to target languages"
+  where
+    elements = [
+      toBinding transformToCompiledTests,
+      toBinding transformTestCase,
+      toBinding buildConvertCaseCall,
+      toBinding encodeCaseConvention,
+      toBinding addGenerationPrefix,
+      toBinding transformModule,
+      toBinding collectTestCases,
+      toBinding buildTopologicalSortCall,
+      toBinding buildTopologicalSortSCCCall,
+      toBinding encodeAdjacencyList,
+      toBinding encodeInt,
+      toBinding encodeEitherListList,
+      toBinding encodeListList,
+      toBinding encodeIntList]
+
+
+-- | Transform test group hierarchy to only include delegated evaluation tests
+transformToCompiledTests :: TBinding (TestGroup -> Maybe TestGroup)
+transformToCompiledTests = define "transformToCompiledTests" $
+  doc "Transform test group hierarchy to only include delegated evaluation tests" $
+  lambda "tg" $ lets [
+    "name_">: project _TestGroup _TestGroup_name @@ var "tg",
+    "desc">: project _TestGroup _TestGroup_description @@ var "tg",
+    "subgroups">: project _TestGroup _TestGroup_subgroups @@ var "tg",
+    "cases_">: project _TestGroup _TestGroup_cases @@ var "tg",
+    "transformedCases">: Maybes.cat (Lists.map (lambda "tc" $ transformTestCase @@ var "tc") (var "cases_")),
+    "transformedSubgroups">: Maybes.cat (Lists.map (lambda "sg" $ transformToCompiledTests @@ var "sg") (var "subgroups"))] $
+    Logic.ifElse
+      (Logic.and (Lists.null (var "transformedCases")) (Lists.null (var "transformedSubgroups")))
+      nothing
+      (just $ Testing.testGroup (var "name_") (var "desc") (var "transformedSubgroups") (var "transformedCases"))
+
+
+-- | Transform a test case to DelegatedEvaluationTestCase if applicable
+transformTestCase :: TBinding (TestCaseWithMetadata -> Maybe TestCaseWithMetadata)
+transformTestCase = define "transformTestCase" $
+  doc "Transform a test case to DelegatedEvaluationTestCase if applicable" $
+  lambda "tcm" $ lets [
+    "name_">: project _TestCaseWithMetadata _TestCaseWithMetadata_name @@ var "tcm",
+    "tc">: project _TestCaseWithMetadata _TestCaseWithMetadata_case @@ var "tcm",
+    "desc">: project _TestCaseWithMetadata _TestCaseWithMetadata_description @@ var "tcm",
+    "tags_">: project _TestCaseWithMetadata _TestCaseWithMetadata_tags @@ var "tcm"] $
+    cases _TestCase (var "tc") (Just nothing) [
+      _TestCase_caseConversion>>: lambda "ccase" $ lets [
+        "fromConv">: project _CaseConversionTestCase _CaseConversionTestCase_fromConvention @@ var "ccase",
+        "toConv">: project _CaseConversionTestCase _CaseConversionTestCase_toConvention @@ var "ccase",
+        "fromStr">: project _CaseConversionTestCase _CaseConversionTestCase_fromString @@ var "ccase",
+        "toStr">: project _CaseConversionTestCase _CaseConversionTestCase_toString @@ var "ccase"] $
+        just $ Testing.testCaseWithMetadata (var "name_")
+          (Testing.testCaseDelegatedEvaluation $ Testing.delegatedEvaluationTestCase
+            (buildConvertCaseCall @@ var "fromConv" @@ var "toConv" @@ var "fromStr")
+            (Core.termLiteral $ Core.literalString (var "toStr")))
+          (var "desc") (var "tags_"),
+
+      _TestCase_evaluation>>: lambda "ecase" $ lets [
+        "input_">: project _EvaluationTestCase _EvaluationTestCase_input @@ var "ecase",
+        "output_">: project _EvaluationTestCase _EvaluationTestCase_output @@ var "ecase"] $
+        just $ Testing.testCaseWithMetadata (var "name_")
+          (Testing.testCaseDelegatedEvaluation $ Testing.delegatedEvaluationTestCase
+            (var "input_") (var "output_"))
+          (var "desc") (var "tags_"),
+
+      _TestCase_delegatedEvaluation>>: lambda "_" $
+        just (var "tcm"),
+
+      _TestCase_topologicalSort>>: lambda "tscase" $ lets [
+        "adjList">: project _TopologicalSortTestCase _TopologicalSortTestCase_adjacencyList @@ var "tscase",
+        "expected">: project _TopologicalSortTestCase _TopologicalSortTestCase_expected @@ var "tscase"] $
+        just $ Testing.testCaseWithMetadata (var "name_")
+          (Testing.testCaseDelegatedEvaluation $ Testing.delegatedEvaluationTestCase
+            (buildTopologicalSortCall @@ var "adjList")
+            (encodeEitherListList @@ var "expected"))
+          (var "desc") (var "tags_"),
+
+      _TestCase_topologicalSortSCC>>: lambda "scccase" $ lets [
+        "adjList">: project _TopologicalSortSCCTestCase _TopologicalSortSCCTestCase_adjacencyList @@ var "scccase",
+        "expected">: project _TopologicalSortSCCTestCase _TopologicalSortSCCTestCase_expected @@ var "scccase"] $
+        just $ Testing.testCaseWithMetadata (var "name_")
+          (Testing.testCaseDelegatedEvaluation $ Testing.delegatedEvaluationTestCase
+            (buildTopologicalSortSCCCall @@ var "adjList")
+            (encodeListList @@ var "expected"))
+          (var "desc") (var "tags_")]
+
+
+-- | Build a Term representing a convertCase function call
+buildConvertCaseCall :: TBinding (CaseConvention -> CaseConvention -> String -> Term)
+buildConvertCaseCall = define "buildConvertCaseCall" $
+  doc "Build a Term representing a convertCase function call" $
+  lambdas ["fromConv", "toConv", "input_"] $
+    Core.termApplication $ Core.application
+      (Core.termApplication $ Core.application
+        (Core.termApplication $ Core.application
+          (Core.termVariable $ Core.name (string "hydra.formatting.convertCase"))
+          (encodeCaseConvention @@ var "fromConv"))
+        (encodeCaseConvention @@ var "toConv"))
+      (Core.termLiteral $ Core.literalString (var "input_"))
+
+
+-- | Encode CaseConvention as a Term (unit variant)
+encodeCaseConvention :: TBinding (CaseConvention -> Term)
+encodeCaseConvention = define "encodeCaseConvention" $
+  doc "Encode CaseConvention as a Term (unit variant)" $
+  lambda "conv" $
+    Core.termUnion $ Core.injection
+      (Core.nameLift _CaseConvention)
+      (Core.field
+        (cases _CaseConvention (var "conv") Nothing [
+          _CaseConvention_lowerSnake>>: constant (Core.nameLift _CaseConvention_lowerSnake),
+          _CaseConvention_upperSnake>>: constant (Core.nameLift _CaseConvention_upperSnake),
+          _CaseConvention_camel>>: constant (Core.nameLift _CaseConvention_camel),
+          _CaseConvention_pascal>>: constant (Core.nameLift _CaseConvention_pascal)])
+        Core.termUnit)
+
+
+-- | Add "generation" namespace prefix
+addGenerationPrefix :: TBinding (Namespace -> Namespace)
+addGenerationPrefix = define "addGenerationPrefix" $
+  doc "Add generation namespace prefix" $
+  lambda "ns_" $
+    wrap _Namespace (Strings.cat2 (string "generation.") (unwrap _Namespace @@ var "ns_"))
+
+
+-- | Transform module with generation namespace
+transformModule :: TBinding (Module -> Module)
+transformModule = define "transformModule" $
+  doc "Transform module with generation namespace" $
+  lambda "m" $
+    Module.module_
+      (addGenerationPrefix @@ (project _Module _Module_namespace @@ var "m"))
+      (project _Module _Module_elements @@ var "m")
+      (project _Module _Module_termDependencies @@ var "m")
+      (project _Module _Module_typeDependencies @@ var "m")
+      (project _Module _Module_description @@ var "m")
+
+
+-- | Collect all test cases from a test group (flattening hierarchy)
+collectTestCases :: TBinding (TestGroup -> [TestCaseWithMetadata])
+collectTestCases = define "collectTestCases" $
+  doc "Collect all test cases from a test group (flattening hierarchy)" $
+  lambda "tg" $
+    Lists.concat2
+      (project _TestGroup _TestGroup_cases @@ var "tg")
+      (Lists.concat (Lists.map (lambda "sg" $ collectTestCases @@ var "sg")
+        (project _TestGroup _TestGroup_subgroups @@ var "tg")))
+
+
+-- | Build a Term representing a topologicalSort function call
+buildTopologicalSortCall :: TBinding ([(Int, [Int])] -> Term)
+buildTopologicalSortCall = define "buildTopologicalSortCall" $
+  doc "Build a Term representing a topologicalSort function call" $
+  lambda "adjList" $
+    Core.termApplication $ Core.application
+      (Core.termVariable $ Core.name (string "hydra.sorting.topologicalSort"))
+      (encodeAdjacencyList @@ var "adjList")
+
+
+-- | Build a Term representing a topologicalSortComponents function call
+buildTopologicalSortSCCCall :: TBinding ([(Int, [Int])] -> Term)
+buildTopologicalSortSCCCall = define "buildTopologicalSortSCCCall" $
+  doc "Build a Term representing a topologicalSortComponents function call" $
+  lambda "adjList" $
+    Core.termApplication $ Core.application
+      (Core.termVariable $ Core.name (string "hydra.sorting.topologicalSortComponents"))
+      (encodeAdjacencyList @@ var "adjList")
+
+
+-- | Encode an adjacency list as a Term
+encodeAdjacencyList :: TBinding ([(Int, [Int])] -> Term)
+encodeAdjacencyList = define "encodeAdjacencyList" $
+  doc "Encode an adjacency list as a Term" $
+  lambda "pairs" $
+    Core.termList (Lists.map
+      (lambda "p" $
+        Core.termPair (pair
+          (encodeInt @@ (Pairs.first (var "p")))
+          (Core.termList (Lists.map (lambda "d" $ encodeInt @@ var "d") (Pairs.second (var "p"))))))
+      (var "pairs"))
+
+
+-- | Encode an Int as a Term
+encodeInt :: TBinding (Int -> Term)
+encodeInt = define "encodeInt" $
+  doc "Encode an Int as a Term" $
+  lambda "n" $
+    Core.termLiteral $ Core.literalInteger $ Core.integerValueInt32 (var "n")
+
+
+-- | Encode Either [[Int]] [Int] as a Term
+encodeEitherListList :: TBinding (Either [[Int]] [Int] -> Term)
+encodeEitherListList = define "encodeEitherListList" $
+  doc "Encode Either [[Int]] [Int] as a Term" $
+  lambda "e" $
+    Core.termEither (Eithers.bimap
+      (lambda "cycles" $ encodeListList @@ var "cycles")
+      (lambda "sorted" $ encodeIntList @@ var "sorted")
+      (var "e"))
+
+
+-- | Encode [[Int]] as a Term
+encodeListList :: TBinding ([[Int]] -> Term)
+encodeListList = define "encodeListList" $
+  doc "Encode [[Int]] as a Term" $
+  lambda "lists" $
+    Core.termList (Lists.map (lambda "l" $ encodeIntList @@ var "l") (var "lists"))
+
+
+-- | Encode [Int] as a Term
+encodeIntList :: TBinding ([Int] -> Term)
+encodeIntList = define "encodeIntList" $
+  doc "Encode [Int] as a Term" $
+  lambda "ints" $
+    Core.termList (Lists.map (lambda "n" $ encodeInt @@ var "n") (var "ints"))
---
+++ hydra-python/src/gen-main/python/hydra/test/transform.py
@@ -0,0 +1,144 @@
+# Note: this is an automatically generated file. Do not edit.
+
+r"""Transform test cases for code generation, filtering to tests that can be compiled to target languages."""
+
+from __future__ import annotations
+from collections.abc import Callable
+from functools import lru_cache
+from hydra.dsl.python import Either, Just, Maybe, Nothing, frozenlist
+from typing import cast
+import hydra.core
+import hydra.lib.eithers
+import hydra.lib.lists
+import hydra.lib.logic
+import hydra.lib.maybes
+import hydra.lib.pairs
+import hydra.lib.strings
+import hydra.module
+import hydra.testing
+import hydra.util
+
+def add_generation_prefix(ns_: hydra.module.Namespace) -> hydra.module.Namespace:
+    r"""Add generation namespace prefix."""
+    
+    return hydra.module.Namespace(hydra.lib.strings.cat2("generation.", ns_.value))
+
+def encode_case_convention(conv: hydra.util.CaseConvention) -> hydra.core.Term:
+    def _hoist_hydra_test_transform_encode_case_convention_1(v1):
+        match v1:
+            case hydra.util.CaseConvention.LOWER_SNAKE:
+                return hydra.core.Name("lowerSnake")
+            
+            case hydra.util.CaseConvention.UPPER_SNAKE:
+                return hydra.core.Name("upperSnake")
+            
+            case hydra.util.CaseConvention.CAMEL:
+                return hydra.core.Name("camel")
+            
+            case hydra.util.CaseConvention.PASCAL:
+                return hydra.core.Name("pascal")
+            
+            case _:
+                raise AssertionError("Unreachable: all variants handled")
+    return cast(hydra.core.Term, hydra.core.TermUnion(hydra.core.Injection(hydra.core.Name("hydra.util.CaseConvention"), hydra.core.Field(_hoist_hydra_test_transform_encode_case_convention_1(conv), cast(hydra.core.Term, hydra.core.TermUnit())))))
+
+def build_convert_case_call(from_conv: hydra.util.CaseConvention, to_conv: hydra.util.CaseConvention, input_: str) -> hydra.core.Term:
+    r"""Build a Term representing a convertCase function call."""
+    
+    return cast(hydra.core.Term, hydra.core.TermApplication(hydra.core.Application(cast(hydra.core.Term, hydra.core.TermApplication(hydra.core.Application(cast(hydra.core.Term, hydra.core.TermApplication(hydra.core.Application(cast(hydra.core.Term, hydra.core.TermVariable(hydra.core.Name("hydra.formatting.convertCase"))), encode_case_convention(from_conv)))), encode_case_convention(to_conv)))), cast(hydra.core.Term, hydra.core.TermLiteral(cast(hydra.core.Literal, hydra.core.LiteralString(input_)))))))
+
+def encode_int(n: int) -> hydra.core.Term:
+    r"""Encode an Int as a Term."""
+    
+    return cast(hydra.core.Term, hydra.core.TermLiteral(cast(hydra.core.Literal, hydra.core.LiteralInteger(cast(hydra.core.IntegerValue, hydra.core.IntegerValueInt32(n))))))
+
+def encode_adjacency_list(pairs: frozenlist[tuple[int, frozenlist[int]]]) -> hydra.core.Term:
+    r"""Encode an adjacency list as a Term."""
+    
+    return cast(hydra.core.Term, hydra.core.TermList(hydra.lib.lists.map((lambda p: cast(hydra.core.Term, hydra.core.TermPair((encode_int(hydra.lib.pairs.first(p)), cast(hydra.core.Term, hydra.core.TermList(hydra.lib.lists.map((lambda d: encode_int(d)), hydra.lib.pairs.second(p)))))))), pairs)))
+
+def build_topological_sort_call(adj_list: frozenlist[tuple[int, frozenlist[int]]]) -> hydra.core.Term:
+    r"""Build a Term representing a topologicalSort function call."""
+    
+    return cast(hydra.core.Term, hydra.core.TermApplication(hydra.core.Application(cast(hydra.core.Term, hydra.core.TermVariable(hydra.core.Name("hydra.sorting.topologicalSort"))), encode_adjacency_list(adj_list))))
+
+def build_topological_sort_s_c_c_call(adj_list: frozenlist[tuple[int, frozenlist[int]]]) -> hydra.core.Term:
+    r"""Build a Term representing a topologicalSortComponents function call."""
+    
+    return cast(hydra.core.Term, hydra.core.TermApplication(hydra.core.Application(cast(hydra.core.Term, hydra.core.TermVariable(hydra.core.Name("hydra.sorting.topologicalSortComponents"))), encode_adjacency_list(adj_list))))
+
+def collect_test_cases(tg: hydra.testing.TestGroup) -> frozenlist[hydra.testing.TestCaseWithMetadata]:
+    r"""Collect all test cases from a test group (flattening hierarchy)."""
+    
+    return hydra.lib.lists.concat2(tg.cases, hydra.lib.lists.concat(hydra.lib.lists.map((lambda sg: collect_test_cases(sg)), tg.subgroups)))
+
+def encode_int_list(ints: frozenlist[int]) -> hydra.core.Term:
+    r"""Encode [Int] as a Term."""
+    
+    return cast(hydra.core.Term, hydra.core.TermList(hydra.lib.lists.map((lambda n: encode_int(n)), ints)))
+
+def encode_list_list(lists: frozenlist[frozenlist[int]]) -> hydra.core.Term:
+    r"""Encode [[Int]] as a Term."""
+    
+    return cast(hydra.core.Term, hydra.core.TermList(hydra.lib.lists.map((lambda l: encode_int_list(l)), lists)))
+
+def encode_either_list_list(e: Either[frozenlist[frozenlist[int]], frozenlist[int]]) -> hydra.core.Term:
+    r"""Encode Either [[Int]] [Int] as a Term."""
+    
+    return cast(hydra.core.Term, hydra.core.TermEither(hydra.lib.eithers.bimap((lambda cycles: encode_list_list(cycles)), (lambda sorted: encode_int_list(sorted)), e)))
+
+def transform_module(m: hydra.module.Module) -> hydra.module.Module:
+    r"""Transform module with generation namespace."""
+    
+    return hydra.module.Module(add_generation_prefix(m.namespace), m.elements, m.term_dependencies, m.type_dependencies, m.description)
+
+def transform_test_case(tcm: hydra.testing.TestCaseWithMetadata) -> Maybe[hydra.testing.TestCaseWithMetadata]:
+    r"""Transform a test case to DelegatedEvaluationTestCase if applicable."""
+    
+    name_ = tcm.name
+    tc = tcm.case
+    desc = tcm.description
+    tags_ = tcm.tags
+    match tc:
+        case hydra.testing.TestCaseCaseConversion(value=ccase):
+            from_conv = ccase.from_convention
+            to_conv = ccase.to_convention
+            from_str = ccase.from_string
+            to_str = ccase.to_string
+            return Just(hydra.testing.TestCaseWithMetadata(name_, cast(hydra.testing.TestCase, hydra.testing.TestCaseDelegatedEvaluation(hydra.testing.DelegatedEvaluationTestCase(build_convert_case_call(from_conv, to_conv, from_str), cast(hydra.core.Term, hydra.core.TermLiteral(cast(hydra.core.Literal, hydra.core.LiteralString(to_str))))))), desc, tags_))
+        
+        case hydra.testing.TestCaseEvaluation(value=ecase):
+            input_ = ecase.input
+            output_ = ecase.output
+            return Just(hydra.testing.TestCaseWithMetadata(name_, cast(hydra.testing.TestCase, hydra.testing.TestCaseDelegatedEvaluation(hydra.testing.DelegatedEvaluationTestCase(input_, output_))), desc, tags_))
+        
+        case hydra.testing.TestCaseDelegatedEvaluation():
+            return Just(tcm)
+        
+        case hydra.testing.TestCaseTopologicalSort(value=tscase):
+            adj_list = tscase.adjacency_list
+            expected = tscase.expected
+            return Just(hydra.testing.TestCaseWithMetadata(name_, cast(hydra.testing.TestCase, hydra.testing.TestCaseDelegatedEvaluation(hydra.testing.DelegatedEvaluationTestCase(build_topological_sort_call(adj_list), encode_either_list_list(expected)))), desc, tags_))
+        
+        case hydra.testing.TestCaseTopologicalSortSCC(value=scccase):
+            adj_list = scccase.adjacency_list
+            expected = scccase.expected
+            return Just(hydra.testing.TestCaseWithMetadata(name_, cast(hydra.testing.TestCase, hydra.testing.TestCaseDelegatedEvaluation(hydra.testing.DelegatedEvaluationTestCase(build_topological_sort_s_c_c_call(adj_list), encode_list_list(expected)))), desc, tags_))
+        
+        case _:
+            return Nothing()
+
+def transform_to_compiled_tests(tg: hydra.testing.TestGroup) -> Maybe[hydra.testing.TestGroup]:
+    r"""Transform test group hierarchy to only include delegated evaluation tests."""
+    
+    name_ = tg.name
+    desc = tg.description
+    subgroups = tg.subgroups
+    cases_ = tg.cases
+    @lru_cache(1)
+    def transformed_cases() -> frozenlist[hydra.testing.TestCaseWithMetadata]:
+        return hydra.lib.maybes.cat(hydra.lib.lists.map((lambda tc: transform_test_case(tc)), cases_))
+    @lru_cache(1)
+    def transformed_subgroups() -> frozenlist[hydra.testing.TestGroup]:
+        return hydra.lib.maybes.cat(hydra.lib.lists.map((lambda sg: transform_to_compiled_tests(sg)), subgroups))
+    return hydra.lib.logic.if_else(hydra.lib.logic.and_(hydra.lib.lists.null(transformed_cases()), hydra.lib.lists.null(transformed_subgroups())), (lambda : Nothing()), (lambda : Just(hydra.testing.TestGroup(name_, desc, transformed_subgroups(), transformed_cases()))))
