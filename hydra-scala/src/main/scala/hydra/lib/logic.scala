352062ce1f9d9c0becfbc70dd63d5f4bdfd57677
Joshua Shinavier <josh@fortytwo.net>
2026-03-21 16:27:06 -0700

    Fix lazy evaluation (ifElse, maybe by-name params) and safe list ops — Scala generates Haskell

--- hydra-scala/src/main/scala/hydra/lib/logic.scala
+++ hydra-scala/src/main/scala/hydra/lib/logic.scala
@@ -2,6 +2,6 @@ package hydra.lib
 
 object logic:
   def and(x: Boolean)(y: Boolean): Boolean = x && y
-  def ifElse[A](cond: Boolean)(ifTrue: A)(ifFalse: A): A = if cond then ifTrue else ifFalse
+  def ifElse[A](cond: Boolean)(ifTrue: => A)(ifFalse: => A): A = if cond then ifTrue else ifFalse
   def not(x: Boolean): Boolean = !x
   def or(x: Boolean)(y: Boolean): Boolean = x || y
--- hydra-scala/src/main/scala/hydra/lib/maybes.scala
+++ hydra-scala/src/main/scala/hydra/lib/maybes.scala
@@ -9,11 +9,11 @@ object maybes:
   def cat[A](xs: Seq[Option[A]]): Seq[A] = xs.flatten
   def compose[A, B, C](f: A => Option[B])(g: B => Option[C])(a: A): Option[C] = f(a).flatMap(g)
   def fromJust[A](ma: Option[A]): A = ma.get
-  def fromMaybe[A](d: A)(ma: Option[A]): A = ma.getOrElse(d)
+  def fromMaybe[A](d: => A)(ma: Option[A]): A = ma.getOrElse(d)
   def isJust[A](ma: Option[A]): Boolean = ma.isDefined
   def isNothing[A](ma: Option[A]): Boolean = ma.isEmpty
   def map[A, B](f: A => B)(ma: Option[A]): Option[B] = ma.map(f)
   def mapMaybe[A, B](f: A => Option[B])(xs: Seq[A]): Seq[B] = xs.flatMap(f)
-  def maybe[B, A](d: B)(f: A => B)(ma: Option[A]): B = ma.map(f).getOrElse(d)
+  def maybe[B, A](d: => B)(f: A => B)(ma: Option[A]): B = ma.map(f).getOrElse(d)
   def pure[A](a: A): Option[A] = Some(a)
   def toList[A](ma: Option[A]): Seq[A] = ma.toSeq
