3c5e605588d32d1c74d1b1e7adc28d99635397a2
Joshua Shinavier <josh@fortytwo.net>
2026-03-21 20:48:35 -0700

    Merge hydra.tarjan into hydra.sorting. Resolves #220

--- hydra-haskell/src/main/haskell/Hydra/Sources/Kernel/Terms/Sorting.hs
+++ hydra-haskell/src/main/haskell/Hydra/Sources/Kernel/Terms/Sorting.hs
@@ -3,8 +3,9 @@ module Hydra.Sources.Kernel.Terms.Sorting where
 
 -- Standard imports for kernel terms modules
 import Hydra.Kernel hiding (
-  adjacencyListToMap, createOrderingIsomorphism, findReachableNodes, propagateTags, topologicalSort,
-  topologicalSortComponents, topologicalSortNodes)
+  adjacencyListToMap, adjacencyListsToGraph, createOrderingIsomorphism, findReachableNodes,
+  initialState, popStackUntil, propagateTags, strongConnect, stronglyConnectedComponents,
+  topologicalSort, topologicalSortComponents, topologicalSortNodes)
 import Hydra.Sources.Libraries
 import qualified Hydra.Dsl.Accessors    as Accessors
 import qualified Hydra.Dsl.Annotations       as Annotations
@@ -55,7 +56,7 @@ import qualified Data.Map                    as M
 import qualified Data.Set                    as S
 import qualified Data.Maybe                  as Y
 
-import qualified Hydra.Sources.Kernel.Terms.Tarjan as Tarjan
+import qualified Hydra.Sources.Kernel.Terms.Constants as Constants
 
 import qualified Hydra.Topology as Topo
 
@@ -65,21 +66,61 @@ ns = Namespace "hydra.sorting"
 
 module_ :: Module
 module_ = Module ns elements
-    [Tarjan.ns]
+    [Constants.ns]
     kernelTypesNamespaces $
-    Just ("Utilities for sorting.")
+    Just ("Utilities for sorting."
+      <> " This module includes an implementation of Tarjan's algorithm,"
+      <> " originally based on GraphSCC by Iavor S. Diatchki:"
+      <> " https://hackage.haskell.org/package/GraphSCC.")
   where
    elements = [
      toBinding adjacencyListToMap,
+     toBinding adjacencyListsToGraph,
      toBinding createOrderingIsomorphism,
      toBinding findReachableNodes,
+     toBinding initialState,
+     toBinding popStackUntil,
      toBinding propagateTags,
+     toBinding strongConnect,
+     toBinding stronglyConnectedComponents,
      toBinding topologicalSort,
      toBinding topologicalSortComponents,
      toBinding topologicalSortNodes]
 
 define :: String -> TTerm a -> TBinding a
 define = definitionInModule module_
+
+adjacencyListsToGraph :: TBinding ([(key, [key])] -> (Topo.Graph, Topo.Vertex -> key))
+adjacencyListsToGraph = define "adjacencyListsToGraph" $
+  doc ("Given a list of adjacency lists represented as (key, [key]) pairs,"
+    <> " construct a graph along with a function mapping each vertex (an Int)"
+    <> " back to its original key.") $
+  "edges0" ~>
+  "sortedEdges" <~ Lists.sortOn (unaryFunction Pairs.first) (var "edges0") $
+  "indexedEdges" <~ Lists.zip (Math.range (int32 0) (Lists.length (var "sortedEdges"))) (var "sortedEdges") $
+  "keyToVertex" <~ Maps.fromList (Lists.map
+    ("vkNeighbors" ~>
+      "v" <~ Pairs.first (var "vkNeighbors") $
+      "kNeighbors" <~ Pairs.second (var "vkNeighbors") $
+      "k" <~ Pairs.first (var "kNeighbors") $
+      pair (var "k") (var "v"))
+    (var "indexedEdges")) $
+  "vertexMap" <~ Maps.fromList (Lists.map
+    ("vkNeighbors" ~>
+      "v" <~ Pairs.first (var "vkNeighbors") $
+      "kNeighbors" <~ Pairs.second (var "vkNeighbors") $
+      "k" <~ Pairs.first (var "kNeighbors") $
+      pair (var "v") (var "k"))
+    (var "indexedEdges")) $
+  "graph" <~ Maps.fromList (Lists.map
+    ("vkNeighbors" ~>
+      "v" <~ Pairs.first (var "vkNeighbors") $
+      "kNeighbors" <~ Pairs.second (var "vkNeighbors") $
+      "neighbors" <~ Pairs.second (var "kNeighbors") $
+      pair (var "v") (Maybes.mapMaybe ("k" ~> Maps.lookup (var "k") (var "keyToVertex")) (var "neighbors")))
+    (var "indexedEdges")) $
+  "vertexToKey" <~ ("v" ~> Maybes.fromJust (Maps.lookup (var "v") (var "vertexMap"))) $
+  pair (var "graph") (var "vertexToKey")
 
 adjacencyListToMap :: TBinding ([(a, [b])] -> M.Map a [b])
 adjacencyListToMap = define "adjacencyListToMap" $
@@ -105,7 +146,6 @@ createOrderingIsomorphism = define "createOrderingIsomorphism" $
     Maybes.cat $ Lists.map ("n" ~> Maps.lookup (var "n") (var "mp")) (var "sourceOrd")) $
   Topology.orderingIsomorphism (var "sourceToTargetMapping") (var "targetToSourceMapping")
 
--- Note: not a sorting function
 findReachableNodes :: TBinding ((a -> S.Set a) -> a -> S.Set a)
 findReachableNodes = define "findReachableNodes" $
   doc "Given an adjacency function and a distinguished root node, find all reachable nodes (including the root node)" $
@@ -119,6 +159,26 @@ findReachableNodes = define "findReachableNodes" $
         (var "visited")
         (Sets.toList $ var "toVisit"))) $
   var "visit" @@ Sets.singleton (var "root") @@ var "root"
+
+initialState :: TBinding Topo.TarjanState
+initialState = define "initialState" $
+  doc "Initial state for Tarjan's algorithm" $
+  Topology.tarjanState (int32 0) Maps.empty Maps.empty (list ([] :: [TTerm Topo.Vertex])) Sets.empty (list ([] :: [TTerm [Topo.Vertex]]))
+
+popStackUntil :: TBinding (Topo.Vertex -> Topo.TarjanState -> ([Topo.Vertex], Topo.TarjanState))
+popStackUntil = define "popStackUntil" $
+  doc "Pop vertices off the stack until the given vertex is reached, collecting the current strongly connected component" $
+  "v" ~> "st0" ~>
+  "go" <~ ("acc" ~> "st" ~>
+    "x" <~ Lists.head (Topology.tarjanStateStack (var "st")) $
+    "xs" <~ Lists.tail (Topology.tarjanStateStack (var "st")) $
+    "newSt" <~ Topology.tarjanStateWithStack (var "st") (var "xs") $
+    "newSt2" <~ Topology.tarjanStateWithOnStack (var "newSt") (Sets.delete (var "x") (Topology.tarjanStateOnStack (var "st"))) $
+    "acc'" <~ Lists.cons (var "x") (var "acc") $
+    Logic.ifElse (Equality.equal (var "x") (var "v"))
+      (pair (Lists.reverse (var "acc'")) (var "newSt2"))
+      (var "go" @@ var "acc'" @@ var "newSt2")) $
+  var "go" @@ list ([] :: [TTerm Topo.Vertex]) @@ var "st0"
 
 propagateTags :: TBinding ([(a, [a])] -> [(a, [t])] -> [(a, S.Set t)])
 propagateTags = define "propagateTags" $
@@ -145,6 +205,57 @@ propagateTags = define "propagateTags" $
       (Sets.toList $ var "reachable")) $
   Lists.map ("n" ~> pair (var "n") (var "getTagsForNode" @@ var "n")) (var "allNodes")
 
+strongConnect :: TBinding (Topo.Graph -> Topo.Vertex -> Topo.TarjanState -> Topo.TarjanState)
+strongConnect = define "strongConnect" $
+  doc "Visit a vertex and recursively explore its successors" $
+  "graph" ~> "v" ~> "st" ~>
+  "i" <~ Topology.tarjanStateCounter (var "st") $
+  "newSt" <~ Topology.tarjanState
+    (Math.add (var "i") (int32 1))
+    (Maps.insert (var "v") (var "i") (Topology.tarjanStateIndices (var "st")))
+    (Maps.insert (var "v") (var "i") (Topology.tarjanStateLowLinks (var "st")))
+    (Lists.cons (var "v") (Topology.tarjanStateStack (var "st")))
+    (Sets.insert (var "v") (Topology.tarjanStateOnStack (var "st")))
+    (Topology.tarjanStateSccs (var "st")) $
+  "neighbors" <~ Maps.findWithDefault (list ([] :: [TTerm Topo.Vertex])) (var "v") (var "graph") $
+  "processNeighbor" <~ ("st_" ~> "w" ~>
+    "lowLink" <~ ("s" ~>
+      "lowV1" <~ Maps.findWithDefault Constants.maxInt32 (var "v") (Topology.tarjanStateLowLinks (var "s")) $
+      "idx_w" <~ Maps.findWithDefault Constants.maxInt32 (var "w") (Topology.tarjanStateIndices (var "s")) $
+      Topology.tarjanStateWithLowLinks (var "s")
+        (Maps.insert (var "v") (Equality.min (var "lowV1") (var "idx_w")) (Topology.tarjanStateLowLinks (var "s")))) $
+    Logic.ifElse (Logic.not (Maps.member (var "w") (Topology.tarjanStateIndices (var "st_"))))
+      ("stAfter" <~ strongConnect @@ var "graph" @@ var "w" @@ var "st_" $
+       "lowV2" <~ Maps.findWithDefault Constants.maxInt32 (var "v") (Topology.tarjanStateLowLinks (var "stAfter")) $
+       "low_w" <~ Maps.findWithDefault Constants.maxInt32 (var "w") (Topology.tarjanStateLowLinks (var "stAfter")) $
+       Topology.tarjanStateWithLowLinks (var "stAfter")
+         (Maps.insert (var "v") (Equality.min (var "lowV2") (var "low_w")) (Topology.tarjanStateLowLinks (var "stAfter"))))
+      (Logic.ifElse (Sets.member (var "w") (Topology.tarjanStateOnStack (var "st_")))
+        (var "lowLink" @@ var "st_")
+        (var "st_"))) $
+  "stAfterNeighbors" <~ Lists.foldl (var "processNeighbor") (var "newSt") (var "neighbors") $
+  "low_v" <~ Maps.findWithDefault Constants.maxInt32 (var "v") (Topology.tarjanStateLowLinks (var "stAfterNeighbors")) $
+  "idx_v" <~ Maps.findWithDefault Constants.maxInt32 (var "v") (Topology.tarjanStateIndices (var "stAfterNeighbors")) $
+  Logic.ifElse (Equality.equal (var "low_v") (var "idx_v"))
+    ("compResult" <~ popStackUntil @@ var "v" @@ var "stAfterNeighbors" $
+     "comp" <~ Pairs.first (var "compResult") $
+     "stPopped" <~ Pairs.second (var "compResult") $
+     Topology.tarjanStateWithSccs (var "stPopped") (Lists.cons (var "comp") (Topology.tarjanStateSccs (var "stPopped"))))
+    (var "stAfterNeighbors")
+
+stronglyConnectedComponents :: TBinding (Topo.Graph -> [[Topo.Vertex]])
+stronglyConnectedComponents = define "stronglyConnectedComponents" $
+  doc "Compute the strongly connected components of the given graph. The components are returned in reverse topological order" $
+  "graph" ~>
+  "verts" <~ Maps.keys (var "graph") $
+  "finalState" <~ Lists.foldl
+    ("st" ~> "v" ~> Logic.ifElse (Maps.member (var "v") (Topology.tarjanStateIndices (var "st")))
+      (var "st")
+      (strongConnect @@ var "graph" @@ var "v" @@ var "st"))
+    (asTerm initialState)
+    (var "verts") $
+  Lists.reverse (Lists.map (unaryFunction Lists.sort) (Topology.tarjanStateSccs (var "finalState")))
+
 topologicalSort :: TBinding ([(a, [a])] -> Either [[a]] [a])
 topologicalSort = define "topologicalSort" $
   doc ("Sort a directed acyclic graph (DAG) based on an adjacency list."
@@ -162,10 +273,10 @@ topologicalSortComponents = define "topologicalSortComponents" $
   doc ("Find the strongly connected components (including cycles and isolated vertices) of a graph,"
     <> " in (reverse) topological order, i.e. dependencies before dependents") $
   "pairs" ~>
-  "graphResult" <~ Tarjan.adjacencyListsToGraph @@ var "pairs" $
+  "graphResult" <~ adjacencyListsToGraph @@ var "pairs" $
   "g" <~ Pairs.first (var "graphResult") $
   Lists.map ("comp" ~> Lists.map (Pairs.second $ var "graphResult") (var "comp")) $
-    Tarjan.stronglyConnectedComponents @@ var "g"
+    stronglyConnectedComponents @@ var "g"
 
 topologicalSortNodes :: TBinding ((x -> a) -> (x -> [a]) -> [x] -> [[x]])
 topologicalSortNodes = define "topologicalSortNodes" $
