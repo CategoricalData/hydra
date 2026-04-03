2d84d05b71f88f49279ccecfa4b589ab332d0c04
Joshua Shinavier <josh@fortytwo.net>
2026-04-03 03:48:42 -0700

    Bootstrap generated Hydra.Ext.Haskell.Environment for new Source module

---
+++ hydra-haskell/src/gen-main/haskell/Hydra/Ext/Haskell/Environment.hs
@@ -0,0 +1,36 @@
+-- Note: this is an automatically generated file. Do not edit.
+
+-- | Environment types for Haskell code generation
+
+module Hydra.Ext.Haskell.Environment where
+
+import qualified Hydra.Core as Core
+import Prelude hiding  (Enum, Ordering, decodeFloat, encodeFloat, fail, map, pure, sum)
+import qualified Data.ByteString as B
+import qualified Data.Int as I
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+-- | Metadata used to determine which standard imports are needed in a generated Haskell module
+data HaskellModuleMetadata =
+  HaskellModuleMetadata {
+    -- | Whether the module uses Data.ByteString (B.ByteString)
+    haskellModuleMetadataUsesByteString :: Bool,
+    -- | Whether the module uses Data.Int (I.Int8, I.Int16, I.Int64)
+    haskellModuleMetadataUsesInt :: Bool,
+    -- | Whether the module uses Data.Map (M.Map, M.fromList, M.empty)
+    haskellModuleMetadataUsesMap :: Bool,
+    -- | Whether the module uses Data.Set (S.Set, S.fromList, S.empty)
+    haskellModuleMetadataUsesSet :: Bool}
+  deriving (Eq, Ord, Read, Show)
+
+_HaskellModuleMetadata = Core.Name "hydra.ext.haskell.environment.HaskellModuleMetadata"
+
+_HaskellModuleMetadata_usesByteString = Core.Name "usesByteString"
+
+_HaskellModuleMetadata_usesInt = Core.Name "usesInt"
+
+_HaskellModuleMetadata_usesMap = Core.Name "usesMap"
+
+_HaskellModuleMetadata_usesSet = Core.Name "usesSet"
