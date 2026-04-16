b53d463310584417a80d115112e89ba661329a9f
Joshua Shinavier <josh@fortytwo.net>
2026-04-15 19:11:09 -0700

    Implement hydra.lib.{strings,pairs,logic,math,maybes,eithers} primitives in Coq. For #337

--- heads/haskell/src/main/coq/hydra/lib/logic.v
+++ heads/haskell/src/main/coq/hydra/lib/logic.v
@@ -1,11 +1,12 @@
-(* Note: this is an automatically generated file. Do not edit. *)
-
 (* Hydra primitive library: hydra.lib.logic *)
 
 Require Import Stdlib.Strings.String Stdlib.Lists.List Stdlib.ZArith.ZArith Stdlib.QArith.QArith.
 
 Definition and (a b : bool) : bool := andb a b.
-Axiom ifElse : forall (x : Type), bool -> x -> x -> x.
+
+Definition ifElse (x : Type) (b : bool) (t f : x) : x := if b then t else f.
 Arguments ifElse {x}.
+
 Definition not (a : bool) : bool := negb a.
+
 Definition or (a b : bool) : bool := orb a b.
