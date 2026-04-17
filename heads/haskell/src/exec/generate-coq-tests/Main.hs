6180a7ac2b9f842fc0841262384d873f08aa1aba
Joshua Shinavier <josh@fortytwo.net>
2026-04-17 13:09:35 -0700

    Add Coq test runner walking 31 test modules; copy hand-written test support files. For #337

--- heads/haskell/src/exec/generate-coq-tests/Main.hs
+++ heads/haskell/src/exec/generate-coq-tests/Main.hs
@@ -4,6 +4,29 @@ import Hydra.Sources.All (kernelModules, mainModules)
 import Hydra.Sources.Test.All (testModules)
 import Hydra.ExtGeneration (writeCoq)
 
+import Control.Monad (forM_)
+import Data.List (sort)
+import System.Directory (copyFile, createDirectoryIfMissing, doesDirectoryExist, listDirectory)
+import System.FilePath ((</>))
+
+-- | Copy hand-written test-support .v files (e.g. runner.v) into the
+-- generated test tree so coqc can see them alongside the generated modules.
+-- Source path is resolved relative to the current working directory, which
+-- is heads/haskell/ when generate-coq-tests runs.
+copyCoqTestFiles :: FilePath -> IO ()
+copyCoqTestFiles baseDir = do
+  let srcDir = "src/test/coq" </> "hydra" </> "test"
+      dstDir = baseDir </> "hydra" </> "test"
+  srcExists <- doesDirectoryExist srcDir
+  if not srcExists
+    then return ()
+    else do
+      createDirectoryIfMissing True dstDir
+      files <- sort <$> listDirectory srcDir
+      forM_ files $ \f -> do
+        copyFile (srcDir </> f) (dstDir </> f)
+        putStrLn ("  hydra/test/" ++ f)
+
 main :: IO ()
 main = do
   let outputDir = "../../dist/coq/hydra-kernel/src/test/coq"
@@ -11,6 +34,8 @@ main = do
   putStrLn "Generating Coq (.v) files from Hydra test modules..."
   n <- writeCoq outputDir universe testModules
   putStrLn $ "Generated " ++ show n ++ " test .v files"
+  putStrLn "Copying hand-written test support files..."
+  copyCoqTestFiles outputDir
   -- Kernel modules are in dist/coq/hydra-kernel/src/main/coq. The generated
   -- test files Require their dependencies via fully qualified `hydra.*`
   -- module paths, so both roots need to be on coqc's library path. See the
