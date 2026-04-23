3a3bc44ddb1723940db1f720d93ebbb3c9c767bc
Joshua Shinavier <josh@fortytwo.net>
2026-04-23 06:33:12 -0700

    Regenerate dist/json + dist/haskell TestGraph bootstrap baseline after TestEnv DSL change. For #290

--- dist/haskell/hydra-kernel/src/test/haskell/Hydra/Test/TestGraph.hs
+++ dist/haskell/hydra-kernel/src/test/haskell/Hydra/Test/TestGraph.hs
@@ -7,10 +7,9 @@ module Hydra.Test.TestGraph where
 import qualified Hydra.Context as Context
 import qualified Hydra.Core as Core
 import qualified Hydra.Graph as Graph
-import qualified Hydra.Lexical as Lexical
-import qualified Hydra.Test.TestEnv as TestEnv
 import qualified Hydra.Lib.Maps as Maps
 import qualified Hydra.Packaging as Packaging
+import qualified Hydra.Test.TestEnv as TestEnv
 import qualified Hydra.Test.TestTerms as TestTerms
 import qualified Hydra.Test.TestTypes as TestTypes
 import Prelude hiding  (Enum, Ordering, decodeFloat, encodeFloat, fail, map, pure, sum)
