22725b22231bc1059656cbd85c66de20f7abc617
Joshua Shinavier <josh@fortytwo.net>
2026-05-05 23:34:08 -0700

    Cat-7 (term-encoded tests) retag: 'shallow DSL tests' → 'term-encoded tests'. For #308

--- packages/hydra-kernel/src/main/haskell/Hydra/Sources/Test/Lib/Pairs.hs
+++ packages/hydra-kernel/src/main/haskell/Hydra/Sources/Test/Lib/Pairs.hs
@@ -2,7 +2,7 @@
 
 module Hydra.Sources.Test.Lib.Pairs where
 
--- Standard imports for shallow DSL tests
+-- Standard imports for term-encoded tests
 import Hydra.Kernel
 import Hydra.Dsl.Meta.Testing                 as Testing
 import Hydra.Dsl.Meta.Terms                   as Terms
