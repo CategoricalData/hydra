f28c83709d70041c4529e5f5cfa413955bba4bfc
Joshua Shinavier <josh@fortytwo.net>
2026-05-12 16:50:51 -0700

    Adopt hash-table maps/sets and lazy-let loader in Hydra-Emacs-Lisp. Resolves #361

--- heads/lisp/emacs-lisp/src/main/emacs-lisp/hydra/lib/sets.el
+++ heads/lisp/emacs-lisp/src/main/emacs-lisp/hydra/lib/sets.el
@@ -2,32 +2,84 @@
 
 (require 'cl-lib)
 
-;; Sets are sorted lists (no duplicates), using generic-compare for ordering.
+;; Sets are backed by Emacs `make-hash-table :test 'equal` with values
+;; uniformly bound to `t`. All mutating operations (insert, delete,
+;; union, difference, intersection, …) copy the table first, preserving
+;; the immutable semantics Hydra expects.
+;;
+;; The empty set is represented as `nil` (back-compat with prior
+;; sorted-list code that passed nil for empty). Any value-bearing set
+;; is a hash-table. Iteration helpers (to_list) return entries in
+;; key-sorted order via `generic-compare`.
+
+(defun hydra-set-p (s)
+  (hash-table-p s))
+
+(defun hydra-set-empty-p (s)
+  (or (null s) (and (hash-table-p s) (zerop (hash-table-count s)))))
+
+(defun hydra-set-size (s)
+  (cond
+   ((null s) 0)
+   ((hash-table-p s) (hash-table-count s))
+   ((listp s) (length s))
+   (t (error "hydra-set-size: not a set: %S" s))))
+
+(defun hydra-set-member-p (x s)
+  (cond
+   ((null s) nil)
+   ((hash-table-p s) (not (eq (gethash x s :hydra-not-found) :hydra-not-found)))
+   ((listp s) (cl-some (lambda (y) (equal x y)) s))
+   (t (error "hydra-set-member-p: not a set: %S" s))))
+
+(defun hydra-set-to-hash (s)
+  "Return a hash-table view of S. Always safe to mutate."
+  (cond
+   ((null s) (make-hash-table :test 'equal))
+   ((hash-table-p s) (copy-hash-table s))
+   ((listp s)
+    (let ((h (make-hash-table :test 'equal :size (max 1 (length s)))))
+      (dolist (x s h) (puthash x t h))))
+   (t (error "hydra-set-to-hash: not a set: %S" s))))
+
+(defun hydra-set-foreach (f s)
+  (cond
+   ((null s) nil)
+   ((hash-table-p s) (maphash (lambda (k _v) (funcall f k)) s))
+   ((listp s) (dolist (x s) (funcall f x)))
+   (t (error "hydra-set-foreach: not a set: %S" s))))
+
+(defun hydra-set-sorted-list (s)
+  "Return the elements of S as a list, sorted via `generic-compare`."
+  (let ((xs nil))
+    (hydra-set-foreach (lambda (x) (push x xs)) s)
+    (sort xs (lambda (a b) (< (generic-compare a b) 0)))))
 
 (defun set-insert (x s)
-  (cond
-    ((null s) (list x))
-    ((= (generic-compare x (car s)) 0) s)
-    ((< (generic-compare x (car s)) 0) (cons x s))
-    (t (cons (car s) (set-insert x (cdr s))))))
+  "Return S with X inserted, copy-on-write."
+  (let ((h (hydra-set-to-hash s)))
+    (puthash x t h)
+    h))
 
 (defun set-delete (x s)
+  "Return S with X removed, copy-on-write."
   (cond
-    ((null s) nil)
-    ((equal x (car s)) (cdr s))
-    (t (cons (car s) (set-delete x (cdr s))))))
+   ((null s) nil)
+   ((hash-table-p s)
+    (if (eq (gethash x s :hydra-not-found) :hydra-not-found)
+        s
+      (let ((h (copy-hash-table s)))
+        (remhash x h)
+        h)))
+   ((listp s) (cl-remove-if (lambda (y) (equal x y)) s))
+   (t (error "set-delete: not a set: %S" s))))
 
 (defun set-member-p (x s)
-  (cond
-    ((null s) nil)
-    ((equal x (car s)) t)
-    ((< (generic-compare x (car s)) 0) nil)
-    (t (set-member-p x (cdr s)))))
+  (hydra-set-member-p x s))
 
 (defun set-from-list (xs)
-  (let ((acc nil))
-    (dolist (x xs acc)
-      (setq acc (set-insert x acc)))))
+  (let ((h (make-hash-table :test 'equal :size (max 1 (length xs)))))
+    (dolist (x xs h) (puthash x t h))))
 
 ;; delete :: a -> Set a -> Set a
 (defvar hydra_lib_sets_delete
@@ -41,10 +93,11 @@
   (lambda (s1)
     "Compute the difference of two sets."
     (lambda (s2)
-      (let ((acc nil))
-        (dolist (x s1 (nreverse acc))
-          (unless (set-member-p x s2)
-            (push x acc)))))))
+      (let ((result (make-hash-table :test 'equal)))
+        (hydra-set-foreach
+         (lambda (x) (unless (hydra-set-member-p x s2) (puthash x t result)))
+         s1)
+        result))))
 
 ;; empty :: Set a
 (defvar hydra_lib_sets_empty nil
@@ -68,66 +121,71 @@
   (lambda (s1)
     "Compute the intersection of two sets."
     (lambda (s2)
-      (let ((acc nil))
-        (dolist (x s1 (nreverse acc))
-          (when (set-member-p x s2)
-            (push x acc)))))))
+      (let ((result (make-hash-table :test 'equal)))
+        (hydra-set-foreach
+         (lambda (x) (when (hydra-set-member-p x s2) (puthash x t result)))
+         s1)
+        result))))
 
 ;; map :: (a -> b) -> Set a -> Set b
 (defvar hydra_lib_sets_map
   (lambda (f)
     "Map a function over a set."
     (lambda (s)
-      (let ((acc nil))
-        (dolist (x s acc)
-          (setq acc (set-insert (funcall f x) acc)))))))
+      (let ((result (make-hash-table :test 'equal :size (max 1 (hydra-set-size s)))))
+        (hydra-set-foreach
+         (lambda (x) (puthash (funcall f x) t result))
+         s)
+        result))))
 
 ;; member :: a -> Set a -> Bool
 (defvar hydra_lib_sets_member
   (lambda (x)
     "Check if an element is in a set."
     (lambda (s)
-      (set-member-p x s))))
+      (hydra-set-member-p x s))))
 
 ;; null :: Set a -> Bool
 (defvar hydra_lib_sets_null
   (lambda (s)
     "Check if a set is empty."
-    (null s)))
+    (hydra-set-empty-p s)))
 
 ;; singleton :: a -> Set a
 (defvar hydra_lib_sets_singleton
   (lambda (x)
     "Create a singleton set."
-    (list x)))
+    (let ((h (make-hash-table :test 'equal :size 1)))
+      (puthash x t h)
+      h)))
 
 ;; size :: Set a -> Int
 (defvar hydra_lib_sets_size
   (lambda (s)
     "Get the size of a set."
-    (length s)))
+    (hydra-set-size s)))
 
 ;; to_list :: Set a -> [a]
 (defvar hydra_lib_sets_to_list
   (lambda (s)
-    "Convert a set to a list."
-    s))
+    "Convert a set to a sorted list."
+    (hydra-set-sorted-list s)))
 
 ;; union :: Set a -> Set a -> Set a
 (defvar hydra_lib_sets_union
   (lambda (s1)
     "Compute the union of two sets."
     (lambda (s2)
-      (let ((acc s1))
-        (dolist (x s2 acc)
-          (setq acc (set-insert x acc)))))))
+      (let ((result (hydra-set-to-hash s2)))
+        (hydra-set-foreach (lambda (x) (puthash x t result)) s1)
+        result))))
 
 ;; unions :: [Set a] -> Set a
 (defvar hydra_lib_sets_unions
   (lambda (sets)
     "Compute the union of multiple sets."
-    (let ((acc nil))
-      (dolist (s sets acc)
-        (setq acc (funcall (funcall hydra_lib_sets_union acc) s))))))
+    (let ((result (make-hash-table :test 'equal)))
+      (dolist (s sets result)
+        (hydra-set-foreach (lambda (x) (puthash x t result)) s)))))
 
 (provide 'hydra.lib.sets)
