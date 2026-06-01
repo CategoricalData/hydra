89e641f07c45bc29a7e69139f2193a38b35f3a33
Joshua Shinavier <josh@fortytwo.net>
2026-06-01 14:04:10 -0700

    WIP: fix Encode/Packaging termDefinition/primitiveDefinition field order + term/typeScheme->body wire keys; Decode requireField body. For #369

--- dist/haskell/hydra-kernel/src/main/haskell/Hydra/Encode/Packaging.hs
+++ dist/haskell/hydra-kernel/src/main/haskell/Hydra/Encode/Packaging.hs
@@ -195,11 +195,11 @@ primitiveDefinition x =
           Core.fieldName = (Core.Name "name"),
           Core.fieldTerm = (EncodeCore.name (Packaging.primitiveDefinitionName x))},
         Core.Field {
-          Core.fieldName = (Core.Name "signature"),
-          Core.fieldTerm = (Typing.termSignature (Packaging.primitiveDefinitionSignature x))},
-        Core.Field {
           Core.fieldName = (Core.Name "metadata"),
           Core.fieldTerm = ((\opt -> Core.TermMaybe (Maybes.map entityMetadata opt)) (Packaging.primitiveDefinitionMetadata x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "signature"),
+          Core.fieldTerm = (Typing.termSignature (Packaging.primitiveDefinitionSignature x))},
         Core.Field {
           Core.fieldName = (Core.Name "isPure"),
           Core.fieldTerm = ((\x2 -> Core.TermLiteral (Core.LiteralBoolean x2)) (Packaging.primitiveDefinitionIsPure x))},
@@ -234,11 +234,11 @@ termDefinition x =
           Core.fieldName = (Core.Name "metadata"),
           Core.fieldTerm = ((\opt -> Core.TermMaybe (Maybes.map entityMetadata opt)) (Packaging.termDefinitionMetadata x))},
         Core.Field {
-          Core.fieldName = (Core.Name "term"),
-          Core.fieldTerm = (EncodeCore.term (Packaging.termDefinitionBody x))},
-        Core.Field {
           Core.fieldName = (Core.Name "signature"),
-          Core.fieldTerm = ((\opt -> Core.TermMaybe (Maybes.map Typing.termSignature opt)) (Packaging.termDefinitionSignature x))}]})
+          Core.fieldTerm = ((\opt -> Core.TermMaybe (Maybes.map Typing.termSignature opt)) (Packaging.termDefinitionSignature x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "body"),
+          Core.fieldTerm = (EncodeCore.term (Packaging.termDefinitionBody x))}]})
 -- | Encoder for hydra.packaging.TypeDefinition
 typeDefinition :: Packaging.TypeDefinition -> Core.Term
 typeDefinition x =
@@ -252,7 +252,7 @@ typeDefinition x =
           Core.fieldName = (Core.Name "metadata"),
           Core.fieldTerm = ((\opt -> Core.TermMaybe (Maybes.map entityMetadata opt)) (Packaging.typeDefinitionMetadata x))},
         Core.Field {
-          Core.fieldName = (Core.Name "typeScheme"),
+          Core.fieldName = (Core.Name "body"),
           Core.fieldTerm = (EncodeCore.typeScheme (Packaging.typeDefinitionBody x))}]})
 -- | Encoder for hydra.packaging.Version
 version :: Packaging.Version -> Core.Term
