c279c2d83cfb72f667094ca651ec29284e3519c8
joshsh <josh@fortytwo.aero>
2026-06-05 20:36:29 +0000

    Regenerated dist/{haskell,json} for Language type extension. For #421

--- dist/haskell/hydra-kernel/src/main/haskell/Hydra/Decode/Coders.hs
+++ dist/haskell/hydra-kernel/src/main/haskell/Hydra/Decode/Coders.hs
@@ -7,6 +7,7 @@ import qualified Hydra.Core as Core
 import qualified Hydra.Decode.Core as DecodeCore
 import qualified Hydra.Decode.Errors as DecodeErrors
 import qualified Hydra.Decode.Typing as Typing
+import qualified Hydra.Decode.Util as DecodeUtil
 import qualified Hydra.Decode.Variants as Variants
 import qualified Hydra.Errors as Errors
 import qualified Hydra.Extract.Core as ExtractCore
@@ -20,6 +21,24 @@ import qualified Hydra.Rewriting as Rewriting
 import qualified Hydra.Util as Util
 import Prelude hiding  (Enum, Ordering, decodeFloat, encodeFloat, fail, map, pure, sum)
 import qualified Data.Scientific as Sci
+-- | Decoder for hydra.coders.CaseConventions
+caseConventions :: Graph.Graph -> Core.Term -> Either Errors.DecodingError Coders.CaseConventions
+caseConventions cx raw =
+    Eithers.either (\err -> Left err) (\stripped -> case stripped of
+      Core.TermRecord v0 ->
+        let fieldMap = ExtractCore.toFieldMap v0
+        in (Eithers.bind (ExtractCore.requireField "constant" DecodeUtil.caseConvention fieldMap cx) (\field_constant -> Eithers.bind (ExtractCore.requireField "directory" DecodeUtil.caseConvention fieldMap cx) (\field_directory -> Eithers.bind (ExtractCore.requireField "enumValue" DecodeUtil.caseConvention fieldMap cx) (\field_enumValue -> Eithers.bind (ExtractCore.requireField "field" DecodeUtil.caseConvention fieldMap cx) (\field_field -> Eithers.bind (ExtractCore.requireField "file" DecodeUtil.caseConvention fieldMap cx) (\field_file -> Eithers.bind (ExtractCore.requireField "module" DecodeUtil.caseConvention fieldMap cx) (\field_module -> Eithers.bind (ExtractCore.requireField "term" DecodeUtil.caseConvention fieldMap cx) (\field_term -> Eithers.bind (ExtractCore.requireField "termVariable" DecodeUtil.caseConvention fieldMap cx) (\field_termVariable -> Eithers.bind (ExtractCore.requireField "type" DecodeUtil.caseConvention fieldMap cx) (\field_type -> Eithers.bind (ExtractCore.requireField "typeVariable" DecodeUtil.caseConvention fieldMap cx) (\field_typeVariable -> Right (Coders.CaseConventions {
+          Coders.caseConventionsConstant = field_constant,
+          Coders.caseConventionsDirectory = field_directory,
+          Coders.caseConventionsEnumValue = field_enumValue,
+          Coders.caseConventionsField = field_field,
+          Coders.caseConventionsFile = field_file,
+          Coders.caseConventionsModule = field_module,
+          Coders.caseConventionsTerm = field_term,
+          Coders.caseConventionsTermVariable = field_termVariable,
+          Coders.caseConventionsType = field_type,
+          Coders.caseConventionsTypeVariable = field_typeVariable}))))))))))))
+      _ -> Left (Errors.DecodingError "expected a record of type hydra.coders.CaseConventions")) (ExtractCore.stripWithDecodingError cx raw)
 -- | Decoder for hydra.coders.CoderDirection
 coderDirection :: Graph.Graph -> Core.Term -> Either Errors.DecodingError Coders.CoderDirection
 coderDirection cx raw =
@@ -32,6 +51,30 @@ coderDirection cx raw =
                     Maps.fromList [
                       (Core.Name "encode", (\input -> Eithers.map (\t -> Coders.CoderDirectionEncode) (ExtractCore.decodeUnit cx input))),
                       (Core.Name "decode", (\input -> Eithers.map (\t -> Coders.CoderDirectionDecode) (ExtractCore.decodeUnit cx input)))]
+        in (Maybes.maybe (Left (Errors.DecodingError (Strings.cat [
+          "no such field ",
+          (Core.unName fname),
+          " in union"]))) (\f -> f fterm) (Maps.lookup fname variantMap))
+      _ -> Left (Errors.DecodingError "expected union")) (ExtractCore.stripWithDecodingError cx raw)
+-- | Decoder for hydra.coders.LanguageFeature
+languageFeature :: Graph.Graph -> Core.Term -> Either Errors.DecodingError Coders.LanguageFeature
+languageFeature cx raw =
+    Eithers.either (\err -> Left err) (\stripped -> case stripped of
+      Core.TermInject v0 ->
+        let field = Core.injectionField v0
+            fname = Core.fieldName field
+            fterm = Core.fieldTerm field
+            variantMap =
+                    Maps.fromList [
+                      (
+                        Core.Name "partialApplication",
+                        (\input -> Eithers.map (\t -> Coders.LanguageFeaturePartialApplication) (ExtractCore.decodeUnit cx input))),
+                      (
+                        Core.Name "nestedCaseStatements",
+                        (\input -> Eithers.map (\t -> Coders.LanguageFeatureNestedCaseStatements) (ExtractCore.decodeUnit cx input))),
+                      (
+                        Core.Name "nestedPolymorphicLetBindings",
+                        (\input -> Eithers.map (\t -> Coders.LanguageFeatureNestedPolymorphicLetBindings) (ExtractCore.decodeUnit cx input)))]
         in (Maybes.maybe (Left (Errors.DecodingError (Strings.cat [
           "no such field ",
           (Core.unName fname),
--- dist/haskell/hydra-kernel/src/main/haskell/Hydra/Encode/Coders.hs
+++ dist/haskell/hydra-kernel/src/main/haskell/Hydra/Encode/Coders.hs
@@ -7,9 +7,46 @@ import qualified Hydra.Core as Core
 import qualified Hydra.Encode.Core as EncodeCore
 import qualified Hydra.Encode.Errors as Errors
 import qualified Hydra.Encode.Typing as Typing
+import qualified Hydra.Encode.Util as Util
 import qualified Hydra.Encode.Variants as Variants
 import Prelude hiding  (Enum, Ordering, decodeFloat, encodeFloat, fail, map, pure, sum)
 import qualified Data.Scientific as Sci
+-- | Encoder for hydra.coders.CaseConventions
+caseConventions :: Coders.CaseConventions -> Core.Term
+caseConventions x =
+    Core.TermRecord (Core.Record {
+      Core.recordTypeName = (Core.Name "hydra.coders.CaseConventions"),
+      Core.recordFields = [
+        Core.Field {
+          Core.fieldName = (Core.Name "constant"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsConstant x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "directory"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsDirectory x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "enumValue"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsEnumValue x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "field"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsField x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "file"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsFile x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "module"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsModule x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "term"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsTerm x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "termVariable"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsTermVariable x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "type"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsType x))},
+        Core.Field {
+          Core.fieldName = (Core.Name "typeVariable"),
+          Core.fieldTerm = (Util.caseConvention (Coders.caseConventionsTypeVariable x))}]})
 -- | Encoder for hydra.coders.CoderDirection
 coderDirection :: Coders.CoderDirection -> Core.Term
 coderDirection x =
@@ -23,6 +60,25 @@ coderDirection x =
         Core.injectionTypeName = (Core.Name "hydra.coders.CoderDirection"),
         Core.injectionField = Core.Field {
           Core.fieldName = (Core.Name "decode"),
+          Core.fieldTerm = Core.TermUnit}})
+-- | Encoder for hydra.coders.LanguageFeature
+languageFeature :: Coders.LanguageFeature -> Core.Term
+languageFeature x =
+    case x of
+      Coders.LanguageFeaturePartialApplication -> Core.TermInject (Core.Injection {
+        Core.injectionTypeName = (Core.Name "hydra.coders.LanguageFeature"),
+        Core.injectionField = Core.Field {
+          Core.fieldName = (Core.Name "partialApplication"),
+          Core.fieldTerm = Core.TermUnit}})
+      Coders.LanguageFeatureNestedCaseStatements -> Core.TermInject (Core.Injection {
+        Core.injectionTypeName = (Core.Name "hydra.coders.LanguageFeature"),
+        Core.injectionField = Core.Field {
+          Core.fieldName = (Core.Name "nestedCaseStatements"),
+          Core.fieldTerm = Core.TermUnit}})
+      Coders.LanguageFeatureNestedPolymorphicLetBindings -> Core.TermInject (Core.Injection {
+        Core.injectionTypeName = (Core.Name "hydra.coders.LanguageFeature"),
+        Core.injectionField = Core.Field {
+          Core.fieldName = (Core.Name "nestedPolymorphicLetBindings"),
           Core.fieldTerm = Core.TermUnit}})
 -- | Encoder for hydra.coders.LanguageName
 languageName :: Coders.LanguageName -> Core.Term
