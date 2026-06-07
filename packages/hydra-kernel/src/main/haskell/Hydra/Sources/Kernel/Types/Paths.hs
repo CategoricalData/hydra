d86a645667b1bc831190c9b6e665456c8c2a7d39
Joshua Shinavier <josh@fortytwo.net>
2026-06-06 22:59:10 -0700

    Rename kernel SubtermStep.maybeTerm→optionalTerm + show repr just→given. For #401

--- packages/hydra-kernel/src/main/haskell/Hydra/Sources/Kernel/Types/Paths.hs
+++ packages/hydra-kernel/src/main/haskell/Hydra/Sources/Kernel/Types/Paths.hs
@@ -117,8 +117,8 @@ subtermStep = define "SubtermStep" $
     "mapValue">:
       doc "Access a value in a map by index"
       T.int32,
-    "maybeTerm">:
-      doc "Access the term inside a Just value"
+    "optionalTerm">:
+      doc "Access the term inside a given (present) optional value"
       T.unit,
     "productTerm">:
       doc "Access an element of a product (tuple) by index"
