50b9d32f6ca792417160964cae254a2b2952cb26
joshsh <josh@fortytwo.aero>
2026-06-08 22:03:25 +0000

    WIP: Drop deleted DefaultsPrimitives.hs from bootstrap haskell-target setup. For #437

--- demos/bootstrapping/bin/setup-haskell-target.sh
+++ demos/bootstrapping/bin/setup-haskell-target.sh
@@ -170,7 +170,6 @@ mkdir -p "$OUTPUT_DIR/src/test/haskell/Hydra"
 cp "$HYDRA_HASKELL_HEAD_DIR/src/test/haskell/Hydra/TestSuiteSpec.hs" "$OUTPUT_DIR/src/test/haskell/Hydra/"
 cp "$HYDRA_HASKELL_HEAD_DIR/src/test/haskell/Hydra/TestUtils.hs" "$OUTPUT_DIR/src/test/haskell/Hydra/"
 cp "$HYDRA_HASKELL_HEAD_DIR/src/test/haskell/Hydra/ArbitraryCore.hs" "$OUTPUT_DIR/src/test/haskell/Hydra/"
-cp "$HYDRA_HASKELL_HEAD_DIR/src/test/haskell/Hydra/DefaultsPrimitives.hs" "$OUTPUT_DIR/src/test/haskell/Hydra/"
 
 # TestEnv: provides the real test graph with primitives and kernel term bindings.
 # Must be copied to src/test (where the generated TestGraph.hs imports it).
