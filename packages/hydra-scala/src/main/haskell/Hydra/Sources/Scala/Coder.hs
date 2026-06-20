bb37a2227da69cbde716f63d3dffa035ac31ecd1
joshsh <josh@fortytwo.aero>
2026-06-20 12:55:45 +0000

    Qualify Scala primitive and collection type refs in coder to prevent shadowing. For #491

--- packages/hydra-scala/src/main/haskell/Hydra/Sources/Scala/Coder.hs
+++ packages/hydra-scala/src/main/haskell/Hydra/Sources/Scala/Coder.hs
@@ -860,24 +860,28 @@ encodeType = def "encodeType" $
         Eithers.bind
           (asTerm encodeType @@ var "cx" @@ var "g" @@ var "lt")
           ("slt" ~>
-            right (ScalaUtilsSource.stapply1 @@ stref (string "Seq") @@ var "slt"))),
+            right (ScalaUtilsSource.stapply1 @@ stref (string "scala.collection.immutable.Seq") @@ var "slt"))),
       _Type_literal>>: ("lt" ~> cases _LiteralType (var "lt") (Just $ left (Error.errorOther $ Error.otherError (string "unsupported literal type"))) [
-        _LiteralType_binary>>: (constant $ right (ScalaUtilsSource.stapply @@ stref (string "Array") @@ list [stref (string "Byte")])),
-        _LiteralType_boolean>>: (constant $ right (stref (string "Boolean"))),
-        _LiteralType_decimal>>: (constant $ right (stref (string "BigDecimal"))),
+        -- Use fully qualified names for all primitive types. Unqualified names
+        -- like Boolean/Byte/Int/Long/Float/Double can alias-cycle in packages
+        -- (e.g. hydra.xml.schema) that define type aliases with the same names.
+        -- String is already qualified as scala.Predef.String for the same reason.
+        _LiteralType_binary>>: (constant $ right (ScalaUtilsSource.stapply @@ stref (string "scala.Array") @@ list [stref (string "scala.Byte")])),
+        _LiteralType_boolean>>: (constant $ right (stref (string "scala.Boolean"))),
+        _LiteralType_decimal>>: (constant $ right (stref (string "scala.math.BigDecimal"))),
         _LiteralType_float>>: ("ft" ~> cases _FloatType (var "ft") (Just $ left (Error.errorOther $ Error.otherError (string "unsupported float type"))) [
-          _FloatType_float32>>: (constant $ right (stref (string "Float"))),
-          _FloatType_float64>>: (constant $ right (stref (string "Double")))]),
+          _FloatType_float32>>: (constant $ right (stref (string "scala.Float"))),
+          _FloatType_float64>>: (constant $ right (stref (string "scala.Double")))]),
         _LiteralType_integer>>: ("it" ~> cases _IntegerType (var "it") (Just $ left (Error.errorOther $ Error.otherError (string "unsupported integer type"))) [
-          _IntegerType_bigint>>: (constant $ right (stref (string "BigInt"))),
-          _IntegerType_int8>>: (constant $ right (stref (string "Byte"))),
-          _IntegerType_int16>>: (constant $ right (stref (string "Short"))),
-          _IntegerType_int32>>: (constant $ right (stref (string "Int"))),
-          _IntegerType_int64>>: (constant $ right (stref (string "Long"))),
-          _IntegerType_uint8>>: (constant $ right (stref (string "Byte"))),
-          _IntegerType_uint16>>: (constant $ right (stref (string "Int"))),
-          _IntegerType_uint32>>: (constant $ right (stref (string "Long"))),
-          _IntegerType_uint64>>: (constant $ right (stref (string "BigInt")))]),
+          _IntegerType_bigint>>: (constant $ right (stref (string "scala.math.BigInt"))),
+          _IntegerType_int8>>: (constant $ right (stref (string "scala.Byte"))),
+          _IntegerType_int16>>: (constant $ right (stref (string "scala.Short"))),
+          _IntegerType_int32>>: (constant $ right (stref (string "scala.Int"))),
+          _IntegerType_int64>>: (constant $ right (stref (string "scala.Long"))),
+          _IntegerType_uint8>>: (constant $ right (stref (string "scala.Byte"))),
+          _IntegerType_uint16>>: (constant $ right (stref (string "scala.Int"))),
+          _IntegerType_uint32>>: (constant $ right (stref (string "scala.Long"))),
+          _IntegerType_uint64>>: (constant $ right (stref (string "scala.math.BigInt")))]),
         _LiteralType_string>>: (constant $ right (stref (string "scala.Predef.String")))]),
       _Type_map>>: ("mt" ~> lets [
         "kt">: project _MapType _MapType_keys @@ var "mt",
@@ -888,12 +892,12 @@ encodeType = def "encodeType" $
             Eithers.bind
               (asTerm encodeType @@ var "cx" @@ var "g" @@ var "vt")
               ("svt" ~>
-                right (ScalaUtilsSource.stapply2 @@ stref (string "Map") @@ var "skt" @@ var "svt")))),
+                right (ScalaUtilsSource.stapply2 @@ stref (string "scala.collection.immutable.Map") @@ var "skt" @@ var "svt")))),
       _Type_optional>>: ("ot" ~>
         Eithers.bind
           (asTerm encodeType @@ var "cx" @@ var "g" @@ var "ot")
           ("sot" ~>
-            right (ScalaUtilsSource.stapply1 @@ stref (string "Option") @@ var "sot"))),
+            right (ScalaUtilsSource.stapply1 @@ stref (string "scala.Option") @@ var "sot"))),
       _Type_pair>>: ("pt" ~> lets [
         "ft">: project _PairType _PairType_first @@ var "pt",
         "st">: project _PairType _PairType_second @@ var "pt"] $
