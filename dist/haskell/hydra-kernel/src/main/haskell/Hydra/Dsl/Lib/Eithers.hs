6a527986e9c1b72f5febc29c115898b5c7a6a21e
Joshua Shinavier <josh@fortytwo.net>
2026-06-21 15:43:44 -0700

    Regenerate dist/haskell + dist/json after merging integration (effect/file/text + hydra.dsl.lib.*). For #467

--- dist/haskell/hydra-kernel/src/main/haskell/Hydra/Dsl/Lib/Eithers.hs
+++ dist/haskell/hydra-kernel/src/main/haskell/Hydra/Dsl/Lib/Eithers.hs
@@ -10,8 +10,10 @@ import qualified Hydra.Dsl.Coders as DslCoders
 import qualified Hydra.Dsl.Core as DslCore
 import qualified Hydra.Dsl.Error.Checking as ErrorChecking
 import qualified Hydra.Dsl.Error.Core as DslErrorCore
+import qualified Hydra.Dsl.Error.File as DslErrorFile
 import qualified Hydra.Dsl.Error.Packaging as DslErrorPackaging
 import qualified Hydra.Dsl.Errors as DslErrors
+import qualified Hydra.Dsl.File as DslFile
 import qualified Hydra.Dsl.Graph as DslGraph
 import qualified Hydra.Dsl.Json.Model as JsonModel
 import qualified Hydra.Dsl.Packaging as DslPackaging
@@ -21,6 +23,7 @@ import qualified Hydra.Dsl.Query as DslQuery
 import qualified Hydra.Dsl.Relational as DslRelational
 import qualified Hydra.Dsl.Tabular as DslTabular
 import qualified Hydra.Dsl.Testing as DslTesting
+import qualified Hydra.Dsl.Time as DslTime
 import qualified Hydra.Dsl.Topology as DslTopology
 import qualified Hydra.Dsl.Typing as DslTyping
 import qualified Hydra.Dsl.Util as DslUtil
@@ -28,8 +31,10 @@ import qualified Hydra.Dsl.Validation as DslValidation
 import qualified Hydra.Dsl.Variants as DslVariants
 import qualified Hydra.Error.Checking as Checking
 import qualified Hydra.Error.Core as ErrorCore
+import qualified Hydra.Error.File as ErrorFile
 import qualified Hydra.Error.Packaging as ErrorPackaging
 import qualified Hydra.Errors as Errors
+import qualified Hydra.File as File
 import qualified Hydra.Graph as Graph
 import qualified Hydra.Json.Model as Model
 import qualified Hydra.Packaging as Packaging
@@ -39,6 +44,7 @@ import qualified Hydra.Query as Query
 import qualified Hydra.Relational as Relational
 import qualified Hydra.Tabular as Tabular
 import qualified Hydra.Testing as Testing
+import qualified Hydra.Time as Time
 import qualified Hydra.Topology as Topology
 import qualified Hydra.Typed as Typed
 import qualified Hydra.Typing as Typing
