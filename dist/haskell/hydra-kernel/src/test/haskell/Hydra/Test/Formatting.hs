8fe36888965b8442f714bd68848b0997015f758a
Joshua Shinavier <josh@fortytwo.net>
2026-06-23 13:37:16 -0700

    Regenerate dist (JSON + Haskell) and lexicon for hydra.lib.system across the kernel. Resolves #498

--- dist/haskell/hydra-kernel/src/test/haskell/Hydra/Test/Formatting.hs
+++ dist/haskell/hydra-kernel/src/test/haskell/Hydra/Test/Formatting.hs
@@ -9,6 +9,7 @@ import qualified Hydra.Error.Checking as Checking
 import qualified Hydra.Error.Core as ErrorCore
 import qualified Hydra.Error.File as ErrorFile
 import qualified Hydra.Error.Packaging as ErrorPackaging
+import qualified Hydra.Error.System as ErrorSystem
 import qualified Hydra.Errors as Errors
 import qualified Hydra.File as File
 import qualified Hydra.Formatting as Formatting
@@ -19,6 +20,7 @@ import qualified Hydra.Parsing as Parsing
 import qualified Hydra.Paths as Paths
 import qualified Hydra.Query as Query
 import qualified Hydra.Relational as Relational
+import qualified Hydra.System as System
 import qualified Hydra.Tabular as Tabular
 import qualified Hydra.Test.TestGraph as TestGraph
 import qualified Hydra.Testing as Testing
