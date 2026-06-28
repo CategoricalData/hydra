87a8f16e8de29c1c39bcf438528690e062f5ba64
Joshua Shinavier <josh@fortytwo.net>
2026-06-28 13:02:34 -0700

    Fold hydra-pg-dsl/neo4j/tinkerpop -> overlay/java/hydra-pg + rdf4j -> overlay/java/hydra-rdf (hydra.overlay.java.*). For #511

---
+++ overlay/java/hydra-pg/src/main/java/hydra/overlay/java/tools/AntlrReaderBase.java
@@ -0,0 +1,35 @@
+package hydra.overlay.java.tools;
+
+import hydra.overlay.java.util.Optional;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.function.Function;
+
+/**
+ * A base class for utilities which convert ANTLR parse trees into domain-specific objects.
+ */
+public class AntlrReaderBase extends MapperBase {
+
+    /**
+     * Match a parser context against a list of functions, returning the result of the first match.
+     * @param <P0> the parser context type
+     * @param <P> the result type
+     * @param ctx the parser context
+     * @param funs the functions to try
+     * @return the result of the first matching function
+     */
+    protected static <P0 extends ParserRuleContext, P> P match(P0 ctx,
+                                                               Function<P0, Optional<P>>... funs) {
+        if (null != ctx.exception) {
+            throw new MapperException(ctx.exception);
+        }
+
+        for (Function<P0, Optional<P>> f : funs) {
+            Optional<P> res = f.apply(ctx);
+            if (res.isGiven()) {
+                return res.fromGiven();
+            }
+        }
+        return invalid("union failed to match");
+    }
+}
