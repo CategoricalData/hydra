20103077cabb6b2a64bed624d2eeed51d4faf2eb
joshsh <josh@fortytwo.aero>
2026-08-09 12:11:49 +0000

    Fix Clojure decimal-to-float32: (float (.doubleValue ...)) throws on Float.MAX_VALUE; use (.floatValue (bigdec x)) which saturates. For #630

--- overlay/clojure/hydra-kernel/src/main/clojure/hydra/overlay/clojure/lib/literals.clj
+++ overlay/clojure/hydra-kernel/src/main/clojure/hydra/overlay/clojure/lib/literals.clj
@@ -20,7 +20,7 @@
 
 ;; Decimal conversions
 (def hydra_overlay_clojure_lib_literals_decimal_to_bigint (fn [x] (.toBigInteger (.setScale (bigdec x) 0 java.math.RoundingMode/HALF_EVEN))))
-(def hydra_overlay_clojure_lib_literals_decimal_to_float32 (fn [x] (float (.doubleValue (bigdec x)))))
+(def hydra_overlay_clojure_lib_literals_decimal_to_float32 (fn [x] (.floatValue (bigdec x))))
 (def hydra_overlay_clojure_lib_literals_decimal_to_float64 (fn [x] (double (.doubleValue (bigdec x)))))
 
 ;; Float conversions
